38c67d0b2c978f21f7c3046dfa89a14d00550e7e
Fabien Chéreau <fabien.chereau@gmail.com>
2002-10-21 00:26:46 +0000

    Added stelconfig.h stelconfig.cpp and default config files.

---
+++ src/stelconfig.h
@@ -0,0 +1,38 @@
+/* 
+ * Stellarium
+ * Copyright (C) 2002 Fabien Chéreau
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _STELCONFIG_H_
+#define _STELCONFIG_H_
+
+
+#include "stellarium.h"
+
+// Set the data directories
+void setDirectories(void);
+
+// Read the configuration file
+void loadConfig(char * configFile, char * locationFile);
+
+// Dump the configuration file
+void dumpConfig(void);
+
+// Dump the location file
+void dumpLocation(void);
+
+#endif //_STELCONFIG_H_
