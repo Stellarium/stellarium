5074e403e965f9091ddf91c92ec0df228ad507d9
Fabien Chereau <fabien.chereau@gmail.com>
2003-05-02 21:40:49 +0000

    Add the new files.

---
+++ src/solarsystem.cpp
@@ -0,0 +1,26 @@
+/*
+ * Stellarium
+ * Copyright (C) 2002 Fabien Chéreau
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "solarsystem.h"
+
+ // Create and init the solar system
+void InitSolarSystem(void)
+{
+	Sun=new sun_planet("Sun", NO_HALO, 696000./UA, vec3_t _color, s_texture * _planetTexture, s_texture * _haloTexture, s_texture * _bigHaloTexture, void (*_coord_func)(double JD, struct ln_rect_posn * position));
+}
