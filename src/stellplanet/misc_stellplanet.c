a4262aeac4792e0b22a98a6cd8d7fe0ea3c7bba2
Fabien Chereau <fabien.chereau@gmail.com>
2003-05-04 16:22:05 +0000

    Added the 2 new libraries.

---
+++ src/stellplanet/misc_stellplanet.c
@@ -0,0 +1,34 @@
+/*
+* Copyright (C) 1999, 2000 Juan Carlos Remis
+* Copyright (C) 2002 Liam Girdwood
+* Copyright (C) 2003 Fabien Chéreau
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <math.h>
+
+// Transform spheric coordinate in rectangular
+void sphe_to_rect(double lng, double lat, double r, double *x, double *y, double *z)
+{
+	double c = cos(lat);
+    *x = sin(lng) * c;
+    *y = sin(lat);
+    *z = cos(lng) * c;
+	c = r/sqrt((*x) * (*x) + (*y) * (*y) + (*z) * (*z));
+	(*x)*=c;
+	(*y)*=c;
+	(*z)*=c;
+}
---
+++ src/stellplanet/stellplanet.h
@@ -0,0 +1,60 @@
+/*
+Copyright (C) 2000 Liam Girdwood <liam@nova-ioe.org>
+Copyright (C) 2003 Fabien Chéreau
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef _STELLPLANET_H_
+#define _STELLPLANET_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Chapter 31 Pg 206-207 Equ 31.1 31.2 , 31.3 using VSOP 87
+ * Calculate planets rectangular heliocentric ecliptical coordinates
+ * for given julian day. Values are in UA.
+ * params : Julian day, rect coords */
+void get_mercury_helio_coords(double JD, double * X, double * Y, double * Z);
+void get_venus_helio_coords(double JD, double * X, double * Y, double * Z);
+void get_earth_helio_coords(double JD, double * X, double * Y, double * Z);
+void get_mars_helio_coords(double JD, double * X, double * Y, double * Z);
+void get_jupiter_helio_coords(double JD, double * X, double * Y, double * Z);
+void get_saturn_helio_coords(double JD, double * X, double * Y, double * Z);
+void get_uranus_helio_coords(double JD, double * X, double * Y, double * Z);
+void get_neptune_helio_coords(double JD, double * X, double * Y, double * Z);
+void get_pluto_helio_coords(double JD, double * X, double * Y, double * Z);
+
+/* Calculate the rectangular geocentric lunar coordinates to the inertial mean
+ * ecliptic and equinox of J2000.
+ * The geocentric coordinates returned are in units of UA.
+ * The truncation level of the series in radians for longitude
+ * and latitude and in km for distance. (Valid range 0 - 0.01, 0 being highest accuracy)
+ * This function is based upon the Lunar Solution ELP2000-82B by
+ * Michelle Chapront-Touze and Jean Chapront of the Bureau des Longitudes,
+ * Paris. ELP 2000-82B theory
+ * param JD Julian day, rect pos, precision */
+void get_lunar_geo_posn_prec(double JD, double * X, double * Y, double * Z, double prcision);
+
+/* Same with lowest precision by default */
+void get_lunar_geo_posn(double JD, double * X, double * Y, double * Z);
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif /* _STELLPLANET_H_ */
