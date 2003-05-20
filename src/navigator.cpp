51992bed60d4534effccdede7e8fb0821137223b
xalioth <>
2003-05-20 18:22:08 +0000

    Improved navigation.

--- src/navigator.cpp
+++ src/navigator.cpp
@@ -1,17 +1,17 @@
 /*
  * Stellarium
  * Copyright (C) 2003 Fabien Chéreau
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -305,11 +305,11 @@ void navigator::update_transform_matrices(void)
 	Mat4d GEI_to_HSE = 	Mat4d::translation(Earth->get_ecliptic_pos()) * 
 	                    Mat4d::xrotation(-get_mean_obliquity(JDay)*M_PI/180.) *
 	                    Mat4d::translation(LOC_to_GEI * Vec3d(0.,0.,6378.1/UA+(double)position.altitude/UA*100));
-	                    
-	Mat4d HSE_to_GEI = 	Mat4d::translation(LOC_to_GEI * Vec3d(0.,0.,-6378.1/UA-(double)position.altitude/UA*100)) * 
-	                    Mat4d::xrotation(get_mean_obliquity(JDay)*M_PI/180.) * 
+
+	Mat4d HSE_to_GEI = 	Mat4d::translation(LOC_to_GEI * Vec3d(0.,0.,-6378.1/UA-(double)position.altitude/UA*100)) *
+	                    Mat4d::xrotation(get_mean_obliquity(JDay)*M_PI/180.) *
 	                    Mat4d::translation(-Earth->get_ecliptic_pos());
-						
+
 
 	Mat4d LOC_to_HSE = GEI_to_HSE * LOC_to_GEI;
 	Mat4d HSE_to_LOC = GEI_to_LOC * HSE_to_GEI;
