3115fd6c505f1a595a64c008ac9c7030e64ec83f
Fabien Chereau <fabien.chereau@gmail.com>
2003-06-03 14:52:22 +0000

    First attempt for elliptical orbits.

---
+++ src/orbit.cpp
@@ -0,0 +1,267 @@
+// orbit.cpp
+//
+// Copyright (C) 2001, Chris Laurel <claurel@shatters.net>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#include <functional>
+#include <algorithm>
+#include <cmath>
+//#include <cassert>
+//#include <celmath/mathlib.h>
+//#include <celmath/solve.h>
+//#include "astro.h"
+#include "solve.h"
+#include "orbit.h"
+
+using namespace std;
+
+
+EllipticalOrbit::EllipticalOrbit(double _pericenterDistance,
+                                 double _eccentricity,
+                                 double _inclination,
+                                 double _ascendingNode,
+                                 double _argOfPeriapsis,
+                                 double _meanAnomalyAtEpoch,
+                                 double _period,
+                                 double _epoch) :
+    pericenterDistance(_pericenterDistance),
+    eccentricity(_eccentricity),
+    inclination(_inclination),
+    ascendingNode(_ascendingNode),
+    argOfPeriapsis(_argOfPeriapsis),
+    meanAnomalyAtEpoch(_meanAnomalyAtEpoch),
+    period(_period),
+    epoch(_epoch)
+{
+}
+
+
+// Standard iteration for solving Kepler's Equation
+struct SolveKeplerFunc1 : public unary_function<double, double>
+{
+    double ecc;
+    double M;
+
+    SolveKeplerFunc1(double _ecc, double _M) : ecc(_ecc), M(_M) {};
+
+    double operator()(double x) const
+    {
+        return M + ecc * sin(x);
+    }
+};
+
+
+// Faster converging iteration for Kepler's Equation; more efficient
+// than above for orbits with eccentricities greater than 0.3.  This
+// is from Jean Meeus's _Astronomical Algorithms_ (2nd ed), p. 199
+struct SolveKeplerFunc2 : public unary_function<double, double>
+{
+    double ecc;
+    double M;
+
+    SolveKeplerFunc2(double _ecc, double _M) : ecc(_ecc), M(_M) {};
+
+    double operator()(double x) const
+    {
+        return x + (M + ecc * sin(x) - x) / (1 - ecc * cos(x));
+    }
+};
+
+double sign(double x)
+{
+    if (x < 0.)
+        return -1.;
+    else if (x > 0.)
+        return 1.;
+    else
+        return 0.;
+}
+
+struct SolveKeplerLaguerreConway : public unary_function<double, double>
+{
+    double ecc;
+    double M;
+
+    SolveKeplerLaguerreConway(double _ecc, double _M) : ecc(_ecc), M(_M) {};
+
+    double operator()(double x) const
+    {
+        double s = ecc * sin(x);
+        double c = ecc * cos(x);
+        double f = x - s - M;
+        double f1 = 1 - c;
+        double f2 = s;
+        x += -5 * f / (f1 + sign(f1) * sqrt(abs(16 * f1 * f1 - 20 * f * f2)));
+
+        return x;
+    }
+};
+
+struct SolveKeplerLaguerreConwayHyp : public unary_function<double, double>
+{
+    double ecc;
+    double M;
+
+    SolveKeplerLaguerreConwayHyp(double _ecc, double _M) : ecc(_ecc), M(_M) {};
+
+    double operator()(double x) const
+    {
+        double s = ecc * sinh(x);
+        double c = ecc * cosh(x);
+        double f = s - x - M;
+        double f1 = c - 1;
+        double f2 = s;
+        x += -5 * f / (f1 + sign(f1) * sqrt(abs(16 * f1 * f1 - 20 * f * f2)));
+
+        return x;
+    }
+};
+
+typedef pair<double, double> Solution;
+
+
+double EllipticalOrbit::eccentricAnomaly(double M) const
+{
+    if (eccentricity == 0.0)
+    {
+        // Circular orbit
+        return M;
+    }
+    else if (eccentricity < 0.2)
+    {
+        // Low eccentricity, so use the standard iteration technique
+        Solution sol = solve_iteration_fixed(SolveKeplerFunc1(eccentricity, M), M, 5);
+        return sol.first;
+    }
+    else if (eccentricity < 0.9)
+    {
+        // Higher eccentricity elliptical orbit; use a more complex but
+        // much faster converging iteration.
+        Solution sol = solve_iteration_fixed(SolveKeplerFunc2(eccentricity, M), M, 6);
+        // Debugging
+        // printf("ecc: %f, error: %f mas\n",
+        //        eccentricity, radToDeg(sol.second) * 3600000);
+        return sol.first;
+    }
+    else if (eccentricity < 1.0)
+    {
+        // Extremely stable Laguerre-Conway method for solving Kepler's
+        // equation.  Only use this for high-eccentricity orbits, as it
+        // requires more calcuation.
+        double E = M + 0.85 * eccentricity * sign(sin(M));
+        Solution sol = solve_iteration_fixed(SolveKeplerLaguerreConway(eccentricity, M), E, 8);
+        return sol.first;
+    }
+    else if (eccentricity == 1.0)
+    {
+        // Nearly parabolic orbit; very common for comets
+        // TODO: handle this
+        return M;
+    }
+    else
+    {
+        // Laguerre-Conway method for hyperbolic (ecc > 1) orbits.
+        double E = log(2 * M / eccentricity + 1.85);
+        Solution sol = solve_iteration_fixed(SolveKeplerLaguerreConwayHyp(eccentricity, M), E, 30);
+        return sol.first;
+    }
+}
+
+
+Vec3d EllipticalOrbit::positionAtE(double E) const
+{
+    double x, z;
+
+    if (eccentricity < 1.0)
+    {
+        double a = pericenterDistance / (1.0 - eccentricity);
+        x = a * (cos(E) - eccentricity);
+        z = a * sqrt(1 - eccentricity * eccentricity) * -sin(E);
+    }
+    else if (eccentricity > 1.0)
+    {
+        double a = pericenterDistance / (1.0 - eccentricity);
+        x = -a * (eccentricity - cosh(E));
+        z = -a * sqrt(eccentricity * eccentricity - 1) * -sinh(E);
+    }
+    else
+    {
+        // TODO: Handle parabolic orbits
+        x = 0.0;
+        z = 0.0;
+    }
+
+    Mat4d R = (Mat4d::yrotation(ascendingNode) *
+               Mat4d::xrotation(inclination) *
+               Mat4d::yrotation(argOfPeriapsis));
+
+    return R * Vec3d(x, 0, z);
+}
+
+
+// Return the offset from the center
+Vec3d EllipticalOrbit::positionAtTime(double t) const
+{
+    t = t - epoch;
+    double meanMotion = 2.0 * M_PI / period;
+    double meanAnomaly = meanAnomalyAtEpoch + t * meanMotion;
+    double E = eccentricAnomaly(meanAnomaly);
+    
+    return positionAtE(E);
+}
+
+void EllipticalOrbit::positionAtTime(double JD, double * X, double * Y, double * Z)
+{
+	Vec3d pos = positionAtTime(JD);
+	*X=pos[2];
+	*Y=pos[0];
+	*Z=pos[1];
+}
+
+double EllipticalOrbit::getPeriod() const
+{
+    return period;
+}
+
+
+double EllipticalOrbit::getBoundingRadius() const
+{
+    // TODO: watch out for unbounded parabolic and hyperbolic orbits
+    return pericenterDistance * ((1.0 + eccentricity) / (1.0 - eccentricity));
+}
+
+
+void EllipticalOrbit::sample(double start, double t, int nSamples,
+                             OrbitSampleProc& proc) const
+{
+    double dE = 2 * M_PI / (double) nSamples;
+    for (int i = 0; i < nSamples; i++)
+        proc.sample(positionAtE(dE * i));
+}
+
+
+
+Vec3d CachingOrbit::positionAtTime(double jd) const
+{
+    if (jd != lastTime)
+    {
+        lastTime = jd;
+        lastPosition = computePosition(jd);
+    }
+    return lastPosition;
+}
+
+
+void CachingOrbit::sample(double start, double t, int nSamples,
+                          OrbitSampleProc& proc) const
+{
+    double dt = t / (double) nSamples;
+    for (int i = 0; i < nSamples; i++)
+        proc.sample(positionAtTime(start + dt * i));
+}
+
+
---
+++ src/orbit.h
@@ -0,0 +1,91 @@
+// orbit.h
+//
+// Copyright (C) 2001, Chris Laurel <claurel@shatters.net>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _ORBIT_H_
+#define _ORBIT_H_
+
+#include "vecmath.h"
+
+
+class OrbitSampleProc;
+
+class Orbit
+{
+public:
+    virtual Vec3d positionAtTime(double) const = 0;
+    virtual double getPeriod() const = 0;
+    virtual double getBoundingRadius() const = 0;
+    virtual void sample(double, double, int, OrbitSampleProc&) const = 0;
+};
+
+
+class EllipticalOrbit : public Orbit
+{
+public:
+    EllipticalOrbit(double, double, double, double, double, double, double,
+                    double _epoch = 2451545.0);
+
+    // Compute the orbit for a specified Julian date and return a "stellarium compliant" function
+	void positionAtTime(double JD, double * X, double * Y, double * Z);
+	// Original one
+	Vec3d positionAtTime(double) const;
+    double getPeriod() const;
+    double getBoundingRadius() const;
+    virtual void sample(double, double, int, OrbitSampleProc&) const;
+
+private:
+    double eccentricAnomaly(double) const;
+    Vec3d positionAtE(double) const;
+
+    double pericenterDistance;
+    double eccentricity;
+    double inclination;
+    double ascendingNode;
+    double argOfPeriapsis;
+    double meanAnomalyAtEpoch;
+    double period;
+    double epoch;
+};
+
+
+class OrbitSampleProc
+{
+ public:
+    virtual void sample(const Vec3d&) = 0;
+};
+
+
+
+// Custom orbit classes should be derived from CachingOrbit.  The custom
+// orbits can be expensive to compute, with more than 50 periodic terms.
+// Celestia may need require position of a planet more than once per frame; in
+// order to avoid redundant calculation, the CachingOrbit class saves the
+// result of the last calculation and uses it if the time matches the cached
+// time.
+class CachingOrbit : public Orbit
+{
+public:
+    CachingOrbit() : lastTime(1.0e-30) {};
+
+    virtual Vec3d computePosition(double jd) const = 0;
+    virtual double getPeriod() const = 0;
+    virtual double getBoundingRadius() const = 0;
+
+    Vec3d positionAtTime(double jd) const;
+
+    virtual void sample(double, double, int, OrbitSampleProc& proc) const;
+
+private:
+    mutable Vec3d lastPosition;
+    mutable double lastTime;
+};
+
+
+
+#endif // _ORBIT_H_
