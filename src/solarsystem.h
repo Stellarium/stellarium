9348d51b0bbd4bf7828cd782e2e517a000900876
digitalises <>
2004-08-18 21:35:58 +0000

    tui changes

--- src/solarsystem.h
+++ src/solarsystem.h
@@ -30,13 +30,13 @@
 class SolarSystem
 {
 public:
-	SolarSystem();
+    SolarSystem(const string& _data_dir, const string& _sky_locale, const string& _font_filename, Vec3f label_color, Vec3f orbit_color);
 
     virtual ~SolarSystem();
 
 	// Init and load the solar system data from the file "planetfile".
     //	void init(const string& font_fileName, const string& planetfile, Vec3f label_color,  Vec3f orbit_color);
-	void init(const string& _data_dir, const string& _sky_locale, const string& _font_filename, Vec3f label_color, Vec3f orbit_color);
+    //	void init(const string& _data_dir, const string& _sky_locale, const string& _font_filename, Vec3f label_color, Vec3f orbit_color);
 
 	// Compute the position for every elements of the solar system.
 	void compute_positions(double date);
