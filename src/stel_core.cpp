8ad500a99af3a3bf9a9af05c79d43a365be7b5c3
Fabien Chéreau <fabien.chereau@gmail.com>
2004-10-13 22:09:02 +0000

    Fixed the flashing screen bug at startup.

--- src/stel_core.cpp
+++ src/stel_core.cpp
@@ -162,6 +162,8 @@ void stel_core::init(void)
 	// Load the nebulas data TODO : add NGC objects
 	projection->set_orthographic_projection();
 	glClear(GL_COLOR_BUFFER_BIT);
+	SDL_GL_SwapBuffers();
+	glClear(GL_COLOR_BUFFER_BIT);
 	
 	// Load constellations
 	asterisms = new Constellation_mgr(DataDir, SkyCulture, SkyLocale, hip_stars, "spacefont.txt", screen_W/2-150, screen_H/2-20, ConstLinesColor, ConstNamesColor);
