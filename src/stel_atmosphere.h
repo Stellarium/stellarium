01f36e6e8948b974b8fa0a80eb7a7dc98adde414
Fabien Chéreau <fabien.chereau@gmail.com>
2005-03-25 22:22:37 +0000

    Cleaned atmosphere intensity stuffs. Moved atmosphere color computation into the core::update() part instead of draw().

--- src/stel_atmosphere.h
+++ src/stel_atmosphere.h
@@ -30,6 +30,7 @@
 #include "navigator.h"
 #include "tone_reproductor.h"
 #include "skybright.h"
+#include "fader.h"
 
 using namespace std;
 
@@ -38,24 +39,27 @@ class stel_atmosphere
 public:
     stel_atmosphere();
     virtual ~stel_atmosphere();
-	void compute_color(double JD, int delta_time, Vec3d sunPos, Vec3d moonPos, float moon_phase, tone_reproductor * eye, Projector* prj,
+	void compute_color(double JD, Vec3d sunPos, Vec3d moonPos, float moon_phase, tone_reproductor * eye, Projector* prj,
 		float latitude = 45.f, float altitude = 200.f,
 		float temperature = 15.f, float relative_humidity = 40.f);
-	void show_atmosphere(void);
-	void hide_atmosphere(void);
-	float get_intensity(void);  // let's you know how far faded in or out the atm is (0-1)
 	void draw(Projector* prj, int delta_time);
-	void set_fade_duration(float duration);
+	void update(int delta_time) {fader.update(delta_time);}
+	void set_fade_duration(float duration) {fader.set_duration((int)(duration*1000.f));}
+	void show(bool b){fader = b;}
+	float get_fade_intensity(void) {return fader.get_interstate();}  // let's you know how far faded in or out the atm is (0-1)
+	float get_world_adaptation_luminance(void) const {return world_adaptation_luminance;}
 private:
 	skylight sky;
 	skybright skyb;
 	int sky_resolution;
 	Vec3f ** tab_sky;	// For Atmosphere calculation
 	int startY;			// intern variable used to store the Horizon Y screen value
-	bool atm_on;
+	float world_adaptation_luminance;
+// 	bool atm_on;
 	float atm_intensity;
-	float ai;               // used to calculate atm_intensity via curve function
-	float fade_duration;    // length of time, in miliseconds, for fade in and out
+// 	float ai;               // used to calculate atm_intensity via curve function
+// 	float fade_duration;    // length of time, in miliseconds, for fade in and out
+	linear_fader fader;
 };
 
 #endif // _STEL_ATMOSTPHERE_H_
