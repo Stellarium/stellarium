8112645a317ae6be3d1293a93080d8381daeff1d
Fabien Chéreau <fabien.chereau@gmail.com>
2005-06-18 12:18:52 +0000

    Added the loading bar class.

---
+++ src/loadingbar.h
@@ -0,0 +1,42 @@
+/*
+ * Stellarium
+ * Copyright (C) 2005 Fabien Chéreau
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _LOADINGBAR_H_
+#define _LOADINGBAR_H_
+
+#include "projector.h"
+#include "s_font.h"
+
+// Class which is used to display loading bar
+class LoadingBar
+{
+public:
+	// Create and initialise
+	LoadingBar(Projector* prj, string _font_filename, int barx, int bary);
+	virtual ~LoadingBar();
+	void SetMessage(string m) {message=m;}
+	void Draw(float val);
+private:
+	string message;
+	Projector* prj;
+	int barx, bary;
+	s_font* barfont;
+};
+
+#endif //_LOADINGBAR_H_
