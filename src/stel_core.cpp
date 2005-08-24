7452159c7205dac98d6aa46df0a2ceebdb17a7cb
digitalises <>
2005-08-24 15:20:45 +0000

    Bug fix for missing degree characters in stel_utility.cpp - broke lat/lon in config file

--- src/stel_core.cpp
+++ src/stel_core.cpp
@@ -320,7 +320,7 @@ void stel_core::update(int delta_time)
 	// compute global sky brightness TODO : make this more "scientifically"
 	// TODO: also add moonlight illumination
 
-	if(sunPos[2] < -0.1 ) {
+	if(sunPos[2] < -0.1/1.5 ) {
 		sky_brightness = 0;
 	} else {
 		sky_brightness = (0.1 + 1.5*sunPos[2]) * atmosphere->get_intensity();
