8f1df8fdfd3918798429852eba1ffe743035f167
Fabien Chereau <fabien.chereau@gmail.com>
2005-08-28 20:10:05 +0000

    Removed buggy auto-gettext installation and added all necessary files to the repository.

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-SETUP_GETTEXT=./setup-gettext
-
-($SETUP_GETTEXT --gettext-tool) < /dev/null > /dev/null 2>&1 || {
-	echo;
-	echo "You must have gettext installed to compile Stellarium";
-	echo;
-	exit;
-}
-
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo;
 	echo "You must have automake installed to compile Stellarium";
@@ -25,18 +16,6 @@ SETUP_GETTEXT=./setup-gettext
 
 echo "Generating configuration files for Stellarium, please wait...."
 echo;
-
-# Backup the po/ChangeLog. This should prevent the annoying
-# gettext ChangeLog modifications.
-
-cp -p po/ChangeLog po/ChangeLog.save
-
-echo "Running gettextize, please ignore non-fatal messages...."
-$SETUP_GETTEXT
-
-# Restore the po/ChangeLog file.
-mv po/ChangeLog.save po/ChangeLog
-
 
 aclocal $ACLOCAL_FLAGS || exit;
 autoheader || exit;
