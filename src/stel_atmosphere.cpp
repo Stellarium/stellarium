aa77259e5bfd4455a4503628f3a18b307371b944
xalioth <>
2006-03-05 23:56:04 +0000

    Little optimizations.

--- src/stel_atmosphere.cpp
+++ src/stel_atmosphere.cpp
@@ -48,8 +48,8 @@ Atmosphere::~Atmosphere()
 }
 
 void Atmosphere::compute_color(double JD, Vec3d sunPos, Vec3d moonPos, float moon_phase,
-	ToneReproductor * eye, Projector* prj,
-	float latitude, float altitude, float temperature, float relative_humidity)
+                               ToneReproductor * eye, Projector* prj,
+                               float latitude, float altitude, float temperature, float relative_humidity)
 {
 
 	float min_mw_lum = 0.13;
@@ -67,7 +67,7 @@ void Atmosphere::compute_color(double JD, Vec3d sunPos, Vec3d moonPos, float moo
 		atm_intensity = fader.getInterstate();
 	}
 
-	
+
 	//Vec3d obj;
 	skylight_struct2 b2;
 
@@ -90,19 +90,22 @@ void Atmosphere::compute_color(double JD, Vec3d sunPos, Vec3d moonPos, float moo
 	// TODO: correct for atmospheric diffusion
 	// TODO: use better coverage function (non-linear)
 	// because of above issues, this algorithm darkens more quickly than reality
-	if( separation_angle < touch_angle) {
+	if( separation_angle < touch_angle)
+	{
 		float min;
-		if(dark_angle < 0) {
+		if(dark_angle < 0)
+		{
 			// annular eclipse
 			float asun = sun_angular_size*sun_angular_size;
 			min = (asun - moon_angular_size*moon_angular_size)/asun;  // minimum proportion of sun uncovered
 			dark_angle *= -1;
-		} else min = 0.004;  // so bright stars show up at total eclipse
+		}
+		else min = 0.004;  // so bright stars show up at total eclipse
 
 		if(separation_angle < dark_angle) atm_intensity = min;
 		else atm_intensity *= min + (1.-min)*(separation_angle-dark_angle)/(touch_angle-dark_angle);
 
-		//		printf("atm int %f (min %f)\n", atm_intensity, min);		
+		//		printf("atm int %f (min %f)\n", atm_intensity, min);
 	}
 
 	float sun_pos[3];
@@ -157,18 +160,18 @@ void Atmosphere::compute_color(double JD, Vec3d sunPos, Vec3d moonPos, float moo
 			b2.pos[2] = point[2];
 
 			// Use the Skylight model for the color
-			sky.get_xyY_valuev(&b2);
+			sky.get_xyY_valuev(b2);
 
 			// Use the Skybright.cpp 's models for brightness which gives better results.
 			b2.color[2] = skyb.get_luminance(moon_pos[0]*b2.pos[0]+moon_pos[1]*b2.pos[1]+
-					moon_pos[2]*b2.pos[2], sun_pos[0]*b2.pos[0]+sun_pos[1]*b2.pos[1]+
-					sun_pos[2]*b2.pos[2], b2.pos[2]);
+			                                 moon_pos[2]*b2.pos[2], sun_pos[0]*b2.pos[0]+sun_pos[1]*b2.pos[1]+
+			                                 sun_pos[2]*b2.pos[2], b2.pos[2]);
 
 
 			sum_lum+=b2.color[2];
 			++nb_lum;
 			eye->xyY_to_RGB(b2.color);
-			tab_sky[x][y].set(b2.color[0],b2.color[1],b2.color[2]);
+			tab_sky[x][y].set(atm_intensity*b2.color[0],atm_intensity*b2.color[1],atm_intensity*b2.color[2]);
 		}
 	}
 
@@ -186,32 +189,29 @@ void Atmosphere::draw(Projector* prj, int delta_time)
 {
 	if(fader.getInterstate())
 	{
+		// printf("Atm int: %f\n", atm_intensity);
+		glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_COLOR);
 
-	  // printf("Atm int: %f\n", atm_intensity);
-	  glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_COLOR);
+		float stepX = (float)prj->viewW() / sky_resolution;
+		float stepY = (float)prj->viewH() / sky_resolution;
+		float viewport_left = (float)prj->view_left();
+		float view_bottom = (float)prj->view_bottom();
 
-	  float stepX = (float)prj->viewW() / sky_resolution;
-	  float stepY = (float)prj->viewH() / sky_resolution;
-	  float viewport_left = (float)prj->view_left();
-	  float view_bottom = (float)prj->view_bottom();
-
-	  glDisable(GL_TEXTURE_2D);
-	  glEnable(GL_BLEND);
-	  prj->set_orthographic_projection();	// set 2D coordinate
-	  for (int y2=0; y2<sky_resolution; ++y2)
-	    {
-	      glBegin(GL_QUAD_STRIP);
-	      for(int x2=0; x2<sky_resolution+1; ++x2)
+		glDisable(GL_TEXTURE_2D);
+		glEnable(GL_BLEND);
+		prj->set_orthographic_projection();	// set 2D coordinate
+		for (int y2=0; y2<sky_resolution; ++y2)
 		{
-		  glColor3f(atm_intensity*tab_sky[x2][y2][0],atm_intensity*tab_sky[x2][y2][1],
-			    atm_intensity*tab_sky[x2][y2][2]);
-		  glVertex2i((int)(viewport_left+x2*stepX),(int)(view_bottom+y2*stepY));
-		  glColor3f(atm_intensity*tab_sky[x2][y2+1][0],atm_intensity*tab_sky[x2][y2+1][1],
-			    atm_intensity*tab_sky[x2][y2+1][2]);
-		  glVertex2i((int)(viewport_left+x2*stepX),(int)(view_bottom+(y2+1)*stepY));
+			glBegin(GL_QUAD_STRIP);
+			for(int x2=0; x2<sky_resolution+1; ++x2)
+			{
+				glColor3fv(tab_sky[x2][y2]);
+				glVertex2i((int)(viewport_left+x2*stepX),(int)(view_bottom+y2*stepY));
+				glColor3fv(tab_sky[x2][y2+1]);
+				glVertex2i((int)(viewport_left+x2*stepX),(int)(view_bottom+(y2+1)*stepY));
+			}
+			glEnd();
 		}
-	      glEnd();
-	    }
-	  prj->reset_perspective_projection();
+		prj->reset_perspective_projection();
 	}
 }
