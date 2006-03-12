f8abf079d704f829abffb89427e96bca2a8ff828
Fabien Chéreau <fabien.chereau@gmail.com>
2006-03-12 22:59:17 +0000

    Fixed blinking bug when text moves.

--- src/typeface.cpp
+++ src/typeface.cpp
@@ -245,8 +245,8 @@ createCacheTexture(const Vec2size_t& aGlyphSize, size_t numGlyphsToCache)
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
     info.size_ = cacheTextureSize(aGlyphSize, numGlyphsToCache);
 
