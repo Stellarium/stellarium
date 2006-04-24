e211b9feb40f3346f21e57e87d987cfca6c425a5
gajdosik <>
2006-04-24 22:19:49 +0000

    new implementation of vsop87 replaces old implemention, which actually was vsop87b

--- src/stellplanet/stellplanet.h
+++ src/stellplanet/stellplanet.h
@@ -24,41 +24,52 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 extern "C" {
 #endif
 
-void get_sun_helio_coordsv(double JD, double* XYZ);
-void get_mercury_helio_coordsv(double JD, double* XYZ);
-void get_venus_helio_coordsv(double JD, double* XYZ);
-void get_earth_helio_coordsv(double JD, double* XYZ);
-void get_mars_helio_coordsv(double JD, double* XYZ);
-void get_jupiter_helio_coordsv(double JD, double* XYZ);
-void get_saturn_helio_coordsv(double JD, double* XYZ);
-void get_uranus_helio_coordsv(double JD, double* XYZ);
-void get_neptune_helio_coordsv(double JD, double* XYZ);
-void get_pluto_helio_coordsv(double JD, double* XYZ);
+void get_sun_helio_coordsv(double jd,double xyz[3]);
 
-void get_lunar_parent_coordsv(double JD, double* XYZ);
+void get_mercury_helio_coordsv(double jd,double xyz[3]);
+void get_venus_helio_coordsv(double jd,double xyz[3]);
+void get_earth_helio_coordsv(double jd,double xyz[3]);
+void get_mars_helio_coordsv(double jd,double xyz[3]);
+void get_jupiter_helio_coordsv(double jd,double xyz[3]);
+void get_saturn_helio_coordsv(double jd,double xyz[3]);
+void get_uranus_helio_coordsv(double jd,double xyz[3]);
+void get_neptune_helio_coordsv(double jd,double xyz[3]);
+void get_pluto_helio_coordsv(double jd,double xyz[3]);
 
-void get_phobos_parent_coordsv(double JD, double* XYZ);
-void get_deimos_parent_coordsv(double JD, double* XYZ);
+void get_mercury_helio_osculating_coords(double jd0,double jd,double xyz[3]);
+void get_venus_helio_osculating_coords(double jd0,double jd,double xyz[3]);
+void get_earth_helio_osculating_coords(double jd0,double jd,double xyz[3]);
+void get_mars_helio_osculating_coords(double jd0,double jd,double xyz[3]);
+void get_jupiter_helio_osculating_coords(double jd0,double jd,double xyz[3]);
+void get_saturn_helio_osculating_coords(double jd0,double jd,double xyz[3]);
+void get_uranus_helio_osculating_coords(double jd0,double jd,double xyz[3]);
+void get_neptune_helio_osculating_coords(double jd0,double jd,double xyz[3]);
+void get_pluto_helio_osculating_coords(double jd0,double jd,double xyz[3]);
 
-void get_io_parent_coordsv(double JD, double* XYZ);
-void get_europa_parent_coordsv(double JD, double* XYZ);
-void get_ganymede_parent_coordsv(double JD, double* XYZ);
-void get_callisto_parent_coordsv(double JD, double* XYZ);
+void get_lunar_parent_coordsv(double jd,double xyz[3]);
 
-void get_mimas_parent_coordsv(double JD, double* XYZ);
-void get_enceladus_parent_coordsv(double JD, double* XYZ);
-void get_tethys_parent_coordsv(double JD, double* XYZ);
-void get_dione_parent_coordsv(double JD, double* XYZ);
-void get_rhea_parent_coordsv(double JD, double* XYZ);
-void get_titan_parent_coordsv(double JD, double* XYZ);
-void get_hyperion_parent_coordsv(double JD, double* XYZ);
-void get_iapetus_parent_coordsv(double JD, double* XYZ);
+void get_phobos_parent_coordsv(double jd,double xyz[3]);
+void get_deimos_parent_coordsv(double jd,double xyz[3]);
 
-void get_miranda_parent_coordsv(double JD, double* XYZ);
-void get_ariel_parent_coordsv(double JD, double* XYZ);
-void get_umbriel_parent_coordsv(double JD, double* XYZ);
-void get_titania_parent_coordsv(double JD, double* XYZ);
-void get_oberon_parent_coordsv(double JD, double* XYZ);
+void get_io_parent_coordsv(double jd,double xyz[3]);
+void get_europa_parent_coordsv(double jd,double xyz[3]);
+void get_ganymede_parent_coordsv(double jd,double xyz[3]);
+void get_callisto_parent_coordsv(double jd,double xyz[3]);
+
+void get_mimas_parent_coordsv(double jd,double xyz[3]);
+void get_enceladus_parent_coordsv(double jd,double xyz[3]);
+void get_tethys_parent_coordsv(double jd,double xyz[3]);
+void get_dione_parent_coordsv(double jd,double xyz[3]);
+void get_rhea_parent_coordsv(double jd,double xyz[3]);
+void get_titan_parent_coordsv(double jd,double xyz[3]);
+void get_hyperion_parent_coordsv(double jd,double xyz[3]);
+void get_iapetus_parent_coordsv(double jd,double xyz[3]);
+
+void get_miranda_parent_coordsv(double jd,double xyz[3]);
+void get_ariel_parent_coordsv(double jd,double xyz[3]);
+void get_umbriel_parent_coordsv(double jd,double xyz[3]);
+void get_titania_parent_coordsv(double jd,double xyz[3]);
+void get_oberon_parent_coordsv(double jd,double xyz[3]);
 
 #ifdef __cplusplus
 };
--- src/stellplanet/vsop87.h
+++ src/stellplanet/vsop87.h
@@ -1,35 +1,79 @@
-/*
+/************************************************************************
 
-Copyright 2000 Liam Girdwood
-Modified 2003 Fabien Chéreau
+The PLANETARY SOLUTION VSOP87 by Bretagnon P. and Francou G. can be found at
+ftp://ftp.imcce.fr/pub/ephem/planets/vsop87
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+I (Johannes Gajdosik) have just taken the data obtained from above
+(VSOP87.mer,...,VSOP87.nep) and rearranged it into this piece of software.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+I can neigther allow nor forbid the usage of VSOP87.
+The copyright notice below covers not the work of Bretagnon P. and Francou G.
+but just my work, that is the compilation of the VSOP87 data
+into the software supplied in this file.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-*/
+Copyright (c) 2006 Johannes Gajdosik
 
-#ifndef LN_VSOP87_H
-#define LN_VSOP87_H
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
 
-struct vsop
-{
-	float A;
-	float B;
-	float C;
-};
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
 
-double calc_series (const struct vsop * data, int terms, double t);
-void vsop87_to_fk5 (double *L, double *B, double JD);
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+This is my implementation of the VSOP87 planetary solution.
+I tried to optimize for speed by rearranging the terms so that
+for a given touple of (a[0],a[1],...,a[11]) the values
+  (cos,sin)(a[0]*lambda[0](T)+...a[11]*lambda[11](T))
+have only to be calculated once.
+Furthermore I used the addition formulas
+  (cos,sin)(x+y) = ...
+so that for given T the functions cos and sin have only to be called 12 times.
+
+****************************************************************/
+
+
+#ifndef _VSOP87_H_
+#define _VSOP87_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define VSOP87_MERCURY  0
+#define VSOP87_VENUS    1
+#define VSOP87_EMB      2
+#define VSOP87_MARS     3
+#define VSOP87_JUPITER  4
+#define VSOP87_SATURN   5
+#define VSOP87_URANUS   6
+#define VSOP87_NEPTUNE  7
+
+void GetVsop87Coor(double jd,int body,double *xyz);
+  /* Return the rectangular coordinates of the given planet
+     and the given julian date jd expressed in dynamical time (TAI+32.184s).
+     The origin of the xyz-coordinates is the center of the sun.
+     The reference frame is "dynamical equinox and ecliptic J2000",
+     which is the reference frame in VSOP87 and VSOP87A.
+  */
+
+void GetVsop87OsculatingCoor(double jd0,double jd,int body,double *xyz);
+  /* The oculating orbit of epoch jd0, evatuated at jd, is returned.
+  */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
