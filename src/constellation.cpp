fee9991819b1765696ab5eec96ebd6a95c10221f
Rob Spearman <rob@digitaliseducation.com>
2006-04-27 23:05:55 +0000

    put alpha on label fade in/out for planets and const.

--- src/constellation.cpp
+++ src/constellation.cpp
@@ -125,7 +125,7 @@ void Constellation::draw_optim(Projector* prj) const
 void Constellation::draw_name(s_font *constfont, Projector* prj) const
 {
 	if(!name_fader.getInterstate()) return;
-	glColor3fv(labelColor*name_fader.getInterstate());
+	glColor4f(labelColor[0], labelColor[1], labelColor[2], name_fader.getInterstate());
 	prj->getFlagGravityLabels() ?
 		prj->print_gravity180(constfont, XYname[0], XYname[1], nameI18, 1, -constfont->getStrLen(nameI18)/2) :
 		constfont->print(XYname[0]-constfont->getStrLen(nameI18)/2, XYname[1], nameI18);
--- src/planet.cpp
+++ src/planet.cpp
@@ -537,9 +537,8 @@ void Planet::draw_hints(const Navigator* nav, const Projector* prj)
 	// Draw nameI18 + scaling if it's not == 1.
 	float tmp = 10.f + get_on_screen_size(prj, nav)/sphere_scale/2.f; // Shift for nameI18 printing
 
-	//	glColor4f(label_color[0], label_color[1], label_color[2],1.f);
-	//	glColor4f(label_color[0], label_color[1], label_color[2],hint_fader.getInterstate());
-	glColor3fv(label_color*hint_fader.getInterstate());
+	glColor4f(label_color[0], label_color[1], label_color[2],hint_fader.getInterstate());
+
 	prj->getFlagGravityLabels() ? 
 		prj->print_gravity180(planet_name_font, screenPos[0],screenPos[1], getShortInfoString(nav), 1, tmp, tmp) :
 		planet_name_font->print(screenPos[0]+tmp,screenPos[1]+tmp, getShortInfoString(nav));
@@ -547,8 +546,7 @@ void Planet::draw_hints(const Navigator* nav, const Projector* prj)
 	// hint disapears smoothly on close view
 	tmp -= 10.f;
 	if (tmp<1) tmp=1;
-	//glColor4f(label_color[0]/tmp, label_color[1]/tmp, label_color[2]/tmp,hint_fader.getInterstate()/tmp);
-	glColor3fv(label_color*hint_fader.getInterstate()/tmp);
+	glColor4f(label_color[0], label_color[1], label_color[2],hint_fader.getInterstate()/tmp);
 
 	// Draw the 2D small circle
 	glCircle(screenPos, 8);
