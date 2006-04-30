f2190933302d9fa579c1409884edcc2503fa5d7f
Fabien Chereau <fabien.chereau@gmail.com>
2006-04-30 18:53:00 +0000

    Fixed wrong language during boot.

--- src/translator.h
+++ src/translator.h
@@ -41,78 +41,82 @@ using namespace std;
 
 //! Class used to translate strings to any language.
 //! Implements a nice interface to gettext which is UTF-8 compliant and is somewhat multiplateform
-//! All its operations do not modify the global locale. 
+//! All its operations do not modify the global locale.
 //! The purpose of this class is to remove all non-OO C locale functions from stellarium.
 //! It could be extended for all locale management using e.g the portable IBM ICU library.
 //! @author Fabien Chereau
-class Translator{
+class Translator
+{
 public:
 
-	 //! @brief Create a translator from a language name.
-     //! If the passed locale name cannot be handled by the system, default value will be used.
-     //! The passed language name is a language code string like "fr" or "fr_FR".
-     //! This class wrap gettext to simulate an object oriented multiplateform gettext UTF8 translator
-     //! @param _domain The name of the domain to use for translation
-	 //! @param _moDirectory The directory where to look for the domain.mo translation files.
-     //! @param _langName The C locale name or language name like "fr" or "fr_FR". If string is "" or "system" it will use the system locale.
-     Translator(const std::string& _domain, const std::string& _moDirectory, const std::string& _langName) :
-     	domain(_domain), moDirectory(_moDirectory), langName(_langName)
-     {
-     	Translator::lastUsed = NULL;
-	 }
-	 
-	 //! @brief Translate input message.
-	 //! @param s input string in english.
-	 //! @return The translated string in UTF-8 characters.
-	 std::string translateUTF8(const std::string& s)
-	 {
+	//! @brief Create a translator from a language name.
+	//! If the passed locale name cannot be handled by the system, default value will be used.
+	//! The passed language name is a language code string like "fr" or "fr_FR".
+	//! This class wrap gettext to simulate an object oriented multiplateform gettext UTF8 translator
+	//! @param _domain The name of the domain to use for translation
+	//! @param _moDirectory The directory where to look for the domain.mo translation files.
+	//! @param _langName The C locale name or language name like "fr" or "fr_FR". If string is "" or "system" it will use the system locale.
+	Translator(const std::string& _domain, const std::string& _moDirectory, const std::string& _langName) :
+			domain(_domain), moDirectory(_moDirectory), langName(_langName)
+	{
+		Translator::lastUsed = NULL;
+	}
+
+	//! @brief Translate input message.
+	//! @param s input string in english.
+	//! @return The translated string in UTF-8 characters.
+	std::string translateUTF8(const std::string& s)
+	{
 		reload();
 		return gettext(s.c_str());
-	 }
-	 
-	 //! @brief Translate input message.
-	 //! @param s input string in english.
-	 //! @return The translated string in wide characters.
-	 std::wstring translate(const std::string& s)
-	 {
-	 	if (s=="") return L"";
-	 	return UTF8stringToWstring(translateUTF8(s));
-	 }	 
-	 
-	 //! @brief Get translator locale name. This name can be used to create a translator.
-	 //! @return Locale name e.g "fr_FR"
-	 std::string getLocaleName(void)
-	 {
-	 	return langName;
-	 }
-	 
-	 //! Used as a global translator by the whole app
-	 static Translator globalTranslator;
-	 
+	}
+
+	//! @brief Translate input message.
+	//! @param s input string in english.
+	//! @return The translated string in wide characters.
+	std::wstring translate(const std::string& s)
+	{
+		if (s=="") return L"";
+		return UTF8stringToWstring(translateUTF8(s));
+	}
+
+	//! @brief Get translator locale name. This name can be used to create a translator.
+	//! @return Locale name e.g "fr_FR"
+	std::string getLocaleName(void)
+	{
+		return langName;
+	}
+
+	//! Used as a global translator by the whole app
+	static Translator globalTranslator;
+
 	//! Convert from UTF-8 to wchar_t
 	static std::wstring UTF8stringToWstring(const string& s);
-	
+
 	//! Get available language codes from passed locales directory
 	static std::string getAvailableLanguagesCodes(const string& localeDir);
-	
+
+	//! Try to determine system language from system configuration
+	static void initSystemLanguage(void);
+
 private:
 	//! Reload the current locale info so that gettext use them
 	void reload();
-	
+
 	//! The domain name
 	string domain;
-	
+
 	//! The directory where the locale file tree stands
 	string moDirectory;
-	
+
 	//! The two letter string defining the current language name
 	string langName;
-	
+
 	//! Keep in memory which one was the last used transator to prevent reloading it at each tranlate() call
 	static Translator* lastUsed;
-	
+
 	//! Store the system default language name as taken from LANGUAGE environement variable
-	static const string systemLangName;
+	static string systemLangName;
 };
 
 #endif
