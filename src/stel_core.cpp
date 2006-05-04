98c4deb81a11888a9ddc631e0669d5d91ad97d54
digitalises <>
2006-05-04 00:34:59 +0000

    allow loading of non-NGC nebulas

--- src/stel_core.cpp
+++ src/stel_core.cpp
@@ -536,7 +536,8 @@ StelObject * StelCore::clever_find(const Vec3d& v) const
 			if( nebulas->getFlagHints() )
 			{
 				// make very easy to select if labeled
-				mag = -1;
+				// mag = -1;  Too easy with NGC data
+				mag -= 5;
 			}
 		}
 		if ((*iter)->get_type()==StelObject::STEL_OBJECT_PLANET)
