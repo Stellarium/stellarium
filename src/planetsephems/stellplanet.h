7714c544b2a926946a0e938b14d8c52325f44de6
Fabien Chéreau <fabien.chereau@gmail.com>
2006-08-23 20:31:26 +0000

    Moved stellplanet directory to planetsephems.

--- src/planetsephems/stellplanet.h
+++ src/planetsephems/stellplanet.h
@@ -1,6 +1,6 @@
 /*
 Copyright (C) 2000 Liam Girdwood <liam@nova-ioe.org>
-Copyright (C) 2003 Fabien Chéreau
+Copyright (C) 2003 Fabien Chereau
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Library General Public License as published by
