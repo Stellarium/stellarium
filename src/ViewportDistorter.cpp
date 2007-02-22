2d1617e36c7e603060bf4506982124e466de1851
Johannes Gajdosik <glunatic.johannes.gajdosik@gmail.com>
2007-02-22 13:26:30 +0000

    tiny improvement

--- src/ViewportDistorter.cpp
+++ src/ViewportDistorter.cpp
@@ -66,8 +66,8 @@ private:
 
   int viewport[2],viewport_texture_offset[2];
   int viewport_w,viewport_h;
-  double viewport_center[2];
-  double viewport_fov_diameter;
+  float viewport_center[2];
+  float viewport_fov_diameter;
 
   int texture_wh;
 
@@ -122,10 +122,10 @@ ViewportDistorterFisheyeToSphericMirror
   cout << "ViewportDistorterFisheyeToSphericMirror::"
           "ViewportDistorterFisheyeToSphericMirror: "
           "flag_use_ext_framebuffer_object: "
-     << flag_use_ext_framebuffer_object << endl;
+       << flag_use_ext_framebuffer_object << endl;
 
   double texture_triangle_base_length
-    = conf.get_double("spheric_mirror","texture_triangle_base_length",8.0);  
+    = conf.get_double("spheric_mirror","texture_triangle_base_length",16.0);  
   if (texture_triangle_base_length > 256.0)
     texture_triangle_base_length = 256.0;
   else if (texture_triangle_base_length < 2.0)
@@ -168,22 +168,22 @@ ViewportDistorterFisheyeToSphericMirror
     viewport[0] = (screen_w-viewport_w) >> 1;
     viewport[1] = (screen_h-viewport_h) >> 1;
   }
-cout << "texture_wh: " << texture_wh << endl;
-cout << "viewport_fov_diameter: " << viewport_fov_diameter << endl;
-cout << "screen: " << screen_w << ", " << screen_h << endl;
-cout << "viewport: " << viewport[0] << ", " << viewport[1] << ", "
-     << viewport_w << ", " << viewport_h << endl;
-cout << "viewport_texture_offset: "
-     << viewport_texture_offset[0] << ", "
-     << viewport_texture_offset[1] << endl;
+//cout << "texture_wh: " << texture_wh << endl;
+//cout << "viewport_fov_diameter: " << viewport_fov_diameter << endl;
+//cout << "screen: " << screen_w << ", " << screen_h << endl;
+//cout << "viewport: " << viewport[0] << ", " << viewport[1] << ", "
+//     << viewport_w << ", " << viewport_h << endl;
+//cout << "viewport_texture_offset: "
+//     << viewport_texture_offset[0] << ", "
+//     << viewport_texture_offset[1] << endl;
 
 
 
     // initialize mirror_texture:
   glGenTextures(1, &mirror_texture);
   glBindTexture(GL_TEXTURE_2D, mirror_texture);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP /*_TO_EDGE*/);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP /*_TO_EDGE*/);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   if (flag_use_ext_framebuffer_object) {
@@ -209,11 +209,20 @@ cout << "viewport_texture_offset: "
         cout << "could not initialize GL_FRAMEBUFFER_EXT" << endl;
         assert(0);
     }
-    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);    // Unbind the FBO for now
+      // clear the texture
+    glClear(GL_COLOR_BUFFER_BIT);
+      // Unbind the FBO
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
   } else {
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    const int size = 3*texture_wh*texture_wh;
+    unsigned char *pixel_data = new unsigned char[size];
+    assert(pixel_data);
+    memset(pixel_data,0,size);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
-                 texture_wh, texture_wh, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+                 texture_wh, texture_wh, 0, GL_RGB, GL_UNSIGNED_BYTE,
+                 pixel_data);
+    delete pixel_data;
   }
 
   prj->setMaxFov(distorter_max_fov);
@@ -222,12 +231,15 @@ cout << "viewport_texture_offset: "
   double gamma
     = conf.get_double("spheric_mirror","projector_gamma",0.45);
   if (gamma < 0.0) gamma = 0.0;
-  const double view_scaling_fact
-    = prj->getMapping().fovToViewScalingFactor(distorter_max_fov);
+  const float view_scaling_factor
+    = viewport_fov_diameter
+    * prj->getMapping().fovToViewScalingFactor(distorter_max_fov);
   max_x = (int)trunc(0.5 + screen_w/texture_triangle_base_length);
   step_x = screen_w / (double)(max_x-0.5);
   max_y = (int)trunc(screen_h/(texture_triangle_base_length*0.5*sqrt(3.0)));
   step_y = screen_h/ (double)max_y;
+//cout << "max_x: " << max_x << ", max_y: " << max_y
+//     << ", step_x: " << step_x << ", step_y: " << step_y << endl;
   texture_point_array = new TexturePoint[(max_x+1)*(max_y+1)];
   VertexPoint *vertex_point_array = new VertexPoint[(max_x+1)*(max_x+1)];
   double max_h = 0;
@@ -249,14 +261,21 @@ cout << "viewport_texture_offset: "
       v[2] = -h;
 
       rc &= prj->getMapping().forward(v);
-      float x = viewport_center[0] + v[0] * view_scaling_fact * viewport_fov_diameter;
-      float y = viewport_center[1] + v[1] * view_scaling_fact * viewport_fov_diameter;
+      const float x = viewport_center[0] + v[0] * view_scaling_factor;
+      const float y = viewport_center[1] + v[1] * view_scaling_factor;
       vertex_point.h = rc ? (v_x^v_y).length() : 0.0;
 
-      if (x < 0.f) {x=0.f;vertex_point.h=0;}
-      else if (x > viewport_w) {x=viewport_w;vertex_point.h=0;}
-      if (y < 0.f) {y=0.f;vertex_point.h=0;}
-      else if (y > viewport_h) {y=viewport_h;vertex_point.h=0;}
+        // sharp image up to the border of the fisheye image, an the cost of
+        // accepting clamping artefacts. You can get rid of the clamping
+        // artefacts by specifying a viewport size a little less then
+        // (1<<n)*(1<<n), for instance 1022*1022. With a viewport size
+        // of 512*512 and viewport_fov_diameter=512 you will get clamping
+        // artefacts in the 3 otherwise black hills on the bottom of the image.
+
+//      if (x < 0.f) {x=0.f;vertex_point.h=0;}
+//      else if (x > viewport_w) {x=viewport_w;vertex_point.h=0;}
+//      if (y < 0.f) {y=0.f;vertex_point.h=0;}
+//      else if (y > viewport_h) {y=viewport_h;vertex_point.h=0;}
 
       texture_point.tex_xy[0] = (viewport_texture_offset[0]+x)/texture_wh;
       texture_point.tex_xy[1] = (viewport_texture_offset[1]+y)/texture_wh;
