ee81601875d0abfbd64c65a7e0406aafdc303888
xalioth <>
2007-05-13 09:32:52 +0000

    Added.

---
+++ config.h.cmake
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#cmakedefine HAVE_UNISTD_H
+#cmakedefine HAVE_STRING_H
+#cmakedefine HAVE_TIMEGM
+#cmakedefine HAVE_MKTIME
+#cmakedefine HAVE_POW10
+#cmakedefine HAVE_SETLOCALE
+#cmakedefine HAVE_LIBCURL
+#cmakedefine HAVE_SNPRINTF
+#cmakedefine HAVE_BYTESWAP_H
+#cmakedefine WORDS_BIGENDIAN
+
+/* Define whether QT4 is to be used. */
+#cmakedefine USE_QT4
+
+/* Define whether SDL is to be used. */
+#cmakedefine USE_SDL
+
+#endif
