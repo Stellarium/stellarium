541c181c64ba63ad107d00105a23d52836fdd8e5
Fabien Chéreau <fabien.chereau@gmail.com>
2007-07-05 12:17:24 +0000

    Commented.

--- src/AstroImage.hpp
+++ src/AstroImage.hpp
@@ -30,23 +30,39 @@ class ToneReproducer;
 class AstroImage
 {
 public:
+	//! Constructor
 	AstroImage();
+
+	//! Create an AstroImage from a texture and the 4 corners of its projection in the sky
+	//! The 4 corners can be given in any coordinate frame
 	AstroImage(STextureSP tex, const Vec3d& v0, const Vec3d& v1, const Vec3d& v2, const Vec3d& v3);
+
+	//! Create an empty AstroImage from the 4 corners of the image
+	//! Call setTexture later to set its texture
 	AstroImage(const Vec3d& v0, const Vec3d& v1, const Vec3d& v2, const Vec3d& v3);
+
+	//! Desctructor
 	virtual ~AstroImage();
 	
 	//! Draw the image on the screen. Assume that we are in Orthographic projection mode.
 	void draw(Projector *prj, const Navigator *nav, ToneReproducer *eye);
 	
-	//! Return the matching ConvexPolygon
+	//! Return the ConvexPolygon describing the 4 corners of the sky-projected image
 	const StelGeom::ConvexPolygon& getPolygon(void) const {return poly;}
 	
+	//! Get the texture of the image
 	const STextureSP getTexture() const {return tex;}
+	//! Set the texture for this image
 	void setTexture(STextureSP atex) {tex=atex;}
 
+	//! Get the loading status for the texture
+	//! @return true once the texture loading is over
 	bool getLoadStatus() const {return loadStatus;}
 
+	//! Get the path of the texture image file
+	//! @return the full path of the texture image file or an empty string if it is unknown
 	const std::string& getFullPath() const {return fullPath;}
+	
 private:
 	friend class FitsAstroImage;
 	
