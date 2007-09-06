d3ef6e6a006464f96a3e70df8210682a4f4089bc
Matthew Gates <matthewg42@gmail.com>
2007-09-06 16:55:37 +0000

    API docs for LandscapeMgr, plus a little coding standards formatting.

--- src/LandscapeMgr.hpp
+++ src/LandscapeMgr.hpp
@@ -27,10 +27,10 @@ class Landscape;
 class Atmosphere;
 class Cardinals;
 
-//! Class which manage all the rendering a the level of the observer's surrounding.
+//! @class LandscapeMgr Manages all the rendering a the level of the observer's surrounding.
 //! This includes landscape textures, fog, atmosphere and cardinal points
 //! I decided to put all these elements together in a single class because there are 
-//! inherently linked, especially when we start moving the observer in altitude
+//! inherently linked, especially when we start moving the observer in altitude.
 class LandscapeMgr : public StelModule
 {
 public:
@@ -39,103 +39,156 @@ public:
 	
 	///////////////////////////////////////////////////////////////////////////
 	// Methods defined in the StelModule class
+	//! Initialize the LandscapeManager class.
+	//! Operations performed:
+	//! - Load the default landscape as specified in the ini parser object
+	//! - Set up landscape-releated display flags from ini parser object
+	//! @param conf the ini parser object.
+	//! @param lb the loading bar object used to show loading progress.
 	virtual void init(const InitParser& conf, LoadingBar& lb);
+	
+	//! Get the module ID, returns "landscape".
 	virtual string getModuleID() const {return "landscape";}
+	
+	//! Draw the landscape graphics, cardinal points and atmosphere.
 	virtual double draw(Projector *prj, const Navigator *nav, ToneReproducer *eye);
+	
+	//! Update time-dependent state.
+	//! Includes:
+	//! - Landscape, atmosphere and cardinal point on/off fading.
+	//! - Atmophere colour calulation based on location, position of sun 
+	//!   and moon.
+	//! - updates adaptation lumenescence lased on visible bright objects.
 	virtual void update(double deltaTime);
+	
+	//! Translate labels to new language settings.
 	virtual void updateI18n();
+	
+	//! Called for all registered modules if the sky culture changes.
 	virtual void updateSkyCulture(LoadingBar& lb) {;}
+	
+	//! Sets the color of the cardinal points based on the values in the ini parser object.
+	//! @param conf the ini parser object from which to read settings.
+	//! @param section the section of the ini settings to read the color value from.
 	virtual void setColorScheme(const InitParser& conf, const std::string& section);
+	
+	//! Get the order in which this module will draw it's objects relative to other modules.
 	virtual double getCallOrder(StelModuleActionName actionName) const;
 	
 	///////////////////////////////////////////////////////////////////////////
 	// Method specific to the landscape manager
-	Landscape* create_from_file(const string& landscape_file, const string& section_name);
-	Landscape* create_from_hash(map<string, string> & param);
+	//! Create a new landscape from the files which describe it.
+	//! Reads a landscape.ini file which is passed as the first parameter, determines
+	//! the landscape type, and creates a new object for the landscape of the proper
+	//! type.  The load member is then called, passing both parameters.
+	//! @param landscapeFile This is the path to a landscape.ini file.
+	//! @param landscapeId This is the landscape ID, which is also the name of the
+	//! directory in which the files (textures and so on) for the landscape reside.
+	//! @return A pointer to the newly created landscape object.
+	Landscape* createFromFile(const string& landscapeFile, const string& landscapeId);
 	
-	//! retrieve a list of the available landscape IDs separated by the \n character
-	//! the ID in this context is the name of the directory in which the landscape
-	//! exists, i.e. .../landscapes/ID/...
+	//! Create landscape from parameters passed in a hash.
+	//! This is similar in function to createFromFile, except the landscape details
+	//! are passed in a hash rather than a ini parser object.
+	//! NOTE: maptex must be full path and filename
+	//! @param param an STL map of the keys and values which describe the landscape.
+	//! @return a pointer to the newly create landscape object.
+	Landscape* createFromHash(map<string, string> & param);
+	
+	//! Retrieve a list of the available landscape IDs.  This function looks in the
+	//! file search path sub-directories of the landscape area which contain a file called
+	//! landscape.ini, and returns a list of the directory names separated by the \n 
+	//! character. 
+	//! e.g. the landscape ID is "bananas" for the landscape whose ini file is found
+	//! at [USERDATADIR]/landscapes/bananas/landscape.ini
 	string getLandscapeNames();
 	
-	//! convert the name of a landscape (from the name field in the ini file)
-	//! to the ID for the landscape
+	//! For a given landscape name, return the landscape ID.
+	//! This takes a name of the landscape, as described in the landscape:name item in the 
+	//! landscape.ini, and returns the landscape ID which corresponds to that name.
 	string nameToKey(const string& name);
 
 	///////////////////////////////////////////////////////////////////////////////////////
 	// Set and gets
+	//! Get the landscape ID.
 	const string& getLandscapeId() {return landscapeSectionName;}
 	
-	//! Set the landscape
-	bool setLandscape(const string& new_landscape_name);
+	//! Change the current landscape to the landscape with the name specified.
+	//! @param newLandscapeName the name of the new landscape, as found in the 
+	//! landscape:name key of the landscape.ini file.
+	//! @return false if the new landscape could not be set (e.g. no landscape 
+	//! of that name was found). True on success.
+	bool setLandscape(const string& newLandscapeName);
 
-	//! Load a landscape based on a hash of parameters mirroring the landscape.ini file
-	//! and make it the current landscape
+	//! Load a landscape based on a hash of parameters mirroring the landscape.ini 
+	//! file and make it the current landscape.
 	bool loadLandscape(map<string, string>& param);
 	
-	//! Set flag for displaying Landscape
+	//! Set flag for displaying Landscape.
 	void setFlagLandscape(bool b);
-	//! Get flag for displaying Landscape
+	//! Get flag for displaying Landscape.
 	bool getFlagLandscape(void) const;
 
-	//! Set flag for displaying Fog
+	//! Set flag for displaying Fog.
 	void setFlagFog(bool b);
-	//! Get flag for displaying Fog
+	//! Get flag for displaying Fog.
 	bool getFlagFog(void) const;
 
-	//! Return the real name of the current landscape
+	//! Return the real name of the current landscape.
 	wstring getLandscapeName(void);
-	//! Return the author name of the current landscape
+	//! Return the author name of the current landscape.
 	wstring getLandscapeAuthorName(void);
-	//! Return the description of the current landscape
+	//! Return the description of the current landscape.
 	wstring getLandscapeDescription(void);
-	//! Return the translated name of the planet where the current landscape is from
+	//! Return the translated name of the planet where the current landscape is located.
 	wstring getLandscapePlanetName(void);
-	//! Return a wstring with the longitude, latitude and altitude of the current landscape
+	//! Return a wstring with the longitude, latitude and altitude of the current landscape.
 	wstring getLandscapeLocationDescription(void);
 
+	//! Return the value of the flag determining if a change of landscape will update the observer location.
 	bool getFlagLandscapeSetsLocation(void) const {return flagLandscapeSetsLocation;}
+	//! Set the value of the flag determining if a change of landscape will update the observer location.
 	void setFlagLandscapeSetsLocation(bool b) {flagLandscapeSetsLocation=b;}
 
-	//! Set flag for displaying Cardinals Points
+	//! Set flag for displaying Cardinals Points.
 	void setFlagCardinalsPoints(bool b);
-	//! Get flag for displaying Cardinals Points
+	//! Get flag for displaying Cardinals Points.
 	bool getFlagCardinalsPoints(void) const;
 
-	//! Set Cardinals Points color
+	//! Set Cardinals Points color.
 	void setColorCardinalPoints(const Vec3f& v);
-	//! Get Cardinals Points color
+	//! Get Cardinals Points color.
 	Vec3f getColorCardinalPoints(void) const;
 	
 	///////////////////////////////////////////////////////////////////////////////////////
 	// Atmosphere
-	//! Set flag for displaying Atmosphere
+	//! Set flag for displaying Atmosphere.
 	void setFlagAtmosphere(bool b);
-	//! Get flag for displaying Atmosphere
+	//! Get flag for displaying Atmosphere.
 	bool getFlagAtmosphere(void) const;
 
-	//! Set atmosphere fade duration in s
+	//! Set atmosphere fade duration in s.
 	void setAtmosphereFadeDuration(float f);
-	//! Get atmosphere fade duration in s
+	//! Get atmosphere fade duration in s.
 	float getAtmosphereFadeDuration(void) const;
 
-	//! Set light pollution luminance level
+	//! Set light pollution luminance level.
 	void setAtmosphereLightPollutionLuminance(float f);
-	//! Get light pollution luminance level
+	//! Get light pollution luminance level.
 	float getAtmosphereLightPollutionLuminance(void) const;
 	
-	//! Return the global landscape luminance, for beeing used e.g for setting eye adaptation
+	//! Return the global landscape luminance, for being used e.g for setting eye adaptation.
 	float getLuminance(void);
 	
-	//! Return a map of landscape name to landscape ID (directory name)
+	//! Return a map of landscape name to landscape ID (directory name).
 	std::map<std::string,std::string> getNameToDirMap(void);
 
 private:
 	Atmosphere* atmosphere;			// Atmosphere
 	Cardinals* cardinals_points;		// Cardinals points
-	Landscape* landscape;				// The landscape ie the fog, the ground and "decor"
+	Landscape* landscape;			// The landscape i.e. the fog, the ground and "decor"
 	
-	// Define whether the location is set by the landscape
+	// Define whether the observer location is to be updated when the landscape is updated.
 	bool flagLandscapeSetsLocation;
 	
 	std::string landscapeSectionName;
