c2ad60d1b9c7bbf6bc43b12a9778dc7a8ef13188
gajdosik <>
2007-09-12 22:27:43 +0000

    Star repacking: enable loading of BE catalogs on LE machines

--- src/StarMgr.cpp
+++ src/StarMgr.cpp
@@ -7,7 +7,7 @@
  *  (I will move these parts to new files,
  *   so that the Copyright is more clear).
  *
- * MSB unpacking of gcc bitfields by Nigel Kerr
+ * Thanks go to Nigel Kerr for ideas and testing of BE/LE star repacking
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -154,136 +154,98 @@ struct ZoneData { // a single Triangle
 };
 
 
-//#ifdef WORDS_BIGENDIAN
-
-// MSB UnpackBits and UnpackUBits really implemented the same as LSB (below)
-// but we use a local char[4] to swap the bytes about before the cast to int
-// or unsigned int.
-
 static
-int UnpackBits(const char *addr,int bits_begin,const int bits_size) {
-	while (bits_begin >= 8) {
-		bits_begin -= 8;
-		addr++;
-	}
-
-	char swapper[4];
-	swapper[3] = addr[0];
-	swapper[2] = addr[1];
-	swapper[1] = addr[2];
-	swapper[0] = addr[3];
-	
-	int rval = ((const int*)(&(swapper[0])))[0];
-	
-	const int bits_end = bits_begin + bits_size;
-	if (bits_end <= 32) {
-		if (bits_end < 32) rval <<= (32-bits_end);
-		if (bits_size < 32) rval >>= (32-bits_size);
-		
-	} else {
-		rval >>= bits_begin;
-		const int mask = (1<<(32-bits_begin))-1;
-		rval &= mask;
-		swapper[3] = addr[4];
-		swapper[2] = addr[5];
-		swapper[1] = addr[6];
-		swapper[0] = addr[7];
-		
-		int rval_hi = ((const int*)(&(swapper[0])))[0];
-		rval_hi <<= (64-bits_end);
-		rval_hi >>= (32-bits_size);
-		rval_hi &= ~mask;
-		rval |= rval_hi;
-	}
-	return rval;
-}
-
-static
-unsigned int UnpackUBits(const char *addr,int bits_begin,const int bits_size) {
-	while (bits_begin >= 8) {
-		bits_begin -= 8;
-		addr++;
-	}
-
-	char swapper[4];
-	swapper[3] = addr[0];
-	swapper[2] = addr[1];
-	swapper[1] = addr[2];
-	swapper[0] = addr[3];
-	
-	unsigned int rval = ((const unsigned int*)(&(swapper[0])))[0];
-	
-	const int bits_end = bits_begin + bits_size;
-	if (bits_end <= 32) {
-		if (bits_begin > 0) rval >>= bits_begin;
-	} else {
-		rval >>= bits_begin;
-		const int mask = (((unsigned int)1)<<(32-bits_begin))-1;
-		rval &= mask;
-		swapper[3] = addr[4];
-		swapper[2] = addr[5];
-		swapper[1] = addr[6];
-		swapper[0] = addr[7];
-		
-		unsigned int rval_hi = ((const unsigned int*)(&(swapper[0])))[0];
-		rval_hi <<= (32-bits_begin);
-		rval_hi &= ~mask;
-		rval |= rval_hi;
-	}
-	if (bits_size < 32) rval &= ((((unsigned int)1)<<bits_size)-1);
-	return rval;
-}
-
-//#else
-/*
-static
-int UnpackBits(const char *addr,int bits_begin,const int bits_size) {
+int UnpackBits(bool from_be,const char *addr,int bits_begin,
+               const int bits_size) {
+  assert(bits_size <= 32);
   while (bits_begin >= 8) {
     bits_begin -= 8;
     addr++;
   }
-  int rval = ((const int*)addr)[0];
   const int bits_end = bits_begin + bits_size;
-  if (bits_end <= 32) {
-    if (bits_end < 32) rval <<= (32-bits_end);
+  int rval;
+  if (from_be) {
+    rval = (int)((( (( (((unsigned int)(unsigned char)(addr[0]))  << 8) |
+                        ((unsigned int)(unsigned char)(addr[1]))) << 8) |
+                        ((unsigned int)(unsigned char)(addr[2]))) << 8) |
+                        ((unsigned int)(unsigned char)(addr[3])));
+    if (bits_end <= 32) {
+      if (bits_begin > 0) rval <<= bits_begin;
+    } else {
+      rval <<= bits_begin;
+      unsigned int rval_lo = (unsigned char)(addr[4]);
+      rval_lo >>= (8-bits_begin);
+      rval |= rval_lo;
+    }
     if (bits_size < 32) rval >>= (32-bits_size);
   } else {
-    rval >>= bits_begin;
-    const int mask = (1<<(32-bits_begin))-1;
-    rval &= mask;
-    int rval_hi = ((const int*)addr)[1];
-    rval_hi <<= (64-bits_end);
-    rval_hi >>= (32-bits_size);
-    rval_hi &= ~mask;
-    rval |= rval_hi;
+    rval = (int)((( (( (((unsigned int)(unsigned char)(addr[3]))  << 8) |
+                        ((unsigned int)(unsigned char)(addr[2]))) << 8) |
+                        ((unsigned int)(unsigned char)(addr[1]))) << 8) |
+                        ((unsigned int)(unsigned char)(addr[0])));
+    if (bits_end <= 32) {
+      if (bits_end < 32) rval <<= (32-bits_end);
+      if (bits_size < 32) rval >>= (32-bits_size);
+    } else {
+      int rval_hi = addr[4];
+      rval_hi <<= (64-bits_end);
+      rval_hi >>= (32-bits_size);
+      rval = ((unsigned int)rval) >> bits_begin;
+      rval |= rval_hi;
+    }
   }
   return rval;
 }
 
+
+
 static
-unsigned int UnpackUBits(const char *addr,int bits_begin,const int bits_size) {
+unsigned int UnpackUBits(bool from_be,const char *addr,int bits_begin,
+                         const int bits_size) {
+  assert(bits_size <= 32);
   while (bits_begin >= 8) {
     bits_begin -= 8;
     addr++;
   }
-  unsigned int rval = ((const unsigned int*)addr)[0];
   const int bits_end = bits_begin + bits_size;
-  if (bits_end <= 32) {
-    if (bits_begin > 0) rval >>= bits_begin;
+  unsigned int rval;
+  if (from_be) {
+    rval = (( (( (((unsigned int)(unsigned char)(addr[0]))  << 8) |
+                  ((unsigned int)(unsigned char)(addr[1]))) << 8) |
+                  ((unsigned int)(unsigned char)(addr[2]))) << 8) |
+                  ((unsigned int)(unsigned char)(addr[3]));
+    if (bits_end <= 32) {
+      if (bits_begin > 0) rval <<= bits_begin;
+    } else {
+      rval <<= bits_begin;
+      unsigned int rval_lo = (unsigned char)(addr[4]);
+      rval_lo >>= (8-bits_begin);
+      rval |= rval_lo;
+    }
+    if (bits_size < 32) rval >>= (32-bits_size);
   } else {
-    rval >>= bits_begin;
-    const int mask = (((unsigned int)1)<<(32-bits_begin))-1;
-    rval &= mask;
-    unsigned int rval_hi = ((const unsigned int*)addr)[1];
-    rval_hi <<= (32-bits_begin);
-    rval_hi &= ~mask;
-    rval |= rval_hi;
+    rval = (( (( (((unsigned int)(unsigned char)(addr[3]))  << 8) |
+                  ((unsigned int)(unsigned char)(addr[2]))) << 8) |
+                  ((unsigned int)(unsigned char)(addr[1]))) << 8) |
+                  ((unsigned int)(unsigned char)(addr[0]));
+    if (bits_end <= 32) {
+      if (bits_begin > 0) rval >>= bits_begin;
+    } else {
+      unsigned int rval_hi = (unsigned char)(addr[4]);
+      rval_hi <<= (32-bits_begin);
+      rval = rval >> bits_begin;
+      rval |= rval_hi;
+    }
+    if (bits_size < 32) rval &= ((((unsigned int)1)<<bits_size)-1);
   }
-  if (bits_size < 32) rval &= ((((unsigned int)1)<<bits_size)-1);
   return rval;
 }
-*/
-//#endif
+
+
+
+
+
+
 
 static inline float IndexToBV(unsigned char b_v) {
   return b_v*(4.f/127.f)-0.5f;
@@ -304,15 +266,19 @@ struct Star3 {  // 6 byte
   }
   float getBV(void) const {return IndexToBV(b_v);}
   wstring getNameI18n(void) const {return L"";}
-  void repack(void);
+  void repack(bool from_be);
   void print(void);
 } __attribute__ ((__packed__)) ;
 
-void Star3::repack(void) {
-  const int _x0  = UnpackBits((const char*)this, 0,18);
-  const int _x1  = UnpackBits((const char*)this,18,18);
-  const unsigned int _b_v = UnpackUBits((const char*)this,36, 7);
-  const unsigned int _mag = UnpackUBits((const char*)this,43, 5);
+void Star3::repack(bool from_be) {
+  const int _x0  = UnpackBits(from_be,(const char*)this, 0,18);
+  const int _x1  = UnpackBits(from_be,(const char*)this,18,18);
+  const unsigned int _b_v = UnpackUBits(from_be,(const char*)this,36, 7);
+  const unsigned int _mag = UnpackUBits(from_be,(const char*)this,43, 5);
+//assert(x0 == _x0);
+//assert(x1 == _x1);
+//assert(b_v == _b_v);
+//assert(mag == _mag);
   x0 = _x0;
   x1 = _x1;
   b_v = _b_v;
@@ -347,17 +313,23 @@ struct Star2 {  // 10 byte
   }
   float getBV(void) const {return IndexToBV(b_v);}
   wstring getNameI18n(void) const {return L"";}
-  void repack(void);
+  void repack(bool from_be);
   void print(void);
 } __attribute__ ((__packed__));
 
-void Star2::repack(void) {
-  const int _x0  = UnpackBits((const char*)this, 0,20);
-  const int _x1  = UnpackBits((const char*)this,20,20);
-  const int _dx0 = UnpackBits((const char*)this,40,14);
-  const int _dx1 = UnpackBits((const char*)this,54,14);
-  const unsigned int _b_v = UnpackUBits((const char*)this,68, 7);
-  const unsigned int _mag = UnpackUBits((const char*)this,75, 5);
+void Star2::repack(bool from_be) {
+  const int _x0  = UnpackBits(from_be,(const char*)this, 0,20);
+  const int _x1  = UnpackBits(from_be,(const char*)this,20,20);
+  const int _dx0 = UnpackBits(from_be,(const char*)this,40,14);
+  const int _dx1 = UnpackBits(from_be,(const char*)this,54,14);
+  const unsigned int _b_v = UnpackUBits(from_be,(const char*)this,68, 7);
+  const unsigned int _mag = UnpackUBits(from_be,(const char*)this,75, 5);
+//assert(x0 == _x0);
+//assert(x1 == _x1);
+//assert(dx0 == _dx0);
+//assert(dx1 == _dx1);
+//assert(b_v == _b_v);
+//assert(mag == _mag);
   x0 = _x0;
   x1 = _x1;
   dx0 = _dx0;
@@ -410,21 +382,31 @@ struct Star1 { // 28 byte
     }
     return L"";
   }
-  void repack(void);
+  void repack(bool from_be);
   void print(void);
 } __attribute__ ((__packed__));
 
-void Star1::repack(void) {
-  const int _hip  = UnpackBits((const char*)this, 0,24);
-  const unsigned int _cids = UnpackUBits((const char*)this,24, 8);
-  const int _x0  = UnpackBits((const char*)this,32,32);
-  const int _x1  = UnpackBits((const char*)this,64,32);
-  const unsigned int _b_v = UnpackUBits((const char*)this, 96, 8);
-  const unsigned int _mag = UnpackUBits((const char*)this,104, 8);
-  const unsigned int _sp_int = UnpackUBits((const char*)this,112,16);
-  const int _dx0 = UnpackBits((const char*)this,128,32);
-  const int _dx1 = UnpackBits((const char*)this,160,32);
-  const int _plx = UnpackBits((const char*)this,192,32);
+void Star1::repack(bool from_be) {
+  const int _hip  = UnpackBits(from_be,(const char*)this, 0,24);
+  const unsigned int _cids = UnpackUBits(from_be,(const char*)this,24, 8);
+  const int _x0  = UnpackBits(from_be,(const char*)this,32,32);
+  const int _x1  = UnpackBits(from_be,(const char*)this,64,32);
+  const unsigned int _b_v = UnpackUBits(from_be,(const char*)this, 96, 8);
+  const unsigned int _mag = UnpackUBits(from_be,(const char*)this,104, 8);
+  const unsigned int _sp_int = UnpackUBits(from_be,(const char*)this,112,16);
+  const int _dx0 = UnpackBits(from_be,(const char*)this,128,32);
+  const int _dx1 = UnpackBits(from_be,(const char*)this,160,32);
+  const int _plx = UnpackBits(from_be,(const char*)this,192,32);
+//assert(hip == _hip);
+//assert(component_ids == _cids);
+//assert(x0 == _x0);
+//assert(x1 == _x1);
+//assert(b_v == _b_v);
+//assert(mag == _mag);
+//assert(sp_int == _sp_int);
+//assert(dx0 == _dx0);
+//assert(dx1 == _dx1);
+//assert(plx == _plx);
   hip = _hip;
   component_ids = _cids;
   x0 = _x0;
@@ -1190,7 +1172,7 @@ ZoneArray *ZoneArray::create(const StarMgr &hip_star_mgr,
   }
   if (rval && rval->isInitialized()) {
     printf("stars: %d\n",rval->getNrOfStars());
-    rval->generateNativeDebugFile((fname+".debug").c_str());
+//    rval->generateNativeDebugFile((fname+".debug").c_str());
   } else {
     printf("initialization failed\n");
     if (rval) {
@@ -1397,9 +1379,25 @@ SpecialZoneArray<Star>::SpecialZoneArray(FILE *f,bool byte_swap,bool use_mmap,
             getZones()[z].stars = s;
             s += getZones()[z].size;
           }
-          if (byte_swap) {
+          if (
+#if (!defined(__GNUC__))
+              true
+#else
+              byte_swap
+#endif
+             ) {
             s = stars;
-            for (unsigned int i=0;i<nr_of_stars;i++,s++) s->repack();
+            for (unsigned int i=0;i<nr_of_stars;i++,s++) {
+              s->repack(
+#ifdef WORDS_BIGENDIAN
+  // need for byte_swap on a BE machine means that catalog is LE
+                !byte_swap
+#else
+  // need for byte_swap on a LE machine means that catalog is BE
+                byte_swap
+#endif
+              );
+            }
           }
           cout << endl
                << "SpecialZoneArray<Star>::SpecialZoneArray(" << level
