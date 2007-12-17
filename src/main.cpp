9c3d92f71b35b375255b734946747aa5af31d6fa
Fabien Chereau <fabien.chereau@gmail.com>
2007-12-17 12:38:42 +0000

    Headers cleaning.

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,10 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#include <string>
-#include <iostream>
 #include <cstdlib>
-#include <config.h>
 #include <clocale>
 
+#include <config.h>
 #include "StelApp.hpp"
 #include "Translator.hpp"
 
