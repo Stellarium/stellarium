79ec4526d5b71a5a2ee542eddcae8e5febd2edbb
Fabien Chéreau <fabien.chereau@gmail.com>
2008-02-11 10:50:23 +0000

    Appled Guillaume's patch for improving windows.

--- src/gui/Dialog.cpp
+++ src/gui/Dialog.cpp
@@ -22,74 +22,16 @@
 
 #include <iostream>
 
-DialogFrame::DialogFrame(QWidget *parent) : 
-      QFrame(parent), border(4),
-      followLeft(false), followRight(false),
-      followTop(false), followBottom(false)
-{
-  setMouseTracking(true);
-  setMinimumSize(50,20);
-}
-
-void DialogFrame::mouseMoveEvent(QMouseEvent *event)
-{
-  if (onLeft(event->pos()) || onRight(event->pos()))
-  {
-    setCursor(Qt::SizeHorCursor);
-  }
-  else if (onTop(event->pos()) || onBottom(event->pos()))
-  {
-    setCursor(Qt::SizeVerCursor);
-  }
-  else {unsetCursor();}
-  
-  QPoint dpos = event->pos() - mouse_pos;
-  if (followLeft)
-  {
-    QSize old_size = size();
-    resize(size().width() - dpos.x(), size().height());
-    move(pos().x() + (old_size - size()).width(), pos().y());
-  }
-  if (followRight)
-  {
-    resize(size().width() + dpos.x(), size().height());
-    mouse_pos += QPoint(dpos.x(), 0);
-  }
-  if (followTop)
-  {
-    QSize old_size = size();
-    resize(size().width(), size().height() - dpos.y());
-    move(pos().x(), pos().y() + (old_size - size()).height());
-  }
-  if (followBottom)
-  {
-    resize(size().width(), size().height() + dpos.y());
-    mouse_pos += QPoint(0, dpos.y());
-  }
-}
-
-void DialogFrame::mousePressEvent(QMouseEvent *event)
-{
-  followLeft = onLeft(event->pos());
-  followRight = onRight(event->pos());
-  followTop = onTop(event->pos());
-  followBottom = onBottom(event->pos());
-  mouse_pos = event->pos();
-}
-
-void DialogFrame::mouseReleaseEvent(QMouseEvent *event)
-{
-  followLeft = false;
-  followRight = false;
-  followTop = false;
-  followBottom = false;
-  
-  unsetCursor();
-}
-
 void BarFrame::mouseMoveEvent(QMouseEvent *event)
 {
   QPoint dpos = event->pos() - mouse_pos;
   QWidget* p = dynamic_cast<QWidget*>(QFrame::parent());
   p->move(p->pos() + dpos);
+}
+
+void ResizeFrame::mouseMoveEvent(QMouseEvent *event)
+{
+  QPoint dpos = event->pos() - mouse_pos;
+  QWidget* p = dynamic_cast<QWidget*>(QFrame::parent()->parent());
+  p->resize(p->size().width() + dpos.x(), p->size().height() + dpos.y());
 }
