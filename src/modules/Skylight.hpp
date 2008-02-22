d87c119f4489f94607cfee5bcaad3cbeef20f2f8
Fabien Chereau <fabien.chereau@gmail.com>
2008-02-22 18:01:16 +0000

    Fixed a openGL state problem.

--- src/modules/Skylight.hpp
+++ src/modules/Skylight.hpp
@@ -46,7 +46,7 @@ public:
 	// This funtion has to be called once before any call to get_*_value()
     void set_params(float sun_zenith_angle, float turbidity);
 	// Compute the sky color at the given position in the xyY color system and store it in position.color
-	void get_xyY_value(skylight_struct * position);
+// 	void get_xyY_value(skylight_struct * position);
 	// Return the current zenith color
 	inline void get_zenith_color(float * v) const;
 
