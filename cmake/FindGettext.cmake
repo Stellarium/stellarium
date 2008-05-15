54bbc763f025aca192d7068d53d37263fd8ffe94
Andras Mohari <andras.mohari@gmail.com>
2008-05-15 16:05:55 +0000

    Modified translation build rules.

--- cmake/FindGettext.cmake
+++ cmake/FindGettext.cmake
@@ -4,13 +4,6 @@
 #  GETTEXT_MSGMERGE_EXECUTABLE: the full path to the msgmerge tool.
 #  GETTEXT_MSGFMT_EXECUTABLE: the full path to the msgfmt tool.
 #  GETTEXT_FOUND: True if gettext has been found.
-#
-# Additionally it provides the following macros:
-# GETTEXT_CREATE_TRANSLATIONS ( outputFile [ALL] file1 ... fileN )
-#    This will create a target "translations" which will convert the 
-#    given input po files into the binary output mo file. If the 
-#    ALL option is used, the translations will also be created when
-#    building the default target.
 
 
 FIND_PROGRAM(GETTEXT_XGETTEXT_EXECUTABLE xgettext)
@@ -18,48 +11,6 @@ FIND_PROGRAM(GETTEXT_XGETTEXT_EXECUTABLE xgettext)
 FIND_PROGRAM(GETTEXT_MSGMERGE_EXECUTABLE msgmerge)
 
 FIND_PROGRAM(GETTEXT_MSGFMT_EXECUTABLE msgfmt)
-
-MACRO(GETTEXT_CREATE_TRANSLATIONS _potFile _firstPoFile)
-
-   SET(_gmoFiles)
-   GET_FILENAME_COMPONENT(_potBaseName ${_potFile} NAME_WE)
-   GET_FILENAME_COMPONENT(_absPotFile ${_potFile} ABSOLUTE)
-   GET_FILENAME_COMPONENT(_absPotFileIn POTFILES.in ABSOLUTE)
-
-	# Update message strings from the code [TODO: run even if pot file exists!]
-	ADD_CUSTOM_COMMAND( 
-    	OUTPUT ${_absPotFile}
-        COMMAND ${GETTEXT_XGETTEXT_EXECUTABLE} -o ${_absPotFile} --keyword=_ --keyword=q_ --qt --keyword=N_ --add-comments=TRANSLATORS: --keyword=translate:2 -C --default-domain=${_potBaseName} --directory=${PROJECT_SOURCE_DIR} --directory=${CMAKE_BINARY_DIR} --files-from=${_absPotFileIn}  --copyright-holder="Stellarium's team"  
-        DEPENDS POTFILES.in
-    )
-
-   SET(_addToAll)
-   IF(${_firstPoFile} STREQUAL "ALL")
-      SET(_addToAll "ALL")
-      SET(_firstPoFile)
-   ENDIF(${_firstPoFile} STREQUAL "ALL")
-
-   FOREACH (_currentPoFile ${ARGN})
-      GET_FILENAME_COMPONENT(_absFile ${_currentPoFile} ABSOLUTE)
-      GET_FILENAME_COMPONENT(_abs_PATH ${_absFile} PATH)
-      GET_FILENAME_COMPONENT(_lang ${_absFile} NAME_WE)
-      SET(_gmoFile ${CMAKE_CURRENT_BINARY_DIR}/${_lang}.gmo)
-
-      ADD_CUSTOM_COMMAND( 
-         OUTPUT ${_gmoFile} 
-         COMMAND ${GETTEXT_MSGMERGE_EXECUTABLE} --quiet --update -m --backup=none -s ${_absFile} ${_absPotFile}
-         COMMAND ${GETTEXT_MSGFMT_EXECUTABLE} -o ${_gmoFile} ${_absFile}
-         DEPENDS ${_absPotFile} ${_absFile} 
-      )
-
-      INSTALL(FILES ${_gmoFile} DESTINATION share/locale/${_lang}/LC_MESSAGES RENAME ${_potBaseName}.mo) 
-      SET(_gmoFiles ${_gmoFiles} ${_gmoFile})
-
-   ENDFOREACH (_currentPoFile )
-
-   ADD_CUSTOM_TARGET(translations ${_addToAll} DEPENDS ${_potFile} ${_gmoFiles})
-
-ENDMACRO(GETTEXT_CREATE_TRANSLATIONS )
 
 IF (GETTEXT_MSGMERGE_EXECUTABLE AND GETTEXT_MSGFMT_EXECUTABLE )
    SET(GETTEXT_FOUND TRUE)
