e1c7c4eaae111ca9c18aefdbe1171e23b064c5d1
Fabien Chereau <fabien.chereau@gmail.com>
2008-06-25 15:31:13 +0000

    Improved tests.

--- src/tests/testSphereGeometry.cpp
+++ src/tests/testSphereGeometry.cpp
@@ -40,6 +40,12 @@ void testContains()
 	Vec3d v2;
 	Vec3d v3;
 	
+	// Triangle polygons
+	StelGeom::ConvexPolygon triangle1(Vec3d(0,0,1), Vec3d(0,1,0), Vec3d(1,0,0));
+	_assert(contains(triangle1, p1), "Triangle contains point failure");
+	_assert(intersect(triangle1, p1), "Triangle intersect point failure");
+	
+	// polygons-point intersect
 	double deg5 = 5.*M_PI/180.;
 	double deg2 = 2.*M_PI/180.;
 	StelUtils::sphe_to_rect(-deg5, -deg5, v0);
@@ -49,7 +55,10 @@ void testContains()
 	StelGeom::ConvexPolygon square1(v3, v2, v1, v0);
 	_assert(contains(square1, p0), "Square contains point failure");
 	_assert(!contains(square1, p1), "Square not contains point failure");
+	_assert(intersect(square1, p0), "Square intersect point failure");
+	_assert(!intersect(square1, p1), "Square not intersect point failure");
 	
+	// polygons-polygons intersect
 	StelUtils::sphe_to_rect(-deg2, -deg2, v0);
 	StelUtils::sphe_to_rect(+deg2, -deg2, v1);
 	StelUtils::sphe_to_rect(+deg2, +deg2, v2);
@@ -57,10 +66,25 @@ void testContains()
 	StelGeom::ConvexPolygon square2(v3, v2, v1, v0);
 	_assert(contains(square1, square2), "Square contains square failure");
 	_assert(!contains(square2, square1), "Square not contains square failure");
+	_assert(intersect(square1, square2), "Square intersect square failure");
+	_assert(intersect(square2, square1), "Square intersect square failure");
 	
-	// Triangle 1, 1 quadrant
-	StelGeom::ConvexPolygon triangle1(Vec3d(0,0,1), Vec3d(0,1,0), Vec3d(1,0,0));
-	_assert(contains(triangle1, p1), "Triangle contains point failure");
+	// Test the tricky case where 2 polygons intersect without having point within each other
+	StelUtils::sphe_to_rect(-deg5, -deg2, v0);
+	StelUtils::sphe_to_rect(+deg5, -deg2, v1);
+	StelUtils::sphe_to_rect(+deg5, +deg2, v2);
+	StelUtils::sphe_to_rect(-deg5, +deg2, v3);
+	StelGeom::ConvexPolygon squareHoriz(v3, v2, v1, v0);
+	StelUtils::sphe_to_rect(-deg2, -deg5, v0);
+	StelUtils::sphe_to_rect(+deg2, -deg5, v1);
+	StelUtils::sphe_to_rect(+deg2, +deg5, v2);
+	StelUtils::sphe_to_rect(-deg2, +deg5, v3);
+	StelGeom::ConvexPolygon squareVerti(v3, v2, v1, v0);
+	_assert(!contains(squareHoriz, squareVerti), "Special intersect contains failure");
+	_assert(!contains(squareVerti, squareHoriz), "Special intersect contains failure");
+	_assert(intersect(squareHoriz, squareVerti), "Special intersect failure");
+	_assert(intersect(squareVerti, squareHoriz), "Special intersect failure");
+	
 	
 	qDebug() << "Passed intersection tests";
 }
