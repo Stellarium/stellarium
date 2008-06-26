d05f4a6aef522cf96fbf477cc420ecebd1b6084d
Fabien Chéreau <fabien.chereau@gmail.com>
2008-06-26 09:02:42 +0000

    Improved nebula labeling.

--- src/modules/Nebula.hpp
+++ src/modules/Nebula.hpp
@@ -29,33 +29,11 @@
 #include "STextureTypes.hpp"
 
 class SFont;
-class ToneReproducer;
-
-/*
-Gx Galaxy
-OC Open star cluster
-Gb Globular star cluster, usually in the Milky Way Galaxy
-Nb Bright emission or reflection nebula
-Pl Planetary nebula
-C+N Cluster associated with nebulosity
-Ast Asterism or group of a few stars
-Kt Knot or nebulous region in an external galaxy
-*** Triple star
-D* Double star
-* Single star
-? Uncertain type or may not exist
-blank Unidentified at the place given, or type unknown
-- Object called nonexistent in the RNGC (Sulentic and Tifft 1973)
-PD Photographic plate defect
-*/
-
 
 class Nebula : public StelObject
 {
 friend class NebulaMgr;
 public:
-	enum nebula_type { NEB_GX, NEB_OC, NEB_GC, NEB_N, NEB_PN, NEB_DN, NEB_IG, NEB_CN, NEB_UNKNOWN };
-
 	Nebula();
 	~Nebula();
 
@@ -63,46 +41,52 @@ public:
 	//! @param core the StelCore object
 	//! @param flags StelObject::InfoStringGroups to be included in the return value.
 	//! The StelObject::InfoStringGroups Extra1 is the "type" - e.g. "Galaxy" or "Open Cluster".
-	QString getInfoString(const StelCore *core, const InfoStringGroup& flags) const;
-	QString getShortInfoString(const StelCore *core) const;
-	QString getType(void) const {return "Nebula";}
-	// observer centered J2000 coordinates
-	Vec3d getObsJ2000Pos(const Navigator *nav) const {return XYZ;}
-	double getCloseViewFov(const Navigator * nav = NULL) const;
-	float getMagnitude(const Navigator * nav = NULL) const {return mag;}
-	float getSelectPriority(const Navigator *nav) const;
-
+	virtual QString getInfoString(const StelCore *core, const InfoStringGroup& flags) const;
+	virtual QString getShortInfoString(const StelCore *core) const;
+	virtual QString getType(void) const {return "Nebula";}
+	virtual Vec3d getObsJ2000Pos(const Navigator *nav) const {return XYZ;}
+	virtual double getCloseViewFov(const Navigator * nav = NULL) const;
+	virtual float getMagnitude(const Navigator * nav = NULL) const {return mag;}
+	virtual float getSelectPriority(const Navigator *nav) const;
 	virtual Vec3f getInfoColor(void) const;
-
+	virtual QString getNameI18n(void) const {return nameI18;}
+	virtual QString getEnglishName(void) const {return englishName;}
+	virtual double getAngularSize(const StelCore *core) const {return angularSize/2;}
+	
+	// Methods specific to Nebula
 	void setLabelColor(const Vec3f& v) {label_color = v;}
 	void setCircleColor(const Vec3f& v) {circle_color = v;}
 
-	bool readNGC(char *record);
-
-	QString getNameI18n(void) const {return nameI18;}
-	QString getEnglishName(void) const {return englishName;}
-    
 	//! Get the printable nebula Type.
 	//! @return the nebula type code.
 	QString getTypeString(void) const;
-
+	
+private:
+	
+	//!	NEB_GX Galaxy
+	//! NEB_OC Open star cluster
+	//! NEB_GC Globular star cluster, usually in the Milky Way Galaxy
+	//! NEB_N Bright emission or reflection nebula
+	//! NEB_PN Planetary nebula
+	//! CN Cluster associated with nebulosity
+	//! Other Undocumented type
+	enum nebula_type { NEB_GX, NEB_OC, NEB_GC, NEB_N, NEB_PN, NEB_DN, NEB_IG, NEB_CN, NEB_UNKNOWN };
+	
 	//! Translate nebula name using the passed translator
 	void translateName(Translator& trans) {nameI18 = trans.qtranslate(englishName);}
-	
-	virtual double getAngularSize(const StelCore *core) const {return angularSize;}
 
-private:
-	void draw_no_tex(const StelCore* core);
-	void draw_name(const StelCore* core);
-	void draw_circle(const StelCore* core);
+	bool readNGC(char *record);
+	
+	void drawLabel(const StelCore* core, float maxMagLabel);
+	void drawHints(const StelCore* core, float maxMagHints);
     
-	unsigned int M_nb;              // Messier Catalog number
-	unsigned int NGC_nb;            // New General Catalog number
-	unsigned int IC_nb;// Index Catalog number
-	QString englishName;             // English name
+	unsigned int M_nb;				// Messier Catalog number
+	unsigned int NGC_nb;			// New General Catalog number
+	unsigned int IC_nb;				// Index Catalog number
+	QString englishName;			// English name
 	QString nameI18;                // Nebula name
 	float mag;                      // Apparent magnitude
-	float angularSize;             // Angular size in degree
+	float angularSize;				// Angular size in degree
 	Vec3f XYZ;                      // Cartesian equatorial position
 	Vec3d XY;                       // Store temporary 2D position
 	nebula_type nType;
