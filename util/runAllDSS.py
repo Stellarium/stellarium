5af8fd6344ef517ca4e50e1c2309cae33b9e3f58
Fabien Chereau <fabien.chereau@gmail.com>
2008-07-04 09:24:19 +0000

    Improved.

---
+++ util/runAllDSS.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# Simple script which convert the FITS headers associated to Brian McLean DSS images into simplified JSON files
+# Fabien Chereau fchereau@eso.org
+
+import sys
+import os
+import subprocess
+
+def all():
+	for i in range(2,898):
+		str = "./dssheaderToJSON.py" + " N %i %i" % (i,i+1)
+		print str
+		try:
+			retcode = subprocess.call(str, shell=True)
+			if retcode < 0:
+				print >>sys.stderr, "Child was terminated by signal", -retcode
+			else:
+				print >>sys.stderr, "Child returned", retcode
+		except OSError, e:
+			print >>sys.stderr, "Execution failed:", e
+
+if __name__ == "__main__":
+    all()
