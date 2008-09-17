7ae17da7d5fd9ee438a73b361f72005f530dd199
Fabien Chéreau <fabien.chereau@gmail.com>
2008-09-17 09:27:15 +0000

    Reformat.

--- src/core/Observer.cpp
+++ src/core/Observer.cpp
@@ -32,17 +32,18 @@
 #include <QSettings>
 #include <QStringList>
 
-class ArtificialPlanet : public Planet {
+class ArtificialPlanet : public Planet
+{
 public:
-  ArtificialPlanet(const Planet &orig);
-  void setDest(const Planet &dest);
-  void computeAverage(double f1);
+	ArtificialPlanet(const Planet &orig);
+	void setDest(const Planet &dest);
+	void computeAverage(double f1);
 private:
-  void setRot(const Vec3d &r);
-  static Vec3d GetRot(const Planet *p);
-  const Planet *dest;
-  const QString orig_name;
-  const QString orig_name_i18n;
+	void setRot(const Vec3d &r);
+	static Vec3d GetRot(const Planet *p);
+	const Planet *dest;
+	const QString orig_name;
+	const QString orig_name_i18n;
 };
 
 ArtificialPlanet::ArtificialPlanet(const Planet &orig) : 
@@ -50,17 +51,21 @@ ArtificialPlanet::ArtificialPlanet(const Planet &orig) :
 		posFuncType(), 0, false, true, false), dest(0),
 		orig_name(orig.getEnglishName()), orig_name_i18n(orig.getNameI18n())
 {
-  radius = 0;
-    // set parent = sun:
-  if (orig.getParent()) {
-    parent = orig.getParent();
-    while (parent->getParent()) parent = parent->getParent();
-  } else {
-    parent = &orig; // sun
-  }
-  re = orig.getRotationElements();
-  setRotEquatorialToVsop87(orig.getRotEquatorialToVsop87());
-  setHeliocentricEclipticPos(orig.getHeliocentricEclipticPos());
+	radius = 0;
+	// set parent = sun:
+	if (orig.getParent())
+	{
+		parent = orig.getParent();
+		while (parent->getParent())
+			parent = parent->getParent();
+	}
+	else 
+	{
+		parent = &orig; // sun
+	}
+	re = orig.getRotationElements();
+	setRotEquatorialToVsop87(orig.getRotEquatorialToVsop87());
+	setHeliocentricEclipticPos(orig.getHeliocentricEclipticPos());
 }
 
 void ArtificialPlanet::setDest(const Planet &dest)
@@ -81,69 +86,80 @@ void ArtificialPlanet::setDest(const Planet &dest)
 	if (re.offset - r.offset >  180.f) re.offset -= 360.f;
 }
 
-void ArtificialPlanet::setRot(const Vec3d &r) {
-  const double ca = cos(r[0]);
-  const double sa = sin(r[0]);
-  const double cd = cos(r[1]);
-  const double sd = sin(r[1]);
-  const double cp = cos(r[2]);
-  const double sp = sin(r[2]);
-  Mat4d m;
-  m.r[ 0] = cd*cp;
-  m.r[ 4] = sd;
-  m.r[ 8] = cd*sp;
-  m.r[12] = 0;
-  m.r[ 1] = -ca*sd*cp -sa*sp;
-  m.r[ 5] =  ca*cd;
-  m.r[ 9] = -ca*sd*sp +sa*cp;
-  m.r[13] = 0;
-  m.r[ 2] =  sa*sd*cp -ca*sp;
-  m.r[ 6] = -sa*cd;
-  m.r[10] =  sa*sd*sp +ca*cp;
-  m.r[14] = 0;
-  m.r[ 3] = 0;
-  m.r[ 7] = 0;
-  m.r[11] = 0;
-  m.r[15] = 1.0;
-  setRotEquatorialToVsop87(m);
+void ArtificialPlanet::setRot(const Vec3d &r)
+{
+	const double ca = cos(r[0]);
+	const double sa = sin(r[0]);
+	const double cd = cos(r[1]);
+	const double sd = sin(r[1]);
+	const double cp = cos(r[2]);
+	const double sp = sin(r[2]);
+	Mat4d m;
+	m.r[ 0] = cd*cp;
+	m.r[ 4] = sd;
+	m.r[ 8] = cd*sp;
+	m.r[12] = 0;
+	m.r[ 1] = -ca*sd*cp -sa*sp;
+	m.r[ 5] =  ca*cd;
+	m.r[ 9] = -ca*sd*sp +sa*cp;
+	m.r[13] = 0;
+	m.r[ 2] =  sa*sd*cp -ca*sp;
+	m.r[ 6] = -sa*cd;
+	m.r[10] =  sa*sd*sp +ca*cp;
+	m.r[14] = 0;
+	m.r[ 3] = 0;
+	m.r[ 7] = 0;
+	m.r[11] = 0;
+	m.r[15] = 1.0;
+	setRotEquatorialToVsop87(m);
 }
 
-Vec3d ArtificialPlanet::GetRot(const Planet *p) {
-  const Mat4d m(p->getRotEquatorialToVsop87());
-  const double cos_r1 = sqrt(m.r[0]*m.r[0]+m.r[8]*m.r[8]);
-  Vec3d r;
-  r[1] = atan2(m.r[4],cos_r1);
-    // not well defined if cos(r[1])==0:
-  if (cos_r1 <= 0.0) {
-    // if (m.r[4]>0.0) sin,cos(a-p)=m.r[ 9],m.r[10]
-    // else sin,cos(a+p)=m.r[ 9],m.r[10]
-    // so lets say p=0:
-    r[2] = 0.0;
-    r[0] = atan2(m.r[9],m.r[10]);
-  } else {
-    r[0] = atan2(-m.r[6],m.r[5]);
-    r[2] = atan2( m.r[8],m.r[0]);
-  }
-  return r;
+Vec3d ArtificialPlanet::GetRot(const Planet *p)
+{
+	const Mat4d m(p->getRotEquatorialToVsop87());
+	const double cos_r1 = sqrt(m.r[0]*m.r[0]+m.r[8]*m.r[8]);
+	Vec3d r;
+	r[1] = atan2(m.r[4],cos_r1);
+	// not well defined if cos(r[1])==0:
+	if (cos_r1 <= 0.0)
+	{
+		// if (m.r[4]>0.0) sin,cos(a-p)=m.r[ 9],m.r[10]
+		// else sin,cos(a+p)=m.r[ 9],m.r[10]
+		// so lets say p=0:
+		r[2] = 0.0;
+		r[0] = atan2(m.r[9],m.r[10]);
+	}
+	else
+	{
+		r[0] = atan2(-m.r[6],m.r[5]);
+		r[2] = atan2( m.r[8],m.r[0]);
+	}
+	return r;
 }
 
-void ArtificialPlanet::computeAverage(double f1) {
-  const double f2 = 1.0 - f1;
-     // position:
-  setHeliocentricEclipticPos(getHeliocentricEclipticPos()*f1
-                      + dest->getHeliocentricEclipticPos()*f2);
-
-    // 3 Euler angles:
-  Vec3d a1(GetRot(this));
-  const Vec3d a2(GetRot(dest));
-  if (a1[0]-a2[0] >  M_PI) a1[0] -= 2.0*M_PI; else
-  if (a1[0]-a2[0] < -M_PI) a1[0] += 2.0*M_PI;
-  if (a1[2]-a2[2] >  M_PI) a1[2] -= 2.0*M_PI; else
-  if (a1[2]-a2[2] < -M_PI) a1[2] += 2.0*M_PI;
-  setRot(a1*f1 + a2*f2);
-
-    // rotation offset:
-  re.offset = f1*re.offset + f2*dest->getRotationElements().offset;
+void ArtificialPlanet::computeAverage(double f1)
+{
+	const double f2 = 1.0 - f1;
+	// position
+	setHeliocentricEclipticPos(getHeliocentricEclipticPos()*f1 + dest->getHeliocentricEclipticPos()*f2);
+	
+	// 3 Euler angles
+	Vec3d a1(GetRot(this));
+	const Vec3d a2(GetRot(dest));
+	if (a1[0]-a2[0] >  M_PI)
+		a1[0] -= 2.0*M_PI;
+	else
+		if (a1[0]-a2[0] < -M_PI)
+			a1[0] += 2.0*M_PI;
+	if (a1[2]-a2[2] >  M_PI)
+		a1[2] -= 2.0*M_PI;
+	else
+		if (a1[2]-a2[2] < -M_PI)
+			a1[2] += 2.0*M_PI;
+	setRot(a1*f1 + a2*f2);
+	
+	// rotation offset
+	re.offset = f1*re.offset + f2*dest->getRotationElements().offset;
 }
 
 
