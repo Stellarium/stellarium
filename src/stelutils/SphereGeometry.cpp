72f0cbe208a68aab4429f064ef750f2f31c46b9c
xalioth <>
2008-11-06 14:34:21 +0000

    Optimized.

--- src/stelutils/SphereGeometry.cpp
+++ src/stelutils/SphereGeometry.cpp
@@ -151,48 +151,52 @@ bool planeIntersect2(const HalfSpace& h1, const HalfSpace& h2, Vec3d& p1, Vec3d&
 	
 	u.normalize();
 	
-	// U gives the direction of the line, still need to find a suitable start point p0
+	// u gives the direction of the line, still need to find a suitable start point p0
 	// Find the axis on which the line varies the fastest, and solve the system for value == 0 on this axis
 	int maxI = (fabs(u[0])>fabs(u[1])) ? (fabs(u[0])>fabs(u[2]) ? 0 : 2) : (fabs(u[1])>fabs(u[2]) ? 1 : 2);
 	Vec3d p0(0,0,0);
-	if (maxI==0)
+	switch (maxI)
 	{
-		// Intersection of the line with the plane x=0
-		const double denom = c1*b2-c2*b1;
-		p0[1] = (d2*c1-d1*c2)/denom;
-		p0[2] = (d1*b2-d2*b1)/denom;
-	}
-	else if (maxI==1)
-	{
-		// Intersection of the line with the plane y=0
-		const double denom = a1*c2-a2*c1;
-		p0[0]=(c1*d2-c2*d1)/denom;
-		p0[2]=(a2*d1-d2*d1)/denom;
-	}
-	else if (maxI==2)
-	{
-		// Intersection of the line with the plane z=0	
-		const double denom = a1*b2-a2*b1;
-		p0[0]=(b1*d2-b2*d1)/denom;
-		p0[1]=(a2*d1-a1*d2)/denom;
+		case 0:
+		{
+			// Intersection of the line with the plane x=0
+			const double denom = c1*b2-c2*b1;
+			p0[1] = (d2*c1-d1*c2)/denom;
+			p0[2] = (d1*b2-d2*b1)/denom;
+			break;
+		}
+		case 1:
+		{
+			// Intersection of the line with the plane y=0
+			const double denom = a1*c2-a2*c1;
+			p0[0]=(c1*d2-c2*d1)/denom;
+			p0[2]=(a2*d1-d2*d1)/denom;
+			break;
+		}
+		case 2:
+		{
+			// Intersection of the line with the plane z=0	
+			const double denom = a1*b2-a2*b1;
+			p0[0]=(b1*d2-b2*d1)/denom;
+			p0[1]=(a2*d1-a1*d2)/denom;
+		}
 	}
 	
 	// The intersection line is now fully defined by the parametric equation p0 + u*t
 	
 	// The points are on the unit sphere x^2+y^2+z^2=1, replace x, y and z by the parametric equation to get something of the form at^2+b*t+c=0
-	const double a = 1.;
-	const double b = 0;
+	// const double a = 1.;
+	// const double b = 0;
 	const double c = p0.lengthSquared()-1.;
 	
 	// If discriminant <=0, zero or 1 real solution
-	const double D = b*b-4.*a*c;
-	if (D<=0)
+	const double D = -4.*c;
+	if (D<=0.)
 		return false;
 	
-	const double t1 = (-b+std::sqrt(D))/(2.*a);
-	const double t2 = (-b-std::sqrt(D))/(2.*a);
+	const double t1 = std::sqrt(D)/2.;
 	p1 = p0+u*t1;
-	p2 = p0+u*t2;
+	p2 = p0-u*t1;
 	return true;
 }
 
