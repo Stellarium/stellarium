84abf465d0bed304c0d2ecfcd2542ec7d047dc59
Fabien Chéreau <fabien.chereau@gmail.com>
2009-01-16 15:33:34 +0000

    Improved the tool for generating multi-resolution images.

--- util/createTilesAndJSON.py
+++ util/createTilesAndJSON.py
@@ -9,8 +9,8 @@ import ImageOps
 import math
 from astLib import astWCS
 from skyTile import *
-
-TILE_SIZE=256
+from optparse import OptionParser
+from optparse import IndentedHelpFormatter
 
 def sphe2rect3d(lng, lat):
 	cosLat = math.cos(lat)
@@ -25,19 +25,19 @@ def lengthSquared(v):
 def angularDist(v1, v2):
 	return math.acos(dot(v1,v2)/math.sqrt(lengthSquared(v1)*lengthSquared(v2)));
 
-def createTile(currentLevel, maxLevel, i, j, wcs, im, doImage):
+def createTile(currentLevel, maxLevel, i, j, wcs, im, doImage, tileSize):
 	if (currentLevel>=maxLevel):
 		return None
 	
 	# Create the jpg sub tile
-	realTileSize = TILE_SIZE*2**(maxLevel-currentLevel-1)
+	realTileSize = tileSize*2**(maxLevel-currentLevel-1)
 	box = ( i*realTileSize, j*realTileSize, min((i+1)*realTileSize, im.size[0]), min((j+1)*realTileSize, im.size[1]) )
 	if (box[0]>=im.size[0] or box[1]>=im.size[1]):
 		return None
 	imgName = 'x%.2i_%.2i_%.2i.jpg' % (2**currentLevel, i, j)
 	if doImage:
 		region = im.crop(box)
-		r2 = region.resize((TILE_SIZE, TILE_SIZE), Image.ANTIALIAS)
+		r2 = region.resize((tileSize, tileSize), Image.ANTIALIAS)
 		r2.save(imgName)
 	
 	# Create the associated tile description
@@ -52,56 +52,68 @@ def createTile(currentLevel, maxLevel, i, j, wcs, im, doImage):
 	v11 = wcs.pix2wcs(box[2]+0.5,im.size[1]-box[1]+0.5)
 	t.skyConvexPolygons = [[ [v00[0],v00[1]], [v10[0],v10[1]], [v11[0],v11[1]], [v01[0],v01[1]] ]]
 	t.textureCoords = [[ [0,0], [1,0], [1,1], [0,1] ]]
-	t.minResolution = max( angularDist(sphe2rect3d(v10[0]*math.pi/180., v10[1]*math.pi/180.), sphe2rect3d(v00[0]*math.pi/180., v00[1]*math.pi/180.)), angularDist(sphe2rect3d(v01[0]*math.pi/180., v01[1]*math.pi/180.), sphe2rect3d(v00[0]*math.pi/180.,v00[1]*math.pi/180.)) )*180./math.pi/TILE_SIZE
+	t.minResolution = max( angularDist(sphe2rect3d(v10[0]*math.pi/180., v10[1]*math.pi/180.), sphe2rect3d(v00[0]*math.pi/180., v00[1]*math.pi/180.)), angularDist(sphe2rect3d(v01[0]*math.pi/180., v01[1]*math.pi/180.), sphe2rect3d(v00[0]*math.pi/180.,v00[1]*math.pi/180.)) )*180./math.pi/tileSize
 	
 	# Recursively creates the 4 sub-tiles
-	sub = createTile(currentLevel+1, maxLevel, i*2, j*2, wcs, im, doImage)
+	sub = createTile(currentLevel+1, maxLevel, i*2, j*2, wcs, im, doImage, tileSize)
 	if sub!=None:
 		t.subTiles.append(sub)
-	sub = createTile(currentLevel+1, maxLevel, i*2+1, j*2, wcs, im, doImage)
+	sub = createTile(currentLevel+1, maxLevel, i*2+1, j*2, wcs, im, doImage, tileSize)
 	if sub!=None:
 		t.subTiles.append(sub)
-	sub = createTile(currentLevel+1, maxLevel, i*2+1, j*2+1, wcs, im, doImage)
+	sub = createTile(currentLevel+1, maxLevel, i*2+1, j*2+1, wcs, im, doImage, tileSize)
 	if sub!=None:
 		t.subTiles.append(sub)
-	sub = createTile(currentLevel+1, maxLevel, i*2, j*2+1, wcs, im, doImage)
+	sub = createTile(currentLevel+1, maxLevel, i*2, j*2+1, wcs, im, doImage, tileSize)
 	if sub!=None:
 		t.subTiles.append(sub)
 	return t
 
 
 def main():
+	parser = OptionParser(usage="%prog [options]", version="0.1", description="This tool generates multi-resolution astronomical images from large image for display by Stellarium/VirGO. The passed image is split in small tiles and the corresponding index JSON files are generated. Everything is output in the current directory.", formatter=IndentedHelpFormatter(max_help_position=33, width=80))
+	parser.add_option("-f", "--fitsheader", dest="fitsHeader", help="use the FITS header from FILE to get WCS info", metavar="FILE")
+	parser.add_option("-g", "--gzipcompress", dest="gzipCompress", action="store_true", default=False, help="compress the produced JSON index using gzip")
+	parser.add_option("-t", "--tilesize", dest="tileSize", type="int", default=256, help="output image tiles size in pixel (default: %default)", metavar="SIZE")
+	parser.add_option("-i", "--onlyindex", dest="makeImageTiles", action="store_false", default=True, help="output only the JSON index")
+	parser.add_option("-l", "--maxLevelPerIndex", dest="maxLevelPerIndex", default=3, type="int", help="put up to 3 levels per index file (default: %default)", metavar="MAX")
+	parser.add_option("-b", "--maxBrightness", dest="maxBrightness", default=13., type="float", help="the surface brightness of a white pixel of the image in V mag/arcmin^2 (default: %default)", metavar="MAG")
+	(options, args) = parser.parse_args()
+	
 	headerFile = None
-	if len(sys.argv) == 3:
-		headerFile = sys.argv[2]
-	elif len(sys.argv) != 15:
-		print "Usage: "+sys.argv[0]+" imageFile [FITSHeader] [nxpix, nypix, ctype1, ctype2, crpix1, crpix2, crval1, crval2, cd, cdelt1, cdelt2, crota, equinox, epoch]"
-		exit(0)
-
+	if len(args) < 1:
+		print "Usage: "+sys.argv[0]+" imageFile [Options]"
+		exit(0)		
 	imgFile = sys.argv[1]
 	
-	# Try to read the header file provided to extract the WCS
-	if headerFile != None:
-		wcs = astWCS.WCS(headerFile)
+	# We know have valid arguments
+	
+	if options.fitsHeader!=None:
+		# Try to read the provided FITS header file to extract the WCS
+		wcs = astWCS.WCS(options.fitsHeader)
 	else:
-		# Or try to generate the WCS by hand
+		# Else try to generate the WCS from the passed options
+		print "WCS options not yet supported, you need to give a fits header!"
+		exit(0)
 		(nxpix, nypix, ctype1, ctype2, crpix1, crpix2, crval1, crval2, cd, cdelt1, cdelt2, crota, equinox, epoch) = sys.argv[2:]
 		worldCoord = astWCS.wcs.wcskinit(nxpix, nypix, ctype1, ctype2, crpix1, crpix2, crval1, crval2, cd, cdelt1, cdelt2, crota, equinox, epoch)
 		coordFrameCstr = 'J2000'
 		wcs = astWCS.wcs.wcsininit(worldCoord, coordFrameCstr);
 	
+	#[nxpix, nypix, ctype1, ctype2, crpix1, crpix2, crval1, crval2, cd, cdelt1, cdelt2, crota, equinox, epoch]"
+
+	
 	im = Image.open(imgFile)
 	
-	nbTileX = (im.size[0]+TILE_SIZE)//TILE_SIZE
-	nbTileY = (im.size[1]+TILE_SIZE)//TILE_SIZE
+	nbTileX = (im.size[0]+options.tileSize)//options.tileSize
+	nbTileY = (im.size[1]+options.tileSize)//options.tileSize
 	nbLevels = int(math.log(max(nbTileX, nbTileY))/math.log(2)+1)
 	print "Will tesselate image (",im.size[0],"x",im.size[1],") in", nbTileX,'x',nbTileY,'tiles on', nbLevels, 'levels'
 	
 	# Create the master level 0 tile, which recursively creates the subtiles
-	# Set the last boolean to False if you just want to regenerate the json files
-	masterTile = createTile(0, nbLevels, 0, 0, wcs, im, True)
+	masterTile = createTile(0, nbLevels, 0, 0, wcs, im, options.makeImageTiles, options.tileSize)
 	masterTile.maxBrightness = 13.
-	masterTile.outputJSON(qCompress=True, maxLevelPerFile=3)
+	masterTile.outputJSON(qCompress=options.gzipCompress, maxLevelPerFile=options.maxLevelPerIndex)
 	
 if __name__ == "__main__":
     main()
