63a1136225e84551694746365ff15e1eafd02ba6
Fabien Chéreau <fabien.chereau@gmail.com>
2009-01-22 13:49:04 +0000

    Fixed a C style bug using clean Qt style process.

--- src/core/StelApp.cpp
+++ src/core/StelApp.cpp
@@ -584,16 +584,10 @@ void StelApp::setupLog()
 		infoFile.close();
 	}
 	
-	QString pciData;
-	FILE* lspci = popen("lspci -v", "r");
-	int s;
-	while(!feof(lspci))
-	{
-		char* buf = (char*)malloc(1024);
-		s = fread(buf, 1024, 1, lspci);
-		pciData += buf;
-	}
-	pclose(lspci);
+	QProcess lspci;
+	lspci.start("lspci -v", QIODevice::ReadOnly);
+	lspci.waitForFinished(100);
+	QString pciData(lspci.readAll());
 	QStringList pciLines = pciData.split('\n', QString::SkipEmptyParts);
 	for(int i = 0; i < pciLines.size(); i++)
 	{
