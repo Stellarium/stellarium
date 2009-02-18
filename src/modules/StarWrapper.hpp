9b4366e26256a61dc9f5737d3bf5ca7b5afb6cf8
Fabien Chereau <fabien.chereau@gmail.com>
2009-02-18 15:20:23 +0000

    indentation + cleaning.

--- src/modules/StarWrapper.hpp
+++ src/modules/StarWrapper.hpp
@@ -34,117 +34,112 @@ template <class Star> struct SpecialZoneArray;
 template <class Star> struct SpecialZoneData;
 
 
-// A Star (Star1,Star2,Star3,...) cannot be a StelObject. The additional
-// overhead of having a dynamic type would simply be too much.
-// Therefore the StarWrapper is needed when returning Stars as StelObjects,
-// e.g. for searching, and for constellations.
-// The StarWrapper is destroyed when it is not needed anymore, by utilizing
-// reference counting and boost::intrusive_ptr. So there is no chance that
-// more than a few hundreds of StarWrappers are alive simultanousely.
-// Another reason for having the StarWrapper is to encapsulate the differences
-// between the different kinds of Stars (Star1,Star2,Star3).
-
-
-class StarWrapperBase : public StelObject {
+//! @class StarWrapperBase
+//! A Star (Star1,Star2,Star3,...) cannot be a StelObject. The additional
+//! overhead of having a dynamic type would simply be too much.
+//! Therefore the StarWrapper is needed when returning Stars as StelObjects, e.g. for searching, and for constellations.
+//! The StarWrapper is destroyed when it is not needed anymore, by utilizing reference counting and boost::intrusive_ptr.
+//! So there is no chance that more than a few hundreds of StarWrappers are alive simultanousely.
+//! Another reason for having the StarWrapper is to encapsulate the differences between the different kinds of Stars (Star1,Star2,Star3).
+class StarWrapperBase : public StelObject
+{
 protected:
-  StarWrapperBase(void) : ref_count(0) {}
-  virtual ~StarWrapperBase(void) {}
-  QString getType(void) const {return "Star";}
+	StarWrapperBase(void) : ref_count(0) {;}
+	virtual ~StarWrapperBase(void) {;}
+	QString getType(void) const {return "Star";}
+	
+	QString getEnglishName(void) const {return "";}
+	QString getNameI18n(void) const = 0;
+	
+	//! StarWrapperBase supports the following InfoStringGroup flags <ul>
+	//! <li> Name
+	//! <li> Magnitude
+	//! <li> RaDecJ2000
+	//! <li> RaDec
+	//! <li> AltAzi
+	//! <li> PlainText </ul>
+	//! @param core the StelCore object
+	//! @param flags a set of InfoStringGroup items to include in the return value.
+	//! @return a QString containing an HMTL encoded description of the StarWrapperBase.
+	QString getInfoString(const StelCore *core, const InfoStringGroup& flags) const;
+	virtual float getBV(void) const = 0;
 
-  QString getEnglishName(void) const {return "";}
-  QString getNameI18n(void) const = 0;
-
-
-  //! StarWrapperBase supports the following InfoStringGroup flags:
-  //! - Name
-  //! - Magnitude
-  //! - RaDecJ2000
-  //! - RaDec
-  //! - AltAzi
-  //! - PlainText
-  //! @param core the StelCore object
-  //! @param flags a set of InfoStringGroup items to include in the return value.
-  //! @return a QString containing an HMTL encoded description of the StarWrapperBase.
-  //! is included in the return value.  
-  QString getInfoString(const StelCore *core, const InfoStringGroup& flags) const;
-  virtual float getBV(void) const = 0;
 private:
-  int ref_count;
-  void retain(void) {Q_ASSERT(ref_count>=0);ref_count++;}
-  void release(void) {Q_ASSERT(ref_count>0);if (--ref_count==0) delete this;}
+	int ref_count;
+	void retain(void) {Q_ASSERT(ref_count>=0);++ref_count;}
+	void release(void) {Q_ASSERT(ref_count>0);if (--ref_count==0) delete this;}
 };
 
-template <class Star>
-class StarWrapper : public StarWrapperBase {
+template <class Star> class StarWrapper : public StarWrapperBase
+{
 protected:
-  StarWrapper(const SpecialZoneArray<Star> *a,
-              const SpecialZoneData<Star> *z,
-              const Star *s) : a(a),z(z),s(s) {}
-  Vec3d getJ2000EquatorialPos(const StelNavigator*) const {
-    const double d2000 = 2451545.0;
-    return s->getJ2000Pos(z,
-                  (M_PI/180)*(0.0001/3600)
-                   * ((StarMgr::getCurrentJDay()-d2000)/365.25)
-                   / a->star_position_scale
-                 );
-  }
-  Vec3f getInfoColor(void) const {return StelApp::getInstance().getVisionModeNight() ? Vec3f(0.8, 0.2, 0.2) : StelSkyDrawer::indexToColor(s->bV);}
-  float getVMagnitude(const StelNavigator *nav) const
-    {return 0.001f*a->mag_min + s->mag*(0.001f*a->mag_range)/a->mag_steps;}
-  float getSelectPriority(const StelNavigator *nav) const
-  {
-  	return getVMagnitude(nav);
-  }
-  float getBV(void) const {return s->getBV();}
-  QString getEnglishName(void) const {
-    return "";
-  }
-  QString getNameI18n(void) const {
-    return s->getNameI18n();
-  }
-  virtual double getAngularSize(const StelCore* core) const {return 0.;}
+	StarWrapper(const SpecialZoneArray<Star> *a,
+		const SpecialZoneData<Star> *z,
+		const Star *s) : a(a), z(z), s(s) {;}
+	Vec3d getJ2000EquatorialPos(const StelNavigator*) const
+	{
+		const double d2000 = 2451545.0;
+		return s->getJ2000Pos(z, (M_PI/180.)*(0.0001/3600.)	* ((StarMgr::getCurrentJDay()-d2000)/365.25) / a->star_position_scale);
+	}
+	Vec3f getInfoColor(void) const
+	{
+		return StelApp::getInstance().getVisionModeNight() ? Vec3f(0.8, 0.2, 0.2) : StelSkyDrawer::indexToColor(s->bV);
+	}
+	float getVMagnitude(const StelNavigator *nav) const
+	{
+		return 0.001f*a->mag_min + s->mag*(0.001f*a->mag_range)/a->mag_steps;
+	}
+	float getSelectPriority(const StelNavigator *nav) const {return getVMagnitude(nav);}
+	float getBV(void) const {return s->getBV();}
+	QString getEnglishName(void) const {return QString();}
+	QString getNameI18n(void) const {return s->getNameI18n();}
+	virtual double getAngularSize(const StelCore* core) const {return 0.;}
 protected:
-  const SpecialZoneArray<Star> *const a;
-  const SpecialZoneData<Star> *const z;
-  const Star *const s;
+	const SpecialZoneArray<Star> *const a;
+	const SpecialZoneData<Star> *const z;
+	const Star *const s;
 };
 
 
-class StarWrapper1 : public StarWrapper<Star1> {
+class StarWrapper1 : public StarWrapper<Star1>
+{
 public:
-  StarWrapper1(const SpecialZoneArray<Star1> *a,
-               const SpecialZoneData<Star1> *z,
-               const Star1 *s) : StarWrapper<Star1>(a,z,s) {}
-  //! StarWrapper1 supports the following InfoStringGroup flags:
-  //! - Name
-  //! - CatalogNumber
-  //! - Magnitude
-  //! - RaDecJ2000
-  //! - RaDec
-  //! - AltAzi
-  //! - Extra1 (spectral type)
-  //! - Distance
-  //! - Extra2 (parallax)
-  //! - PlainText
-  //! @param core the StelCore object
-  //! @param flags a set of InfoStringGroup items to include in the return value.
-  //! @return a QString containing an HMTL encoded description of the StarWrapper1.
-  QString getInfoString(const StelCore *core, const InfoStringGroup& flags) const;
-  QString getEnglishName(void) const;
+	StarWrapper1(const SpecialZoneArray<Star1> *a,
+		const SpecialZoneData<Star1> *z,
+		const Star1 *s) : StarWrapper<Star1>(a,z,s) {;}
+	
+	//! StarWrapper1 supports the following InfoStringGroup flags:
+	//! - Name
+	//! - CatalogNumber
+	//! - Magnitude
+	//! - RaDecJ2000
+	//! - RaDec
+	//! - AltAzi
+	//! - Extra1 (spectral type)
+	//! - Distance
+	//! - Extra2 (parallax)
+	//! - PlainText
+	//! @param core the StelCore object
+	//! @param flags a set of InfoStringGroup items to include in the return value.
+	//! @return a QString containing an HMTL encoded description of the StarWrapper1.
+	QString getInfoString(const StelCore *core, const InfoStringGroup& flags) const;
+	QString getEnglishName(void) const;
 };
 
-class StarWrapper2 : public StarWrapper<Star2> {
+class StarWrapper2 : public StarWrapper<Star2>
+{
 public:
-  StarWrapper2(const SpecialZoneArray<Star2> *a,
+	StarWrapper2(const SpecialZoneArray<Star2> *a,
                const SpecialZoneData<Star2> *z,
-               const Star2 *s) : StarWrapper<Star2>(a,z,s) {}
+               const Star2 *s) : StarWrapper<Star2>(a,z,s) {;}
 };
 
-class StarWrapper3 : public StarWrapper<Star3> {
+class StarWrapper3 : public StarWrapper<Star3>
+{
 public:
-  StarWrapper3(const SpecialZoneArray<Star3> *a,
+	StarWrapper3(const SpecialZoneArray<Star3> *a,
                const SpecialZoneData<Star3> *z,
-               const Star3 *s) : StarWrapper<Star3>(a,z,s) {}
+               const Star3 *s) : StarWrapper<Star3>(a,z,s) {;}
 };
 
 } // namespace BigStarCatalogExtension
