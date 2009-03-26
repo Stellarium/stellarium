3dc007ae74d4044bc71029bb4e251b46115995ee
Fabien Chéreau <fabien.chereau@gmail.com>
2009-03-26 11:43:52 +0000

    Started using QtTest for testing geometry routines.

--- src/tests/testStelSphereGeometry.cpp
+++ src/tests/testStelSphereGeometry.cpp
@@ -1,35 +1,25 @@
 #include <config.h>
-#include <QtDebug>
 
+#include <QObject>
+#include <QtDebug>
+#include <QtTest>
 #include "StelSphereGeometry.hpp"
 #include "StelUtils.hpp"
 
-void _assert(bool b, QString msg = "")
-{
-	if (!b)
-	{
-		qFatal("FAILED: %s",  qPrintable(msg));
-	}
-}
+#include "tests/testStelSphereGeometry.hpp"
 
-void _assertEquals(int a, int b, QString msg="")
-{
-	_assert(a == b, msg);
-}
+QTEST_MAIN(TestStelSphericalGeometry);
 
-void testHalfSpace()
+void TestStelSphericalGeometry::testHalfSpace()
 {
 	Vec3d p0(1,0,0);
-	
-	// Half Space
 	StelGeom::HalfSpace h0(p0, 0);
-	_assert(contains(h0, p0), "HalfSpace contains point failure");
+	QVERIFY2(contains(h0, p0), "HalfSpace contains point failure");
 	StelGeom::HalfSpace h1(p0, 0.8);
-	_assert(contains(h1, p0), "HalfSpace contains point failure");
-	qDebug() << "Passed HalfSpace tests";
+	QVERIFY2(contains(h1, p0), "HalfSpace contains point failure");
 }
 
-void testContains()
+void TestStelSphericalGeometry::testContains()
 {
 	Vec3d p0(1,0,0);
 	Vec3d p1(1,1,1);
@@ -42,8 +32,8 @@ void testContains()
 	
 	// Triangle polygons
 	StelGeom::ConvexPolygon triangle1(Vec3d(0,0,1), Vec3d(0,1,0), Vec3d(1,0,0));
-	_assert(contains(triangle1, p1), "Triangle contains point failure");
-	_assert(intersect(triangle1, p1), "Triangle intersect point failure");
+	QVERIFY2(contains(triangle1, p1), "Triangle contains point failure");
+	QVERIFY2(intersect(triangle1, p1), "Triangle intersect point failure");
 	
 	// polygons-point intersect
 	double deg5 = 5.*M_PI/180.;
@@ -53,10 +43,10 @@ void testContains()
 	StelUtils::spheToRect(+deg5, +deg5, v2);
 	StelUtils::spheToRect(-deg5, +deg5, v3);
 	StelGeom::ConvexPolygon square1(v3, v2, v1, v0);
-	_assert(contains(square1, p0), "Square contains point failure");
-	_assert(!contains(square1, p1), "Square not contains point failure");
-	_assert(intersect(square1, p0), "Square intersect point failure");
-	_assert(!intersect(square1, p1), "Square not intersect point failure");
+	QVERIFY2(contains(square1, p0), "Square contains point failure");
+	QVERIFY2(!contains(square1, p1), "Square not contains point failure");
+	QVERIFY2(intersect(square1, p0), "Square intersect point failure");
+	QVERIFY2(!intersect(square1, p1), "Square not intersect point failure");
 	
 	// polygons-polygons intersect
 	StelUtils::spheToRect(-deg2, -deg2, v0);
@@ -64,10 +54,10 @@ void testContains()
 	StelUtils::spheToRect(+deg2, +deg2, v2);
 	StelUtils::spheToRect(-deg2, +deg2, v3);
 	StelGeom::ConvexPolygon square2(v3, v2, v1, v0);
-	_assert(contains(square1, square2), "Square contains square failure");
-	_assert(!contains(square2, square1), "Square not contains square failure");
-	_assert(intersect(square1, square2), "Square intersect square failure");
-	_assert(intersect(square2, square1), "Square intersect square failure");
+	QVERIFY2(contains(square1, square2), "Square contains square failure");
+	QVERIFY2(!contains(square2, square1), "Square not contains square failure");
+	QVERIFY2(intersect(square1, square2), "Square intersect square failure");
+	QVERIFY2(intersect(square2, square1), "Square intersect square failure");
 	
 	// Test the tricky case where 2 polygons intersect without having point within each other
 	StelUtils::spheToRect(-deg5, -deg2, v0);
@@ -80,42 +70,28 @@ void testContains()
 	StelUtils::spheToRect(+deg2, +deg5, v2);
 	StelUtils::spheToRect(-deg2, +deg5, v3);
 	StelGeom::ConvexPolygon squareVerti(v3, v2, v1, v0);
-	_assert(!contains(squareHoriz, squareVerti), "Special intersect contains failure");
-	_assert(!contains(squareVerti, squareHoriz), "Special intersect contains failure");
-	_assert(intersect(squareHoriz, squareVerti), "Special intersect failure");
-	_assert(intersect(squareVerti, squareHoriz), "Special intersect failure");
-	
-	
-	qDebug() << "Passed intersection tests";
+	QVERIFY2(!contains(squareHoriz, squareVerti), "Special intersect contains failure");
+	QVERIFY2(!contains(squareVerti, squareHoriz), "Special intersect contains failure");
+	QVERIFY2(intersect(squareHoriz, squareVerti), "Special intersect failure");
+	QVERIFY2(intersect(squareVerti, squareHoriz), "Special intersect failure");
 }
 
-void testPlaneIntersect2()
+void TestStelSphericalGeometry::testPlaneIntersect2()
 {
 	Vec3d p1,p2;
 	Vec3d vx(1,0,0);
 	Vec3d vz(0,0,1);
 	StelGeom::HalfSpace hx(vx, 0);
 	StelGeom::HalfSpace hz(vz, 0);
-	_assert(StelGeom::planeIntersect2(hx, hz, p1, p2)==true, "Plane intersect failed");
-	_assert(p1==Vec3d(0,-1,0));
-	_assert(p2==Vec3d(0,1,0));
-	_assert(StelGeom::planeIntersect2(hx, hx, p1, p2)==false, "Plane non-intersecting failure");
+	QVERIFY2(StelGeom::planeIntersect2(hx, hz, p1, p2)==true, "Plane intersect failed");
+	QVERIFY(p1==Vec3d(0,-1,0));
+	QVERIFY(p2==Vec3d(0,1,0));
+	QVERIFY2(StelGeom::planeIntersect2(hx, hx, p1, p2)==false, "Plane non-intersecting failure");
 	
 	hx.d = std::sqrt(2.)/2.;
-	_assert(StelGeom::planeIntersect2(hx, hz, p1, p2)==true, "Plane/convex intersect failed");
+	QVERIFY2(StelGeom::planeIntersect2(hx, hz, p1, p2)==true, "Plane/convex intersect failed");
 	Vec3d res(p1-Vec3d(hx.d,-hx.d,0));
-	_assert(res.length()<0.0000001, QString("p1 wrong: %1").arg(p1.toString()));
+	QVERIFY2(res.length()<0.0000001, QString("p1 wrong: %1").arg(p1.toString()).toUtf8());
 	res = p2-Vec3d(hx.d,hx.d,0);
-	_assert(res.length()<0.0000001, QString("p2 wrong: %1").arg(p2.toString()));
+	QVERIFY2(res.length()<0.0000001, QString("p2 wrong: %1").arg(p2.toString()).toUtf8());
 }
-
-/************************************************************************
- Run several of the time-related functions through paces.
-************************************************************************/
-int main(int argc, char* argv[])
-{
-	testHalfSpace();
-	testContains();
-	testPlaneIntersect2();
-}
-
