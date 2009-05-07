24843492161f1afc0c339b0cab77fb243246e5a5
Fabien Chereau <fabien.chereau@gmail.com>
2009-05-07 08:23:22 +0000

    Replaced some boost_shared_ptr by QSharedPointer.

--- src/core/StelProjectorType.hpp
+++ src/core/StelProjectorType.hpp
@@ -1,17 +1,17 @@
 /*
  * Stellarium
  * Copyright (C) 2008 Fabien Chereau
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -20,7 +20,7 @@
 #ifndef _STELPROJECTORTYPE_HPP_
 #define _STELPROJECTORTYPE_HPP_
 
-#include <boost/shared_ptr.hpp>
+#include <QSharedPointer>
 
 //! @file StelProjectorType.hpp
 //! Define the StelProjectorP type.
@@ -29,6 +29,6 @@ class StelProjector;
 
 //! @typedef StelProjectorP
 //! Shared pointer on a StelProjector instance (implement reference counting)
-typedef boost::shared_ptr<StelProjector> StelProjectorP;
+typedef QSharedPointer<StelProjector> StelProjectorP;
 
 #endif // _STELPROJECTORTYPE_HPP_
--- src/core/StelTextureMgr.hpp
+++ src/core/StelTextureMgr.hpp
@@ -1,17 +1,17 @@
 /*
  * Stellarium
  * Copyright (C) 2006 Fabien Chereau
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -30,7 +30,7 @@
 
 #include "StelTexture.hpp"
 
-//! @class ImageLoader 
+//! @class ImageLoader
 //! Abstract class for any Image loaders.
 class ImageLoader
 {
@@ -45,7 +45,7 @@ public:
 		GLenum type;
 		QString fullPath;
 	};
-	
+
 	virtual ~ImageLoader() {;}
 	//! Load the data from the image and store it into tex.texels
 	//! The caller is responsible for freeing the memory allocated in tex.texels
@@ -85,55 +85,55 @@ class StelTextureMgr : QObject
 public:
 	StelTextureMgr();
 	virtual ~StelTextureMgr();
-	
+
 	//! Initialize some variable from the openGL contex.
 	//! Must be called after the creation of the GLContext.
 	void init();
-	
+
 	//! Load an image from a file and create a new texture from it
 	//! @param filename the texture file name, can be absolute path if starts with '/' otherwise
 	//!    the file will be looked in stellarium standard textures directories.
 	StelTextureSP createTexture(const QString& filename);
-	
+
 	//! Load an image from a file and create a new texture from it in a new thread.
 	//! @param url the texture file name or URL, can be absolute path if starts with '/' otherwise
 	//!    the file will be looked in stellarium standard textures directories.
 	//! @param fileExtension the file extension to assume. If not set the extension is determined from url
 	//! @param lazyLoading define whether the texture should be actually loaded only when needed, i.e. when bind() is called the first time.
 	StelTextureSP createTextureThread(const QString& url, const QString& fileExtension="", bool lazyLoading=true);
-	
+
 	//! Define if mipmaps must be created while creating textures
 	void setMipmapsMode(bool b = false) {mipmapsMode = b;}
-	
+
 	//! Define the texture wrapping mode to use while creating textures
 	//! @param m can be either GL_CLAMP, GL_CLAMP_TO_EDGE, or GL_REPEAT.
 	//! See doc for glTexParameter for more info.
 	void setWrapMode(GLint m = GL_CLAMP) {wrapMode = m;}
-	
+
 	//! Define the texture min filter to use while creating textures
 	//! @param m can be either GL_NEAREST, GL_LINEAR, GL_NEAREST_MIPMAP_NEAREST,
 	//! GL_LINEAR_MIPMAP_NEAREST, GL_NEAREST_MIPMAP_LINEAR, GL_LINEAR_MIPMAP_LINEAR
 	//! See doc for glTexParameter for more info.
 	void setMinFilter(GLint m = GL_NEAREST) {minFilter = m;}
-	
+
 	//! Define the texture magnification filter to use while creating textures
 	//! @param m can be either GL_NEAREST, GL_LINEAR
 	//! See doc for glTexParameter for more info.
 	void setMagFilter(GLint m = GL_LINEAR) {magFilter = m;}
-	
+
 	//! Set default parameters for Mipmap mode, wrap mode, min and mag filters
 	void setDefaultParams();
-	
+
 	//! Define how the dynamic range of the image will be adapted to fit on 8 bits
 	//! Note that using linear mode on 8 bits images does nothing
-	void setDynamicRangeMode(StelTextureTypes::DynamicRangeMode dMode = StelTextureTypes::Linear) {dynamicRangeMode = dMode;}
-	
+	void setDynamicRangeMode(StelTexture::DynamicRangeMode dMode = StelTexture::Linear) {dynamicRangeMode = dMode;}
+
 	//! Register a new image loader for a given image file extension
 	void registerImageLoader(const QString& fileExtension, ImageLoader* loader)
 	{
 		imageLoaders[fileExtension] = loader;
 	}
-	
+
 private:
 	friend class StelTexture;
 
@@ -142,22 +142,22 @@ private:
 
 	//! Load the image memory
 	bool loadImage(StelTexture* tex);
-	
+
 	//! Adapt the scaling for the texture. Return true if there was no errors
 	bool reScale(StelTexture* tex);
 
 	//! List of image loaders providing image loading for the given files extensions
 	QMap<QString, ImageLoader*> imageLoaders;
-	
+
 	bool mipmapsMode;
 	GLint wrapMode;
 	GLint minFilter;
 	GLint magFilter;
-	StelTextureTypes::DynamicRangeMode dynamicRangeMode;
-	
+	StelTexture::DynamicRangeMode dynamicRangeMode;
+
 	//! The maximum texture size supported by the video card
 	GLint maxTextureSize;
-	
+
 	//! Whether ARB_texture_non_power_of_two is supported on this card
 	bool isNoPowerOfTwoAllowed;
 };
