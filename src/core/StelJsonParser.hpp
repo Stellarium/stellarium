c075f0cbfb12120de2ca7b962a99dc09072662d3
xalioth <>
2009-05-08 12:22:15 +0000

    Added part of Mike Storm's patch (+bug fixes) for loading JSON list with an iterator.

--- src/core/StelJsonParser.hpp
+++ src/core/StelJsonParser.hpp
@@ -23,6 +23,52 @@
 #include <QVariant>
 #include <QByteArray>
 
+
+//! Qt-style iterator over a JSON array. An actual list is not kept in memory,
+//! so only forward iteration is supported and all methods, including the constructor,
+//! involve read() calls on the QIODevice. Because of this, do not modify the
+//! QIODevice between calls to JsonListIterator methods. Also, the toFront()
+//! method has a special function and reset() is provided for convenience. Only
+//! peekNext() is guaranteed not to modify the QIODevice.
+class JsonListIterator
+{
+public:
+	//! Sets up JsonListIterator to read an array. Swallows all whitespace
+	//! up to a beginning '[' character. If '[' is not the first non-whitespace
+	//! character encountered, reset() is called and an exception is thrown.
+	JsonListIterator(QIODevice& input);
+
+	//! Reads and parses the next object from input. Advances QIODevice to
+	//! just after the object.
+	//! @return the next object from the array
+	QVariant next() const;
+
+	//! Reads and parses the next object from input. Does not advance QIODevice.
+	//! @return the next object from the array
+	QVariant peekNext() const;
+
+	//! Returns true if the next non-whitespace character is not a ']' character.
+	bool hasNext();
+
+	//! Seeks QIODevice to the position it was at when the JsonListIterator
+	//! constructor was called.
+	//! @return QIODevice::seek()'s return value
+	bool reset();
+
+	//! Seeks QIODevice to just after the beginning '[' character of the array.
+	void toBack();
+
+	//! Seeks QIODevice to just after the terminating ']' character of the
+	//! array. If a ']' character is not encountered, something bad will
+	//! probably happen.
+	void toFront();
+
+private:
+	QIODevice& input;
+	qint64 startPos;
+};
+
+//! @class StelJsonParser
 //! Qt-based simple JSON reader inspired by the one from <a href='http://zoolib.sourceforge.net/'>Zoolib</a>.
 //! JSON is JavaScript Object Notation. See http://www.json.org/
 /*! <p>The mapping with Qt types is done as following:
@@ -39,11 +85,14 @@ number        QVariant::Int or QVariant::Double
 class StelJsonParser
 {
 public:
-	//! Parse the given input stream
-	QVariant parse(QIODevice& input) const;
-	
-	//! Serialize the passed QVariant as JSON into the output QIODevice
-	void write(const QVariant& jsonObject, QIODevice& output, int indentLevel=0) const;
+	//! Create a JsonListIterator from the given input device.
+	static JsonListIterator initListIterator(QIODevice& in) {return JsonListIterator(in);}
+
+	//! Parse the given input stream.
+	static QVariant parse(QIODevice& input);
+
+	//! Serialize the passed QVariant as JSON into the output QIODevice.
+	static void write(const QVariant& jsonObject, QIODevice& output, int indentLevel=0);
 };
 
 #endif // _STELJSONPARSER_HPP_
