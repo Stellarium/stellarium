fa404879d50c4d59e2d42b4a8365b1c2d45b76f6
Fabien Chereau <fabien.chereau@gmail.com>
2009-05-08 09:22:49 +0000

    Fixed compilation issues on some plateforms.

--- src/modules/Telescope.hpp
+++ src/modules/Telescope.hpp
@@ -6,12 +6,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -22,6 +22,8 @@
 
 #include <QList>
 #include <QString>
+#include <cstdlib>
+#include <sys/select.h>
 
 #if defined (_MSC_VER)
 #include <winsock2.h>
@@ -41,7 +43,7 @@ class Telescope : public StelObject
 public:
 	static Telescope *create(const QString &url);
 	virtual ~Telescope(void) {}
-	
+
 	// Method inherited from StelObject
 	QString getEnglishName(void) const {return name;}
 	QString getNameI18n(void) const {return nameI18n;}
@@ -56,14 +58,14 @@ public:
 	QString getInfoString(const StelCore* core, const InfoStringGroup& flags) const;
 	QString getType(void) const {return "Telescope";}
 	virtual double getAngularSize(const StelCore* core) const {Q_ASSERT(0); return 0;}	// TODO
-		
+
 	// Methods specific to telescope
 	virtual void telescopeGoto(const Vec3d &j2000Pos) = 0;
 	virtual bool isConnected(void) const = 0;
 	virtual bool hasKnownPosition(void) const = 0;
 	void addOcular(double fov) {if (fov>=0.0) oculars.push_back(fov);}
 	const QList<double> &getOculars(void) const {return oculars;}
-	
+
 	// all TCP (and all possible other style) communication shall be done in these functions:
 	virtual void prepareSelectFds(fd_set &read_fds,fd_set &write_fds, int &fdmax) = 0;
 	virtual void handleSelectFds(const fd_set &read_fds, const fd_set &write_fds) {}
