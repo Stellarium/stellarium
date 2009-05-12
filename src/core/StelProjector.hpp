58a042e729830e59acc9cc124ade1b006e2f415f
Fabien Chereau <fabien.chereau@gmail.com>
2009-05-12 14:32:05 +0000

    Doc.

--- src/core/StelProjector.hpp
+++ src/core/StelProjector.hpp
@@ -36,7 +36,7 @@ class StelProjector
 public:
 	friend class StelPainter;
 	friend class StelCore;
-	
+
 	//! @enum StelProjectorMaskType
 	//! Define viewport mask types
 	enum StelProjectorMaskType
@@ -44,7 +44,7 @@ public:
 		MaskNone,	//!< Regular - no mask.
 		MaskDisk	//!< For disk viewport mode (circular mask to seem like bins/telescope)
 	};
-	
+
 	//! @struct StelProjectorParams
 	//! Contains all the param needed to initialize a StelProjector
 	struct StelProjectorParams
@@ -59,10 +59,10 @@ public:
 		double viewportFovDiameter;    //! diameter of the FOV disk in pixel
 		bool flipHorz, flipVert;       //! Whether to flip in horizontal or vertical directions
 	};
-	
+
 	//! Destructor
 	virtual ~StelProjector() {;}
-	
+
 	///////////////////////////////////////////////////////////////////////////
 	// Methods which must be reimplemented by all instance of StelProjector
 	//! Get a human-readable name for this projection type
@@ -72,7 +72,7 @@ public:
 	//! Get a HTML version of the short description for this projection type
 	QString getHtmlSummary() const;
 	//! Get the maximum FOV apperture in degree
-	virtual double getMaxFov() const = 0;	
+	virtual double getMaxFov() const = 0;
 	//! Apply the transformation in the forward direction in place.
 	//! After transformation v[2] will always contain the length of the original v: sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2])
 	//! regardless of the projection type. This makes it possible to implement depth buffer testing in a way independent of the
@@ -86,7 +86,7 @@ public:
 	virtual double deltaZoom(double fov) const = 0;
 
 	//! Determine whether a great circle connection p1 and p2 intersects with a projection discontinuity.
-	//! For many projections without discontinuity, this should return always false, but for other like 
+	//! For many projections without discontinuity, this should return always false, but for other like
 	//! cylindrical projection it will return true if the line cuts the wrap-around line (i.e. at lon=180 if the observer look at lon=0).
 	bool intersectViewportDiscontinuity(const Vec3d& p1, const Vec3d& p2) const
 	{
@@ -94,14 +94,14 @@ public:
 			return false;
 		return intersectViewportDiscontinuityInternal(modelViewMatrix*p1, modelViewMatrix*p2);
 	}
-	
+
 	//! Convert a Field Of View radius value in radians in ViewScalingFactor (used internally)
 	virtual double fovToViewScalingFactor(double fov) const = 0;
 	//! Convert a ViewScalingFactor value (used internally) in Field Of View radius in radians
 	virtual double viewScalingFactorToFov(double vsf) const = 0;
-	
-	//! Get the current state of the flag which decides whether to 
-	//! arrage labels so that they are aligned with the bottom of a 2d 
+
+	//! Get the current state of the flag which decides whether to
+	//! arrage labels so that they are aligned with the bottom of a 2d
 	//! screen, or a 3d dome.
 	bool getFlagGravityLabels() const { return gravityLabels; }
 
@@ -113,7 +113,7 @@ public:
 	{
 		return Vec2d(viewportCenter[0]-viewportXywh[0],viewportCenter[1]-viewportXywh[1]);
 	}
-	
+
 	//! Get the horizontal viewport offset in pixels.
 	int getViewportPosX(void) const {return viewportXywh[0];}
 	//! Get the vertical viewport offset in pixels.
@@ -122,26 +122,26 @@ public:
 	int getViewportWidth(void) const {return viewportXywh[2];}
 	//! Get the viewport height in pixels.
 	int getViewportHeight(void) const {return viewportXywh[3];}
-	
+
 	//! Return a convex polygon on the sphere which includes the viewport in the current frame.
 	//! @param marginX an extra margin in pixel which extends the polygon size in the X direction.
 	//! @param marginY an extra margin in pixel which extends the polygon size in the Y direction.
-	//! @return a SphericalConvexPolygon or the special fullSky region if the viewport cannot be 
+	//! @return a SphericalConvexPolygon or the special fullSky region if the viewport cannot be
 	//! represented by a convex polygon (e.g. if aperture > 180 deg).
 	SphericalRegionP getViewportConvexPolygon(double marginX=0., double marginY=0.) const;
-	
+
 	//! Return a Halfspace containing the whole viewport
 	SphericalCap getBoundingSphericalCap() const;
-	
+
 	//! Get size of a radian in pixels at the center of the viewport disk
 	double getPixelPerRadAtCenter(void) const {return pixelPerRad;}
-	
+
 	//! Get the current FOV diameter in degree
 	double getFov() const {return 360./M_PI*viewScalingFactorToFov(0.5*viewportFovDiameter/pixelPerRad);}
-	
+
 	//! Get whether front faces need to be oriented in the clockwise direction
 	bool needGlFrontFaceCW(void) const {return (flipHorz*flipVert < 0.0);}
-	
+
 	///////////////////////////////////////////////////////////////////////////
 	// Full projection methods
 	//! Check to see if a 2d position is inside the viewport.
@@ -169,7 +169,7 @@ public:
 		}
 		return v;
 	}
-		
+
 	//! Project the vector v from the current frame into the viewport.
 	//! @param v the vector in the current frame.
 	//! @param win the projected vector in the viewport 2D frame.
@@ -195,14 +195,14 @@ public:
 	}
 
 	//! Project the vector v from the current frame into the viewport.
-	//! @param v the vector in the current frame.
-	//! @param win the projected vector in the viewport 2D frame.
+	//! @param v the direction vector in the current frame. Does not need to be normalized.
+	//! @param win the projected vector in the viewport 2D frame. win[0] and win[1] are in screen pixels, win[2] is unused.
 	//! @return true if the projected point is inside the viewport.
 	bool projectCheck(const Vec3d& v, Vec3d& win) const {return (project(v, win) && checkInViewport(win));}
 
 	//! Project the vector v from the viewport frame into the current frame.
-	//! @param win the vector in the viewport 2D frame.
-	//! @param v the projected vector in the current frame.
+	//! @param win the vector in the viewport 2D frame. win[0] and win[1] are in screen pixels, win[2] is unused.
+	//! @param v the unprojected direction vector in the current frame.
 	//! @return true if the projected coordinate is valid.
 	bool unProject(const Vec3d& win, Vec3d& v) const {return unProject(win[0], win[1], v);}
 	bool unProject(double x, double y, Vec3d& v) const;
@@ -218,29 +218,29 @@ public:
 
 	//! Get the current model view matrix.
 	const Mat4d& getModelViewMatrix() const {return modelViewMatrix;}
-	
+
 	///////////////////////////////////////////////////////////////////////////
 	//! Get a string description of a StelProjectorMaskType.
 	static const QString maskTypeToString(StelProjectorMaskType type);
 	//! Get a StelProjectorMaskType from a string description.
 	static StelProjectorMaskType stringToMaskType(const QString &s);
-	
+
 	//! Get the current type of the mask if any.
 	StelProjectorMaskType getMaskType(void) const {return maskType;}
-	
+
 protected:
 	//! Private constructor. Only StelCore can create instances of StelProjector.
 	StelProjector(const Mat4d& modelViewMat) : modelViewMatrix(modelViewMat) {;}
 	//! Return whether the projection presents discontinuities. Used for optimization.
 	virtual bool hasDiscontinuity() const =0;
 	//! Determine whether a great circle connection p1 and p2 intersects with a projection discontinuity.
-	//! For many projections without discontinuity, this should return always false, but for other like 
+	//! For many projections without discontinuity, this should return always false, but for other like
 	//! cylindrical projection it will return true if the line cuts the wrap-around line (i.e. at lon=180 if the observer look at lon=0).
 	virtual bool intersectViewportDiscontinuityInternal(const Vec3d& p1, const Vec3d& p2) const = 0;
 private:
 	//! Initialise the StelProjector from a param instance
 	void init(const StelProjectorParams& param);
-	
+
 	StelProjectorMaskType maskType;    // The current projector mask
 	double zNear, oneOverZNearMinusZFar;  // Near and far clipping planes
 	Vec4i viewportXywh;     // Viewport parameters
