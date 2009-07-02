9c7040647f78f240bb82a607e5f601d930bc4e97
xalioth <>
2009-07-02 09:59:08 +0000

    Got rid of some glBegin()/glEnd() and Optimized.

--- src/core/StelLoadingBar.cpp
+++ src/core/StelLoadingBar.cpp
@@ -78,42 +78,18 @@ void StelLoadingBar::Draw(float val)
 		glColor3f(1, 1, 1);
 		splash->bind();
 
-		glBegin(GL_QUADS);
-			glTexCoord2i(0, 0);		// Bottom Left
-			glVertex2f(splashx, splashy);
-			glTexCoord2i(1, 0);		// Bottom Right
-			glVertex2f(splashx + width, splashy);
-			glTexCoord2i(1, 1);		// Top Right
-			glVertex2f(splashx + width, splashy + height);
-			glTexCoord2i(0, 1);		// Top Left
-			glVertex2f(splashx, splashy + height);
-		glEnd();
+		sPainter.drawRect2d(splashx, splashy, width, height);
 	}
 	glDisable(GL_BLEND);
 	glDisable(GL_TEXTURE_2D);
 
 	// black out background of text for redraws (so can keep sky unaltered)
 	glColor3f(0, 0, 0);
-	glBegin(GL_TRIANGLE_STRIP);
-		glVertex3f(barx + barwidth, bary-17, 0.0f);
-		glVertex3f(barx, bary-17, 0.0f);
-		glVertex3f(barx + barwidth, bary-5, 0.0f);
-		glVertex3f(barx, bary-5, 0.0f);
-	glEnd();
+	sPainter.drawRect2d(barx, bary-5, barwidth, 12.);
 	glColor3f(0.8, 0.8, 1);
-	glBegin(GL_TRIANGLE_STRIP);
-		glVertex3f(barx + barwidth, bary + barheight, 0.0f);
-		glVertex3f(barx, bary + barheight, 0.0f);
-		glVertex3f(barx + barwidth, bary, 0.0f);
-		glVertex3f(barx, bary, 0.0f);
-	glEnd();
+	sPainter.drawRect2d(barx, bary, barwidth, barheight);
 	glColor3f(0.4f, 0.4f, 0.6f);
-	glBegin(GL_TRIANGLE_STRIP);
-		glVertex3f(-1 + barx + barwidth * val, bary + barheight - 1, 0.0f);
-		glVertex3f(1 + barx, bary + barheight - 1, 0.0f);
-		glVertex3f(-1 + barx + barwidth * val, bary + 1, 0.0f);
-		glVertex3f(1 + barx, bary + 1, 0.0f);
-	glEnd();
+	sPainter.drawRect2d(barx+1, bary+1, barwidth * val-2, barheight-2);
 	
 	glColor3f(1, 1, 1);
 	glEnable(GL_TEXTURE_2D);
