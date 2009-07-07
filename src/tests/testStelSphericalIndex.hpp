c6ffd6e4c5d55e1baffa579ffe623d8ca62c41ec
xalioth <>
2009-07-07 09:30:00 +0000

    Added.

---
+++ src/tests/testStelSphericalIndex.hpp
@@ -0,0 +1,38 @@
+/*
+ * Stellarium
+ * Copyright (C) 2009 Fabien Chereau
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _TESTSTELSPHERICALINDEX_HPP_
+#define _TESTSTELSPHERICALINDEX_HPP_
+
+#include <QObject>
+#include <QtTest>
+#include "StelSphereGeometry.hpp"
+#include "StelSphericalIndex.hpp"
+
+class TestStelSphericalIndex : public QObject
+{
+Q_OBJECT
+private slots:
+	void initTestCase();
+	void testBase();
+
+private:
+};
+
+#endif // _TESTSTELSPHERICALINDEX_HPP_
