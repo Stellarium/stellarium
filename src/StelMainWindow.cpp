d46adc870fe93ef3233d91778e75ef34988664ff
Fabien Chéreau <fabien.chereau@gmail.com>
2009-09-11 12:02:33 +0000

    Cosmetics.

--- src/StelMainWindow.cpp
+++ src/StelMainWindow.cpp
@@ -1,17 +1,17 @@
 /*
  * Stellarium
  * Copyright (C) 2007 Fabien Chereau
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -34,7 +34,6 @@
 #include "StelFileMgr.hpp"
 #include "StelModuleMgr.hpp"
 #include "StelGui.hpp"
-
 #include "StelTranslator.hpp"
 
 // Initialize static variables
@@ -44,7 +43,7 @@ StelMainWindow::StelMainWindow(QWidget* parent) : QMainWindow(parent)
 {
 	// Can't create 2 StelMainWindow instances
 	Q_ASSERT(!singleton);
-    singleton = this;
+	singleton = this;
 }
 
 // Update the translated title
@@ -58,7 +57,7 @@ void StelMainWindow::init()
 {
 	setWindowIcon(QIcon(":/mainWindow/icon.bmp"));
 	initTitleI18n();
-	
+
 	QString fName;
 	try
 	{
@@ -72,13 +71,13 @@ void StelMainWindow::init()
 	}
 	if (!fName.isEmpty())
 		QFontDatabase::addApplicationFont(fName);
-	
+
 	// Init the main window. It must be done here because it is not the responsability of StelApp to do that
 	QCoreApplication::processEvents();
-	
+
 	QSettings* settings = StelApp::getInstance().getSettings();
 	resize(settings->value("video/screen_w", 800).toInt(), settings->value("video/screen_h", 600).toInt());
-	
+
 	if (settings->value("video/fullscreen", true).toBool())
 	{
 		showFullScreen();
@@ -87,7 +86,7 @@ void StelMainWindow::init()
 	{
 		show();
 	}
-	
+
 	StelMainGraphicsView::getInstance().init();
 }
 
