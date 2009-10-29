a1824c72f122a000b26c67b06a134e21d85aa2c9
xalioth <>
2009-10-29 09:53:12 +0000

    Style.

--- src/tests/testDates.cpp
+++ src/tests/testDates.cpp
@@ -16,40 +16,40 @@ QTEST_MAIN(TestDates);
 void TestDates::dateRoundTrip()
 {
 	QMap <double, QString> map;
-		map[0.0] = "-4712-01-01T12:00:00";
-		map[-1.0] = "-4713-12-31T12:00:00";
-		map[2454466.0] = "2007-12-31T12:00:00";
-		map[1721058.0] = "0000-01-01T12:00:00";
-		map[2500000.0] = "2132-08-31T12:00:00";
-		map[366.0] = "-4711-01-01T12:00:00";
-		map[2454534] = "2008-03-08T12:00:00";
-		map[2299161.0] = "1582-10-15T12:00:00";
-		map[2454466.5] = "2008-01-01T00:00:00";
-		map[1720692.0] = "-0002-12-31T12:00:00";
-		map[1720693.0] = "-0001-01-01T12:00:00";
-		map[2400000.0] = "1858-11-16T12:00:00";
-		map[2110516.00000] = "1066-04-12T12:00:00";
-		map[1918395.00000] = "0540-04-12T12:00:00";
-		map[1794575.00000] = "0201-04-12T12:00:00";
-		map[1757319.00000] = "0099-04-12T12:00:00";
-		map[1721424.0] = "0001-01-01T12:00:00";
-		map[1721789.0] = "0002-01-01T12:00:00";
-		map[-31.0] = "-4713-12-01T12:00:00";
-		map[-61.0] = "-4713-11-01T12:00:00";
-		map[-92.0] = "-4713-10-01T12:00:00";
-		map[-122.0] = "-4713-09-01T12:00:00";
-		map[-153.0] = "-4713-08-01T12:00:00";
-		map[-184.0] = "-4713-07-01T12:00:00";
-		map[-214.0] = "-4713-06-01T12:00:00";
-		map[-245.0] = "-4713-05-01T12:00:00";
-		map[-275.0] = "-4713-04-01T12:00:00";
-		map[-306.0] = "-4713-03-01T12:00:00";
-		map[-334.0] = "-4713-02-01T12:00:00"; // 28 days
-		map[-365.0] = "-4713-01-01T12:00:00";
-		map[-699.0] = "-4714-02-01T12:00:00"; // 28 days
-		map[-1064.0] = "-4715-02-01T12:00:00"; // 28 days
-		map[-1420.0] = "-4716-02-01T12:00:00"; // 29 days
-		map[-1785.0] = "-4717-02-01T12:00:00"; // 28 days
+	map[0.0] = "-4712-01-01T12:00:00";
+	map[-1.0] = "-4713-12-31T12:00:00";
+	map[2454466.0] = "2007-12-31T12:00:00";
+	map[1721058.0] = "0000-01-01T12:00:00";
+	map[2500000.0] = "2132-08-31T12:00:00";
+	map[366.0] = "-4711-01-01T12:00:00";
+	map[2454534] = "2008-03-08T12:00:00";
+	map[2299161.0] = "1582-10-15T12:00:00";
+	map[2454466.5] = "2008-01-01T00:00:00";
+	map[1720692.0] = "-0002-12-31T12:00:00";
+	map[1720693.0] = "-0001-01-01T12:00:00";
+	map[2400000.0] = "1858-11-16T12:00:00";
+	map[2110516.00000] = "1066-04-12T12:00:00";
+	map[1918395.00000] = "0540-04-12T12:00:00";
+	map[1794575.00000] = "0201-04-12T12:00:00";
+	map[1757319.00000] = "0099-04-12T12:00:00";
+	map[1721424.0] = "0001-01-01T12:00:00";
+	map[1721789.0] = "0002-01-01T12:00:00";
+	map[-31.0] = "-4713-12-01T12:00:00";
+	map[-61.0] = "-4713-11-01T12:00:00";
+	map[-92.0] = "-4713-10-01T12:00:00";
+	map[-122.0] = "-4713-09-01T12:00:00";
+	map[-153.0] = "-4713-08-01T12:00:00";
+	map[-184.0] = "-4713-07-01T12:00:00";
+	map[-214.0] = "-4713-06-01T12:00:00";
+	map[-245.0] = "-4713-05-01T12:00:00";
+	map[-275.0] = "-4713-04-01T12:00:00";
+	map[-306.0] = "-4713-03-01T12:00:00";
+	map[-334.0] = "-4713-02-01T12:00:00"; // 28 days
+	map[-365.0] = "-4713-01-01T12:00:00";
+	map[-699.0] = "-4714-02-01T12:00:00"; // 28 days
+	map[-1064.0] = "-4715-02-01T12:00:00"; // 28 days
+	map[-1420.0] = "-4716-02-01T12:00:00"; // 29 days
+	map[-1785.0] = "-4717-02-01T12:00:00"; // 28 days
 
 	// does jdToIsoString() of item in t1d match string in t1s?
 	QList<int> fromString;
@@ -68,23 +68,14 @@ void TestDates::dateRoundTrip()
 		}
 		else
 		{
-			qWarning() << QString("%1/%2: %3 => %4 != %5")
-										  .arg(testNum)
-										  .arg(total)
-										  .arg(jd)
-									  .arg(StelUtils::jdToIsoString(jd))
-									  .arg(map[jd]);
+			qWarning() << QString("%1/%2: %3 => %4 != %5").arg(testNum).arg(total).arg(jd).
+					arg(StelUtils::jdToIsoString(jd)).arg(map[jd]);
 		}
 
 		QList<int> fromString = StelUtils::getIntsFromISO8601String(map[jd]);
 		double backJd;
-		double success = StelUtils::getJDFromDate(&backJd,
-												  fromString.at(0),
-												  fromString.at(1),
-												  fromString.at(2),
-												  fromString.at(3),
-												  fromString.at(4),
-												  fromString.at(5));
+		double success = StelUtils::getJDFromDate(&backJd,fromString.at(0),fromString.at(1),fromString.at(2),
+					fromString.at(3), fromString.at(4), fromString.at(5));
 
 		if (jd == backJd && success)
 		{
@@ -92,13 +83,12 @@ void TestDates::dateRoundTrip()
 		}
 		else
 		{
-				 qWarning() << QString("reverse %1 => %2").arg(backJd).arg(jd);
+			qWarning() << QString("reverse %1 => %2").arg(backJd).arg(jd);
 		}
 	}
 	QVERIFY2(total==successForwardNum && total==successReverseNum,
-			 qPrintable(QString("%1/%2 jd=>string, %3/%4 string=>jd")
-						.arg(successForwardNum).arg(total)
-						.arg(successReverseNum).arg(total)));
+			 qPrintable(QString("%1/%2 jd=>string, %3/%4 string=>jd").arg(successForwardNum).arg(total)
+			.arg(successReverseNum).arg(total)));
 }
 
 
@@ -132,13 +122,12 @@ void TestDates::formatting()
 			 qPrintable("formatter2 not working: " + StelUtils::localeDateString(2008, 3, 9, 6, hardLong)));
 
 	// test detection of offset from UTC.
-
 	double mar122008 = QDate(2008,3,12).toJulianDay();
 	QVERIFY2(-4.0 == StelUtils::getGMTShiftFromQT(mar122008),
-			 qPrintable("gmt shift wrong: " + QString("%1").arg(StelUtils::getGMTShiftFromQT(mar122008))));
+		qPrintable("gmt shift wrong: " + QString("%1").arg(StelUtils::getGMTShiftFromQT(mar122008))));
 	double mar012008 = QDate(2008,3,1).toJulianDay();
 	QVERIFY2(-5.0 == StelUtils::getGMTShiftFromQT(mar012008),
-			 qPrintable("gmt shift wrong: " + QString("%1").arg(StelUtils::getGMTShiftFromQT(mar012008))));
+		qPrintable("gmt shift wrong: " + QString("%1").arg(StelUtils::getGMTShiftFromQT(mar012008))));
 
 }
 
@@ -197,10 +186,8 @@ void TestDates::qdateRoundTrip()
 		tst = tst.addDays(-1);
 
 		QDate comp = QDate::fromJulianDay(jd);
-
 		QVERIFY2(tst == comp, qPrintable("failed at jd " + QString("%1").arg(jd) + " " + tst.toString() + " vs " + comp.toString()));
 		if (tst!=comp)
 			break;
 	}
 }
-
