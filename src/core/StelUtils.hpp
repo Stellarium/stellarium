4c47a7d4e26c74d0a0ecb6bc51b20005a499d834
xalioth <>
2009-11-23 15:07:57 +0000

    Added J2000 conversion matrix. Added an assertion error when wrong polygons are passed.

--- src/core/StelUtils.hpp
+++ src/core/StelUtils.hpp
@@ -1,17 +1,17 @@
 /*
  * Stellarium
  * Copyright (C) 2002 Fabien Chereau
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -43,58 +43,58 @@ namespace StelUtils
 	//!	@param s second component
 	//! @return angle in radian
 	double hmsToRad(unsigned int h, unsigned int m, double s);
-			   
+
 	//! Convert an angle in +-dms format to radian.
 	//! @param d degree component
 	//! @param m arcmin component
 	//!	@param s arcsec component
 	//! @return angle in radian
 	double dmsToRad(int d, unsigned int m, double s);
-	
+
 	//! Convert an angle in radian to hms format.
 	//! @param rad input angle in radian
 	//! @param h hour component
 	//! @param m minute component
 	//!	@param s second component
 	void radToHms(double rad, unsigned int& h, unsigned int& m, double& s);
-	
+
 	//! Convert an angle in radian to +-dms format.
 	//! @param rad input angle in radian
 	//! @param sign true if positive, false otherwise
 	//! @param d degree component
 	//! @param m minute component
 	//!	@param s second component
-	void radToDms(double rad, bool& sign, unsigned int& d, unsigned int& m, double& s);	
+	void radToDms(double rad, bool& sign, unsigned int& d, unsigned int& m, double& s);
 
 	//! Convert an angle in radian to a hms formatted string.
 	//! If the second, minute part is == 0, it is not output
 	//! @param angle input angle in radian
 	QString radToHmsStrAdapt(double angle);
-	
+
 	//! Convert an angle in radian to a hms formatted string.
 	//! @param angle input angle in radian
 	//! @param decimal output decimal second value
 	QString radToHmsStr(double angle, bool decimal=false);
-	
+
 	//! Convert an angle in radian to a dms formatted string.
 	//! If the second, minute part is == 0, it is not output
 	//! @param angle input angle in radian
 	//! @param useD Define if letter "d" must be used instead of deg sign
 	QString radToDmsStrAdapt(double angle, bool useD=false);
-	
+
 	//! Convert an angle in radian to a dms formatted string.
 	//! @param angle input angle in radian
 	//! @param useD Define if letter "d" must be used instead of deg sign
 	//! @param decimal output decimal second value
 	QString radToDmsStr(double angle, bool decimal=false, bool useD=false);
-	
+
 	//! Obtains a Vec3f from a string.
 	//! @param s the string describing the Vector with the form "x,y,z"
 	//! @return The corresponding vector
 	//! @deprecated Use the >> operator from Vec3f class
 	Vec3f strToVec3f(const QStringList& s);
 	Vec3f strToVec3f(const QString& s);
-	
+
 	//! Converts a Vec3f to HTML color notation.
 	//! @param v The vector
 	//! @return The string in HTML color notation "#rrggbb".
@@ -103,19 +103,19 @@ namespace StelUtils
 	//! Converts a color in HTML notation to a Vec3f.
 	//! @param c The HTML spec color string
 	Vec3f htmlColorToVec3f(const QString& c);
-		
+
 	//! Convert from spherical coordinates to Rectangular direction.
 	//! @param lng longitude in radian
 	//! @param lat latitude in radian
 	//! @param v the resulting 3D unit vector
 	void spheToRect(double lng, double lat, Vec3d& v);
-	
+
 	//! Convert from spherical coordinates to Rectangular direction.
 	//! @param lng longitude in radian
 	//! @param lat latitude in radian
 	//! @param v the resulting 3D unti vector
 	void spheToRect(float lng, float lat, Vec3f& v);
-	
+
 	//! Convert from spherical coordinates to Rectangular direction.
 	//! @param lng double* to store longitude in radian
 	//! @param lat double* to store latitude in radian
@@ -130,7 +130,7 @@ namespace StelUtils
 
 	//! Convert a string longitude, latitude, RA or Declination angle
 	//! to radians.
-	//! @param str the angle in format something like these: 
+	//! @param str the angle in format something like these:
 	//! - +53d  51'21.6"
 	//! - +53d51'21.6"
 	//! - -1d  10'31.8"
@@ -143,24 +143,24 @@ namespace StelUtils
 	//! - 12h 14m 6s
 	//! The degree separator may be a degree symbol (\\xBA) in addition
 	//! to a 'd' or 'D'.
-	//! @return the angle in radians. 
+	//! @return the angle in radians.
 	//! Latitude: North are positive, South are negative.
 	//! Longitude: East is positive, West is negative.
-	//! Note: if there is a N, S, E or W suffix, any leading + or - 
+	//! Note: if there is a N, S, E or W suffix, any leading + or -
 	//! characters are ignored.
 	double getDecAngle(const QString& str);
-	
+
 	//! Check if a number is a power of 2.
 	bool isPowerOfTwo(int value);
-	
+
 	//! Return the first power of two bigger than the given value.
 	int getBiggerPowerOfTwo(int value);
-	
+
 	//! Return the inverse sinus hyperbolic of z.
 	double asinh(double z);
-	
+
 	///////////////////////////////////////////////////
-	// New Qt based General Calendar Functions. 
+	// New Qt based General Calendar Functions.
 	//! Make from julianDay a year, month, day for the Julian Date julianDay represents.
 	void getDateFromJulianDay(double julianDay, int *year, int *month, int *day);
 
@@ -185,7 +185,7 @@ namespace StelUtils
 	double getJDFromSystem(void);
 
 	//! Convert a time of day to the fraction of a Julian Day.
-	//! Note that a Julian Day starts at 12:00, not 0:00, and 
+	//! Note that a Julian Day starts at 12:00, not 0:00, and
 	//! so 12:00 == 0.0 and 0:00 == 0.5
 	double qTimeToJDFraction(const QTime& time);
 
@@ -209,11 +209,11 @@ namespace StelUtils
 
 	int numberOfDaysInMonthInYear(int month, int year);
 	bool changeDateTimeForRollover(int oy, int om, int od, int oh, int omin, int os,
-	                               int* ry, int* rm, int* rd, int* rh, int* rmin, int* rs);
+								   int* ry, int* rm, int* rd, int* rh, int* rmin, int* rs);
 
 	//! Output a QVariantMap to qDebug().  Formats like a tree where there are nested objects.
 	void debugQVariantMap(const QVariant& m, const QString& indent="", const QString& key="");
-	
+
 	//! Use RegExp for final to parse a QString to six ints, use in the event QDateTime cannot handle the date.
 	QList<int> getIntsFromISO8601String(const QString& iso8601Date);
 
@@ -231,6 +231,9 @@ namespace StelUtils
 			(1.f + x*(1.f+ x/2.f*(1.f+ x/3.f*(1.f+x/4.f*(1.f+x/5.f))))):
 				1.f / (1.f -x*(1.f -x/2.f*(1.f- x/3.f*(1.f-x/4.f*(1.f-x/5.f)))));
 	}
+
+	//! Convert a vector from J2000 to Galactic reference frame, using FITS convention.
+	static const Mat4d J2000toGalactic = Mat4d(-0.054875539726, 0.494109453312, -0.867666135858, 0, -0.873437108010, -0.444829589425, -0.198076386122, 0, -0.483834985808, 0.746982251810, 0.455983795705, 0, 0, 0, 0, 1);
 }
 
 #endif // _STELUTILS_HPP_
