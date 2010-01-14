3f293d8d3a5ed0fefb406eacc192df34d91458d8
xalioth <>
2010-01-14 13:09:33 +0000

    Prepare for move to new plugin build setup.

---
+++ plugins/Satellites/src/Satellites.hpp
@@ -0,0 +1,258 @@
+/*
+ * Copyright (C) 2009 Matthew Gates
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _SATELLITES_HPP_
+#define _SATELLITES_HPP_ 1
+
+#include "StelObjectModule.hpp"
+#include "Satellite.hpp"
+#include "StelFader.hpp"
+#include "StelGui.hpp"
+#include "StelDialog.hpp"
+#include "StelLocation.hpp"
+
+#include <QDateTime>
+#include <QSharedPointer>
+
+class StelButton;
+class Planet;
+class QNetworkAccessManager;
+class QNetworkReply;
+class QPixmap;
+class QProgressBar;
+class QSettings;
+class QTimer;
+class SatellitesDialog;
+
+typedef QSharedPointer<Satellite> SatelliteP;
+
+//! @class Satellites
+//! Satellites in low Earth orbith require different orbital calculations from planets, the moon
+//! and so on.  This plugin implements the SGP4/SDP4 algorithms in Stellarium, allowing accurate
+//! prediction of the position of artificial satellites.
+class Satellites : public StelObjectModule
+{
+	Q_OBJECT
+public:
+	//! @enum UpdateState
+	//! Used for keeping track of the download/update status
+	enum UpdateState
+	{
+		Updating,             //!< Update in progress
+		CompleteNoUpdates,    //!< Update completed, there we no updates
+		CompleteUpdates,      //!< Update completed, there were updates
+		DownloadError,        //!< Error during download phase
+		OtherError            //!< Other error
+	};
+
+	enum Visibility
+	{
+		Visible,
+		NotVisible,
+		Both
+	};
+
+	Satellites();
+	virtual ~Satellites();
+
+	///////////////////////////////////////////////////////////////////////////
+	// Methods defined in the StelModule class
+	virtual void init();
+	virtual void deinit();
+	virtual void update(double deltaTime);
+	virtual void draw(StelCore* core);
+	virtual void drawPointer(StelCore* core, StelPainter& painter);
+	virtual double getCallOrder(StelModuleActionName actionName) const;
+
+	///////////////////////////////////////////////////////////////////////////
+	// Methods defined in StelObjectManager class
+	//! Used to get a list of objects which are near to some position.
+	//! @param v a vector representing the position in th sky around which to search for nebulae.
+	//! @param limitFov the field of view around the position v in which to search for satellites.
+	//! @param core the StelCore to use for computations.
+	//! @return an list containing the satellites located inside the limitFov circle around position v.
+	virtual QList<StelObjectP> searchAround(const Vec3d& v, double limitFov, const StelCore* core) const;
+
+	//! Return the matching satellite object's pointer if exists or NULL.
+	//! @param nameI18n The case in-sensistive satellite name
+	virtual StelObjectP searchByNameI18n(const QString& nameI18n) const;
+
+	//! Return the matching satellite if exists or NULL.
+	//! @param name The case in-sensistive standard program name
+	virtual StelObjectP searchByName(const QString& name) const;
+
+	//! Find and return the list of at most maxNbItem objects auto-completing the passed object I18n name.
+	//! @param objPrefix the case insensitive first letters of the searched object
+	//! @param maxNbItem the maximum number of returned object names
+	//! @return a list of matching object name by order of relevance, or an empty list if nothing match
+	virtual QStringList listMatchingObjectsI18n(const QString& objPrefix, int maxNbItem=5) const;
+
+	//! Implment this to tell the main Stellarium GUi that there is a GUI element to configure this
+	//! plugin. 
+	virtual bool configureGui(bool show=true);
+
+	//! Set up the plugin with default values.  This means clearing out the Satellites section in the
+	//! main config.ini (if one already exists), and populating it with default values.  It also 
+	//! creates the default satellites.json file from the resource embedded in the plugin lib/dll file.
+	void restoreDefaults(void);
+
+	//! Read (or re-read) settings from the main config file.  This will be called from init and also
+	//! when restoring defaults (i.e. from the configuration dialog / restore defaults button).
+	void readSettingsFromConfig(void);
+
+	//! Get a list of satellite group names.  A Satellite may be long to one or more group
+	//! e.g. "amateur" and "navigation".  Group names are arbitrary strings defined in the 
+	//! json file.  Think of them like tags.  A satellite may not belong to any group at all.
+	QStringList getGroups(void) const;
+
+	//! get satellite objects filtered by group.  If an empty string is used for the
+	//! group name, return all satallites
+	QStringList getSatellites(const QString& group=QString(), Visibility vis=Both);
+
+	//! get a satellite object by identifier
+	SatelliteP getByID(const QString& id);
+
+	//! get whether or not the plugin will try to update TLE data from the internet
+	//! @return true if updates are set to be done, false otherwise
+	bool getUpdatesEnabled(void) {return updatesEnabled;}
+	//! set whether or not the plugin will try to update TLE data from the internet
+	//! @param b if true, updates will be enabled, else they will be disabled
+	void setUpdatesEnabled(bool b) {updatesEnabled=b;}
+
+	//! get the date and time the TLE elements were updated
+	QDateTime getLastUpdate(void) {return lastUpdate;}
+
+	//! get the update frequency in hours
+	int getUpdateFrequencyHours(void) {return updateFrequencyHours;}
+	void setUpdateFrequencyHours(int hours) {updateFrequencyHours = hours;}
+
+	//! get the number of seconds till the next update
+	int getSecondsToUpdate(void);
+
+	//! get the update frequency in hours
+	//void setUpdateFrequencyHours(int hours);
+
+	//! Get the current updateState
+	UpdateState getUpdateState(void) {return updateState;}
+
+	//! Get a list of URLs which are sources of TLE data.
+	QStringList getTleSources(void) {return updateUrls;}
+
+	//! Set the list of URLs which are sources of TLE data.
+	void setTleSources(QStringList tleSources);
+
+signals:
+	//! emitted when the update status changes, e.g. when 
+	//! an update starts, completes and so on.  Note that
+	//! on completion of an update, TleUpdateComplete is also
+	//! emitted with the number of updates done.
+	//! @param state the new update state.
+	void updateStateChanged(Satellites::UpdateState state);
+
+	//! emitted after a TLE update has run.
+	//! @param updates the number of satellites updated.
+	void TleUpdateComplete(int updates);
+	
+
+public slots:
+	void setFlagHints(bool b) {hintFader=b;}
+	bool getFlagHints(void) {return hintFader;}
+
+	//! get the label font size
+	//! @return the pixel size of the font
+	int getLabelFontSize(void) {return labelFont.pixelSize();}
+	//! set the label font size
+	//! @param size the pixel size of the font
+	void setLabelFontSize(int size) {labelFont.setPixelSize(size);}
+
+	bool getFlagLabels(void);
+	void setFlagLabels(bool b);
+
+	//! Download TLEs from web recources described in the module section of the
+	//! module.ini file and update the TLE values for any satellites for which
+	//! there is new TLE data.
+	void updateTLEs(void);
+
+private:
+	// if existing, delete Satellites section in main config.ini, then create with default values
+	void restoreDefaultConfigIni(void);
+
+	// replace the json file with the default from the compiled-in resource
+	void restoreDefaultJsonFile(void);
+
+	// read the json file and create the satellites.  Removes existing satellites first if there are any
+	// this will be done once at init, and also if the defaults are reset.
+	// @return the number of satellites read from the json file
+	int readJsonFile(void);
+
+	QString satellitesJsonPath;
+	QList<SatelliteP> satellites;
+	LinearFader hintFader;
+	StelTextureSP texPointer;
+	QPixmap* pxmapGlow;
+	QPixmap* pxmapOnIcon;
+	QPixmap* pxmapOffIcon;
+	StelButton* toolbarButton;
+	QSharedPointer<Planet> earth;
+	Vec3f defaultHintColor;
+	QFont labelFont;
+	
+	// variables and functions for the updater
+	UpdateState updateState;
+	QNetworkAccessManager* downloadMgr;
+	QStringList updateUrls;
+	QStringList updateFiles;
+	QProgressBar* progressBar;
+	int currentUpdateUrlIdx;
+	int numberDownloadsComplete;
+	QTimer* updateTimer;
+	bool updatesEnabled;
+	QDateTime lastUpdate;
+	int updateFrequencyHours;
+	void updateFromFiles(void);
+
+	// GUI
+	SatellitesDialog* configDialog;
+
+private slots:
+	//! check to see if an update is required.  This is called periodically by a timer
+	//! if the last update was longer than updateFrequencyHours ago then the update is
+	//! done.
+	void checkForUpdate(void);
+	void updateDownloadComplete(QNetworkReply* reply);
+	void observerLocationChanged(StelLocation loc);
+
+};
+
+
+#include "fixx11h.h"
+#include <QObject>
+#include "StelPluginInterface.hpp"
+
+//! This class is used by Qt to manage a plug-in interface
+class SatellitesStelPluginInterface : public QObject, public StelPluginInterface
+{
+	Q_OBJECT
+	Q_INTERFACES(StelPluginInterface)
+public:
+	virtual StelModule* getStelModule() const;
+	virtual StelPluginInfo getPluginInfo() const;
+};
+
+#endif /*_SATELLITES_HPP_*/
+
---
+++ plugins/Satellites/src/gui/SatellitesDialog.cpp
@@ -0,0 +1,365 @@
+/*
+ * Stellarium Satellites plugin config dialog
+ * 
+ * Copyright (C) 2009 Matthew Gates
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <QDebug>
+#include <QTimer>
+#include <QDateTime>
+#include <QUrl>
+
+#include "StelApp.hpp"
+#include <plugin_config.h>
+#include "ui_satellitesDialog.h"
+#include "SatellitesDialog.hpp"
+#include "Satellites.hpp"
+#include "StelModuleMgr.hpp"
+#include "StelObjectMgr.hpp"
+#include "StelMovementMgr.hpp"
+#include "StelStyle.hpp"
+
+// When i18n is implemented, uncomment the StelTranslator.hpp include
+// and remove the definition of q_
+//#include "StelTranslator.hpp"
+#define q_ QString
+
+SatellitesDialog::SatellitesDialog()
+{
+	ui = new Ui_satellitesDialog;
+}
+
+SatellitesDialog::~SatellitesDialog()
+{	
+	updateTimer->stop(); 
+	delete updateTimer; 
+	updateTimer = NULL;
+	delete ui;
+}
+
+void SatellitesDialog::languageChanged()
+{
+	if (dialog)
+		ui->retranslateUi(dialog);
+}
+
+// Initialize the dialog widgets and connect the signals/slots
+void SatellitesDialog::createDialogContent()
+{
+	ui->setupUi(dialog);
+	ui->tabs->setCurrentIndex(0);
+
+	// Settings tab / updates group
+	connect(ui->updateFromInternetCheckbox, SIGNAL(toggled(bool)), this, SLOT(setUpdatesEnabled(bool)));
+	refreshUpdateValues(); // fetch values for last updated and so on
+	connect(ui->updateNowButton, SIGNAL(clicked()), GETSTELMODULE(Satellites), SLOT(updateTLEs()));
+	connect(GETSTELMODULE(Satellites), SIGNAL(updateStateChanged(Satellites::UpdateState)), this, SLOT(updateStateReceiver(Satellites::UpdateState)));
+	connect(GETSTELMODULE(Satellites), SIGNAL(TleUpdateComplete(int)), this, SLOT(updateCompleteReceiver(int)));
+	connect(ui->updateFrequencySpinBox, SIGNAL(valueChanged(int)), this, SLOT(setUpdateValues(int)));
+
+	updateTimer = new QTimer(this);
+	connect(updateTimer, SIGNAL(timeout()), this, SLOT(refreshUpdateValues()));
+        updateTimer->start(7000);
+
+	// Settings tab / General settings group
+	connect(ui->showLabelsCheckbox, SIGNAL(toggled(bool)), StelApp::getInstance().getGui()->getGuiActions("actionShow_Satellite_Labels"), SLOT(setChecked(bool))); 
+	connect(ui->fontSizeSpinBox, SIGNAL(valueChanged(int)), GETSTELMODULE(Satellites), SLOT(setLabelFontSize(int)));
+	connect(ui->restoreDefaultsButton, SIGNAL(clicked()), this, SLOT(restoreDefaults()));
+	connect(ui->saveSettingsButton, SIGNAL(clicked()), this, SLOT(saveSettings()));
+
+	// Satellites tab
+	connect(ui->closeStelWindow, SIGNAL(clicked()), this, SLOT(close()));
+	connect(ui->satellitesList, SIGNAL(currentTextChanged(const QString&)), this, SLOT(selectedSatelliteChanged(const QString&)));
+	connect(ui->satellitesList, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(satelliteDoubleClick(QListWidgetItem*)));
+	connect(ui->groupsCombo, SIGNAL(currentIndexChanged(int)), this, SLOT(groupFilterChanged(int)));
+	connect(ui->showButton, SIGNAL(clicked()), this, SLOT(showSelectedSatellites()));
+	connect(ui->hideButton, SIGNAL(clicked()), this, SLOT(hideSelectedSatellites()));
+	connect(ui->visibleCheckbox, SIGNAL(stateChanged(int)), this, SLOT(visibleCheckChanged(int)));
+
+	// Sources tab
+	connect(ui->sourceList, SIGNAL(currentTextChanged(const QString&)), ui->sourceEdit, SLOT(setText(const QString&)));
+	connect(ui->sourceEdit, SIGNAL(editingFinished()), this, SLOT(sourceEditingDone()));
+	connect(ui->deleteSourceButton, SIGNAL(clicked()), this, SLOT(deleteSourceRow()));
+	connect(ui->addSourceButton, SIGNAL(clicked()), this, SLOT(addSourceRow()));
+
+	// About tab
+	setAboutHtml();
+	ui->aboutTextBrowser->document()->setDefaultStyleSheet(QString(StelApp::getInstance().getCurrentStelStyle()->htmlStyleSheet));
+
+	updateGuiFromSettings();
+
+}
+
+void SatellitesDialog::groupFilterChanged(int index)
+{
+	QString prevSelection;
+	if (ui->satellitesList->currentItem())
+		prevSelection = ui->satellitesList->currentItem()->text();
+
+	ui->satellitesList->clear();
+	if (ui->groupsCombo->itemData(index).toString() == "all")
+		ui->satellitesList->insertItems(0,GETSTELMODULE(Satellites)->getSatellites());
+	else if (ui->groupsCombo->itemData(index).toString() == "visible")
+		ui->satellitesList->insertItems(0,GETSTELMODULE(Satellites)->getSatellites(QString(), Satellites::Visible));
+	else if (ui->groupsCombo->itemData(index).toString() == "notvisible")
+		ui->satellitesList->insertItems(0,GETSTELMODULE(Satellites)->getSatellites(QString(), Satellites::NotVisible));
+	else
+		ui->satellitesList->insertItems(0,GETSTELMODULE(Satellites)->getSatellites(ui->groupsCombo->currentText()));
+
+	// If the previously selected item is still in the list after the update, select it, 
+	// else selected the first item in the list.
+	QList<QListWidgetItem*> foundItems = ui->satellitesList->findItems(prevSelection, Qt::MatchExactly);
+	if (foundItems.count() > 0 && !prevSelection.isEmpty())
+	{
+		foundItems.at(0)->setSelected(true);
+		ui->satellitesList->scrollToItem(foundItems.at(0));
+	}
+	else if (ui->satellitesList->count() > 0)
+	{
+		ui->satellitesList->setCurrentRow(0);
+		ui->satellitesList->scrollToTop();
+	}
+
+}
+
+void SatellitesDialog::selectedSatelliteChanged(const QString& id)
+{
+	qDebug() << "SatellitesDialog::selectedSatelliteChanged for " << id;
+	if (id.isEmpty() || id=="")
+		return;
+
+	satelliteModified = false;
+
+	SatelliteP sat = GETSTELMODULE(Satellites)->getByID(id);
+	if (!sat->initialized)
+		return;
+
+	ui->idLineEdit->setText(sat->designation);
+	ui->descriptionTextEdit->setText(sat->description);
+	ui->groupsTextEdit->setText(sat->groupIDs.join(", "));
+	ui->tleTextEdit->setText(QString(sat->elements[1]) + "\n" + QString(sat->elements[2]));
+	ui->visibleCheckbox->setChecked(sat->visible);
+	ui->commsButton->setEnabled(sat->comms.count()>0);
+}
+
+void SatellitesDialog::saveSatellite(const QString& id)
+{
+	qDebug() << "Saving satellite" << id;
+}
+
+void SatellitesDialog::setAboutHtml(void)
+{
+	QString html = "<html><head></head><body>";
+	html += "<h2>" + q_("Stellarium Satellites Plugin") + "</h2><table width=\"90%\">";
+	html += "<tr width=\"30%\"><td>" + q_("Version:") + "</td><td>" + PLUGIN_VERSION + "</td></td>";
+	html += "<tr><td>" + q_("Author:") + "</td><td>Matthew Gates &lt;matthew@porpoisehead.net&gt;</td></td>";
+	html += "<tr><td>" + q_("Website:") + "</td><td><a href=\"http://stellarium.org/\">stellarium.org</a></td></td></table>";
+	html += "<p>";
+	html += q_("This is the Satellites plugin for Stellarium. ");
+	html += q_("Please leave feedback in the Stellarium forums, post bugs to the bug tracker and consider making a donation to Stellarium if you find this plugin to be useful or interesting.");
+	html += q_("");
+	html += "</body></html>";
+	ui->aboutTextBrowser->setHtml(html);
+}
+
+void SatellitesDialog::refreshUpdateValues(void)
+{
+	ui->lastUpdateDateTimeEdit->setDateTime(GETSTELMODULE(Satellites)->getLastUpdate());
+	ui->updateFrequencySpinBox->setValue(GETSTELMODULE(Satellites)->getUpdateFrequencyHours());
+	int secondsToUpdate = GETSTELMODULE(Satellites)->getSecondsToUpdate();
+	if (!GETSTELMODULE(Satellites)->getUpdatesEnabled())
+		ui->nextUpdateLabel->setText(q_("Updates disabled"));
+	else if (GETSTELMODULE(Satellites)->getUpdateState() == Satellites::Updating)
+		ui->nextUpdateLabel->setText(q_("Updating now..."));
+	else if (secondsToUpdate <= 60)
+		ui->nextUpdateLabel->setText(q_("Next update: < 1 minute"));
+	else if (secondsToUpdate < 3600)
+		ui->nextUpdateLabel->setText(QString(q_("Next update: %1 minutes")).arg((secondsToUpdate/60)+1));
+	else
+		ui->nextUpdateLabel->setText(QString(q_("Next update: %1 hours")).arg((secondsToUpdate/3600)+1));
+}
+
+void SatellitesDialog::setUpdateValues(int hours)
+{
+	GETSTELMODULE(Satellites)->setUpdateFrequencyHours(hours);
+	refreshUpdateValues();
+}
+
+void SatellitesDialog::setUpdatesEnabled(bool b)
+{
+	GETSTELMODULE(Satellites)->setUpdatesEnabled(b);
+	ui->updateFrequencySpinBox->setEnabled(b);
+	ui->updateNowButton->setEnabled(b);
+}
+
+void SatellitesDialog::updateStateReceiver(Satellites::UpdateState state)
+{
+	qDebug() << "SatellitesDialog::updateStateReceiver got a signal";
+	if (state==Satellites::Updating)
+		ui->nextUpdateLabel->setText(q_("Updating now..."));
+	else if (state==Satellites::DownloadError || state==Satellites::OtherError)
+	{
+		ui->nextUpdateLabel->setText(q_("Update error"));
+		updateTimer->start();  // make sure message is displayed for a while...
+	}
+}
+
+void SatellitesDialog::updateCompleteReceiver(int numUpdated)
+{
+	ui->nextUpdateLabel->setText(QString(q_("Updated %1 satellite(s)")).arg(numUpdated));
+	// display the status for another full interval before refreshing status
+	updateTimer->start();
+	ui->lastUpdateDateTimeEdit->setDateTime(GETSTELMODULE(Satellites)->getLastUpdate());
+	QTimer *timer = new QTimer(this);
+	connect(timer, SIGNAL(timeout()), this, SLOT(refreshUpdateValues()));
+}
+
+void SatellitesDialog::close(void)
+{
+	qDebug() << "Closing Satellites Configure Dialog";
+	StelGui* gui = dynamic_cast<StelGui*>(StelApp::getInstance().getGui());
+	gui->getGuiActions("actionShow_Satellite_ConfigDialog")->setChecked(false);
+}
+
+void SatellitesDialog::sourceEditingDone(void)
+{
+	// don't update the currently selected item in the source list if the text is empty or not a valid URL.
+	QString u = ui->sourceEdit->text();
+	if (u.isEmpty() || u=="")
+	{
+		qDebug() << "SatellitesDialog::sourceEditingDone empty string - not saving";
+		return;
+	}
+
+	if (!QUrl(u).isValid() || !u.contains("://"))
+	{
+		qDebug() << "SatellitesDialog::sourceEditingDone invalid URL - not saving : " << u;
+		return;
+	}
+
+	if (ui->sourceList->currentItem()!=NULL)
+		ui->sourceList->currentItem()->setText(u);
+	else if (ui->sourceList->findItems(u, Qt::MatchExactly).count() <= 0)
+	{
+		QListWidgetItem* i = new QListWidgetItem(u, ui->sourceList);
+		i->setSelected(true);
+	}
+
+	saveSourceList();
+}
+
+void SatellitesDialog::saveSourceList(void)
+{
+	QStringList allSources;
+	for(int i=0; i<ui->sourceList->count(); i++)
+	{
+		allSources << ui->sourceList->item(i)->text();
+	}
+	GETSTELMODULE(Satellites)->setTleSources(allSources);
+}
+
+void SatellitesDialog::deleteSourceRow(void)
+{
+	ui->sourceEdit->clear();
+	if (ui->sourceList->currentItem())
+		delete ui->sourceList->currentItem();
+
+	saveSourceList();
+}
+
+void SatellitesDialog::addSourceRow(void)
+{
+	ui->sourceList->setCurrentItem(NULL);
+	ui->sourceEdit->setText(q_("[new source]"));
+	ui->sourceEdit->selectAll();
+	ui->sourceEdit->setFocus();
+}
+
+void SatellitesDialog::restoreDefaults(void)
+{
+	qDebug() << "Satellites::restoreDefaults";
+	GETSTELMODULE(Satellites)->restoreDefaults();
+	GETSTELMODULE(Satellites)->readSettingsFromConfig();
+	updateGuiFromSettings();
+}
+
+void SatellitesDialog::updateGuiFromSettings(void)
+{
+	ui->updateFromInternetCheckbox->setChecked(GETSTELMODULE(Satellites)->getUpdatesEnabled());
+	refreshUpdateValues();
+
+	ui->showLabelsCheckbox->setChecked(GETSTELMODULE(Satellites)->getFlagLabels());
+	ui->fontSizeSpinBox->setValue(GETSTELMODULE(Satellites)->getLabelFontSize());
+
+	ui->groupsCombo->clear();
+	ui->groupsCombo->addItems(GETSTELMODULE(Satellites)->getGroups());
+	ui->groupsCombo->insertItem(0, q_("[all satellites]"), QVariant("all"));
+	ui->groupsCombo->insertItem(0, q_("[all not visible]"), QVariant("notvisible"));
+	ui->groupsCombo->insertItem(0, q_("[all visible]"), QVariant("visible"));
+	ui->groupsCombo->setCurrentIndex(0);
+
+	ui->sourceList->clear();
+	ui->sourceList->addItems(GETSTELMODULE(Satellites)->getTleSources());
+	if (ui->sourceList->count() > 0) ui->sourceList->setCurrentRow(0);
+}
+
+void SatellitesDialog::saveSettings(void)
+{
+	qDebug() << "Satellites::saveSettings not implemented yet!";
+}
+
+void SatellitesDialog::showSelectedSatellites(void)
+{
+	foreach (QListWidgetItem* i, ui->satellitesList->selectedItems())
+	{
+		SatelliteP sat = GETSTELMODULE(Satellites)->getByID(i->text());
+		sat->visible = true;
+	}
+	groupFilterChanged(ui->groupsCombo->currentIndex());
+}
+
+void SatellitesDialog::hideSelectedSatellites(void)
+{
+	foreach (QListWidgetItem* i, ui->satellitesList->selectedItems())
+	{
+		SatelliteP sat = GETSTELMODULE(Satellites)->getByID(i->text());
+		sat->visible = false;
+	}
+	groupFilterChanged(ui->groupsCombo->currentIndex());
+}
+
+void SatellitesDialog::visibleCheckChanged(int state)
+{
+	foreach (QListWidgetItem* i, ui->satellitesList->selectedItems())
+	{
+		SatelliteP sat = GETSTELMODULE(Satellites)->getByID(i->text());
+		sat->visible = (state==Qt::Checked);
+	}
+	groupFilterChanged(ui->groupsCombo->currentIndex());
+}
+
+void SatellitesDialog::satelliteDoubleClick(QListWidgetItem* item)
+{
+	qDebug() << "SatellitesDialog::satelliteDoubleClick for " << item->text();
+	GETSTELMODULE(Satellites)->getByID(item->text())->visible = true;
+	if (StelApp::getInstance().getStelObjectMgr().findAndSelect(item->text()))
+	{
+		GETSTELMODULE(StelMovementMgr)->autoZoomIn();
+		GETSTELMODULE(StelMovementMgr)->setFlagTracking(true);
+	}
+}
+
