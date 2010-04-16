dd618867e82f7a5d7e1b45f49b73ae0bfcd22feb
Bogdan Marinov <bogdan.marinov84@gmail.com>
2010-04-16 16:21:55 +0000

    TelescopeControl: fixing the licensing mis-match with the telescope servers

--- plugins/TelescopeControl/src/servers/Socket.cpp
+++ plugins/TelescopeControl/src/servers/Socket.cpp
@@ -8,18 +8,18 @@ Author and Copyright of this file and of the stellarium telescope library:
 Johannes Gajdosik, 2006
 
 This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+You should have received a copy of the GNU General Public License
+along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #include "Socket.hpp"
