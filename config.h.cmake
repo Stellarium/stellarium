fbb7914cfe26f5bfd0b93537c78968f497817b2c
Fabien Chéreau <fabien.chereau@gmail.com>
2010-04-28 16:27:36 +0000

    Removed unnecessary cmake stuff.

--- config.h.cmake
+++ config.h.cmake
@@ -1,10 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#cmakedefine HAVE_TIMEGM
-#cmakedefine HAVE_MKTIME
 #cmakedefine HAVE_POW10
-#cmakedefine HAVE_BYTESWAP_H
 
 #cmakedefine INSTALL_DATADIR "${INSTALL_DATADIR}"
 #cmakedefine INSTALL_LOCALEDIR "${INSTALL_LOCALEDIR}"
