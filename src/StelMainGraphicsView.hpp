0217850eb144a9727ef6d63a216293446b90ecd2
Fabien Chereau <fabien.chereau@gmail.com>
2010-05-18 12:23:22 +0000

    Fix other potential plugin crashes when quitting.

--- src/StelMainGraphicsView.hpp
+++ src/StelMainGraphicsView.hpp
@@ -112,7 +112,6 @@ protected:
 	virtual void keyPressEvent(QKeyEvent* event);
 	virtual void keyReleaseEvent(QKeyEvent* event);
 	virtual void wheelEvent(QWheelEvent* wheelEvent);
-	virtual void focusOutEvent(QFocusEvent* event);
 
 	//! Update the mouse pointer state and schedule next redraw.
 	//! This method is called automatically by Qt.
