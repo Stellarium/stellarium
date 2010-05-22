9ed6ecbd7a9d54ed0a2aad001fea56e1d6bd10e5
treaves <>
2010-05-22 15:44:41 +0000

    Oculars: changed the drawing of the Telrad finder from StelPainter to direct GL, as I can not seem to modify line width.

--- plugins/Oculars/src/Oculars.cpp
+++ plugins/Oculars/src/Oculars.cpp
@@ -518,16 +518,36 @@ void Oculars::drawTelrad()
 	if (!flagShowOculars) {
 		const StelProjectorP projector = StelApp::getInstance().getCore()->getProjection(StelCore::FrameEquinoxEqu);
 
-		StelPainter painter(projector);
-		painter.setColor(0.77, 0.14, 0.16, 0.0);
 
 		StelCore *core = StelApp::getInstance().getCore();
 		StelProjector::StelProjectorParams params = core->getCurrentStelProjectorParams();
-		Vec2i centerScreen(projector->getViewportPosX()+projector->getViewportWidth()/2,
-						   projector->getViewportPosY()+projector->getViewportHeight()/2);
-		painter.drawCircle(centerScreen[0], centerScreen[1], 0.5 * projector->getPixelPerRadAtCenter() * (M_PI/180) * (0.5));
-		painter.drawCircle(centerScreen[0], centerScreen[1], 0.5 * projector->getPixelPerRadAtCenter() * (M_PI/180) * (2.0));
-		painter.drawCircle(centerScreen[0], centerScreen[1], 0.5 * projector->getPixelPerRadAtCenter() * (M_PI/180) * (4.0));
+
+		// StelPainter drawing
+//		StelPainter painter(projector);
+//		painter.setColor(0.77, 0.14, 0.16, 0.0);
+//		Vec2i centerScreen(projector->getViewportPosX()+projector->getViewportWidth()/2,
+//						   projector->getViewportPosY()+projector->getViewportHeight()/2);
+//		painter.drawCircle(centerScreen[0], centerScreen[1], 0.5 * projector->getPixelPerRadAtCenter() * (M_PI/180) * (0.5));
+//		painter.drawCircle(centerScreen[0], centerScreen[1], 0.5 * projector->getPixelPerRadAtCenter() * (M_PI/180) * (2.0));
+//		painter.drawCircle(centerScreen[0], centerScreen[1], 0.5 * projector->getPixelPerRadAtCenter() * (M_PI/180) * (4.0));
+
+		// Direct drawing
+		glDisable(GL_BLEND);
+		glColor3f(0.f,0.f,0.f);
+		glPushMatrix();
+		glTranslated(params.viewportCenter[0], params.viewportCenter[1], 0.0);
+		GLUquadricObj *quadric = gluNewQuadric();
+		
+		// the gray circle
+		glColor4f(0.77, 0.14, 0.16, 0.5);
+		float radius = 0.5 * projector->getPixelPerRadAtCenter() * (M_PI/180) * (0.5);
+		gluDisk(quadric, radius - 1.0, radius, 256, 1);
+		radius = 0.5 * projector->getPixelPerRadAtCenter() * (M_PI/180) * (2.0);
+		gluDisk(quadric, radius - 1.0, radius, 256, 1);
+		radius = 0.5 * projector->getPixelPerRadAtCenter() * (M_PI/180) * (4.0);
+		gluDisk(quadric, radius - 1.0, radius, 256, 1);
+		gluDeleteQuadric(quadric);
+		glPopMatrix();		
 	}
 }
 
@@ -822,7 +842,6 @@ void Oculars::paintMask()
 	}
 	glPopMatrix();
 }
-
 
 void Oculars::paintText(const StelCore* core)
 {
