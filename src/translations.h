0c35043d6b662cc0532f2b8c8510232f0ca820f7
Alexander Wolf <alex.v.wolf@gmail.com>
2010-11-10 21:24:32 +0600

    Drop support l10n for Uranus moon

--- src/translations.h
+++ src/translations.h
@@ -55,11 +55,6 @@ class Translations
 		N_("Phoebe");
 		N_("Neptune");
 		N_("Uranus");
-		N_("Titania");
-                N_("Oberon");
-                N_("Miranda");
-                N_("Ariel");
-                N_("Umbriel");
 		N_("Pluto");
 		N_("Charon");
 		N_("Solar System Observer");
