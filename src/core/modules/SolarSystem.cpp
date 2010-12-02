cf8706917378c3ad60e57767ac8b0d868989cf3e
Georg Zotti <Georg.Zotti@univie.ac.at>
2010-12-02 19:26:22 +0100

    refraction into drawPointer

--- src/core/modules/SolarSystem.cpp
+++ src/core/modules/SolarSystem.cpp
@@ -41,6 +41,7 @@
 #include "StelUtils.hpp"
 #include "StelPainter.hpp"
 #include "TrailGroup.hpp"
+#include "RefractionExtinction.hpp"
 
 #include <functional>
 #include <algorithm>
@@ -154,6 +155,29 @@ void SolarSystem::drawPointer(const StelCore* core)
 	{
 		const StelObjectP obj = newSelected[0];
 		Vec3d pos=obj->getJ2000EquatorialPos(nav);
+
+		// GZ: add refraction
+		const StelSkyDrawer *drawer=core->getSkyDrawer();
+		bool withAtmosphericEffects=drawer->getFlagHasAtmosphere();
+		if (withAtmosphericEffects)
+		  {
+		    const RefractionExtinction *refExt=drawer->getRefractionExtinction();
+		    // (2) compute alt-az coordinates from pos
+		    Vec3d altaz=nav->j2000ToAltAz(Vec3d(pos[0], pos[1], pos[2]));
+		    // (2) Affect only if above -2 altitude.
+		    if (altaz[2]>=-0.035f) {
+		      float dummy_mag;
+		      // (3) compute refraction and extinction effects:
+		      refExt->forward(&altaz, &dummy_mag, 1);
+		      // (4) return to equatorial system, but refracted.
+		      Vec3d pos_refracted=nav->altAzToJ2000(altaz); 
+		      pos[0]=pos_refracted[0];
+		      pos[1]=pos_refracted[1];
+		      pos[2]=pos_refracted[2];
+		    }
+		  }
+		// GZ: done
+
 		Vec3d screenpos;
 		// Compute 2D pos and return if outside screen
 		if (!prj->project(pos, screenpos))
