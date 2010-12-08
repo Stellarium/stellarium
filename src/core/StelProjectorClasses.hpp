5e50d85648fb12cc28c07b259511c1ce3929aa15
Fabien Chéreau <fabien.chereau@gmail.com>
2010-12-08 17:18:30 +0100

    Integrates refraction directly into StelProjector. Splitted RefractionExtinction into 2 independent classes.

--- src/core/StelProjectorClasses.hpp
+++ src/core/StelProjectorClasses.hpp
@@ -26,7 +26,7 @@
 class StelProjectorPerspective : public StelProjector
 {
 public:
-	StelProjectorPerspective(const Mat4d& modelViewMat) : StelProjector(modelViewMat) {;}
+	StelProjectorPerspective(const Mat4d& modelViewMat, PreModelViewFunc* func=NULL) : StelProjector(modelViewMat, func) {;}
 	virtual QString getNameI18() const;
 	virtual QString getDescriptionI18() const;
 	virtual float getMaxFov() const {return 120.f;}
@@ -63,7 +63,7 @@ protected:
 class StelProjectorEqualArea : public StelProjector
 {
 public:
-	StelProjectorEqualArea(const Mat4d& modelViewMat) : StelProjector(modelViewMat) {;}
+	StelProjectorEqualArea(const Mat4d& modelViewMat, PreModelViewFunc* func=NULL) : StelProjector(modelViewMat, func) {;}
 	virtual QString getNameI18() const;
 	virtual QString getDescriptionI18() const;
 	virtual float getMaxFov() const {return 360.f;}
@@ -89,7 +89,7 @@ protected:
 class StelProjectorStereographic : public StelProjector
 {
 public:
-	StelProjectorStereographic(const Mat4d& modelViewMat) : StelProjector(modelViewMat) {;}
+	StelProjectorStereographic(const Mat4d& modelViewMat, PreModelViewFunc* func=NULL) : StelProjector(modelViewMat, func) {;}
 	virtual QString getNameI18() const;
 	virtual QString getDescriptionI18() const;
 	virtual float getMaxFov() const {return 235.f;}
@@ -116,6 +116,8 @@ public:
 		for (int i = 0; i < n; ++i, ++out)
 		{
 			v = in[i];
+			if (preModelViewFunc)
+				preModelViewFunc->forward(v);
 			v.transfo4d(modelViewMatrix);
 			out->set(v[0], v[1], v[2]);
 			StelProjectorStereographic::forward(*out);
@@ -138,7 +140,7 @@ protected:
 class StelProjectorFisheye : public StelProjector
 {
 public:
-	StelProjectorFisheye(const Mat4d& modelViewMat) : StelProjector(modelViewMat) {;}
+	StelProjectorFisheye(const Mat4d& modelViewMat, PreModelViewFunc* func=NULL) : StelProjector(modelViewMat, func) {;}
 	virtual QString getNameI18() const;
 	virtual QString getDescriptionI18() const;
 	virtual float getMaxFov() const {return 180.00001f;}
@@ -177,7 +179,7 @@ protected:
 class StelProjectorHammer : public StelProjector
 {
 public:
-	StelProjectorHammer(const Mat4d& modelViewMat) : StelProjector(modelViewMat) {;}
+	StelProjectorHammer(const Mat4d& modelViewMat, PreModelViewFunc* func=NULL) : StelProjector(modelViewMat, func) {;}
 	virtual QString getNameI18() const;
 	virtual QString getDescriptionI18() const;
 	virtual float getMaxFov() const {return 360.f;}
@@ -187,6 +189,8 @@ public:
 		for (int i = 0; i < n; ++i)
 		{
 			v = in[i];
+			if (preModelViewFunc)
+				preModelViewFunc->forward(v);
 			v.transfo4d(modelViewMatrix);
 			out[i].set(v[0], v[1], v[2]);
 			StelProjectorHammer::forward(out[i]);
@@ -227,7 +231,7 @@ protected:
 class StelProjectorCylinder : public StelProjector
 {
 public:
-	StelProjectorCylinder(const Mat4d& modelViewMat) : StelProjector(modelViewMat) {;}
+	StelProjectorCylinder(const Mat4d& modelViewMat, PreModelViewFunc* func=NULL) : StelProjector(modelViewMat, func) {;}
 	virtual QString getNameI18() const;
 	virtual QString getDescriptionI18() const;
 	virtual float getMaxFov() const {return 175.f * 4.f/3.f;} // assume aspect ration of 4/3 for getting a full 360 degree horizon
@@ -255,7 +259,7 @@ protected:
 class StelProjectorMercator : public StelProjector
 {
 public:
-	StelProjectorMercator(const Mat4d& modelViewMat) : StelProjector(modelViewMat) {;}
+	StelProjectorMercator(const Mat4d& modelViewMat, PreModelViewFunc* func=NULL) : StelProjector(modelViewMat, func) {;}
 	virtual QString getNameI18() const;
 	virtual QString getDescriptionI18() const;
 	virtual float getMaxFov() const {return 175.f * 4.f/3.f;} // assume aspect ration of 4/3 for getting a full 360 degree horizon
@@ -283,7 +287,7 @@ protected:
 class StelProjectorOrthographic : public StelProjector
 {
 public:
-	StelProjectorOrthographic(const Mat4d& modelViewMat) : StelProjector(modelViewMat) {;}
+	StelProjectorOrthographic(const Mat4d& modelViewMat, PreModelViewFunc* func=NULL) : StelProjector(modelViewMat, func) {;}
 	virtual QString getNameI18() const;
 	virtual QString getDescriptionI18() const;
 	virtual float getMaxFov() const {return 179.9999f;}
