5ae3ad15f2a39e2bb5a1ed1c38b4bfe1af79e4ce
Simon Parzer <simon.parzer@gmail.com>
2011-01-26 23:44:12 +0100

    doxygen

--- plugins/Scenery3d/src/MTL.hpp
+++ plugins/Scenery3d/src/MTL.hpp
@@ -5,27 +5,44 @@
 #include <map>
 #include "StelTextureMgr.hpp"
 
+
+//! Class for loading and representing a .mtl file.
+//! MTL files are used with Wavefront .obj files for
+//! storing material information, such as textures and colors.
+//! For details about the file format, see http://en.wikipedia.org/wiki/Wavefront_.obj_file#Material_template_library
 class MTL
 {
     public:
+        //! Struct for holding a single RGB color.
         struct Color {
             Color() : r(1.0f), g(1.0f), b(1.0f) {}
             float r, g, b;
         };
 
+        //! Struct for holding a material definition.
         struct Material {
             Material() : color(), texture(), shininess(0) {}
-            Color color;
-            std::string texture;
-            int shininess;
+            Color color; //!< Material color.
+            std::string texture; //!< Filename of the texture file.
+            int shininess; //!< Material shininess factor.
         };
 
+        //! Initialize an empty MTL object.
+        //! Usually, you then load a file using the .load() method.
         MTL(void);
         virtual ~MTL(void);
 
+        //! Load an .mtl file into memory.
         void load(const char* filename);
+        //! Upload all textures to GPU memory using OpenGL.
         void uploadTexturesGL(void);
+        //! Returns a stored material definition.
+        //! @param matName Name of the material, as defined in the .mtl file.
+        //! @return Reference to the material definition.
         const Material& getMaterial(std::string matName);
+        //! Returns a shared pointer to a StelTexture reference.
+        //! Make sure to call uploadTexturesGL() before using this.
+        //! @param texName File name of the texture as defined in the .mtl file.
         StelTextureSP getTexture(std::string texName);
     private:
         std::string absolutePath(std::string path);
