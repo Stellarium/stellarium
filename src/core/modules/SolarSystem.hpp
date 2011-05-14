ece27e20e7bee646b62218314c815d7e37510029
Alexander Wolf <alex.v.wolf@gmail.com>
2011-05-14 14:56:06 +0700

    Downgrade SolarSystem.hpp

--- src/core/modules/SolarSystem.hpp
+++ src/core/modules/SolarSystem.hpp
@@ -199,10 +199,7 @@ public:
 
 	//! Get the list of all the planet english names
 	QStringList getAllPlanetEnglishNames() const;
-	
-	//! Get the list of all the planet localized names
-	QStringList getAllPlanetLocalizedNames() const;
-	
+
 	//! Reload the planets
 	void reloadPlanets();
 
