21169c2c51779a20b5721cba7dcd82d5fea0d5a0
Fabien Chéreau <fabien.chereau@gmail.com>
2011-06-08 17:08:06 +0200

    Cleaned up object info

--- src/core/StelObject.cpp
+++ src/core/StelObject.cpp
@@ -91,10 +91,10 @@ QString StelObject::getPositionInfoString(const StelCore *core, const InfoString
 		double dec_sideral, ra_sideral;
 		StelUtils::rectToSphe(&ra_sideral,&dec_sideral,getSideralPosGeometric(core));
 		ra_sideral = 2.*M_PI-ra_sideral;
-		res += q_("Hour angle/DE: %1/%2").arg(StelUtils::radToHmsStr(ra_sideral), StelUtils::radToDmsStr(dec_sideral)) + q_("(geom.)") + "<br>";
+		res += q_("Hour angle/DE: %1/%2").arg(StelUtils::radToHmsStr(ra_sideral), StelUtils::radToDmsStr(dec_sideral)) + " " + q_("(geometric)") + "<br>";
 		StelUtils::rectToSphe(&ra_sideral,&dec_sideral,getSideralPosApparent(core));
 		ra_sideral = 2.*M_PI-ra_sideral;
-		res += q_("Hour angle/DE: %1/%2").arg(StelUtils::radToHmsStr(ra_sideral), StelUtils::radToDmsStr(dec_sideral)) + q_("(app.)") + "<br>";
+		res += q_("Hour angle/DE: %1/%2").arg(StelUtils::radToHmsStr(ra_sideral), StelUtils::radToDmsStr(dec_sideral)) + " " + q_("(apparent)") + "<br>";
 	}
 
 	if (flags&AltAzi)
@@ -105,13 +105,13 @@ QString StelObject::getPositionInfoString(const StelCore *core, const InfoString
 		az = 3.*M_PI - az;  // N is zero, E is 90 degrees
 		if (az > M_PI*2)
 			az -= M_PI*2;
-		res += q_("Az/Alt: %1/%2").arg(StelUtils::radToDmsStr(az), StelUtils::radToDmsStr(alt)) + q_("(geom.)") + "<br>";
+		res += q_("Az/Alt: %1/%2").arg(StelUtils::radToDmsStr(az), StelUtils::radToDmsStr(alt)) + " " + q_("(geometric)") + "<br>";
 
 		StelUtils::rectToSphe(&az,&alt,getAltAzPosApparent(core));
 		az = 3.*M_PI - az;  // N is zero, E is 90 degrees
 		if (az > M_PI*2)
 			az -= M_PI*2;
-		res += q_("Az/Alt: %1/%2").arg(StelUtils::radToDmsStr(az), StelUtils::radToDmsStr(alt)) + q_("(app.)") + "<br>";
+		res += q_("Az/Alt: %1/%2").arg(StelUtils::radToDmsStr(az), StelUtils::radToDmsStr(alt)) + " " + q_("(apparent)") + "<br>";
 	}
 	return res;
 }
