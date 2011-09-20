0c07585ad17152053eca383cd158b28eff12f586
Eleni Maria Stea <elene.mst@gmail.com>
2011-09-20 16:02:49 +0300

    ambient and diffuse values are passed as uniforms to the shader so that the lighting is more consistent to the initial one

--- data/shaders/nmap.f.glsl
+++ data/shaders/nmap.f.glsl
@@ -4,6 +4,9 @@ varying vec3 var_vdir;
 uniform sampler2D tex;
 uniform sampler2D nmap;
 
+uniform vec4 ambient;
+uniform vec4 diffuse;
+
 void main()
 {
 	vec3 normal = texture2D(nmap, gl_TexCoord[0].xy).xyz * 2.0 - 1.0;
@@ -13,11 +16,11 @@ void main()
 	vec3 vdir = normalize(var_vdir);
 	vec3 rdir = normalize(-reflect(ldir, normal));
 
-	float diffuse = max(dot(ldir, normal), 0.0);
-	vec4 dcol = diffuse * texture2D(tex, gl_TexCoord[0].xy) * gl_FrontMaterial.diffuse;
+	float diffusel = max(dot(ldir, normal), 0.0);
+	vec4 dcol = diffusel * texture2D(tex, gl_TexCoord[0].xy) * diffuse;
 
 //	float specular = pow(max(dot(rdir, vdir), 0.0), gl_FrontMaterial.shininess);
 //	vec4 scol = specular * gl_FrontMaterial.specular;
 
-	gl_FragColor = gl_FrontMaterial.ambient * gl_LightModel.ambient + dcol;
+	gl_FragColor = ambient * gl_LightModel.ambient + dcol;
 }
