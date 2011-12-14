21039d3c4270b9355fb4d18c18062364f97ad012
Alexander Wolf <alex.v.wolf@gmail.com>
2011-12-14 20:51:22 +0700

    update convertor and catalog

--- plugins/Quasars/util/tsv2json.pl
+++ plugins/Quasars/util/tsv2json.pl
@@ -17,42 +17,44 @@ print JSON "\t\"shortName\": \"A catalogue of quasars\",\n";
 print JSON "\t\"quasars\":\n";
 print JSON "\t{\n";
 
-for ($i=0;$i<scalar(@catalog);$i++) {
-	($cRA,$cDE,$name,$RA,$DE,$z,$Vmag,$bV,$Amag) = split(";", $catalog[$i]);
+for ($i=15;$i<scalar(@catalog);$i++) {
+	if ($catalog[$i] !~ /#/) {
+		($name,$RA,$DE,$z,$Vmag,$bV,$Amag) = split(";", $catalog[$i]);
 
-	($hour,$min,$sec) = split(" ",$RA);
-	$outRA = $hour."h".$min."m".$sec."s";
+		($hour,$min,$sec) = split(" ",$RA);
+		$outRA = $hour."h".$min."m".$sec."s";
 
-	($deg,$min,$sec) = split(" ",$DE);
-	$outDE = $deg."d".$min."m".$sec."s";
+		($deg,$min,$sec) = split(" ",$DE);
+		$outDE = $deg."d".$min."m".$sec."s";
 
-	$name =~ s/(\s{2,})//gi;
-	$z =~ s/(\s+)//gi;
-	$bV =~ s/(\s+)//gi;
-	$Amag =~ s/(\s+)//gi;
-	$bV =~ s/(\s+)//gi;
+		$name =~ s/(\s{2,})//gi;
+		$z =~ s/(\s+)//gi;
+		$bV =~ s/(\s+)//gi;
+		$Amag =~ s/(\s+)//gi;
+		$bV =~ s/(\s+)//gi;
 
-	# exclude quasar M 31
-	if ($name ne 'M 31') {
-		$out  = "\t\t\"".$name."\":\n";
-		$out .= "\t\t{\n";
-		$out .= "\t\t\t\"RA\": \"".$outRA."\",\n";
-		$out .= "\t\t\t\"DE\": \"".$outDE."\",\n";
-		$out .= "\t\t\t\"z\": ".$z.",\n";
-		$out .= "\t\t\t\"Vmag\": ".$Vmag;
-		if ($Amag ne '') {
-			$out .= ",\n\t\t\t\"Amag\": ".$Amag;
+		# exclude quasar M 31
+		if ($name ne 'M 31') {
+			$out  = "\t\t\"".$name."\":\n";
+			$out .= "\t\t{\n";
+			$out .= "\t\t\t\"RA\": \"".$outRA."\",\n";
+			$out .= "\t\t\t\"DE\": \"".$outDE."\",\n";
+			$out .= "\t\t\t\"z\": ".$z.",\n";
+			$out .= "\t\t\t\"Vmag\": ".$Vmag;
+			if ($Amag ne '') {
+				$out .= ",\n\t\t\t\"Amag\": ".$Amag;
+			}
+			if ($bV ne '') {
+				$out .= ",\n\t\t\t\"bV\": ".$bV;
+			}
+			$out .= "\n\t\t}";
+
+			if ($i<scalar(@catalog)-1) {
+				$out .= ",";
+			}
+
+			print JSON $out."\n";
 		}
-		if ($bV ne '') {
-			$out .= ",\n\t\t\t\"bV\": ".$bV;
-		}
-		$out .= "\n\t\t}";
-
-		if ($i<scalar(@catalog)-1) {
-			$out .= ",";
-		}
-
-		print JSON $out."\n";
 	}
 }
 
