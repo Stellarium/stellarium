19a6e085e6a2b3b335669da4c48f6008bd30506c
Bogdan Marinov <bogdan.marinov84@gmail.com>
2012-01-08 00:59:46 +0200

    + last month's launches (tle-new.txt) to the default source list

--- plugins/Satellites/src/Satellites.cpp
+++ plugins/Satellites/src/Satellites.cpp
@@ -409,6 +409,8 @@ void Satellites::restoreDefaultConfigIni(void)
 	conf->setValue("tle_url5", "http://celestrak.com/NORAD/elements/amateur.txt");
 	conf->setValue("tle_url6", "http://celestrak.com/NORAD/elements/iridium.txt");
 	conf->setValue("tle_url7", "http://celestrak.com/NORAD/elements/geo.txt");
+	conf->setValue("tle_url8", "http://celestrak.com/NORAD/elements/tle-new.txt");
+	//TODO: Better? See http://doc.qt.nokia.com/4.7/qsettings.html#beginWriteArray --BM
 	conf->setValue("update_frequency_hours", 72);
 	conf->setValue("orbit_line_flag", true);
 	conf->setValue("orbit_line_segments", 90);
