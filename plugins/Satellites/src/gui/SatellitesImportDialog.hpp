b1d939cbbfb6c62b4ca77533a4c15c3c095f742e
daggerstab <bogdan.marinov84@gmail.com>
2012-01-07 19:25:37 +0200

    + skeleton of SatellitesImportDialog

---
+++ plugins/Satellites/src/gui/SatellitesImportDialog.hpp
@@ -0,0 +1,55 @@
+/*
+ * Stellarium Satellites Plug-in: satellites import feature
+ * Copyright (C) 2012 Bogdan Marinov
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef IMPORTSATELLITESWINDOW_HPP
+#define IMPORTSATELLITESWINDOW_HPP
+
+#include "StelDialog.hpp"
+#include "Satellites.hpp"
+
+class Ui_satellitesImportDialog;
+
+class SatellitesImportDialog : public StelDialog
+{
+	Q_OBJECT
+	
+public:
+	SatellitesImportDialog();
+	~SatellitesImportDialog();
+	
+signals:
+	void satellitesAccepted(const TleDataList& newSatellites);
+	
+public slots:
+	void languageChanged();
+	void setVisible(bool visible = true){ StelDialog::setVisible(visible); }
+	
+	void getData();
+	void acceptNewSatellites();
+	void discardNewSatellites();
+	
+private:
+	void createDialogContent();
+	Ui_satellitesImportDialog* ui;
+	
+	void reset();
+	void parseData();
+};
+
+#endif // IMPORTSATELLITESWINDOW_HPP
