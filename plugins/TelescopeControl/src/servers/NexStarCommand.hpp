0dd55074ca35e3b0e0ce2e6875a15dc2a642879f
Alexander Wolf <alex.v.wolf@gmail.com>
2012-01-11 17:50:37 +0700

    update address of FSF in about window + update address of FSF into source code again (more correct)

--- plugins/TelescopeControl/src/servers/NexStarCommand.hpp
+++ plugins/TelescopeControl/src/servers/NexStarCommand.hpp
@@ -19,7 +19,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
 */
 
 #ifndef _NEXSTAR_COMMAND_HPP_
