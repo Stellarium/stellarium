c337fa0cc22dcca2d94b05be8261ade36bbbd953
Alexander Wolf <alex.v.wolf@gmail.com>
2012-01-11 17:50:37 +0700

    update address of FSF in about window + update address of FSF into source code again (more correct)

--- src/core/modules/Comet.hpp
+++ src/core/modules/Comet.hpp
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
  */
  
 #ifndef _COMET_HPP_
