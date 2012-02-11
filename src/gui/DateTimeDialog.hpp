a7981d6450ec22b42341df4a94ce844ef411951a
Alexander Wolf <alex.v.wolf@gmail.com>
2012-02-11 09:58:46 +0700

    fix typos

--- src/gui/DateTimeDialog.hpp
+++ src/gui/DateTimeDialog.hpp
@@ -1,7 +1,7 @@
 /*
  * Stellarium
  * Copyright (C) 2008 Nigel Kerr
- * Copyruight (c) 2012 Timothy Reaves
+ * Copyright (C) 2012 Timothy Reaves
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
