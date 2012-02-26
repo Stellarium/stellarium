efdd734cf9ce77c95dd38e68420e78a9fa1e8960
Matthew Gates <matthewg42@gmail.com>
2012-02-26 13:44:46 -0800

    further Satellite plugin tweaks

--- plugins/Satellites/src/Satellites.hpp
+++ plugins/Satellites/src/Satellites.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Matthew Gates
+ * Copyright (C) 2009, 2012 Matthew Gates
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
