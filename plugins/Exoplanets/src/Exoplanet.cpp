51eb4cef97916f47760e01383997fb95b0da8bf8
Alexander Wolf <alex.v.wolf@gmail.com>
2012-05-25 21:57:29 +0700

    improve display info

--- plugins/Exoplanets/src/Exoplanet.cpp
+++ plugins/Exoplanets/src/Exoplanet.cpp
@@ -47,6 +47,20 @@ Exoplanet::Exoplanet(const QVariantMap& map)
 	designation  = map.value("designation").toString();
 	RA = StelUtils::getDecAngle(map.value("RA").toString());
 	DE = StelUtils::getDecAngle(map.value("DE").toString());
+	distance = map.value("distance").toFloat();
+	stype = map.value("stype").toString();
+	smass = map.value("smass").toFloat();
+	smetal = map.value("smetal").toFloat();
+	if (map.contains("Vmag"))
+	{
+		Vmag = map.value("Vmag").toFloat();
+	}
+	else
+	{
+		Vmag = -99;
+	}
+	sradius = map.value("sradius").toFloat();
+	effectiveTemp = map.value("effectiveTemp").toInt();
 
 	if (map.contains("exoplanets"))
 	{
@@ -60,8 +74,8 @@ Exoplanet::Exoplanet(const QVariantMap& map)
 			if (exoplanetMap.contains("radius")) p.radius = exoplanetMap.value("radius").toString();
 			if (exoplanetMap.contains("semiAxis")) p.semiAxis = exoplanetMap.value("semiAxis").toString();
 			if (exoplanetMap.contains("eccentricity")) p.eccentricity = exoplanetMap.value("eccentricity").toString();
-			if (exoplanetMap.contains("inclination")) p.inclination = exoplanetMap.value("inclination").toString();
-			if (exoplanetMap.contains("year")) p.year = exoplanetMap.value("year").toInt();
+			if (exoplanetMap.contains("inclination")) p.inclination = exoplanetMap.value("inclination").toString();			
+			if (exoplanetMap.contains("angleDistance")) p.angleDistance = exoplanetMap.value("angleDistance").toString();
 			exoplanets.append(p);
 		}
 	}
@@ -80,6 +94,13 @@ QVariantMap Exoplanet::getMap(void)
 	map["designation"] = designation;
 	map["RA"] = RA;
 	map["DE"] = DE;
+	map["distance"] = distance;
+	map["stype"] = stype;
+	map["smass"] = smass;
+	map["smetal"] = smetal;
+	map["Vmag"] = Vmag;
+	map["sradius"] = sradius;
+	map["effectiveTemp"] = effectiveTemp;
 	QVariantList exoplanetList;
 	foreach(const exoplanetData &p, exoplanets)
 	{
@@ -91,7 +112,7 @@ QVariantMap Exoplanet::getMap(void)
 		if (!p.semiAxis.isEmpty() && p.semiAxis != "") explMap["semiAxis"] = p.semiAxis;
 		if (!p.inclination.isEmpty() && p.inclination != "") explMap["inclination"] = p.inclination;
 		if (!p.eccentricity.isEmpty() && p.eccentricity != "") explMap["eccentricity"] = p.eccentricity;
-		if (p.year != 0) explMap["year"] = p.year;
+		if (!p.angleDistance.isEmpty() && p.angleDistance != "") explMap["angleDistance"] = p.angleDistance;
 		exoplanetList << explMap;
 	}
 	map["exoplanets"] = exoplanetList;
@@ -115,8 +136,51 @@ QString Exoplanet::getInfoString(const StelCore* core, const InfoStringGroup& fl
 		oss << "<h2>" << designation << "</h2>";
 	}
 
+	if (flags&Magnitude)
+	{
+		if (core->getSkyDrawer()->getFlagHasAtmosphere() && Vmag>-99)
+		{
+			oss << q_("Magnitude: <b>%1</b> (extincted to: <b>%2</b>)").arg(QString::number(getVMagnitude(core, false), 'f', 2),
+											QString::number(getVMagnitude(core, true), 'f', 2)) << "<br>";
+		}
+		else
+		{
+			oss << q_("Magnitude: <b>%1</b>").arg(QString::number(getVMagnitude(core, false), 'f', 2)) << "<br>";
+		}
+	}
+
 	// Ra/Dec etc.
 	oss << getPositionInfoString(core, flags);
+
+	if (flags&Extra1)
+	{
+		oss << q_("Spectral Type: %1").arg(stype) << "<br>";
+	}
+
+	if (flags&Distance)
+	{
+		oss << q_("Distance: %1 Light Years").arg(QString::number(distance/0.306601, 'f', 2)) << "<br>";
+	}
+
+	if (flags&Extra1)
+	{
+		if (smetal!=0)
+		{
+			oss << QString("Fe/H: %1").arg(smetal) << "<br>";
+		}
+		if (smass>0)
+		{
+			oss << QString("%1: %2 M<sub>%3</sub>").arg(q_("Mass")).arg(QString::number(smass, 'f', 3)).arg(q_("Sun")) << "<br>";
+		}
+		if (sradius>0)
+		{
+			oss << QString("%1: %2 R<sub>%3</sub>").arg(q_("Radius")).arg(QString::number(sradius, 'f', 5)).arg(q_("Sun")) << "<br>";
+		}
+		if (effectiveTemp>0)
+		{
+			oss << q_("Effective temperature: %1 K").arg(effectiveTemp) << "<br>";
+		}
+	}
 
 	if (flags&Extra1 && exoplanets.size() > 0)
 	{
@@ -126,8 +190,8 @@ QString Exoplanet::getInfoString(const StelCore* core, const InfoStringGroup& fl
 		QString radiusLabel = QString("<td style=\"padding: 0 2px 0 0;\">%1 (R<sub>%2</sub>)</td>").arg(q_("Radius")).arg(q_("Jup"));
 		QString semiAxisLabel = QString("<td style=\"padding: 0 2px 0 0;\">%1 (%2)</td>").arg(q_("Semi-Major Axis")).arg(q_("AU"));
 		QString eccentricityLabel = QString("<td style=\"padding: 0 2px 0 0;\">%1</td>").arg(q_("Eccentricity"));
-		QString inclinationLabel = QString("<td style=\"padding: 0 2px 0 0;\">%1 (%2)</td>").arg(q_("Inclination")).arg(QChar(0x00B0));
-		QString yearLabel = QString("<td style=\"padding: 0 2px 0 0;\">%1</td>").arg(q_("Year of discovery"));
+		QString inclinationLabel = QString("<td style=\"padding: 0 2px 0 0;\">%1 (%2)</td>").arg(q_("Inclination")).arg(QChar(0x00B0));		
+		QString angleDistanceLabel = QString("<td style=\"padding: 0 2px 0 0;\">%1 (\")</td>").arg(q_("Angle Distance"));
 		foreach(const exoplanetData &p, exoplanets)
 		{
 			if (!p.planetName.isEmpty())
@@ -185,14 +249,14 @@ QString Exoplanet::getInfoString(const StelCore* core, const InfoStringGroup& fl
 			else
 			{
 				semiAxisLabel.append("<td style=\"padding:0 2px;\">&mdash;</td>");
-			}
-			if (p.year>0)
+			}			
+			if (!p.angleDistance.isEmpty())
 			{
-				yearLabel.append("<td style=\"padding:0 2px;\">").append(QString::number(p.year)).append("</td>");
+				angleDistanceLabel.append("<td style=\"padding:0 2px;\">").append(p.angleDistance).append("</td>");
 			}
 			else
 			{
-				yearLabel.append("<td style=\"padding:0 2px;\">&mdash;</td>");
+				angleDistanceLabel.append("<td style=\"padding:0 2px;\">&mdash;</td>");
 			}
 		}
 		oss << "<table>";
@@ -203,7 +267,8 @@ QString Exoplanet::getInfoString(const StelCore* core, const InfoStringGroup& fl
 		oss << "<tr>" << semiAxisLabel << "</tr>";
 		oss << "<tr>" << eccentricityLabel << "</tr>";
 		oss << "<tr>" << inclinationLabel << "</tr>";
-		oss << "<tr>" << yearLabel << "</tr></table>";
+		oss << "<tr>" << angleDistanceLabel << "</tr>";
+		oss << "</table>";
 	}
 
 	postProcessInfoString(str, flags);
@@ -225,10 +290,14 @@ float Exoplanet::getVMagnitude(const StelCore* core, bool withExtinction) const
 	    core->getSkyDrawer()->getExtinction().forward(&altAz[2], &extinctionMag);
 	}
 
-	// Calculate fake visual magnitude - minimal magnitude is 4
-	float vmag = 4.f;
-
-	return vmag + extinctionMag;
+	if (Vmag>-99)
+	{
+		return Vmag + extinctionMag;
+	}
+	else
+	{
+		return 6.f + extinctionMag;
+	}
 }
 
 double Exoplanet::getAngularSize(const StelCore*) const
