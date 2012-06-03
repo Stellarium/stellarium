f418e647fb69206ea5e6f5cf3c86b860a32cbd4f
Anton Samoylov <toshaevil@gmail.com>
2012-06-03 23:27:44 +0400

    refactoring StelGui methods names (addGuiActions -> addGuiAction)

--- src/gui/StelGui.hpp
+++ src/gui/StelGui.hpp
@@ -108,13 +108,13 @@ public:
 	virtual void setInfoTextFilters(const StelObject::InfoStringGroup& aflags);
 	virtual const StelObject::InfoStringGroup& getInfoTextFilters() const;
 	
-	virtual QAction* addGuiActions(const QString& actionName,
-									 const QString& text,
-									 const QString& shortCut,
-									 const QString& helpGroup,
-									 bool checkable=true,
-									 bool autoRepeat=false,
-									 bool global = false);
+	virtual QAction* addGuiAction(const QString& actionName,
+																const QString& text,
+																const QString& shortCut,
+																const QString& helpGroup,
+																bool checkable=true,
+																bool autoRepeat=false,
+																bool global = false);
 	
 public slots:
 	//! Define whether the buttons toggling image flip should be visible
@@ -141,7 +141,7 @@ public slots:
 	//! @param b to hide or not to hide
 	void setAutoHideVerticalButtonBar(bool b);
 
-	#ifndef DISABLE_SCRIPTING
+#ifndef DISABLE_SCRIPTING
 	//! change keys when a script is running / not running
 	void setScriptKeys(bool b);
 	void increaseScriptSpeed();
@@ -150,17 +150,17 @@ public slots:
 	void stopScript();
 	void pauseScript();
 	void resumeScript();
-	#endif
+#endif
 
 	//! Hide or show the GUI.  Public so it can be called from scripts.
 	void setGuiVisible(bool);
 
 private slots:
 	void reloadStyle();
-	#ifndef DISABLE_SCRIPTING
+#ifndef DISABLE_SCRIPTING
 	void scriptStarted();
 	void scriptStopped();
-	#endif
+#endif
 	//! Load color scheme from the given ini file and section name
 	void setStelStyle(const QString& section);
 	void quit();
