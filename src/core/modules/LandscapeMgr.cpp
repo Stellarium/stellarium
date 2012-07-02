12bb439b6643789e329410036eb97b0bd723ce43
Alexander Wolf <alex.v.wolf@gmail.com>
2012-07-02 23:51:00 +0700

    apply atmospheres effects only for bodies with atmosphere through Location menu

--- src/core/modules/LandscapeMgr.cpp
+++ src/core/modules/LandscapeMgr.cpp
@@ -279,7 +279,7 @@ void LandscapeMgr::init()
 	setCurrentLandscapeID(defaultLandscapeID);
 	setFlagLandscape(conf->value("landscape/flag_landscape", conf->value("landscape/flag_ground", true).toBool()).toBool());
 	setFlagFog(conf->value("landscape/flag_fog",true).toBool());
-	setFlagAtmosphere(conf->value("landscape/flag_atmosphere").toBool());
+	setFlagAtmosphere(conf->value("landscape/flag_atmosphere", true).toBool());
 	setAtmosphereFadeDuration(conf->value("landscape/atmosphere_fade_duration",0.5).toFloat());
 	setAtmosphereLightPollutionLuminance(conf->value("viewing/light_pollution_luminance",0.0).toFloat());
 	cardinalsPoints = new Cardinals();
