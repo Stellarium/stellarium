74a643ede2d81858d923306ed30a38efc426ca1b
Ferdinand Majerech <kiithsacmp@gmail.com>
2012-07-12 01:32:41 +0200

    Moved NebulaMgr to the new Renderer API, and Renderer API improvements (see details).
    drawRect was separated into drawRect and drawTexturedRect for readability.
    drawRect/drawTexturedRect now supports rotation (around the center).
    
    Font used for text drawing can now be set.

--- src/core/renderer/StelRenderer.cpp
+++ src/core/renderer/StelRenderer.cpp
@@ -22,37 +22,93 @@ struct TexturedVertex
 	VERTEX_ATTRIBUTES(Vec2f Position, Vec2f TexCoord);
 };
 
-void StelRenderer::drawRect
-	(const float x, const float y, const float width, const float height, 
-	 const bool textured)
+//! Implements drawRect() and drawTexturedRect(). 
+//!
+//! These are pretty much the same function, but with different names to improve
+//! readability.
+//!
+//! @param renderer Renderer that's drawing the rectangle.
+//! @param textured Should the rectangle be textured?
+//! @param x        Horizontal position of the rectangle.
+//! @param y        Vertical position of the rectangle.
+//! @param width    Width of the rectangle.
+//! @param height   Height of the rectangle.
+//! @param angle    Rotation angle of the rectangle in degrees.
+//!
+//! @see drawRect, drawTexturedRect
+void drawRectInternal
+	(StelRenderer* renderer, const bool textured, 
+	 const float x, const float y, const float width, const float height, const float angle)
 {
+	Vec2f ne, nw, se, sw;
+
+	// Faster path for angles that are zero or extremely small.
+	if(abs(angle) < 0.1)
+	{
+		ne = Vec2f(x, y);
+		nw = Vec2f(x + width, y);
+		se = Vec2f(x, y + height);
+		sw = Vec2f(x + width, y + height);
+	}
+	// Need to rotate the rectangle (around its center).
+	else
+	{
+		const float cosr       = std::cos(angle / 180 * M_PI);
+		const float sinr       = std::sin(angle / 180 * M_PI);
+		const float halfWidth  = width  * 0.5;
+		const float halfHeight = height * 0.5;
+		const Vec2f center(x + halfWidth, y + halfHeight);
+
+		const float widthCos  = halfWidth  * cosr;
+		const float heightCos = halfHeight * cosr;
+		const float widthSin  = halfWidth  * sinr;
+		const float heightSin = halfHeight * sinr;
+
+		ne = center + Vec2f(-widthCos + heightSin, -widthSin - heightCos);
+		nw = center + Vec2f(widthCos  + heightSin, widthSin  - heightCos);
+		se = center + Vec2f(-widthCos - heightSin, -widthSin + heightCos);
+		sw = center + Vec2f(widthCos  - heightSin, widthSin  + heightCos);
+	}
+	
 	//! Create a vertex buffer for the rectangle and draw it.
 	if(textured)
 	{
 		StelVertexBuffer<TexturedVertex>* buffer = 
-			createVertexBuffer<TexturedVertex>(PrimitiveType_TriangleStrip);
+			renderer->createVertexBuffer<TexturedVertex>(PrimitiveType_TriangleStrip);
 
-		buffer->addVertex(TexturedVertex(Vec2f(x,         y),          Vec2f(0.0f, 0.0f)));
-		buffer->addVertex(TexturedVertex(Vec2f(x + width, y),          Vec2f(1.0f, 0.0f)));
-		buffer->addVertex(TexturedVertex(Vec2f(x,         y + height), Vec2f(0.0f, 1.0f)));
-		buffer->addVertex(TexturedVertex(Vec2f(x + width, y + height), Vec2f(1.0f, 1.0f)));
+		buffer->addVertex(TexturedVertex(ne, Vec2f(0.0f , 0.0f)));
+		buffer->addVertex(TexturedVertex(nw, Vec2f(1.0f , 0.0f)));
+		buffer->addVertex(TexturedVertex(se, Vec2f(0.0f , 1.0f)));
+		buffer->addVertex(TexturedVertex(sw, Vec2f(1.0f , 1.0f)));
 
 		buffer->lock();
-		drawVertexBuffer(buffer);
+		renderer->drawVertexBuffer(buffer);
 		delete buffer;
 	}
 	else
 	{
 		StelVertexBuffer<Vertex>* buffer = 
-			createVertexBuffer<Vertex>(PrimitiveType_TriangleStrip);
+			renderer->createVertexBuffer<Vertex>(PrimitiveType_TriangleStrip);
 
-		buffer->addVertex(Vertex(Vec2f(0.0f, 0.0f)));
-		buffer->addVertex(Vertex(Vec2f(1.0f, 0.0f)));
-		buffer->addVertex(Vertex(Vec2f(0.0f, 1.0f)));
-		buffer->addVertex(Vertex(Vec2f(1.0f, 1.0f)));
+		buffer->addVertex(Vertex(ne));
+		buffer->addVertex(Vertex(nw));
+		buffer->addVertex(Vertex(se));
+		buffer->addVertex(Vertex(sw));
 
 		buffer->lock();
-		drawVertexBuffer(buffer);
+		renderer->drawVertexBuffer(buffer);
 		delete buffer;
 	}
+}
+
+void StelRenderer::drawRect
+	(const float x, const float y, const float width, const float height, const float angle)
+{
+	drawRectInternal(this, false, x, y, width, height, angle);
+}
+
+void StelRenderer::drawTexturedRect
+	(const float x, const float y, const float width, const float height, const float angle)
+{
+	drawRectInternal(this, true, x, y, width, height, angle);
 }
