00094de5cec888ce492b83c16b82c3fed888e156
Bogdan Marinov <bogdan.marinov84@gmail.com>
2012-07-29 22:00:03 +0300

    + search by NORAD number in the main object search

--- plugins/Satellites/src/Satellites.cpp
+++ plugins/Satellites/src/Satellites.cpp
@@ -310,8 +310,12 @@ StelObjectP Satellites::searchByNameI18n(const QString& nameI18n) const
 {
 	if (!hintFader || StelApp::getInstance().getCore()->getCurrentLocation().planetName != earth->getEnglishName())
 		return NULL;
-
+	
 	QString objw = nameI18n.toUpper();
+	
+	StelObjectP result = searchByNoradNumber(objw);
+	if (result)
+		return result;
 
 	foreach(const SatelliteP& sat, satellites)
 	{
@@ -331,6 +335,11 @@ StelObjectP Satellites::searchByName(const QString& englishName) const
 		return NULL;
 
 	QString objw = englishName.toUpper();
+	
+	StelObjectP result = searchByNoradNumber(objw);
+	if (result)
+		return result;
+	
 	foreach(const SatelliteP& sat, satellites)
 	{
 		if (sat->initialized && sat->visible)
@@ -343,6 +352,34 @@ StelObjectP Satellites::searchByName(const QString& englishName) const
 	return NULL;
 }
 
+StelObjectP Satellites::searchByNoradNumber(const QString &noradNumber) const
+{
+	if (!hintFader || StelApp::getInstance().getCore()->getCurrentLocation().planetName != earth->getEnglishName())
+		return NULL;
+	
+	// If the search string is a catalog number...
+	QRegExp regExp("^(NORAD)\\s*(\\d+)\\s*$");
+	if (regExp.exactMatch(noradNumber))
+	{
+		QString numberString = regExp.capturedTexts().at(2);
+		bool ok;
+		int number = numberString.toInt(&ok);
+		if (!ok)
+			return StelObjectP();
+		
+		foreach(const SatelliteP& sat, satellites)
+		{
+			if (sat->initialized && sat->visible)
+			{
+				if (sat->getCatalogNumberString() == numberString)
+					return qSharedPointerCast<StelObject>(sat);
+			}
+		}
+	}
+	
+	return StelObjectP();
+}
+
 QStringList Satellites::listMatchingObjectsI18n(const QString& objPrefix, int maxNbItem) const
 {
 	QStringList result;
@@ -352,6 +389,16 @@ QStringList Satellites::listMatchingObjectsI18n(const QString& objPrefix, int ma
 
 	QString objw = objPrefix.toUpper();
 
+	QString numberPrefix;
+	QRegExp regExp("^(NORAD)\\s*(\\d+)\\s*$");
+	if (regExp.exactMatch(objw))
+	{
+		QString numberString = regExp.capturedTexts().at(2);
+		bool ok;
+		int number = numberString.toInt(&ok);
+		if (ok)
+			numberPrefix = numberString;
+	}
 	foreach(const SatelliteP& sat, satellites)
 	{
 		if (sat->initialized && sat->visible)
@@ -359,6 +406,10 @@ QStringList Satellites::listMatchingObjectsI18n(const QString& objPrefix, int ma
 			if (sat->getNameI18n().toUpper().left(objw.length()) == objw)
 			{
 				result << sat->getNameI18n().toUpper();
+			}
+			else if (sat->getCatalogNumberString().left(numberPrefix.length()) == numberPrefix)
+			{
+				result << QString("NORAD %1").arg(sat->getCatalogNumberString());
 			}
 		}
 	}
