87c1ff1ca602250c9063257327fe31149477192d
Ferdinand Majerech <kiithsacmp@gmail.com>
2012-08-02 02:08:20 +0200

    Added a generic vertex type with 3D position, 2D texcoord and RGBA color.

--- src/core/renderer/GenericVertexTypes.hpp
+++ src/core/renderer/GenericVertexTypes.hpp
@@ -21,4 +21,21 @@ struct VertexP3T2
 	VERTEX_ATTRIBUTES(Vec3f Position, Vec2f TexCoord);
 };
 
+//! Vertex with a 3D position, 2D texture coordinate and an RGBA color.
+struct VertexP3T2C4
+{
+	Vec3f position;
+	Vec2f texCoord;
+	Vec4f color;
+
+	//! Construct from a 3D position, a texture coordinate and a color,
+	VertexP3T2C4(const Vec3f& position, const Vec2f texCoord, const Vec4f& color) 
+		: position(position), texCoord(texCoord), color(color) {}
+	
+	//! Construct from a 2D position, setting the z coord to 0, a texture coordinate and a color.
+	VertexP3T2C4(const Vec2f pos, const Vec2f texCoord, const Vec4f& color) 
+		: position(pos[0], pos[1], 0.0f), texCoord(texCoord), color(color) {}
+
+	VERTEX_ATTRIBUTES(Vec3f Position, Vec2f TexCoord, Vec4f Color);
+};
 #endif // _GENERICVERTEXTYPES_HPP_
