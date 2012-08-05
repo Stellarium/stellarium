3bbae14c4bbb7b2f431cbf10a9d27b92c2bcfc32
Ferdinand Majerech <kiithsacmp@gmail.com>
2012-08-06 00:58:17 +0200

    Partially reverted the BlendMode change due to an unknown flicker issue. Might revert back in future.

--- src/core/renderer/StelQGLRenderer.hpp
+++ src/core/renderer/StelQGLRenderer.hpp
@@ -138,6 +138,23 @@ public:
 
 	virtual void setBlendMode(const BlendMode blendMode)
 	{
+		switch(blendMode)
+		{
+			case BlendMode_None:
+				glDisable(GL_BLEND);
+				break;
+			case BlendMode_Add:
+				glBlendFunc(GL_ONE, GL_ONE);
+				glEnable(GL_BLEND);
+				break;
+			case BlendMode_Alpha:
+				glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+				glEnable(GL_BLEND);
+				break;
+			default:
+				Q_ASSERT_X(false, Q_FUNC_INFO, "Unknown blend mode");
+		}
+
 		this->blendMode = blendMode;
 	}
 
