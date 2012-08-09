99d46ec0413cc51848c289ef606e49760456a73f
Ferdinand Majerech <kiithsacmp@gmail.com>
2012-08-09 03:31:03 +0200

    Added a class to iterate over triangles in QVectors of vertices (for StelSphereGeometry).

---
+++ src/core/TriangleIterator.hpp
@@ -0,0 +1,113 @@
+#ifndef _TRIANGLEITERATOR_HPP_
+#define _TRIANGLEITERATOR_HPP_
+
+#include <QVector>
+
+// For PrimitiveType
+#include "renderer/StelVertexBuffer.hpp"
+
+
+//! Allows iteration over triangles in QVectors of vertices.
+//!
+//! How the vertices are iterated depends on specified primitive type.
+//! Triangles iterated will match triangles that would be drawn 
+//! if the vertices were drawn with specified primitive type.
+//!
+//! Used by StelSphereGeometry classes.
+template<class V>
+class TriangleIterator
+{
+public:
+	//! Construct a TriangleIterator iterating over specified vertex array.
+	//!
+	//! @param vertices      Vertices to iterate over.
+	//! @param primitiveType Primitive type to determine how the vertices form triangles.
+	//!
+	//! Vertex count can be 0, but can't be 1 or 2. If primitiveType is PrimitiveType_Triangles,
+	//! vertex count must be divisible by 3.
+	TriangleIterator(const QVector<V>& vertices, const PrimitiveType primitiveType)
+		: vertices(vertices)
+		, primitiveType(primitiveType)
+	{
+		Q_ASSERT_X(vertices.size() != 1 && vertices.size() != 2, Q_FUNC_INFO,
+		           "A vertex array containing triangles of any primitive type must not "
+		           "have 1 or 2 vertices");
+		switch(primitiveType)
+		{
+			case PrimitiveType_Triangles:
+				Q_ASSERT_X(vertices.size() % 3 == 0, Q_FUNC_INFO,
+				           "Size of a vertex array of triangles must be divisible by 3");
+				currentVertex = 0;
+				break;
+			case PrimitiveType_TriangleStrip:
+				currentVertex = 2;
+				break;
+			case PrimitiveType_TriangleFan:
+				currentVertex = 1;
+				break;
+			default:
+				Q_ASSERT_X(false, Q_FUNC_INFO, "Not a triangle primitive type");
+		}
+	}
+
+	//! Get the next triangle, if any.
+	//!
+	//! @param a The first vertex of the triangle will be written here.
+	//! @param b The second vertex of the triangle will be written here.
+	//! @param c The third vertex of the triangle will be written here.
+	//!
+	//! @return true if we've got the triangle, false if there are no more triangles
+	//!         (in this case a, b and c are unchanged);
+	bool next(V& a, V& b, V& c)
+	{
+		Q_ASSERT_X(currentVertex > 0, Q_FUNC_INFO, "Current vertex is negative");
+		Q_ASSERT_X(currentVertex <= vertices.size(), Q_FUNC_INFO,
+		           "Iterating outside the vertex array");
+		if(currentVertex == vertices.size()){return false;}
+
+		switch(primitiveType)
+		{
+			case PrimitiveType_Triangles:
+				a = vertices[currentVertex++];
+				b = vertices[currentVertex++];
+				c = vertices[currentVertex++];
+				break;
+			case PrimitiveType_TriangleStrip:
+				if(currentVertex % 2 == 0)
+				{
+					a = vertices[currentVertex - 2];
+					b = vertices[currentVertex - 1];
+				}
+				else
+				{
+					a = vertices[currentVertex - 1];
+					b = vertices[currentVertex - 2];
+				}
+				c = vertices[currentVertex];
+				++currentVertex;
+				break;
+			case PrimitiveType_TriangleFan:
+				a = vertices[0];
+				b = vertices[currentVertex];
+				c = vertices[currentVertex + 1];
+				++currentVertex;
+				break;
+			default:
+				Q_ASSERT_X(false, Q_FUNC_INFO, "Not a triangle primitive type");
+		}
+
+		return true;
+	}
+
+private:
+	//! Vertices we're iterating over;
+	const QVector<V>& vertices;
+
+	//! Primitive type of the vertex array.
+	const PrimitiveType primitiveType;
+
+	//! First vertex of the next triangle to iterate over.
+	int currentVertex;
+};
+
+#endif // _TRIANGLEITERATOR_HPP_
