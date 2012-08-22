a5183109d4d2e2d2ef39d4d8af7b2fe8c6ec4286
Alexander Wolf <alex.v.wolf@gmail.com>
2012-08-23 01:40:39 +0700

    fixed check Windows x64

--- src/StelLogger.cpp
+++ src/StelLogger.cpp
@@ -154,7 +154,7 @@ void StelLogger::init(const QString& logFilePath)
 	writeLog(QString("Qt compilation version: %1").arg(QT_VERSION_STR));
 
 	// write addressing mode
-#ifdef __LP64__
+#if defined(__LP64__) || defined(_WIN64)
 	writeLog("Addressing mode: 64-bit");
 #else
 	writeLog("Addressing mode: 32-bit");
