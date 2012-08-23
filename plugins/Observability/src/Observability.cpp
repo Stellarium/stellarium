f466e4dca2485c0ac7a8d3bbcb1921c1e9607dc6
Ivan Marti-Vidal <i.martividal@gmail.com>
2012-08-23 14:45:05 +0200

    Corrected update of rise-to-rose and set-to-sets when refraction is on (silly mistake)

--- plugins/Observability/src/Observability.cpp
+++ plugins/Observability/src/Observability.cpp
@@ -1159,7 +1159,7 @@ bool Observability::SolarSystemSolve(StelCore* core, int Kind)
 		};
 
 		toRADec(Pos2,RA,Dec);
-		Vec3d MoonAltAz = core->equinoxEquToAltAz(Pos2);
+		Vec3d MoonAltAz = core->equinoxEquToAltAz(Pos2,StelCore::RefractionOff);
 		raised = MoonAltAz[2] > 0.0;
 
 // Initial guesses of rise/set/transit times.
