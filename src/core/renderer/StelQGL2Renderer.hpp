702fc58dcfbe8dfcc202838024e670cad48f15db
Ferdinand Majerech <kiithsacmp@gmail.com>
2012-09-08 15:40:59 +0200

    GL2 driver didn't fail initialization when GL version was too low - that is now fixed.

--- src/core/renderer/StelQGL2Renderer.hpp
+++ src/core/renderer/StelQGL2Renderer.hpp
@@ -91,6 +91,7 @@ public:
 		   !QGLFormat::openGLVersionFlags().testFlag(QGLFormat::OpenGL_ES_Version_2_0))
 		{
 			qWarning() << "StelQGL2Renderer::init : StelQGL2Renderer needs OpenGL 2.1 or OpenGL ES 2.0";
+			return false;
 		}
 
 		// Check for features required for the GL2 backend.
