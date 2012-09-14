09e116a854bcc51c034620d9f1d10590a088f24e
Bogdan Marinov <bogdan.marinov84@gmail.com>
2012-09-14 18:40:18 +0300

    Location List Editor 0.4.2: added end-of-line character handling

--- util/locations-editor/main.cpp
+++ util/locations-editor/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	app.setOrganizationName("Stellarium.org");
 	app.setApplicationName("Location List Editor");
 	// The only place the version number is stored.
-	app.setApplicationVersion("0.4.1");
+	app.setApplicationVersion("0.4.2");
 	LocationListEditor mainWindow;
 	mainWindow.show();
 	
