53785286c0fcf73428d2a8d1aca4ab2ec49fd5e7
Bogdan Marinov <bogdan.marinov84@gmail.com>
2012-10-05 16:00:30 +0300

    collapsible groups for ShortcutsDialog; code cleanup and documentation

--- src/gui/ShortcutsDialog.hpp
+++ src/gui/ShortcutsDialog.hpp
@@ -24,7 +24,9 @@
 
 #include <QLineEdit>
 
-// auxilary class for convenient editing shortcuts
+//! Specialised GUI control for entering keyboard shortcut combinations.
+//! Allows Emacs-style key sequences (for example "Ctrl+E, Ctrl+2",
+//! see the documentation of QKeySequence for details.)
 class ShortcutLineEdit : public QLineEdit
 {
 	Q_OBJECT
@@ -33,17 +35,20 @@ public:
 	ShortcutLineEdit(QWidget* parent);
 
 	QKeySequence getKeySequence();
-	bool isEmpty() const { return (m_keyNum <= 0); }
+	bool isEmpty() const
+	{
+		return (m_keyNum <= 0);
+	}
 
 public slots:
-	// clear contents; also clear stored keys
+	//! Clear contents and stored keys.
 	void clear();
-	// remove last key from keysequence
+	//! Remove the last key from the key sequence.
 	void backspace();
 	void setContents(QKeySequence ks);
 
 signals:
-	// need for enable/disable buttons in dialog
+	//! Needed for enabling/disabling buttons in ShortcutsDialog.
 	void focusChanged(bool focus);
 	void contentsChanged();
 
@@ -53,19 +58,19 @@ protected:
 	void focusOutEvent(QFocusEvent *e);
 
 private:
-	// transform modifiers to int
+	//! transform modifiers to int.
 	static int getModifiers(Qt::KeyboardModifiers state, const QString &text);
 
-	// counter and array for storing entered keys
+	//! Length of the stored key sequence.
 	int m_keyNum;
 	int m_keys[4]; // QKeySequence allows only 4 keys in single shortcut
 };
 
 class Ui_shortcutsDialogForm;
+class StelShortcut;
+class StelShortcutGroup;
+class StelShortcutMgr;
 QT_FORWARD_DECLARE_CLASS(QTreeWidgetItem)
-QT_FORWARD_DECLARE_CLASS(StelShortcutMgr)
-QT_FORWARD_DECLARE_CLASS(StelShortcut)
-QT_FORWARD_DECLARE_CLASS(StelShortcutGroup)
 
 class ShortcutsDialog : public StelDialog
 {
@@ -75,55 +80,57 @@ public:
 	ShortcutsDialog();
 	~ShortcutsDialog();
 
-	// higlight items that have collisions with current lineEdits' state according to css
-	// Note: previous collisions aren't redrawn
+	//! higlight items that have collisions with current lineEdits' state according to css.
+	//! Note: previous collisions aren't redrawn.
 	void drawCollisions();
 
 public slots:
-	// restore colors of all items it TreeWidget to defaults
+	//! restore colors of all items it TreeWidget to defaults.
 	void resetCollisions();
 	void retranslate();
-	// ititialize editors state when current item changed
+	//! ititialize editors state when current item changed.
 	void initEditors();
-	// checks whether one QKeySequence is prefix of another
+	//! checks whether one QKeySequence is prefix of another.
 	bool prefixMatchKeySequence(QKeySequence ks1, QKeySequence ks2);
 	QList<QTreeWidgetItem*> findCollidingItems(QKeySequence ks);
 	void handleCollisions(ShortcutLineEdit* currentEdit);
-	// called when editors' state changed
+	//! called when editors' state changed.
 	void handleChanges();
-	// called when apply button clicked
+	//! called when apply button clicked.
 	void applyChanges() const;
-	// called by doubleclick; if click is on editable item, switch to editors
+	//! called by doubleclick; if click is on editable item, switch to editors
 	void switchToEditors(QTreeWidgetItem* item, int column);
-	// update shortcut representation in tree correspondingly to its actual contents
-	// if no shortcutTreeItem specified, search for it in tree, if no items found, create new item
+	//! update shortcut representation in tree correspondingly to its actual contents
+	//! if no shortcutTreeItem specified, search for it in tree, if no items found, create new item
 	void updateShortcutsItem(StelShortcut* shortcut, QTreeWidgetItem* shortcutsTreeItem = NULL);
 	void restoreDefaultShortcuts();
 	void updateTreeData();
 
 protected:
-	//! Initialize the dialog widgets and connect the signals/slots
+	//! Initialize the dialog widgets and connect the signals/slots.
 	virtual void createDialogContent();
 
 private:
-	// checks whether given item can be changed by editors
+	//! checks whether given item can be changed by editors.
 	static bool itemIsEditable(QTreeWidgetItem *item);
-	//! This function concatenates the header, key codes and footer to build
+	//! Concatenate the header, key codes and footer to build
 	//! up the help text.
-	void updateText(void);
+	//! @todo FIXME: This does nothing? 
+	void updateText();
 
-	// apply style changes, see http://qt-project.org/faq/answer/how_can_my_stylesheet_account_for_custom_properties
+	//! Apply style changes.
+	//! See http://qt-project.org/faq/answer/how_can_my_stylesheet_account_for_custom_properties
 	void polish();
 
 	QTreeWidgetItem* updateGroup(StelShortcutGroup* group);
 
-	// search for first appearence of item with requested data
+	//! search for first appearence of item with requested data.
 	QTreeWidgetItem* findItemByData(QVariant value, int role, int column = 0);
 
-	// pointer to mgr, for not getting it from stelapp every time
+	//! pointer to mgr, for not getting it from stelapp every time.
 	StelShortcutMgr* shortcutMgr;
 
-	// list for storing collisions items, so we can easy restore their colors
+	//! list for storing collisions items, so we can easy restore their colors.
 	QList<QTreeWidgetItem*> collisionItems;
 
 	Ui_shortcutsDialogForm *ui;
