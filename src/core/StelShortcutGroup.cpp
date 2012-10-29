716a8e2e2002115887242abfe0aae9e4ff9278d5
Bogdan Marinov <bogdan.marinov84@gmail.com>
2012-10-29 11:51:17 +0200

    fixed autorepeat flag saving

--- src/core/StelShortcutGroup.cpp
+++ src/core/StelShortcutGroup.cpp
@@ -63,7 +63,7 @@ QVariant StelShortcut::toQVariant() const
 	resMap["primaryKey"] = QVariant(m_primaryKey.toString());
 	resMap["altKey"] = QVariant(m_altKey.toString());
 	resMap["checkable"] = QVariant(m_checkable);
-	resMap["autoRepeat"] = QVariant(m_autoRepeat);
+	resMap["autorepeat"] = QVariant(m_autoRepeat);
 	resMap["global"] = QVariant(m_global);
 	if (!m_scriptFile.isEmpty())
 	{
