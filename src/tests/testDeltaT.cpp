bf7e97629b3a94fd21b6e357ecffc1de757bf157
Alexander Wolf <alex.v.wolf@gmail.com>
2013-01-03 16:27:01 +0700

    update calculations Delta T and unit tests

--- src/tests/testDeltaT.cpp
+++ src/tests/testDeltaT.cpp
@@ -40,11 +40,6 @@ void TestDeltaT::historicalTest()
 	// test data from http://eclipse.gsfc.nasa.gov/SEcat5/deltat.html#tab1
 	QVariantList data;
 
-	data << -1000 << 25400 << 640;
-	data << -900  << 23700 << 590;
-	data << -800  << 22000 << 550;
-	data << -700  << 21000 << 500;
-	data << -600  << 19040 << 460;
 	data << -500  << 17190 << 430;
 	data << -400  << 15530 << 390;
 	data << -300  << 14080 << 360;
@@ -68,36 +63,22 @@ void TestDeltaT::historicalTest()
 	data << 1500  << 200   << 20;
 	data << 1600  << 120   << 20;
 	data << 1700  << 9     << 5;
-	data << 1710  << 10    << 3;
-	data << 1720  << 11    << 3;
-	data << 1730  << 11    << 3;
-	data << 1740  << 12    << 2;
 	data << 1750  << 13    << 2;
-	data << 1760  << 15    << 2;
-	data << 1770  << 16    << 2;
-	data << 1780  << 17    << 1;
-	data << 1790  << 17    << 1;
 	data << 1800  << 14    << 1;
-	data << 1810  << 13    << 1;
-	data << 1820  << 12    << 1;
-	data << 1830  << 8     << 1;
-	data << 1840  << 6     << 0.5;
-	data << 1850  << 7     << 0.5;
-	data << 1860  << 8     << 0.5;
-	data << 1870  << 2     << 0.5;
-	data << 1880  << -5    << 0.5;
-	data << 1890  << -6    << 0.5;
-	data << 1900  << -3    << 0.5;
-	data << 1910  << 10    << 0.5;
-	data << 1920  << 21    << 0.5;
-	data << 1930  << 24    << 0.5;
-	data << 1940  << 24    << 0.5;
+	data << 1850  << 7     << 1;
+	data << 1900  << -3    << 1;
 	data << 1950  << 29    << 0.1;
-	data << 1960  << 33    << 0.1;
-	data << 1970  << 40    << 0.1;
-	data << 1980  << 51    << 0.1;
-	data << 1990  << 57    << 0.1;
-	data << 2000  << 65    << 0.1;
+	data << 1955  << 31.1  << 0.1;
+	data << 1960  << 33.2  << 0.1;
+	data << 1965  << 35.7  << 0.1;
+	data << 1970  << 40.2  << 0.1;
+	data << 1975  << 45.5  << 0.1;
+	data << 1980  << 50.5  << 0.1;
+	data << 1985  << 54.3  << 0.1;
+	data << 1990  << 56.9  << 0.1;
+	data << 1995  << 60.8  << 0.1;
+	data << 2000  << 63.8  << 0.1;
+	data << 2005  << 64.7  << 0.1;
 
 	while(data.count() >= 3) 
 	{
@@ -105,14 +86,12 @@ void TestDeltaT::historicalTest()
 		int yout, mout, dout;
 		double JD;
 		double expectedResult = data.takeFirst().toDouble();
-		double acceptableError = data.takeFirst().toDouble();
-		QString ds = QString("%1-01-01 00:00:00").arg(year,4);
+		double acceptableError = data.takeFirst().toDouble();		
 		StelUtils::getJDFromDate(&JD, year, 1, 1, 0, 0, 0);
 		double result = StelUtils::getDeltaT(JD);
-		double actualError = std::abs(expectedResult) - abs(result);		
+		double actualError = std::abs(expectedResult) - std::abs(result);
 		StelUtils::getDateFromJulianDay(JD, &yout, &mout, &dout);
-		QVERIFY2(actualError <= acceptableError, QString("ds=%1 date=%2 year=%3 result=%4 error=%5 acceptable=%6")
-							.arg(ds)
+		QVERIFY2(actualError <= acceptableError, QString("date=%2 year=%3 result=%4 error=%5 acceptable=%6")
 							.arg(QString("%1-%2-%3 00:00:00").arg(yout).arg(mout).arg(dout))
 							.arg(year)
 							.arg(result)
