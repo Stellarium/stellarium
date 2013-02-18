b1a32bef2f1bd9c0221db817ea72bda603e9771c
Alexander Wolf <alex.v.wolf@gmail.com>
2013-02-18 20:17:15 +0700

    tiny fix for marker to default algorithm of Delta-T

--- src/gui/ConfigurationDialog.cpp
+++ src/gui/ConfigurationDialog.cpp
@@ -1150,7 +1150,7 @@ void ConfigurationDialog::populateDeltaTAlgorithmsList()
 	algorithms->addItem(q_("Reingold & Dershowitz (2001, 2002)"), "ReingoldDershowitz");
 	algorithms->addItem(q_("Morrison & Stephenson (2004, 2005)"), "MorrisonStephenson2004");
 	// Espenak & Meeus (2006) used by default
-	algorithms->addItem(QString("<em>%1</em>").arg(q_("Espenak & Meeus (2006)")), "EspenakMeeus");
+	algorithms->addItem(q_("Espenak & Meeus (2006)").append(" *"), "EspenakMeeus");
 	algorithms->addItem(q_("Reijs (2006)"), "Reijs");
 	algorithms->addItem(q_("Custom equation of %1T").arg(QChar(0x0394)), "Custom");
 
