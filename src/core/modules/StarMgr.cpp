8b3acba916a7e18616d3bd5627a73c78f0077fed
Bogdan Marinov <bogdan.marinov84@gmail.com>
2013-03-06 22:18:30 +0200

    incremented defaultStarsConfig.json version to prevent old versions of Stellarium from choking on revision 5865

--- src/core/modules/StarMgr.cpp
+++ src/core/modules/StarMgr.cpp
@@ -64,7 +64,7 @@ static QStringList component_array;
 // This number must be incremented each time the content or file format of the stars catalogs change
 // It can also be incremented when the defaultStarsConfig.json file change.
 // It should always matchs the version field of the defaultStarsConfig.json file
-static const int StarCatalogFormatVersion = 2;
+static const int StarCatalogFormatVersion = 3;
 
 // Initialise statics
 bool StarMgr::flagSciNames = true;
