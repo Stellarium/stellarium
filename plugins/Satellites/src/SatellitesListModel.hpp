6f372a639156430efb75b670b0616abe964c2351
Bogdan Marinov <bogdan.marinov84@gmail.com>
2013-03-31 15:50:04 +0300

    added handling of data changes in SatellitesListModel

--- plugins/Satellites/src/SatellitesListModel.hpp
+++ plugins/Satellites/src/SatellitesListModel.hpp
@@ -30,6 +30,12 @@
 //! 
 //! It can show satellite names in their hint colors. This behavior can be
 //! toggled with enableColoredNames(), e.g. to honor night mode.
+//! 
+//! @warning The model keeps a pointer to the satellite list, not a copy,
+//! so every time that list is modified outside the model (i.e. satellites are
+//! added or removed), you need to call beginSatellitesChange() and
+//! endSatellitesChange().
+//!
 //! @todo Decide whether to use a table model with multiple columns (current),
 //! or a list model dumping everything through data(). Both require enums - for
 //! column numbers or data roles.
@@ -59,9 +65,25 @@ public:
 signals:
 	
 public slots:
+	//! Tell the model that its internal data structure is about to be modified.
+	//! This should be called every time when the underlying data structure is 
+	//! about to be modified outside the model! (That is, when satellites are
+	//! about to be added or removed.) Otherwise views that use this
+	//! model won't work correctly.
+	void beginSatellitesChange();
+	//! Tell the model that its internal data has been modified.
+	//! Don't call this without calling beginSatellitesChange() first!
+	//! This should be called every time when the underlying data structure has 
+	//! been modified outside the model! (That is, when satellites have been
+	//! added or removed.) Otherwise views that use this model won't work
+	//! correctly.
+	void endSatellitesChange();
 	void enableColoredNames(bool enable = true);
 	
 private:
+	//! Pointer to the external data object represented by the model.
+	//! Call updateSatellitesData() every time that data is modified outside
+	//! the model!
 	QList<SatelliteP>* satelliteList;
 	
 	//! Enabled by default.
