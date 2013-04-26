32c01eae9a545ad41ccdc49803fb75ff5053323d
Bogdan Marinov <bogdan.marinov84@gmail.com>
2013-04-26 22:37:14 +0300

    fix for one of the numerous issues with revision 5997

--- src/gui/LocationDialog.cpp
+++ src/gui/LocationDialog.cpp
@@ -382,7 +382,11 @@ void LocationDialog::moveToAnotherPlanet(const QString&)
 			ls->setCurrentLandscapeID(ls->getDefaultLandscapeID());
 
 	}
-	stelCore->moveObserverTo(loc, 0.);	
+	// Planet transition time also set to null to prevent uglyness when
+	// "use landscape location" is enabled for that planet's landscape. --BM
+	// NOTE: I think it also makes sense in the other cases. --BM
+	// FIXME: Avoid the unnecessary change of the location anyway. --BM
+	stelCore->moveObserverTo(loc, 0., 0.);
 }
 
 void LocationDialog::setPositionFromCoords(int )
