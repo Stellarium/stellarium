1745384636286fbd979e9a8add1d81018cef8632
Fabien Chéreau <fabien.chereau@gmail.com>
2013-09-16 21:48:14 +0200

    Test.

--- src/core/modules/ToastMgr.cpp
+++ src/core/modules/ToastMgr.cpp
@@ -26,7 +26,7 @@
 
 ToastMgr::ToastMgr()
 {
-	survey = new ToastSurvey("http://data.stellarium.org/surveys/DSS2WIP/all/{level}/{x}_{y}.jpg", 11);
+	survey = new ToastSurvey("http://localhost/dss-test7/{level}/{x}_{y}.jpg", 11);
 	survey->setParent(this);
 }
 
