b4d0a221d7ed8d8dd8657477e0df0946b8f5638e
Alexander Wolf <alex.v.wolf@gmail.com>
2013-12-22 16:47:20 +0700

    a tiny collection of patches for plugin

--- plugins/MeteorShowers/src/gui/MeteorShowerDialog.cpp
+++ plugins/MeteorShowers/src/gui/MeteorShowerDialog.cpp
@@ -68,8 +68,8 @@ void MeteorShowerDialog::retranslate()
 void MeteorShowerDialog::createDialogContent()
 {
 	ui->setupUi(dialog);
-	ui->tabs->setCurrentIndex(0);
-	connect(&StelApp::getInstance(), SIGNAL(languageChanged()), this, SLOT(languageChanged()));
+	ui->tabs->setCurrentIndex(0);	
+	connect(&StelApp::getInstance(), SIGNAL(languageChanged()), this, SLOT(retranslate()));
 
 	// Settings tab / updates group
 	ui->displayModeCheckBox->setChecked(GETSTELMODULE(MeteorShowers)->getFlagShowMS());
