dc9a567290c5a28999ff3a8b04123be8fc84941f
Georg Zotti <georg.zotti@univie.ac.at>
2014-01-05 18:04:50 +0100

    Bugfix: StelObject::getVMagnitudeWithExtinction() extinction only if atmosphere is active.

--- src/core/StelObject.cpp
+++ src/core/StelObject.cpp
@@ -96,7 +96,9 @@ float StelObject::getVMagnitudeWithExtinction(const StelCore* core) const
 	Vec3d altAzPos = getAltAzPosGeometric(core);
 	altAzPos.normalize();
 	float vMag = getVMagnitude(core);
-	core->getSkyDrawer()->getExtinction().forward(altAzPos, &vMag); 
+	// GZ 2014-01-02: without the test, planets flicker stupidly in fullsky atmosphere-less view.
+	if (core->getSkyDrawer()->getFlagHasAtmosphere())
+		core->getSkyDrawer()->getExtinction().forward(altAzPos, &vMag);
 	return vMag;
 }
 
