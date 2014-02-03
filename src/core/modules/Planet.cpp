87ada2aeab6b08a480e1fa778e194dd6af075619
Alexander Wolf <alex.v.wolf@gmail.com>
2014-02-03 19:28:06 +0700

    tentative fix for the planet brightness bug

--- src/core/modules/Planet.cpp
+++ src/core/modules/Planet.cpp
@@ -869,14 +869,10 @@ void Planet::draw3dModel(StelCore* core, StelProjector::ModelViewTranformP trans
 			sPainter->getLight().setPosition(Vec4f(sunPos[0],sunPos[1],sunPos[2],1.f));
 
 			// Set the light parameters taking sun as the light source
-			static Vec4f diffuse = Vec4f(0.8f,1.2f,1.2f,1.f);
+			static Vec4f diffuse = Vec4f(1.f,1.f,1.f,1.f);
 			static Vec4f zero = Vec4f(0.f,0.f,0.f,0.f);
 			static Vec4f ambient = Vec4f(0.02f,0.02f,0.02f,0.02f);
 			
-			if (this==GETSTELMODULE(SolarSystem)->getMoon().data())
-			{
-				diffuse[1] = 1.5; diffuse[2] = 1.5;
-			}
 			sPainter->getLight().setAmbient(ambient);
 			sPainter->getLight().setDiffuse(diffuse);
 			sPainter->getLight().setSpecular(zero);
