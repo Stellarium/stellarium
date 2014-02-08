255c4211d56023c423b0c4d636197112a38d2ee4
Alexander Wolf <alex.v.wolf@gmail.com>
2014-02-08 22:48:52 +0700

    repaired the logging of supported OpenGL features

--- src/main.cpp
+++ src/main.cpp
@@ -326,11 +326,6 @@ int main(int argc, char **argv)
 		QMessageBox::warning(0, "Stellarium", q_("This system does not support OpenGL."));
 	}
 
-	qDebug() << "OpenGLVersionFlags: " << QGLFormat::openGLVersionFlags();
-	// http://qt-project.org/forums/viewthread/35584: clarify: Why is the string empty? (at least Win7/64 NVidia)
-	//QString versionString(QLatin1String(reinterpret_cast<const char *>(glGetString(GL_VERSION))));
-	QString versionString((char *)glGetString(GL_VERSION));
-	qDebug() << "Driver version string: " << versionString;
 	mainWin.init(confSettings);
 	splash.finish(&mainWin);
 	app.exec();
