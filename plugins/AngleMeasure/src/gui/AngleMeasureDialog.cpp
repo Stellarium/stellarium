73d8d62193813afcc19fe7034d93ad5729484a0f
Alexander Wolf <alex.v.wolf@gmail.com>
2014-03-01 22:42:49 +0700

    few improvements for MSVC building support

--- plugins/AngleMeasure/src/gui/AngleMeasureDialog.cpp
+++ plugins/AngleMeasure/src/gui/AngleMeasureDialog.cpp
@@ -17,6 +17,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
+
 #include "AngleMeasure.hpp"
 #include "AngleMeasureDialog.hpp"
 #include "ui_angleMeasureDialog.h"
