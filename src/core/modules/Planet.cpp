c9771235f26425cf0376d4ebaaaedcb2ed6642fb
Alexander Wolf <alex.v.wolf@gmail.com>
2014-03-02 17:41:40 +0700

    comment code for displaying obliquity of planets

--- src/core/modules/Planet.cpp
+++ src/core/modules/Planet.cpp
@@ -150,8 +150,8 @@ QString Planet::getInfoString(const StelCore* core, const InfoStringGroup& flags
 		double ecl= ssystem->getEarth()->getRotObliquity(core->getJDay());
 		if (core->getCurrentLocation().planetName=="Earth")
 			oss << q_("Obliquity (of date, for Earth): %1").arg(StelUtils::radToDmsStr(ecl, true)) << "<br>";
-		if (englishName!="Sun")
-			oss << q_("Obliquity (of date): %1").arg(StelUtils::radToDmsStr(getRotObliquity(core->getJDay()), true)) << "<br>";
+		//if (englishName!="Sun")
+		//	oss << q_("Obliquity (of date): %1").arg(StelUtils::radToDmsStr(getRotObliquity(core->getJDay()), true)) << "<br>";
 	}
 
 	if (flags&Distance)
