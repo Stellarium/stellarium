25c58562af86a2dca6b6c65567131c1e9c336141
Alexander Wolf <alex.v.wolf@gmail.com>
2014-03-31 01:48:21 +0700

    the initial sketch of the Coordinates plugin

---
+++ plugins/Coordinates/src/Coordinates.cpp
@@ -0,0 +1,215 @@
+/*
+ * Coordinates plug-in for Stellarium
+ *
+ * Copyright (C) 2014 Alexander Wolf
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "StelProjector.hpp"
+#include "StelPainter.hpp"
+#include "StelApp.hpp"
+#include "StelCore.hpp"
+#include "SkyGui.hpp"
+#include "StelLocaleMgr.hpp"
+#include "StelModuleMgr.hpp"
+#include "StelFileMgr.hpp"
+#include "StelGui.hpp"
+#include "StelGuiItems.hpp"
+#include "StelObjectMgr.hpp"
+#include "StelUtils.hpp"
+#include "Coordinates.hpp"
+#include "CoordinatesWindow.hpp"
+
+#include <QFontMetrics>
+#include <QSettings>
+#include <QPixmap>
+#include <cmath>
+
+StelModule* CoordinatesStelPluginInterface::getStelModule() const
+{
+	return new Coordinates();
+}
+
+StelPluginInfo CoordinatesStelPluginInterface::getPluginInfo() const
+{
+	// Allow to load the resources when used as a static plugin
+	Q_INIT_RESOURCE(Coordinates);
+
+	StelPluginInfo info;
+	info.id = "Coordinates";
+	info.displayedName = N_("Coordinates");
+	info.authors = "Alexander Wolf";
+	info.contact = "http://stellarium.org";
+	info.description = N_("This plugin shows the coordinates of the mouse pointer.");
+	info.version = COORDINATES_PLUGIN_VERSION;
+	return info;
+}
+
+Coordinates::Coordinates()
+	: toolbarButton(NULL)
+{
+	setObjectName("Coordinates");
+	mainWindow = new CoordinatesWindow();	
+}
+
+Coordinates::~Coordinates()
+{
+	delete mainWindow;
+}
+
+void Coordinates::init()
+{
+	StelApp &app = StelApp::getInstance();
+	conf = app.getSettings();
+	gui = dynamic_cast<StelGui*>(app.getGui());
+
+	if (!conf->childGroups().contains("coordinates"))
+	{
+		qDebug() << "Coordinates: no coordinates section exists in main config file - creating with defaults";
+		restoreDefaultConfigIni();
+	}
+
+	// populate settings from main config file.
+	readSettingsFromConfig();
+
+	addAction("actionShow_MousePointer_Coordinates", N_("Coordinates"), N_("Show coordinates of the mouse pointer"), "enabled", "");
+
+	enableCoordinates(getFlagEnableAtStartup());
+	setFlagShowCoordinatesButton(flagShowCoordinatesButton);
+
+	// Initialize the message strings and make sure they are translated when
+	// the language changes.
+	updateMessageText();
+	connect(&app, SIGNAL(languageChanged()), this, SLOT(updateMessageText()));
+}
+
+void Coordinates::deinit()
+{
+	//
+}
+
+void Coordinates::draw(StelCore *core)
+{
+	if (!isEnabled())
+		return;
+
+	const StelProjectorP prj = core->getProjection(StelCore::FrameJ2000);
+	StelPainter sPainter(prj);
+	sPainter.setColor(textColor[0], textColor[1], textColor[2], 1.f);
+	font.setPixelSize(getFontSize());
+	sPainter.setFont(font);
+
+	QPoint p = QCursor::pos();
+	prj->unProject(p.x(), p.y(), coordinatesPoint);
+
+	double dec_j2000, ra_j2000;
+	StelUtils::rectToSphe(&ra_j2000,&dec_j2000,coordinatesPoint);
+	QString coordsText = QString("%1/%2").arg(StelUtils::radToHmsStr(ra_j2000)).arg(StelUtils::radToDmsStr(dec_j2000));
+
+	QFontMetrics fm(font);
+	QSize fs = fm.size(Qt::TextSingleLine, coordsText);
+	sPainter.drawText(gui->getSkyGui()->getSkyGuiWidth()/2 - fs.width()/2, gui->getSkyGui()->getSkyGuiHeight() - fs.height()*1.5, coordsText);
+}
+
+void Coordinates::enableCoordinates(bool b)
+{
+	flagShowCoordinates = b;
+}
+
+double Coordinates::getCallOrder(StelModuleActionName actionName) const
+{
+	if (actionName==StelModule::ActionDraw)
+		return StelApp::getInstance().getModuleMgr().getModule("ConstellationMgr")->getCallOrder(actionName)+10.;
+	return 0;
+}
+
+bool Coordinates::configureGui(bool show)
+{
+	if (show)
+	{
+		mainWindow->setVisible(true);
+	}
+
+	return true;
+}
+
+void Coordinates::restoreDefaults(void)
+{
+	restoreDefaultConfigIni();
+	readSettingsFromConfig();
+}
+
+void Coordinates::restoreDefaultConfigIni(void)
+{
+	conf->beginGroup("coordinates");
+
+	// delete all existing Coordinates settings...
+	conf->remove("");
+
+	conf->setValue("enable_at_startup", false);
+	conf->setValue("flag_show_button", true);
+	conf->setValue("text_color", "0,0.5,1");
+	conf->setValue("font_size", 20);
+
+	conf->endGroup();
+}
+
+void Coordinates::readSettingsFromConfig(void)
+{
+	conf->beginGroup("coordinates");
+
+	setFlagEnableAtStartup(conf->value("enable_at_startup", false).toBool());
+	textColor = StelUtils::strToVec3f(conf->value("text_color", "0,0.5,1").toString());
+	setFontSize(conf->value("font_size", 20).toInt());
+	flagShowCoordinatesButton = conf->value("flag_show_button", true).toBool();
+
+	conf->endGroup();
+}
+
+void Coordinates::saveSettingsToConfig(void)
+{
+	conf->beginGroup("coordinates");
+
+	conf->setValue("enable_at_startup", getFlagEnableAtStartup());
+	conf->setValue("flag_show_button", getFlagShowCoordinatesButton());
+	//conf->setValue("text_color", "0,0.5,1");
+	conf->setValue("font_size", getFontSize());
+
+	conf->endGroup();
+}
+
+void Coordinates::updateMessageText()
+{
+	messageCoordinates = q_("");
+}
+
+void Coordinates::setFlagShowCoordinatesButton(bool b)
+{
+	if (b==true) {
+		if (toolbarButton==NULL) {
+			// Create the button
+			toolbarButton = new StelButton(NULL,
+						       QPixmap(":/Coordinates/bt_Coordinates_On.png"),
+						       QPixmap(":/Coordinates/bt_Coordinates_Off.png"),
+						       QPixmap(":/graphicGui/glow32x32.png"),
+						       "actionShow_MousePointer_Coordinates");
+		}
+		gui->getButtonBar()->addButton(toolbarButton, "065-pluginsGroup");
+	} else {
+		gui->getButtonBar()->hideButton("actionShow_MousePointer_Coordinates");
+	}
+	flagShowCoordinatesButton = b;
+}
+
---
+++ plugins/Coordinates/src/gui/CoordinatesWindow.cpp
@@ -0,0 +1,86 @@
+/*
+ * Coordinates plug-in for Stellarium
+ *
+ * Copyright (C) 2014 Alexander Wolf
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Coordinates.hpp"
+#include "CoordinatesWindow.hpp"
+#include "ui_coordinatesWindow.h"
+
+#include "StelApp.hpp"
+#include "StelLocaleMgr.hpp"
+#include "StelModule.hpp"
+#include "StelModuleMgr.hpp"
+
+CoordinatesWindow::CoordinatesWindow()
+{
+	ui = new Ui_coordinatesWindowForm();
+}
+
+CoordinatesWindow::~CoordinatesWindow()
+{
+	delete ui;
+}
+
+void CoordinatesWindow::retranslate()
+{
+	if (dialog)
+	{
+		ui->retranslateUi(dialog);
+		updateAboutText();
+	}
+}
+
+void CoordinatesWindow::createDialogContent()
+{
+	coord = GETSTELMODULE(Coordinates);
+	ui->setupUi(dialog);
+
+	connect(&StelApp::getInstance(), SIGNAL(languageChanged()), this, SLOT(retranslate()));
+	connect(ui->closeStelWindow, SIGNAL(clicked()), this, SLOT(close()));
+
+	ui->checkBoxEnableAtStartup->setChecked(coord->getFlagEnableAtStartup());
+	connect(ui->checkBoxEnableAtStartup, SIGNAL(clicked(bool)), coord, SLOT(setFlagEnableAtStartup(bool)));
+
+	ui->spinBoxFontSize->setValue(coord->getFontSize());
+	connect(ui->spinBoxFontSize, SIGNAL(valueChanged(int)), coord, SLOT(setFontSize(int)));
+
+	ui->checkBoxShowButton->setChecked(coord->getFlagShowCoordinatesButton());
+	connect(ui->checkBoxShowButton, SIGNAL(clicked(bool)), coord, SLOT(setFlagShowCoordinatesButton(bool)));
+
+	connect(ui->pushButtonSave, SIGNAL(clicked()), this, SLOT(saveCoordinatesSettings()));
+	connect(ui->pushButtonReset, SIGNAL(clicked()), this, SLOT(resetCoordinatesSettings()));
+
+	updateAboutText();
+}
+
+void CoordinatesWindow::updateAboutText()
+{
+	ui->labelTitle->setText(q_("Coordinates plug-in"));
+	QString version = QString(q_("Version %1")).arg(COORDINATES_PLUGIN_VERSION);
+	ui->labelVersion->setText(version);
+}
+
+void CoordinatesWindow::saveCoordinatesSettings()
+{
+	coord->saveSettingsToConfig();
+}
+
+void CoordinatesWindow::resetCoordinatesSettings()
+{
+	coord->restoreDefaults();
+}
