815501f38b191bc7f285e0f45bdd279e78bb771d
Alexander Wolf <alex.v.wolf@gmail.com>
2014-06-07 18:39:09 +0700

    update StelUtils and CMakeLists.txt

--- src/core/StelUtils.cpp
+++ src/core/StelUtils.cpp
@@ -1879,20 +1879,20 @@ float *ComputeCosSinRhoZone(const float dRho, const int segments, const float mi
 	return cos_sin_rho;
 }
 
-static const char* getGLErrorText(int code) {
+const char* getGLErrorText(int code) {
 	switch (code) {
-	case GL_INVALID_ENUM:
-		return "GL_INVALID_ENUM";
-	case GL_INVALID_FRAMEBUFFER_OPERATION:
-		return "GL_INVALID_FRAMEBUFFER_OPERATION";
-	case GL_INVALID_VALUE:
-		return "GL_INVALID_VALUE";
-	case GL_INVALID_OPERATION:
-		return "GL_INVALID_OPERATION";
-	case GL_OUT_OF_MEMORY:
-		return "GL_OUT_OF_MEMORY";
-	default:
-		return "undefined error";
+		case GL_INVALID_ENUM:
+			return "GL_INVALID_ENUM";
+		case GL_INVALID_FRAMEBUFFER_OPERATION:
+			return "GL_INVALID_FRAMEBUFFER_OPERATION";
+		case GL_INVALID_VALUE:
+			return "GL_INVALID_VALUE";
+		case GL_INVALID_OPERATION:
+			return "GL_INVALID_OPERATION";
+		case GL_OUT_OF_MEMORY:
+			return "GL_OUT_OF_MEMORY";
+		default:
+			return "undefined error";
 	}
 }
 
