94bdc681848da9371a7e341b5ec0233ca14d7bbf
Guillaume Chereau <guillaume@noctua-software.com>
2014-06-09 08:49:18 +0800

    Remove useless code

--- src/core/StelApp.hpp
+++ src/core/StelApp.hpp
@@ -150,12 +150,6 @@ public:
 	//! @return the max squared distance in pixels that any object has travelled since the last update.
 	void draw();
 
-	//! Iterate through the drawing sequence.
-	//! This allow us to split the slow drawing operation into small parts,
-	//! we can then decide to pause the painting for this frame and used the cached image instead.
-	//! @return true if we should continue drawing (by calling the method again)
-	bool drawPartial();
-
 	//! Call this when the size of the GL window has changed.
 	void glWindowHasBeenResized(float x, float y, float w, float h);
 
