c1046f9f250850dd44e64ea6abd6118b930d818f
Marcos CARDINOT <mcardinot@gmail.com>
2014-06-21 23:11:01 +0100

    Meteor : refactoring meteor class - splitting meteor model to be able to reuse it

--- src/core/modules/Meteor.cpp
+++ src/core/modules/Meteor.cpp
@@ -37,69 +37,81 @@ which are generally not at the apex of the Earth's way, such as the Perseids sho
 #include "StelPainter.hpp"
 
 Meteor::Meteor(const StelCore* core, double v)
-	: m_minDist(0.)
-	, m_startH(0.)
-	, m_endH(0.)
-	, m_mag(1.)
+	: m_segments(10)
 	, m_distMultiplier(0.)
 {
-	m_speed = 11+(double)rand()/((double)RAND_MAX+1)*(v-11);  // abs range 11-72 km/s by default (see line 427 in StelApp.cpp)
-
-	// the meteor starts dead, after we'll calculate the position
-	// if the position is within the bounds, this parameter will be changed to TRUE
-	m_alive = false;
-
-	double high_range = EARTH_RADIUS + HIGH_ALTITUDE;
-	double low_range = EARTH_RADIUS + LOW_ALTITUDE;
+	// determine meteor velocity
+	// abs range 11-72 km/s by default (see line 427 in StelApp.cpp)
+	m_speed = 11+(double)rand()/((double)RAND_MAX+1)*(v-11);
 
 	// view matrix of sporadic meteors model
 	double alpha = (double)rand()/((double)RAND_MAX+1)*2*M_PI;
 	double delta = M_PI_2 - (double)rand()/((double)RAND_MAX+1)*M_PI;
 	m_viewMatrix = Mat4d::zrotation(alpha) * Mat4d::yrotation(delta);
 
-	// find observer position in meteor coordinate system
-	m_obs = core->altAzToEquinoxEqu(Vec3d(0,0,EARTH_RADIUS));
-	m_obs.transfo4d(m_viewMatrix.transpose());
-
-	// select random trajectory using polar coordinates in XY plane, centered on observer
-	m_xydistance = (double)rand() / ((double)RAND_MAX+1)*(VISIBLE_RADIUS);
-	double angle = (double)rand() / ((double)RAND_MAX+1)*2*M_PI;
-
-	// set meteor start x,y
-	m_position[0] = m_posTrain[0] = m_xydistance*cos(angle) + m_obs[0];
-	m_position[1] = m_posTrain[1] = m_xydistance*sin(angle) + m_obs[1];
-
-	// D is distance from center of earth
-	double D = sqrt(m_position[0]*m_position[0] + m_position[1]*m_position[1]);
-
-	if (D > high_range)     // won't be visible, meteor still dead
+	// building meteor model
+	m_alive = initMeteorModel(core, m_segments, m_viewMatrix, meteor);
+	if (!m_alive)
 	{
 		return;
 	}
 
-	m_posTrain[2] = m_position[2] = m_startH = sqrt(high_range*high_range - D*D);
+	// determine the meteor color
+	m_colors.push_back(MeteorMgr::colorPair("white", 100));
+	// building lineColorArray and trainColorArray
+	buildColorArrays(m_segments, m_colors, m_lineColorArray, m_trainColorArray);
+}
+
+Meteor::~Meteor()
+{   
+}
+
+// returns true if alive
+bool Meteor::initMeteorModel(const StelCore* core, const int segments, const Mat4d viewMatrix, MeteorModel &mm)
+{
+	double high_range = EARTH_RADIUS + HIGH_ALTITUDE;
+	double low_range = EARTH_RADIUS + LOW_ALTITUDE;
+
+	// find observer position in meteor coordinate system
+	mm.obs = core->altAzToEquinoxEqu(Vec3d(0,0,EARTH_RADIUS));
+	mm.obs.transfo4d(viewMatrix.transpose());
+
+	// select random trajectory using polar coordinates in XY plane, centered on observer
+	mm.xydistance = (double)rand() / ((double)RAND_MAX+1)*(VISIBLE_RADIUS);
+	double angle = (double)rand() / ((double)RAND_MAX+1)*2*M_PI;
+
+	// set meteor start x,y
+	mm.position[0] = mm.posTrain[0] = mm.xydistance*cos(angle) + mm.obs[0];
+	mm.position[1] = mm.posTrain[1] = mm.xydistance*sin(angle) + mm.obs[1];
+
+	// D is distance from center of earth
+	double D = sqrt(mm.position[0]*mm.position[0] + mm.position[1]*mm.position[1]);
+
+	if (D > high_range)     // won't be visible, meteor still dead
+	{
+		return false;
+	}
+
+	mm.posTrain[2] = mm.position[2] = mm.startH = sqrt(high_range*high_range - D*D);
 
 	// determine end of burn point, and nearest point to observer for distance mag calculation
 	// mag should be max at nearest point still burning
 	if (D > low_range)
 	{
-		m_endH = -m_startH;  // earth grazing
-		m_minDist = m_xydistance;
+		mm.endH = -mm.startH;  // earth grazing
+		mm.minDist = mm.xydistance;
 	}
 	else
 	{
-		m_endH = sqrt(low_range*low_range - D*D);
-		m_minDist = sqrt(m_xydistance*m_xydistance + pow(m_endH - m_obs[2], 2));
+		mm.endH = sqrt(low_range*low_range - D*D);
+		mm.minDist = sqrt(mm.xydistance*mm.xydistance + pow(mm.endH - mm.obs[2], 2));
 	}
 
-	if (m_minDist > VISIBLE_RADIUS)
+	if (mm.minDist > VISIBLE_RADIUS)
 	{
 		// on average, not visible (although if were zoomed ...)
-		return; //meteor still dead
+		return false; //meteor still dead
 	}
-
-	// If everything is ok until here,
-	m_alive = true;  //the meteor is alive
 
 	// determine intensity [-3; 4.5]
 	float Mag1 = (double)rand()/((double)RAND_MAX+1)*7.5f - 3;
@@ -109,54 +121,24 @@ Meteor::Meteor(const StelCore* core, double v)
 	// compute RMag and CMag
 	RCMag rcMag;
 	core->getSkyDrawer()->computeRCMag(Mag, &rcMag);
-	m_mag = rcMag.luminance;
+	mm.mag = rcMag.luminance;
 
 	// most visible meteors are under about 180km distant
 	// scale max mag down if outside this range
 	float scale = 1;
-	if (m_minDist)
+	if (mm.minDist)
 	{
-		scale = 180*180 / (m_minDist*m_minDist);
+		scale = 180*180 / (mm.minDist*mm.minDist);
 	}
 	if (scale < 1)
 	{
-		m_mag *= scale;
+		mm.mag *= scale;
 	}
 
-	// determine number of intermediate segments (useful to curve along projection distortions)
-	m_segments = 10;
-	m_firstBrightSegment = (double)rand()/((double)RAND_MAX+1)*m_segments;
-	// determine the meteor color
-	m_colors.push_back(MeteorMgr::colorPair("white", 100));
+	mm.firstBrightSegment = (double)rand()/((double)RAND_MAX+1)*segments;
 
-	// building color arrays (line and prism)
-	int totalOfSegments = 0;
-	int currentSegment = 1+(double)rand()/((double)RAND_MAX+1)*(m_segments-1);
-	for (int colorIndex=0; colorIndex<m_colors.size(); colorIndex++) {
-		MeteorMgr::colorPair currentColor = m_colors[colorIndex];
-
-		// segments which we'll paint with the current color
-		int numOfSegments = m_segments*(currentColor.second / 100.f) + 0.4f; // +0.4 affect approximation
-		if (colorIndex == m_colors.size()-1) {
-			numOfSegments = m_segments - totalOfSegments;
-		}
-
-		totalOfSegments += numOfSegments;
-		for (int i=0; i<numOfSegments; i++) {
-			m_lineColorArray.insert(currentSegment, getColor(currentColor.first));
-			m_trainColorArray.insert(currentSegment, getColor(currentColor.first));
-			if (currentSegment >= m_segments-1) {
-				currentSegment = 0;
-			} else {
-				currentSegment++;
-			}
-			m_trainColorArray.insert(currentSegment, getColor(currentColor.first));
-		}
-	}
-}
-
-Meteor::~Meteor()
-{   
+	// If everything is ok until here,
+	return true;  //the meteor is alive
 }
 
 Vec4f Meteor::getColor(QString colorName) {
@@ -190,6 +172,37 @@ Vec4f Meteor::getColor(QString colorName) {
 	return Vec4f(R/255.f, G/255.f, B/255.f, 1);
 }
 
+void Meteor::buildColorArrays(const int segments,
+			      const QList<MeteorMgr::colorPair> colors,
+			      QList<Vec4f> &lineColorArray,
+			      QList<Vec4f> &trainColorArray)
+{
+	// building color arrays (line and prism)
+	int totalOfSegments = 0;
+	int currentSegment = 1+(double)rand()/((double)RAND_MAX+1)*(segments-1);
+	for (int colorIndex=0; colorIndex<colors.size(); colorIndex++) {
+		MeteorMgr::colorPair currentColor = colors[colorIndex];
+
+		// segments which we'll paint with the current color
+		int numOfSegments = segments*(currentColor.second / 100.f) + 0.4f; // +0.4 affect approximation
+		if (colorIndex == colors.size()-1) {
+			numOfSegments = segments - totalOfSegments;
+		}
+
+		totalOfSegments += numOfSegments;
+		for (int i=0; i<numOfSegments; i++) {
+			lineColorArray.insert(currentSegment, getColor(currentColor.first));
+			trainColorArray.insert(currentSegment, getColor(currentColor.first));
+			if (currentSegment >= segments-1) {
+				currentSegment = 0;
+			} else {
+				currentSegment++;
+			}
+			trainColorArray.insert(currentSegment, getColor(currentColor.first));
+		}
+	}
+}
+
 // returns true if alive
 bool Meteor::update(double deltaTime)
 {
@@ -198,37 +211,37 @@ bool Meteor::update(double deltaTime)
 		return false;
 	}
 
-	if (m_position[2] < m_endH)
+	if (meteor.position[2] < meteor.endH)
 	{
 		// burning has stopped so magnitude fades out
 		// assume linear fade out
-		m_mag -= deltaTime/500.0f;
-		if(m_mag < 0)
+		meteor.mag -= deltaTime/500.0f;
+		if(meteor.mag < 0)
 		{
 			m_alive = false;    // no longer visible
 		}
 	}
 
 	// *** would need time direction multiplier to allow reverse time replay
-	m_position[2] -= m_speed*deltaTime/1000.0f;
+	meteor.position[2] -= m_speed*deltaTime/1000.0f;
 
 	// train doesn't extend beyond start of burn
-	if (m_position[2] + m_speed*0.5f > m_startH)
+	if (meteor.position[2] + m_speed*0.5f > meteor.startH)
 	{
-		m_posTrain[2] = m_startH;
+		meteor.posTrain[2] = meteor.startH;
 	}
 	else
 	{
-		m_posTrain[2] -= m_speed*deltaTime/1000.0f;
+		meteor.posTrain[2] -= m_speed*deltaTime/1000.0f;
 	}
 
 	// determine visual magnitude based on distance to observer
-	double dist = sqrt(m_xydistance*m_xydistance + pow(m_position[2]-m_obs[2], 2));
+	double dist = sqrt(meteor.xydistance*meteor.xydistance + pow(meteor.position[2]-meteor.obs[2], 2));
 	if (dist == 0)
 	{
 		dist = .01;    // just to be cautious (meteor hits observer!)
 	}
-	m_distMultiplier = m_minDist*m_minDist / (dist*dist);
+	m_distMultiplier = meteor.minDist*meteor.minDist / (dist*dist);
 
 	return m_alive;
 }
@@ -270,24 +283,24 @@ void Meteor::draw(const StelCore* core, StelPainter& sPainter)
 	QVector<Vec3d> vertexArrayR;
 	QVector<Vec3d> vertexArrayTop;
 
-	Vec3d posTrainB = m_posTrain;
+	Vec3d posTrainB = meteor.posTrain;
 	posTrainB[0] += thickness*0.7;
 	posTrainB[1] += thickness*0.7;
-	Vec3d posTrainL = m_posTrain;
+	Vec3d posTrainL = meteor.posTrain;
 	posTrainL[1] -= thickness;
-	Vec3d posTrainR = m_posTrain;
+	Vec3d posTrainR = meteor.posTrain;
 	posTrainR[0] -= thickness;
 
 	for (int i=0; i<m_segments; i++) {
-		double mag = m_mag * i/(3* (m_segments-1));
-		if (i > m_firstBrightSegment) {
+		double mag = meteor.mag * i/(3* (m_segments-1));
+		if (i > meteor.firstBrightSegment) {
 			mag *= 12/5;
 		}
 
-		double height = m_posTrain[2] + i*(m_position[2] - m_posTrain[2])/(m_segments-1);
+		double height = meteor.posTrain[2] + i*(meteor.position[2] - meteor.posTrain[2])/(m_segments-1);
 		Vec3d posi;
 
-		posi = m_posTrain;
+		posi = meteor.posTrain;
 		posi[2] = height;
 		insertVertex(core, vertexArrayLine, posi);
 
