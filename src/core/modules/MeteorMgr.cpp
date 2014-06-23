8c6ea580654c822278e113b217f1be51a0d9c2ca
Marcos CARDINOT <mcardinot@gmail.com>
2014-06-23 13:15:10 +0100

    Meteor : remove unnused class includes + updates license header

--- src/core/modules/MeteorMgr.cpp
+++ src/core/modules/MeteorMgr.cpp
@@ -1,7 +1,8 @@
 /*
  * Stellarium
- * This file Copyright (C) 2004 Robert Spearman
- * 
+ * Copyright (C) 2004 Robert Spearman
+ * Copyright (C) 2014 Marcos Cardinot
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
