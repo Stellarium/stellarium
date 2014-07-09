e486d3cb6243b9bf14e480ee5c3dc82fec86fa62
Marcos Cardinot <mcardinot@gmail.com>
2014-07-09 11:42:22 +0100

    Creates new directory - src/core/addons + add StelAddOn interface

---
+++ src/core/addons/StelAddOn.hpp
@@ -0,0 +1,33 @@
+/*
+ * Stellarium
+ * Copyright (C) 2014 Marcos Cardinot
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+ */
+
+#ifndef _STELADDON_HPP_
+#define _STELADDON_HPP_
+
+#include <QObject>
+
+class StelAddOn : public QObject
+{
+	Q_OBJECT
+public:
+	// check add-ons which are already installed
+	virtual void checkInstalledAddOns();
+};
+
+#endif // _STELADDON_HPP_
