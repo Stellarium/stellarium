75018b42201855eafc5e3ac8b4195c77a075f06a
Marcos Cardinot <mcardinot@gmail.com>
2014-08-04 07:10:11 +0100

    adding AddOnWidget class - custom widget to be used in the AddOnTableView

---
+++ src/gui/AddOnWidget.hpp
@@ -0,0 +1,38 @@
+/*
+ * Stellarium
+ * Copyright (C) 2014 Marcos Cardinot
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+*/
+
+#ifndef _ADDONWIDGET_HPP
+#define _ADDONWIDGET_HPP
+
+#include <QWidget>
+
+class Ui_AddOnWidget;
+
+class AddOnWidget : public QWidget
+{
+Q_OBJECT
+public:
+	AddOnWidget(QWidget *parent = 0);
+	virtual ~AddOnWidget();
+
+private:
+	Ui_AddOnWidget* ui;
+};
+
+#endif // _ADDONWIDGET_HPP
