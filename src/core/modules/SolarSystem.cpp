48b9a23bbda51947d4bdf2fe87dd67cf065f95c6
georg-zotti <Georg.Zotti@univie.ac.at>
2014-08-09 16:17:48 +0200

    add tail length and coma diameter to comet infostring tail texture coordinate and indices now shared static arrays.

--- src/core/modules/SolarSystem.cpp
+++ src/core/modules/SolarSystem.cpp
@@ -905,6 +905,12 @@ bool SolarSystem::loadPlanets(const QString& filePath)
 	// special case: load earth shadow texture
 	Planet::texEarthShadow = StelApp::getInstance().getTextureManager().createTexture(StelFileMgr::getInstallationDir()+"/textures/earth-shadow.png");
 
+	// GZ: Also comets just have static textures.
+	Comet::comaTexture = StelApp::getInstance().getTextureManager().createTextureThread(StelFileMgr::getInstallationDir()+"/textures/cometComa.png", StelTexture::StelTextureParams(true, GL_LINEAR, GL_CLAMP_TO_EDGE));
+	//GZ: tail textures. We use a paraboloid tail body, textured like a fisheye sphere, i.e. center=head. The texture should be something like a mottled star to give some structure.
+	Comet::tailTexture = StelApp::getInstance().getTextureManager().createTextureThread(StelFileMgr::getInstallationDir()+"/textures/cometTail.png", StelTexture::StelTextureParams(true, GL_LINEAR, GL_CLAMP_TO_EDGE));
+	//GZ: I think we need only one texture for the tails.
+
 	return true;
 }
 
