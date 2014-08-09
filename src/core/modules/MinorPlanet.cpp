ddf2d7bfafb7c472162835474ae1a94780d713db
georg-zotti <Georg.Zotti@univie.ac.at>
2014-08-09 16:22:50 +0200

    change asteroid magnitude computation to float (model is not accurate anyway)

--- src/core/modules/MinorPlanet.cpp
+++ src/core/modules/MinorPlanet.cpp
@@ -141,15 +141,17 @@ MinorPlanet::~MinorPlanet()
 void MinorPlanet::setSemiMajorAxis(double value)
 {
 	semiMajorAxis = value;
-	// GZ: patched for 2012DA14 and other NEA rendez-vous:
-	if (semiMajorAxis < 1.666)
-	{
-		deltaJD = 0.1*StelCore::JD_SECOND;
-	}
-	if (semiMajorAxis < 1.25)
-	{
-		deltaJD = 0.001*StelCore::JD_SECOND;
-	}
+	// GZ: in case we have very many asteroids, this helps improving speed usually without sacrificing accuracy:
+	deltaJD = 2.0*semiMajorAxis*StelCore::JD_SECOND;
+	// GZ: patched for 2012DA14 and other NEA rendez-vous. Actually, deltaJD is now dynamic w.r.t. observer distance!
+//	if (semiMajorAxis < 1.666)
+//	{
+//		deltaJD = 0.1*StelCore::JD_SECOND;
+//	}
+//	if (semiMajorAxis < 1.25)
+//	{
+//		deltaJD = 0.001*StelCore::JD_SECOND;
+//	}
 
 }
 
@@ -307,25 +309,46 @@ float MinorPlanet::getVMagnitude(const StelCore* core) const
 	//Calculate phase angle
 	//(Code copied from Planet::getVMagnitude())
 	//(LOL, this is actually vector subtraction + the cosine theorem :))
+//	const Vec3d& observerHelioPos = core->getObserverHeliocentricEclipticPos();
+//	const double observerRq = observerHelioPos.lengthSquared();
+//	const Vec3d& planetHelioPos = getHeliocentricEclipticPos();
+//	const double planetRq = planetHelioPos.lengthSquared();
+//	const double observerPlanetRq = (observerHelioPos - planetHelioPos).lengthSquared();
+//	const double cos_chi = (observerPlanetRq + planetRq - observerRq)/(2.0*sqrt(observerPlanetRq*planetRq));
+//	double phaseAngle = std::acos(cos_chi);
+
+//	//Calculate reduced magnitude (magnitude without the influence of distance)
+//	//Source of the formulae: http://www.britastro.org/asteroids/dymock4.pdf
+//	const double phi1 = std::exp(-3.33 * std::pow(std::tan(phaseAngle/2), 0.63));
+//	const double phi2 = std::exp(-1.87 * std::pow(std::tan(phaseAngle/2), 1.22));
+//	double reducedMagnitude = absoluteMagnitude - 2.5 * std::log10( (1 - slopeParameter) * phi1 + slopeParameter * phi2 );
+
+//	//Calculate apparent magnitude
+//	//TODO: See if you can "collapse" some calculations
+//	// -- GZ: NO! This is also in Meeus, Astr.Alg. 1998, p.231 and authoritative by IAU commission 20, New Delhi November 1985.
+//	//       (you can collapse and leave away the reducedMagnitude varable, but this is cosmetic)
+//	double apparentMagnitude = reducedMagnitude + 5 * std::log10(std::sqrt(planetRq * observerPlanetRq));
+	// GZ Try that in float... speed diffference is negligible, though.
 	const Vec3d& observerHelioPos = core->getObserverHeliocentricEclipticPos();
-	const double observerRq = observerHelioPos.lengthSquared();
+	const float observerRq = observerHelioPos.lengthSquared();
 	const Vec3d& planetHelioPos = getHeliocentricEclipticPos();
-	const double planetRq = planetHelioPos.lengthSquared();
-	const double observerPlanetRq = (observerHelioPos - planetHelioPos).lengthSquared();
-	const double cos_chi = (observerPlanetRq + planetRq - observerRq)/(2.0*sqrt(observerPlanetRq*planetRq));
-	double phaseAngle = std::acos(cos_chi);
+	const float planetRq = planetHelioPos.lengthSquared();
+	const float observerPlanetRq = (observerHelioPos - planetHelioPos).lengthSquared();
+	const float  cos_chi = (observerPlanetRq + planetRq - observerRq)/(2.0*std::sqrt(observerPlanetRq*planetRq));
+	const float phaseAngle = std::acos(cos_chi);
 
 	//Calculate reduced magnitude (magnitude without the influence of distance)
 	//Source of the formulae: http://www.britastro.org/asteroids/dymock4.pdf
-	const double phi1 = std::exp(-3.33 * std::pow(std::tan(phaseAngle/2), 0.63));
-	const double phi2 = std::exp(-1.87 * std::pow(std::tan(phaseAngle/2), 1.22));
-	double reducedMagnitude = absoluteMagnitude - 2.5 * std::log10( (1 - slopeParameter) * phi1 + slopeParameter * phi2 );
+	const float tanPhaseAngleHalf=std::tan(phaseAngle*0.5f);
+	const float phi1 = std::exp(-3.33f * std::pow(tanPhaseAngleHalf, 0.63f));
+	const float phi2 = std::exp(-1.87f * std::pow(tanPhaseAngleHalf, 1.22f));
+	float reducedMagnitude = absoluteMagnitude - 2.5f * std::log10( (1.0f - slopeParameter) * phi1 + slopeParameter * phi2 );
 
 	//Calculate apparent magnitude
 	//TODO: See if you can "collapse" some calculations
 	// -- GZ: NO! This is also in Meeus, Astr.Alg. 1998, p.231 and authoritative by IAU commission 20, New Delhi November 1985.
 	//       (you can collapse and leave away the reducedMagnitude varable, but this is cosmetic)
-	double apparentMagnitude = reducedMagnitude + 5 * std::log10(std::sqrt(planetRq * observerPlanetRq));
+	float apparentMagnitude = reducedMagnitude + 5.0f * std::log10(std::sqrt(planetRq * observerPlanetRq));
 
 	return apparentMagnitude;
 }
