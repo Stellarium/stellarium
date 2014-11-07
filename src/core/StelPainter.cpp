b2687124fbf77c9886f0a4fd9458fe7c76dc6701
Georg Zotti <georg.zotti@univie.ac.at>
2014-11-07 23:34:35 +0100

    fix for color modulated painting

--- src/core/StelPainter.cpp
+++ src/core/StelPainter.cpp
@@ -1357,6 +1357,7 @@ void StelPainter::drawSphericalRegion(const SphericalRegion* poly, SphericalPoly
 			break;
 		case SphericalPolygonDrawModeFill:
 		case SphericalPolygonDrawModeTextureFill:
+		case SphericalPolygonDrawModeTextureFillColormodulated:
 			glEnable(GL_CULL_FACE);
 			// The polygon is already tesselated as triangles
 			if (doSubDivise || prj->intersectViewportDiscontinuity(poly->getBoundingCap()))
