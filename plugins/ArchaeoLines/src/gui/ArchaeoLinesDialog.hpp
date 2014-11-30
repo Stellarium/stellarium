0f574d22f20f54ebbd3bcb465c1c2892abad89cd
georg-zotti <Georg.Zotti@univie.ac.at>
2014-11-30 19:00:09 +0100

    Created new plugin ArchaeoLines

---
+++ plugins/ArchaeoLines/src/gui/ArchaeoLinesDialog.hpp
@@ -0,0 +1,59 @@
+/*
+ * ArchaeoLines plug-in for Stellarium
+ *
+ * Copyright (C) 2014 Georg Zotti
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _ARCHAEOLINESDIALOG_HPP_
+#define _ARCHAEOLINESDIALOG_HPP_
+
+#include "StelDialog.hpp"
+#include "StelGui.hpp"
+#include "StelTranslator.hpp"
+
+#include <QString>
+
+class Ui_archaeoLinesDialog;
+class ArchaeoLines;
+
+//! Main window of the ArchaeoLines plug-in.
+class ArchaeoLinesDialog : public StelDialog
+{
+	Q_OBJECT
+
+public:
+	ArchaeoLinesDialog();
+	~ArchaeoLinesDialog();
+
+public slots:
+	void retranslate();
+
+protected:
+	void createDialogContent();
+
+private:
+	Ui_archaeoLinesDialog* ui;
+	ArchaeoLines* al;
+
+	void setAboutHtml();
+
+private slots:
+	void saveArchaeoLinesSettings();
+	void resetArchaeoLinesSettings();
+};
+
+
+#endif /* _ARCHAEOLINESDIALOG_HPP_ */
