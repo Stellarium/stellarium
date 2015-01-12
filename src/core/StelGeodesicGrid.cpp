8fce676f6bcb40ab72faa1c354efc71e37a9a295
Guillaume Chereau <guillaume@noctua-software.com>
2015-01-12 15:31:58 +0800

    Fix memory leak on Windows, (maybe fix bug #1406005)
    In some special execution path, the geodesic search will leak memory on
    Windows.  This should fix the problem.

--- src/core/StelGeodesicGrid.cpp
+++ src/core/StelGeodesicGrid.cpp
@@ -374,7 +374,7 @@ void StelGeodesicGrid::searchZones(int lev,int index,
 		if (!corner0_inside[i] && !corner1_inside[i] && !corner2_inside[i])
 		{
 			// totally outside this SphericalCap
-			return;
+			goto end;
 		}
 		else if (corner0_inside[i] && corner1_inside[i] && corner2_inside[i])
 		{
@@ -443,6 +443,7 @@ void StelGeodesicGrid::searchZones(int lev,int index,
 #endif
 		}
 	}
+end:
 #if defined __STRICT_ANSI__ || !defined __GNUC__
 	delete[] halfs_used;
 #endif
