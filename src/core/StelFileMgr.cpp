ed525f38d563fee38de02da79bdac9f05da91c64
Alexander Wolf <alex.v.wolf@gmail.com>
2015-01-15 01:48:05 +0600

    fix screenshot directory creating

--- src/core/StelFileMgr.cpp
+++ src/core/StelFileMgr.cpp
@@ -84,7 +84,23 @@ void StelFileMgr::init()
 		qWarning() << "WARNING: could not locate installation directory";
 	}
 
-	screenshotDir = QDesktopServices::storageLocation(QDesktopServices::PicturesLocation);
+	// Wasn't set path to screenshots directory via --screenshot-dir?
+	if (screenshotDir.isEmpty())
+	{
+		QString screenshotDirSuffix = "/Stellarium";
+		if (!QDesktopServices::storageLocation(QDesktopServices::PicturesLocation).isEmpty())
+			screenshotDir = QDesktopServices::storageLocation(QDesktopServices::PicturesLocation).append(screenshotDirSuffix);
+		else
+			screenshotDir = userDir.append(screenshotDirSuffix);
+	}
+	try
+	{
+		makeSureDirExistsAndIsWritable(screenshotDir);
+	}
+	catch (std::runtime_error &e)
+	{
+		qDebug("Error: cannot create screenshot directory: %s", e.what());
+	}
 }
 
 
