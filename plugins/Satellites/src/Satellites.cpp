fdff837be0edf3bb7d2710abc7fdb332733917ce
Alexander Wolf <alex.v.wolf@gmail.com>
2015-04-25 18:24:29 +0600

    Updates in the plugins

--- plugins/Satellites/src/Satellites.cpp
+++ plugins/Satellites/src/Satellites.cpp
@@ -526,16 +526,41 @@ void Satellites::restoreDefaultSettings()
 	
 	// TLE update sources
 	QStringList urls;
-	urls << "1,http://celestrak.com/NORAD/elements/visual.txt" // Auto-add ON!
-	     << "http://celestrak.com/NORAD/elements/tle-new.txt"
-	     << "http://celestrak.com/NORAD/elements/science.txt"
-	     << "http://celestrak.com/NORAD/elements/noaa.txt"
-	     << "http://celestrak.com/NORAD/elements/goes.txt"
-	     << "http://celestrak.com/NORAD/elements/amateur.txt"
-	     << "http://celestrak.com/NORAD/elements/gps-ops.txt"
-	     << "http://celestrak.com/NORAD/elements/galileo.txt"
-	     << "http://celestrak.com/NORAD/elements/iridium.txt"
-	     << "http://celestrak.com/NORAD/elements/geo.txt";
+	urls << "1,http://www.celestrak.com/NORAD/elements/visual.txt" // Auto-add ON!
+	     << "http://www.celestrak.com/NORAD/elements/tle-new.txt"
+	     << "1,http://www.celestrak.com/NORAD/elements/science.txt"
+	     << "http://www.celestrak.com/NORAD/elements/noaa.txt"
+	     << "http://www.celestrak.com/NORAD/elements/goes.txt"
+	     << "1,http://www.celestrak.com/NORAD/elements/amateur.txt"
+	     << "1,http://www.celestrak.com/NORAD/elements/gps-ops.txt"
+	     << "1,http://www.celestrak.com/NORAD/elements/galileo.txt"
+	     << "1,http://www.celestrak.com/NORAD/elements/iridium.txt"
+	     << "http://www.celestrak.com/NORAD/elements/geo.txt"
+	     << "1,http://www.celestrak.com/NORAD/elements/stations.txt"
+	     << "http://www.celestrak.com/NORAD/elements/weather.txt"
+	     << "http://www.celestrak.com/NORAD/elements/resource.txt"
+	     << "http://www.celestrak.com/NORAD/elements/sarsat.txt"
+	     << "http://www.celestrak.com/NORAD/elements/dmc.txt"
+	     << "http://www.celestrak.com/NORAD/elements/tdrss.txt"
+	     << "http://www.celestrak.com/NORAD/elements/argos.txt"
+	     << "http://www.celestrak.com/NORAD/elements/intelsat.txt"
+	     << "http://www.celestrak.com/NORAD/elements/gorizont.txt"
+	     << "http://www.celestrak.com/NORAD/elements/raduga.txt"
+	     << "http://www.celestrak.com/NORAD/elements/molniya.txt"
+	     << "http://www.celestrak.com/NORAD/elements/orbcomm.txt"
+	     << "http://www.celestrak.com/NORAD/elements/globalstar.txt"
+	     << "http://www.celestrak.com/NORAD/elements/x-comm.txt"
+	     << "http://www.celestrak.com/NORAD/elements/other-comm.txt"
+	     << "1,http://www.celestrak.com/NORAD/elements/glo-ops.txt"
+	     << "http://www.celestrak.com/NORAD/elements/beidou.txt"
+	     << "http://www.celestrak.com/NORAD/elements/sbas.txt"
+	     << "http://www.celestrak.com/NORAD/elements/nnss.txt"
+	     << "http://www.celestrak.com/NORAD/elements/engineering.txt"
+	     << "http://www.celestrak.com/NORAD/elements/education.txt"
+	     << "http://www.celestrak.com/NORAD/elements/geodetic.txt"
+	     << "http://www.celestrak.com/NORAD/elements/radar.txt"
+	     << "http://www.celestrak.com/NORAD/elements/cubesat.txt"
+	     << "http://www.celestrak.com/NORAD/elements/other.txt";
 	saveTleSources(urls);
 }
 
@@ -561,7 +586,7 @@ void Satellites::restoreDefaultCatalog()
 		// the json file has been manually removed, that an update is schreduled in a timely
 		// manner
 		StelApp::getInstance().getSettings()->remove("Satellites/last_update");
-		lastUpdate = QDateTime::fromString("2001-05-25T12:00:00", Qt::ISODate);
+		lastUpdate = QDateTime::fromString("2015-05-01T12:00:00", Qt::ISODate);
 
 	}
 }
@@ -1700,8 +1725,18 @@ void Satellites::translations()
 	N_("non-operational");
 	// TRANSLATORS: Satellite group: Satellites belonging to the GPS constellation (the Global Positioning System)
 	N_("gps");
+	// TRANSLATORS: Satellite group: Satellites belonging to the GLONASS constellation (GLObal NAvigation Satellite System)
+	N_("glonass");
+	// TRANSLATORS: Satellite group: Satellites belonging to the Galileo constellation (global navigation satellite system by the European Union)
+	N_("galileo");
 	// TRANSLATORS: Satellite group: Satellites belonging to the Iridium constellation (Iridium is a proper name)
 	N_("iridium");
+	// TRANSLATORS: Satellite group: Space stations
+	N_("stations");
+	// TRANSLATORS: Satellite group: Education satellites
+	N_("education");
+	// TRANSLATORS: Satellite group: Satellites belonging to the space observatories
+	N_("observatory");
 	
 	/* For copy/paste:
 	// TRANSLATORS: Satellite group: 
@@ -1715,5 +1750,7 @@ void Satellites::translations()
 	N_("The Hubble Space Telescope");
 	// TRANSLATORS: Satellite description.
 	N_("The International Space Station");
+	// TRANSLATORS: Satellite description.
+	N_("China's first space station");
 #endif
 }
