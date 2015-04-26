04f0908a387d94528af2f5ca8aa76e90950a7718
georg-zotti <Georg.Zotti@univie.ac.at>
2015-04-27 01:43:02 +0200

    Started RemoteControl plugin (SoCiS2015)

---
+++ plugins/RemoteControl/src/RemoteControl.cpp
@@ -0,0 +1,200 @@
+/*
+ * Copyright (C) 2009 Matthew Gates
+ * Copyright (C) 2014 Georg Zotti
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+ */
+
+#include "StelUtils.hpp"
+#include "StelProjector.hpp"
+#include "StelPainter.hpp"
+#include "StelApp.hpp"
+#include "StelCore.hpp"
+#include "StelFileMgr.hpp"
+#include "StelLocaleMgr.hpp"
+#include "StelModuleMgr.hpp"
+#include "StelGui.hpp"
+#include "StelGuiItems.hpp"
+#include "StelVertexArray.hpp"
+#include "RemoteControl.hpp"
+#include "RemoteControlDialog.hpp"
+
+#include <QDebug>
+#include <QTimer>
+#include <QPixmap>
+#include <QtNetwork>
+#include <QSettings>
+#include <cmath>
+
+//! This method is the one called automatically by the StelModuleMgr just after loading the dynamic library
+StelModule* RemoteControlStelPluginInterface::getStelModule() const
+{
+	return new RemoteControl();
+}
+
+StelPluginInfo RemoteControlStelPluginInterface::getPluginInfo() const
+{
+	// Allow to load the resources when used as a static plugin
+	Q_INIT_RESOURCE(RemoteControl);
+
+	StelPluginInfo info;
+	info.id = "RemoteControl";
+	info.displayedName = N_("Remote Control");
+	info.authors = "Florian Schaukowitsch and Georg Zotti";
+	info.contact = "http://homepage.univie.ac.at/Georg.Zotti";
+	info.description = N_("<p>Provides remote control functionality using a webserver interface.</p> "
+			      "<p>See manual for detailed description.</p>"
+			      "<p>This plugin was developed during ESA SoCiS 2015.</p>");
+	info.version = REMOTECONTROL_VERSION;
+	return info;
+}
+
+RemoteControl::RemoteControl()
+	: enabled(false)
+	, toolbarButton(NULL)
+{
+	setObjectName("RemoteControl");
+	font.setPixelSize(16);
+
+	configDialog = new RemoteControlDialog();
+	conf = StelApp::getInstance().getSettings();
+
+	messageTimer = new QTimer(this);
+	messageTimer->setInterval(7000);
+	messageTimer->setSingleShot(true);
+
+	connect(messageTimer, SIGNAL(timeout()), this, SLOT(clearMessage()));
+}
+
+RemoteControl::~RemoteControl()
+{
+	delete configDialog;
+}
+
+bool RemoteControl::configureGui(bool show)
+{
+	if (show)
+		configDialog->setVisible(true);
+	return true;
+}
+
+//! Determine which "layer" the plugin's drawing will happen on.
+double RemoteControl::getCallOrder(StelModuleActionName actionName) const
+{
+	// TODO: if there is any graphics, this may have to be adjusted. Else maybe even delete?
+	if (actionName==StelModule::ActionDraw)
+		return StelApp::getInstance().getModuleMgr().getModule("LandscapeMgr")->getCallOrder(actionName)+10.;
+	return 0;
+}
+
+void RemoteControl::init()
+{
+	if (!conf->childGroups().contains("RemoteControl"))
+		restoreDefaultSettings();
+
+	loadSettings();
+
+	StelApp& app = StelApp::getInstance();
+
+	// Create action for enable/disable & hook up signals	
+	addAction("actionShow_Remote_Control", N_("Remote Control"), N_("Remote control"), "enabled", "Ctrl+N");
+
+	// Initialize the message strings and make sure they are translated when
+	// the language changes.
+	updateMessageText();
+	connect(&app, SIGNAL(languageChanged()), this, SLOT(updateMessageText()));
+
+	// Add a toolbar button. TODO:  decide whether a button is necessary at all. Maye the button should not only enable, but call the GUI dialog directly?
+	try
+	{
+		StelGui* gui = dynamic_cast<StelGui*>(app.getGui());
+		if (gui!=NULL)
+		{
+			toolbarButton = new StelButton(NULL,
+						       QPixmap(":/RemoteControl/resources/bt_network_on.png"),
+						       QPixmap(":/RemoteControl/resources/bt_network_off.png"),
+						       QPixmap(":/graphicGui/glow32x32.png"),
+						       "actionShow_Remote_Control");
+			gui->getButtonBar()->addButton(toolbarButton, "065-pluginsGroup");
+		}
+	}
+	catch (std::runtime_error& e)
+	{
+		qWarning() << "WARNING: unable create toolbar button for RemoteControl plugin: " << e.what();
+	}
+}
+
+void RemoteControl::update(double deltaTime)
+{
+	messageFader.update((int)(deltaTime*1000));
+	// TODO: GZ I think most processing will be triggered here...
+}
+
+
+//! Draw any parts on the screen which are for our module
+void RemoteControl::draw(StelCore* core)
+{
+	// TODO: Likely nothing.
+	Q_UNUSED(core);
+}
+
+void RemoteControl::enableRemoteControl(bool b)
+{
+	enabled = b;
+	messageFader = b;
+	if (b)
+	{
+		qDebug() << "RemoteControl enabled";
+		messageTimer->start();
+	}
+}
+
+
+void RemoteControl::updateMessageText()
+{
+	// TRANSLATORS: instructions for using the RemoteControl plugin.
+	messageEnabled = q_("Remote Control enabled.");
+}
+
+void RemoteControl::clearMessage()
+{
+	//qDebug() << "RemoteControl::clearMessage";
+	messageFader = false;
+}
+
+void RemoteControl::restoreDefaultSettings()
+{
+	Q_ASSERT(conf);
+	// Remove the old values...
+	conf->remove("RemoteControl");
+	// ...load the default values...
+	loadSettings();
+	// ...and then save them.
+	saveSettings();
+}
+
+void RemoteControl::loadSettings()
+{
+	conf->beginGroup("RemoteControl");
+	// TODO whatever has to be retrieved here. Don't forget to define default values!
+	conf->endGroup();
+}
+
+void RemoteControl::saveSettings()
+{
+	conf->beginGroup("RemoteControl");
+	// TODO whatever has to be stored here
+	conf->endGroup();
+}
---
+++ plugins/RemoteControl/src/RemoteControl.hpp
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2015 Florian Schaukowitsch
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+ */
+ 
+#ifndef REMOTECONTROL_HPP_
+#define REMOTECONTROL_HPP_
+
+#include <QFont>
+#include <QKeyEvent>
+
+#include "VecMath.hpp"
+#include "StelModule.hpp"
+#include "StelFader.hpp"
+#include "StelCore.hpp"
+
+class QTimer;
+class QPixmap;
+class StelButton;
+class RemoteControlDialog;
+
+//! Main class of the RemoteControl plug-in.
+//! Provides a remote control using a webserver interface, usable for single or even synchronized cluster of clients (via the RemoteSync plugin).
+//! This plugin has been developed during ESA SoCiS 2015.
+class RemoteControl : public StelModule
+{
+	Q_OBJECT
+	Q_PROPERTY(bool enabled
+		   READ isEnabled
+		   WRITE enableRemoteControl)
+public:
+	RemoteControl();
+	virtual ~RemoteControl();
+	
+	///////////////////////////////////////////////////////////////////////////
+	// Methods defined in the StelModule class
+	virtual void init();
+	virtual void update(double deltaTime);
+	virtual void draw(StelCore* core);
+	virtual double getCallOrder(StelModuleActionName actionName) const;
+	virtual void handleKeys(QKeyEvent* event){event->setAccepted(false);}
+
+	//virtual void handleMouseClicks(class QMouseEvent* event);
+	//virtual bool handleMouseMoves(int x, int y, Qt::MouseButtons b);
+	virtual bool configureGui(bool show=true);
+	bool isEnabled() const {return enabled;}
+
+
+	//! Restore the plug-in's settings to the default state.
+	//! Replace the plug-in's settings in Stellarium's configuration file
+	//! with the default values and re-load them.
+	//! Uses internally loadSettings() and saveSettings().
+	void restoreDefaultSettings();
+
+	//! Load the plug-in's settings from the configuration file.
+	//! Settings are kept in the "RemoteControl" section in Stellarium's
+	//! configuration file. If no such section exists, it will load default
+	//! values.
+	//! @see saveSettings(), restoreSettings()
+	void loadSettings();
+
+	//! Save the plug-in's settings to the configuration file.
+	//! @see loadSettings(), restoreSettings()
+	void saveSettings();
+
+public slots:
+	void enableRemoteControl(bool b);
+
+private slots:
+	void updateMessageText();
+	void clearMessage();
+
+private:
+	bool enabled;
+	QFont font;
+	LinearFader messageFader;
+	QTimer* messageTimer;
+	QString messageEnabled;
+
+	StelButton* toolbarButton;
+
+
+	QSettings* conf;
+
+	// GUI
+	RemoteControlDialog* configDialog;
+};
+
+
+
+#include <QObject>
+#include "StelPluginInterface.hpp"
+
+//! This class is used by Qt to manage a plug-in interface
+class RemoteControlStelPluginInterface : public QObject, public StelPluginInterface
+{
+	Q_OBJECT
+	Q_PLUGIN_METADATA(IID "stellarium.StelGuiPluginInterface/1.0")
+	Q_INTERFACES(StelPluginInterface)
+public:
+	virtual StelModule* getStelModule() const;
+	virtual StelPluginInfo getPluginInfo() const;
+};
+
+#endif /*REMOTECONTROL_HPP_*/
+
