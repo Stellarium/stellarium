1ee2ed0be437e701060f366088f32f6b1c7180b5
Florian Schaukowitsch <florian.schaukowitsch@student.tuwien.ac.at>
2015-06-03 22:22:08 +0200

    Allow StelActions to be retrieved and triggered through the web interface.

---
+++ plugins/RemoteControl/src/StelActionService.hpp
@@ -0,0 +1,43 @@
+/*
+ * Stellarium Remote Control plugin
+ * Copyright (C) 2015 Florian Schaukowitsch
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+ */
+
+#ifndef STELACTIONSERVICE_HPP_
+#define STELACTIONSERVICE_HPP_
+
+#include "APIController.hpp"
+
+class StelActionMgr;
+
+class StelActionService : public AbstractAPIService
+{
+	Q_OBJECT
+public:
+	StelActionService(const QByteArray& serviceName, QObject* parent = 0);
+
+	virtual ~StelActionService() {}
+
+	virtual void get(const QByteArray& operation,const QMultiMap<QByteArray,QByteArray>& parameters, HttpResponse& response) Q_DECL_OVERRIDE;
+	virtual void post(const QByteArray &operation, const QMultiMap<QByteArray, QByteArray> &parameters, const QByteArray &data, HttpResponse &response) Q_DECL_OVERRIDE;
+private:
+	StelActionMgr* actionMgr;
+};
+
+
+
+#endif
