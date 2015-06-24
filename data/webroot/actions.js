57268d62e6039788656b96db25d24786305a1797
Florian Schaukowitsch <florian.schaukowitsch@student.tuwien.ac.at>
2015-06-25 00:35:28 +0200

    Add FOV control

--- data/webroot/actions.js
+++ data/webroot/actions.js
@@ -1,56 +1,76 @@
+"use strict";
+
 var Actions = (new function($) {
     //Private variables
     var $actionlist;
+
+    function initControls() {
+        $actionlist = $("#actionlist");
+
+        $actionlist.change(function() {
+            $("#bt_doaction").prop("disabled", false);
+        });
+
+        var runactionfn = function() {
+            var $selection = $("#actionlist").find("option").filter(":selected");
+            var id = $selection.data("id");
+
+            Actions.execute(id);
+        }
+
+        $("#actionlist").dblclick(runactionfn);
+        $("#bt_doaction").click(runactionfn);
+    }
+
+    function fillActionList() {
+        //fill the action list
+        $.ajax({
+            url: "/api/stelaction/list",
+            success: function(data) {
+                $actionlist.empty();
+
+                $.each(data, function(key, val) {
+                    //the key is an optgroup
+                    var group = $("<optgroup/>").attr("label", key);
+                    //the val is an array of StelAction objects
+                    $.each(val, function(idx, v2) {
+                        var option = $("<option/>");
+                        option.data(v2); //store the data
+                        if (v2.isCheckable) {
+                            option.addClass("checkableaction");
+                            option.text(v2.text + " (" + v2.isChecked + ")");
+
+                            $actionlist.on("action:" + v2.id, {
+                                elem: option
+                            }, function(event, newValue) {
+                                var el = event.data.elem;
+                                el.data("isChecked", newValue);
+                                el.text(el.data("text") + " (" + newValue + ")");
+                            });
+                        } else {
+                            option.text(v2.text);
+                        }
+                        option.appendTo(group);
+                    });
+                    group.appendTo($actionlist);
+                });
+            },
+            error: function(xhr, status, errorThrown) {
+                console.log("Error updating action list");
+                console.log("Error: " + errorThrown);
+                console.log("Status: " + status);
+                alert("Could not retrieve action list")
+            }
+        });
+    }
 
     //Public stuff
     return {
         init: function() {
 
-            $actionlist = $("#actionlist");
+            initControls();
+            fillActionList();
 
-            $actionlist.change(function() {
-                $("#bt_doaction").prop("disabled", false);
-            });
-
-            //fill the action list
-            $.ajax({
-                url: "/api/stelaction/list",
-                success: function(data) {
-                    $actionlist.empty();
-
-                    $.each(data, function(key, val) {
-                        //the key is an optgroup
-                        var group = $("<optgroup/>").attr("label", key);
-                        //the val is an array of StelAction objects
-                        $.each(val, function(idx, v2) {
-                            var option = $("<option/>");
-                            option.data(v2); //store the data
-                            if (v2.isCheckable) {
-                                option.addClass("checkableaction");
-                                option.text(v2.text + " (" + v2.isChecked + ")");
-
-                                $actionlist.on("action:" + v2.id, {
-                                    elem: option
-                                }, function(event, newValue) {
-                                    var el = event.data.elem;
-                                    el.data("isChecked", newValue);
-                                    el.text(el.data("text") + " (" + newValue + ")");
-                                });
-                            } else {
-                                option.text(v2.text);
-                            }
-                            option.appendTo(group);
-                        });
-                        group.appendTo($actionlist);
-                    });
-                },
-                error: function(xhr, status, errorThrown) {
-                    console.log("Error updating action list");
-                    console.log("Error: " + errorThrown);
-                    console.log("Status: " + status);
-                    alert("Could not retrieve action list")
-                }
-            });
         },
 
         //Trigger an StelAction on the Server
