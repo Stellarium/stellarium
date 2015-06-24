e6f4f80436afd7c285be7f3b8d337c508531b84c
Marcos CARDINOT <mcardinot@gmail.com>
2015-06-24 13:33:00 +0000

    code improvements + renaming class from 'MeteorStream' to 'MeteorObj'

--- plugins/MeteorShowers/src/MeteorShower.hpp
+++ plugins/MeteorShowers/src/MeteorShower.hpp
@@ -23,6 +23,7 @@
 
 #include <QDateTime>
 
+#include "Meteor.hpp"
 #include "StelFader.hpp"
 #include "StelObject.hpp"
 #include "StelPainter.hpp"
@@ -103,9 +104,6 @@ public:
 		return zhr;
 	}
 
-	//! <colorName, intensity>
-	typedef QPair<QString, int> colorPair;
-
 private:
 	Vec3d XYZ;                      //! Cartesian equatorial position
 	Vec3d XY;                       //! Store temporary 2D position
@@ -138,7 +136,7 @@ private:
 	float driftDelta;		//! Drift of Dec.
 	QString parentObj;		//! Parent object for meteor shower
 	float pidx;			//! The population index
-	QList<colorPair> colors;	//! <colorName, 0-100>
+	QList<Meteor::colorPair> colors;//! <colorName, 0-100>
 
 	//current information
 	double radiantAlpha;            //! Current R.A. for radiant of meteor shower
