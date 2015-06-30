4830795cf4bc1866beade11007444b86a718707c
Florian Schaukowitsch <florian.schaukowitsch@student.tuwien.ac.at>
2015-06-30 19:53:04 +0200

    Buttons can now semi-automatically be connected to StelActions (including tooltip text and active state). Used this for the main Stellarium button bar. Button icons now all share a file, and another one for the active state. Add NOTIFY to tracking property in StelMovementMgr to correctly update the "actionSet_Tracking" StelAction. Make writing to log file thread safe. StelActions that trigger scripts do no longer block the HTTP request.

--- data/webroot/actions.js
+++ data/webroot/actions.js
@@ -7,6 +7,54 @@ var Actions = (new function ($) {
 
     var actionData = {};
 
+
+    //this is a list of all actions together with their button image
+    //that automatically should be created and connected
+    var buttonActions = [
+        [
+            "actionShow_Constellation_Lines", "btConstellationLines"
+        ],
+        [
+            "actionShow_Constellation_Labels", "btConstellationLabels"
+        ],
+        [
+            "actionShow_Constellation_Art", "btConstellationArt"
+        ],
+        [
+            "actionShow_Equatorial_Grid", "btEquatorialGrid"
+        ],
+        [
+            "actionShow_Azimuthal_Grid", "btAzimuthalGrid"
+        ],
+        [
+            "actionShow_Ground", "btGround"
+        ],
+        [
+            "actionShow_Cardinal_Points", "btCardinalPoints"
+        ],
+        [
+            "actionShow_Atmosphere", "btAtmosphere"
+        ],
+        [
+            "actionShow_Nebulas", "btNebula"
+        ],
+        [
+            "actionShow_Planets_Labels", "btPlanets"
+        ],
+        [
+            "actionSwitch_Equatorial_Mount", "btEquatorialMount"
+        ],
+        [   //this is actually a different action than the main GUI, but toggles properly (which the one from the GUI does not for some reason)
+            "actionSet_Tracking", "btGotoSelectedObject"
+        ],
+        [
+            "actionShow_Night_Mode", "btNightView"
+        ],
+        [
+            "actionSet_Full_Screen_Global", "btFullScreen"
+        ]
+    ];
+
     function initControls() {
         $actionlist = $("#actionlist");
 
@@ -24,6 +72,29 @@ var Actions = (new function ($) {
         $("#bt_doaction").click(runactionfn);
     }
 
+    function createAutoButtons() {
+        var $ul = $("#autobuttons");
+        var parent = $ul.parent();
+        $ul.detach();
+
+        buttonActions.forEach(function (val) {
+            var btn = document.createElement("button");
+            btn.className = "button32 " + val[1];
+            btn.value = val[0];
+
+            var li = document.createElement("li");
+            li.className = "button32wrapper";
+            li.appendChild(btn);
+
+            $ul.append(li);
+        });
+
+        //this automatically connects to StelAction
+        connectActionContainer($ul);
+
+        $ul.appendTo(parent);
+    }
+
     function fireActionChanged(actionId) {
         console.log("stelActionChanged: " + actionId);
         $(document).trigger("stelActionChanged", {
@@ -36,8 +107,13 @@ var Actions = (new function ($) {
         var action = actionData[data.id];
         action.option.textContent = action.text + " (" + data.isChecked + ")";
         if (action.checkboxes) {
-            action.checkboxes.forEach(function(val){
+            action.checkboxes.forEach(function (val) {
                 val.checked = data.isChecked;
+            });
+        }
+        if (action.buttons) {
+            action.buttons.forEach(function (val) {
+                $(val).toggleClass("active", data.isChecked);
             });
         }
     }
@@ -66,6 +142,13 @@ var Actions = (new function ($) {
                         };
 
                         $.extend(true, actionData, newData);
+
+                        if (actionData[v2.id].buttons) {
+                            //change tooltips
+                            actionData[v2.id].buttons.forEach(function (val) {
+                                val.title = v2.text;
+                            });
+                        }
 
                         option.value = v2.id;
 
@@ -129,11 +212,56 @@ var Actions = (new function ($) {
         });
     }
 
+    function connectActionContainer(elem) {
+        var e = $(elem);
+
+        //remember checkboxes for updating
+        e.find("input[type='checkbox']").each(function () {
+            if (this.value.lastIndexOf("action", 0) === 0) {
+                var newData = {};
+                newData[this.value] = {
+                    checkboxes: [this]
+                };
+                $.extend(true, actionData, newData);
+
+                //check if action has a checked value, apply it if it does
+                if ("isChecked" in actionData[this.value]) {
+                    this.checked = actionData[this.value].isChecked;
+                }
+            }
+        });
+
+        //give buttons the "active" class if the action is active
+        e.find("input[type='button'], button").each(function () {
+            if (this.value.lastIndexOf("action", 0) === 0) {
+                var newData = {};
+                newData[this.value] = {
+                    buttons: [this]
+                };
+                $.extend(true, actionData, newData);
+
+                //check if action has a checked value, apply it if it does
+                if ("isChecked" in actionData[this.value]) {
+                    this.className += "active";
+                }
+            }
+        });
+
+        //react to clicks by toggling/triggering the action
+        e.on("click", "input[type='checkbox'], input[type='button'], button", function (evt) {
+            //assume all values start with "action"
+            if (this.value.lastIndexOf("action", 0) === 0) {
+                executeAction(this.value);
+            }
+        });
+    }
+
     //Public stuff
     return {
         init: function () {
 
             initControls();
+            createAutoButtons();
 
             fillActionList();
             //attach handler after filling to prevent unnessecary calls
@@ -163,31 +291,6 @@ var Actions = (new function ($) {
         execute: executeAction,
 
         //Connects all checkbox input elements below this container to the StelAction that corresponds to its value
-        connectActionContainer: function (elem) {
-            var e = $(elem);
-
-            //remember the children for updating
-            e.find("input:checkbox").each(function () {
-                if (this.value.lastIndexOf("action", 0) === 0) {
-                    var newData = {};
-                    newData[this.value] = {
-                        checkboxes : [this]
-                    };
-                    $.extend(true, actionData, newData);
-
-                    //check if action has a checked value, apply it if it does
-                    if("isChecked" in actionData[this.value]) {
-                        this.checked = actionData[this.value].isChecked;
-                    }
-                }
-            });
-
-            e.on("click", "input:checkbox", function (evt) {
-                //assume all values start with "action"
-                if (this.value.lastIndexOf("action", 0) === 0) {
-                    executeAction(this.value);
-                }
-            });
-        }
+        connectActionContainer: connectActionContainer
     }
 }(jQuery));
