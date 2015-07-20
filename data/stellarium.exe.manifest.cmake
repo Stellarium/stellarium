70885408adfc3766e05e0e913b747382bb63704e
Alexander Wolf <alex.v.wolf@gmail.com>
2015-07-20 23:03:48 +0600

    stellarium.exe.manifest.cmake is not autogenerated stuff :)

---
+++ data/stellarium.exe.manifest.cmake
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <assemblyIdentity
+        processorArchitecture="*"
+        version="@PACKAGE_VERSION@"
+        type="win32"
+        name="Stellarium.Stellarium.Stellarium"/>
+    <description>Stellarium is a free open source planetarium for your computer.</description>
+    <dependency>
+    <dependentAssembly>
+    <assemblyIdentity
+        type="win32"
+        name="Microsoft.Windows.Common-Controls"
+        version="6.0.0.0"
+        publicKeyToken="6595b64144ccf1df"
+        language="*"
+        processorArchitecture="*"/>
+    </dependentAssembly>
+    </dependency>
+</assembly>
