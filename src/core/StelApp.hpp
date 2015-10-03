a7e0a6ed5f336bae8623311a2f5bb6db12d16589
Alexander Wolf <alex.v.wolf@gmail.com>
2015-10-03 23:52:20 +0600

    Added support SOCKS5 proxy

--- src/core/StelApp.hpp
+++ src/core/StelApp.hpp
@@ -292,7 +292,7 @@ private:
 	QNetworkAccessManager* networkAccessManager;
 
 	//! Get proxy settings from config file... if not set use http_proxy env var
-	void setupHttpProxy();
+	void setupNetworkProxy();
 
 	// The audio manager.  Must execute in the main thread.
 	StelAudioMgr* audioMgr;
