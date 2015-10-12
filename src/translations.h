46d8c0f1b9426a2b099900c7e7c0ebb338bb16aa
Qam1 <qam1@aol.com>
2015-10-12 18:04:48 +0600

    The Phobos scripts and the Solar System Screen Saver also are negativly affected by the changes to the moon longitudes.
    Here are updated versions that will work correctly with Stellarium
    0.14.0

--- src/translations.h
+++ src/translations.h
@@ -302,7 +302,7 @@ class Translations
 		N_("Phobos eclipsing the Sun as seen from Olympus Mons on Jan 10, 2068.");
 		N_("Phobos occultations of Earth are common, as are occultations of Jupiter. But occultations of both on the same day are very rare. Here's one that takes place 1/23/2048. In real speed.");
 		N_("Phobos races ahead of Mars and transits the sun, passes through it and then retrogrades back towards the sun and just partially transits it again (only seen in the southern hemisphere of Deimos), then Mars totally eclipses the sun while Phobos transits in darkness between Mars and Deimos. When Phobos emerges from Mars it is still eclipsed and dimmed in Mars' shadow, only to light up later.");
-		N_("Screensaver of various happenings in the Solar System. 232 events in all!");
+		N_("Screensaver of various happenings in the Solar System. 248 events in all!");
 		N_("A tour of the western constellations.");
 		N_("Look at the Sun from big planets of Solar System and Pluto.");
 		N_("Best views of Earth from other Solar System bodies in the 21st Century.");
