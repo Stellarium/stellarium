a60ad36d6fa43714ccc1485feead016a9dc4e129
Florian Schaukowitsch <florian.schaukowitsch@student.tuwien.ac.at>
2015-10-21 17:14:44 +0200

    Put some order into the the web resources of RemoteControl

--- data/webroot/js/api/remotecontrol.js
+++ data/webroot/js/api/remotecontrol.js
@@ -1,26 +1,5 @@
 /* jshint expr: true */
 
-var UISettings = (function() {
-    "use strict";
-
-    var data = {};
-
-    // Automatically poll the server for updates? Otherwise, only updated after commands are sent.
-    data.updatePoll = true;
-    //the interval for automatic polling
-    data.updateInterval = 1000;
-    //use the Browser's requestAnimationFrame for animation instead of setTimeout
-    data.useAnimationFrame = true;
-    //If animation frame is not used, this is the delay between 2 animation steps
-    data.animationDelay = 500;
-    //If no user changes happen after this time, the changes are sent to the Server
-    data.editUpdateDelay = 500;
-    //This is the delay after which the AJAX request spinner is shown.
-    data.spinnerDelay = 100;
-
-    return data;
-})();
-
 //DOM-ready hook, starting the GUI
 $(document).ready(function() {
     Main.init();
