cb270b9aca9d574e58fc4811bf4eb181404c8401
Alexander Wolf <alex.v.wolf@gmail.com>
2015-10-25 13:23:35 +0600

    Updated list of available COM ports for Windows (Telescope Control Plug-in)

--- plugins/TelescopeControl/src/TelescopeControlGlobals.hpp
+++ plugins/TelescopeControl/src/TelescopeControlGlobals.hpp
@@ -68,7 +68,7 @@ namespace TelescopeControlGlobals {
 	
 	
 	#ifdef Q_OS_WIN32
-	const QStringList SERIAL_PORT_NAMES = QString("COM1 COM2 COM3 COM4").split(' ', QString::SkipEmptyParts);
+	const QStringList SERIAL_PORT_NAMES = QString("COM1 COM2 COM3 COM4 COM5 COM6 COM7 COM8 COM9 COM10 COM11 COM12 COM13 COM14 COM15 COM16 COM17 COM18 COM19 COM20").split(' ', QString::SkipEmptyParts);
 	#elif defined(Q_OS_MAC)
 	const QStringList SERIAL_PORT_NAMES = QString("/dev/ ").split(' ', QString::SkipEmptyParts);
 	#else
