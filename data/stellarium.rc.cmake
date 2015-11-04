31e6af511a826429e3426fc28a4c94a3a6e88331
Alexander Wolf <alex.v.wolf@gmail.com>
2015-11-04 18:18:14 +0600

    Updated resource file for Windows

--- data/stellarium.rc.cmake
+++ data/stellarium.rc.cmake
@@ -1,22 +1,21 @@
 stellarium-icon ICON "stellarium.ico"
 1 Manifest "stellarium.exe.manifest"
 1 VERSIONINFO
-FILEVERSION    	@PACKAGE_VERSION_RC@
-PRODUCTVERSION 	@PACKAGE_VERSION_RC@
-FILEOS         	VOS_WINDOWS32
-FILETYPE       	VFT_APP
+FILEVERSION     @PACKAGE_VERSION_RC@
+PRODUCTVERSION  @PACKAGE_VERSION_RC@
+FILEOS          0x00040004L
+FILETYPE        0x00000001L
+FILESUBTYPE     0x0L
 BEGIN
     BLOCK "StringFileInfo"
     BEGIN
-        BLOCK "040904B0"
+        BLOCK "040904E4"
         BEGIN
-            VALUE "CompanyName",      "Stellarium team"
-            VALUE "FileDescription",  "Stellarium is a free open source planetarium"
+            VALUE "CompanyName",      "Stellarium team\0"
+            VALUE "FileDescription",  "Stellarium is a free open source planetarium\0"
             VALUE "FileVersion",      "@PACKAGE_VERSION@\0"
-            VALUE "InternalName",     "stellarium"
-            VALUE "LegalCopyright",   "Copyright (C) @COPYRIGHT_YEARS@ Stellarium team"
-            VALUE "OriginalFilename", "stellarium.exe"
-            VALUE "ProductName",      "Stellarium"
+            VALUE "LegalCopyright",   "Copyright (C) @COPYRIGHT_YEARS@ Stellarium team\0"
+            VALUE "ProductName",      "Stellarium\0"
             VALUE "ProductVersion",   "@PACKAGE_VERSION@\0"
         END
     END
