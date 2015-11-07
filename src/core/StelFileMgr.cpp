d6a7b0bed191e95e8f4f5ec39a265998d16dca82
Alexander Wolf <alex.v.wolf@gmail.com>
2015-11-07 22:14:46 +0600

    Let's use a special case for Windows for saving/retrieving installation path

--- src/core/StelFileMgr.cpp
+++ src/core/StelFileMgr.cpp
@@ -30,11 +30,12 @@
 #include <stdio.h>
 
 #ifdef Q_OS_WIN
-# include <windows.h>
-# ifndef _SHOBJ_H
-# include <shlobj.h>
-# include <QLibrary>
-# endif
+#include <QSettings>
+#include <windows.h>
+#ifndef _SHOBJ_H
+	#include <shlobj.h>
+	#include <QLibrary>
+#endif
 #endif
 
 #include "StelFileMgr.hpp"
@@ -86,7 +87,7 @@ void StelFileMgr::init()
 	}
 	else
 	{
-#ifdef Q_OS_MAC
+	#ifdef Q_OS_MAC
 		QString relativePath = "/../Resources";
 		if (QCoreApplication::applicationDirPath().contains("src")) {
 			relativePath = "/../..";
@@ -103,12 +104,12 @@ void StelFileMgr::init()
 		}
 		QFileInfo installLocation(ResourcesDir.absolutePath());
 		QFileInfo checkFile(installLocation.filePath() + QString("/") + QString(CHECK_FILE));
-#else
+	#else
 		// Linux, BSD, Solaris etc.
 		// We use the value from the config.h filesystem
 		QFileInfo installLocation(QFile::decodeName(INSTALL_DATADIR));
 		QFileInfo checkFile(QFile::decodeName(INSTALL_DATADIR "/" CHECK_FILE));
-#endif
+	#endif
 		if (checkFile.exists())
 		{
 			installDir = installLocation.filePath();
@@ -121,6 +122,15 @@ void StelFileMgr::init()
 			qFatal("Couldn't find install directory location.");
 		}
 	}
+
+	#ifdef Q_OS_WIN
+	// Special case for Windows - retrive installation path from registry.
+	QSettings registry("HKEY_LOCAL_MACHINE\\Software\\Stellarium", QSettings::NativeFormat);
+	QString installDirReg = registry.value("InstallPath").toString();
+	// Let's check for existence of required registry key
+	if (!installDirReg.isEmpty())
+		installDir = installDirReg;
+	#endif
 	
 	// Then add the installation directory to the search path
 	fileLocations.append(installDir);	
