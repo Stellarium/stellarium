107cc77601276b77415f23d449064737cf7b0cd3
Alexander Wolf <alex.v.wolf@gmail.com>
2015-11-07 17:37:50 +0600

    change algorithm of compression for installer on Windows

--- stellarium.iss.cmake
+++ stellarium.iss.cmake
@@ -28,7 +28,9 @@ DefaultDirName={pf}\Stellarium
 DefaultGroupName=Stellarium
 UninstallDisplayIcon={app}\data\stellarium.ico
 LicenseFile=COPYING
-Compression=zip/9
+; LZMA2/max required 95 MB RAM for compression and 8 MB RAM for decompression
+; Using LZMA2/max algorithm reduce size of package on 10%
+Compression=lzma2/max
 
 [Files]
 Source: "@CMAKE_INSTALL_PREFIX@\bin\stellarium.exe"; DestDir: "{app}"
