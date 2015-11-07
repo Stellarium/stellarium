e9e2577ccb2ce5fe1008fd8b69b47c1d63a08fa6
Alexander Wolf <alex.v.wolf@gmail.com>
2015-11-07 23:02:21 +0600

    typo fix in the ISS script

--- stellarium.iss.cmake
+++ stellarium.iss.cmake
@@ -29,7 +29,7 @@ DefaultGroupName=Stellarium
 UninstallDisplayIcon={app}\data\stellarium.ico
 LicenseFile=COPYING
 ; LZMA2/max required 95 MB RAM for compression and 8 MB RAM for decompression
-; Using LZMA2/max algorithm reduce size of package on 10%
+; Using LZMA2/max algorithm reduces size of package on ~10%
 Compression=lzma2/max
 
 [Files]
