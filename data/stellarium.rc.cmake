f29bca273219581a1fb413bfcf34c3b4ce08703a
Alexander Wolf <alex.v.wolf@gmail.com>
2015-11-21 23:10:36 +0600

    Backport stellarium.rc from version 0.15.0

--- data/stellarium.rc.cmake
+++ data/stellarium.rc.cmake
@@ -8,14 +8,21 @@ FILESUBTYPE     0x0L
 BEGIN
     BLOCK "StringFileInfo"
     BEGIN
-        BLOCK "000004B0"
+        BLOCK "040904B0" // Lang=English (United States, 0x0409, decimal 1033), CharSet=Unicode (0x04B0, decimal 1200).
         BEGIN
-            VALUE "CompanyName",      "Stellarium team"
-            VALUE "FileDescription",  "Stellarium is a free open source planetarium"
-            VALUE "FileVersion",      "@PACKAGE_VERSION@"
-            VALUE "LegalCopyright",   "Copyright (C) @COPYRIGHT_YEARS@ Stellarium team"
-            VALUE "ProductName",      "Stellarium"
-            VALUE "ProductVersion",   "@PACKAGE_VERSION@"
+            VALUE "CompanyName",      "Stellarium team\0"
+            VALUE "FileDescription",  "Stellarium is a free open source planetarium\0"
+            VALUE "FileVersion",      "@PACKAGE_VERSION@\0"
+            VALUE "LegalCopyright",   "Copyright (C) @COPYRIGHT_YEARS@ Stellarium team\0"
+            VALUE "Info",             "http://www.stellarium.org/\0"
+            VALUE "ProductVersion",   "@PACKAGE_VERSION@\0"
+            VALUE "ProductName",      "Stellarium\0"
+            VALUE "InternalName",     "stellarium\0"
+            VALUE "OriginalFilename", "stellarium.exe\0"
         END
+    END
+    BLOCK "VarFileInfo"
+    BEGIN
+        VALUE "Translation", 0x0409, 1200
     END
 END
