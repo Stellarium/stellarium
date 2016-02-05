bc456ccf19342dedde17080a6b4572bb6240cd4d
Nick Fedoseev <nick.ut2uz@gmail.com>
2016-02-05 20:20:53 +0600

    Small cosmetic improvement for Search Tool

--- src/gui/SearchDialog.cpp
+++ src/gui/SearchDialog.cpp
@@ -126,6 +126,7 @@ void CompletionLabel::updateText()
 // Start of members for class SearchDialog
 
 const char* SearchDialog::DEF_SIMBAD_URL = "http://simbad.u-strasbg.fr/";
+QString SearchDialog::extSearchText = "";
 
 SearchDialog::SearchDialog(QObject* parent) : StelDialog(parent), simbadReply(NULL)
 {
@@ -735,6 +736,16 @@ bool SearchDialog::eventFilter(QObject*, QEvent *event)
 			return true;
 		}
 	}
+	if (event->type() == QEvent::Show)
+	{
+		if (!extSearchText.isEmpty())
+		{
+			ui->lineEditSearchSkyObject->setText(extSearchText);
+			ui->lineEditSearchSkyObject->selectAll();
+			extSearchText.clear();
+		}
+	}
+
 
 	return false;
 }
