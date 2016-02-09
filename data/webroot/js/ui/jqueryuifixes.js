b5e3306082f5d76797880c9c2d3d774b19a5215f
Florian Schaukowitsch <florian.schaukowitsch@student.tuwien.ac.at>
2016-02-09 20:37:44 +0100

    Spinner inputs in the remote control can now handle keyboard input correctly. A unified event "spinuserinput" can be used to react to both spinning and changed text input (like Qt Spinner's valueChanged) I tried to follow the behaviour of the Qt UI spinners, which seems to be mostly achieved now.

--- data/webroot/js/ui/jqueryuifixes.js
+++ data/webroot/js/ui/jqueryuifixes.js
@@ -15,10 +15,21 @@ define(["jquery", "jquery-ui"], function($, jqueryui) {
 		}
 	});
 
-	//override spinner _adjustValue to disable spinner always rounding to nearest step
 	$.widget("ui.spinner", $.ui.spinner, {
+		//override spinner _adjustValue to round to the Globalize format, if possible
 		_adjustValue: function(value) {
-			var  options = this.options;
+			var options = this.options;
+
+			//convert precision to the given number format, if possible
+			if (window.Globalize && this.options.numberFormat) {
+				//convert to string and parse again
+				//ugly, but working
+				value = Globalize.format(value, this.options.numberFormat, this.options.culture);
+				value = Globalize.parseFloat(value, 10, this.options.culture);
+			} else {
+				//use the default jQuery UI way - this precision depends on min,max and step precision
+				value = parseFloat(value.toFixed(this._precision()));
+			}
 
 			// clamp the value
 			if (options.max !== null && value > options.max) {
@@ -29,6 +40,100 @@ define(["jquery", "jquery-ui"], function($, jqueryui) {
 			}
 
 			return value;
+		},
+
+		_create: function() {
+			this._super();
+
+			//register additional events to react to keyboard input
+			this._on({
+
+				focus: function(event) {
+					//make sure this is correct before input happens
+					this.prevVal = this.value();
+				},
+
+				//ensure number is correctly formatted after focus loss
+				blur: function(event) {
+					this.element.val(this._format(this.value()));
+				},
+
+				keydown: function(event) {
+					this.prevVal = this.value();
+					//prevent leading zeroes
+					var elem = this.element[0];
+					if ('selectionStart' in elem) {
+						if(event.which === 48 && elem.selectionStart === 0 && elem.selectionEnd === 0)
+							event.preventDefault();
+					}
+
+				},
+
+				input: function(event) {
+					//this.value() performs parsing and returns null on invalid value (e.g. when alphabetic input etc.)
+					var val = this.value();
+					//previous is set by jQuery UI on focus
+					//prevVal is set on focus AND keydown for rolling back invalid keyboard/paste input
+					console.log("previous: " + this.previous + ",prevVal: " + this.prevVal + ",val: " + val);
+
+					if (!val) {
+						//value set failed, roll back to previous value
+						val = this.prevVal;
+						this.value(val);
+					} else {
+						//force rounding to precision and do bounds checking
+						var newval = this._adjustValue(val);
+						if (newval !== val) {
+							val = newval;
+							this.value(val);
+						}
+					}
+
+					console.log("val after adjust: " + val);
+
+					if (val !== this.prevVal) {
+						this._trigger("userinput", event, {
+							value: this.value()
+						});
+					}
+				}
+			});
+		},
+
+		//select whole input field when spin is performed
+		_repeat: function(i, steps, event) {
+			this._super(i, steps, event);
+			this.element.select();
+		},
+
+		//override spin to fire input event
+		_spin: function(step, event) {
+			var val = this.value();
+			this._super(step, event);
+			if (val !== this.value())
+				this._trigger("userinput", event, {
+					value: this.value()
+				});
+		},
+
+		//re-implement _value to prevent caret jumping around on input
+		_value: function(value, allowAny) {
+			var parsed;
+			if (value !== "") {
+				parsed = this._parse(value);
+				if (parsed !== null) {
+					if (!allowAny) {
+						parsed = this._adjustValue(parsed);
+					}
+					value = this._format(parsed);
+				}
+			}
+			//add additional check if UI really needs updating
+			if (value !== this.value()) {
+				this.element.val(value);
+				this._refresh();
+			}
 		}
+
 	});
 });
