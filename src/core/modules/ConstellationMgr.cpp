52e4c6433149dee065ec3ea0a2bb336daf58bc0d
georg-zotti <Georg.Zotti@univie.ac.at>
2016-02-14 01:52:16 +0100

    Make action to select singular constellations.

--- src/core/modules/ConstellationMgr.cpp
+++ src/core/modules/ConstellationMgr.cpp
@@ -140,6 +140,8 @@ void ConstellationMgr::init()
 	addAction("actionShow_Constellation_Art", displayGroup, N_("Constellation art"), "artDisplayed", "R");
 	addAction("actionShow_Constellation_Labels", displayGroup, N_("Constellation labels"), "namesDisplayed", "V");
 	addAction("actionShow_Constellation_Boundaries", displayGroup, N_("Constellation boundaries"), "boundariesDisplayed", "B");
+	addAction("actionShow_Constellation_Isolated", displayGroup, N_("Constellation selection isolated"), "isolateSelected"); // no shortcut
+
 	registerProperty("prop_ConstellationMgr_constellationDisplayStyle","constellationDisplayStyle");
 	registerProperty("prop_ConstellationMgr_constellationLineThickness","constellationLineThickness");
 	registerProperty("prop_ConstellationMgr_artIntensity","artIntensity");
