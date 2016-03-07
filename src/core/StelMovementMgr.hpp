f4e39eb8a814665b4aa36e9ab515f2dcca600f27
Georg Zotti <georg.zotti@univie.ac.at>
2016-03-07 02:35:51 +0100

    Preliminary fix for moveToAltAzi(). This is not complete yet, will be worked on further in April.

--- src/core/StelMovementMgr.hpp
+++ src/core/StelMovementMgr.hpp
@@ -174,6 +174,20 @@ public slots:
 	void moveToJ2000(const Vec3d& aim, const Vec3d &aimUp, float moveDuration = 1., ZoomingMode zooming = ZoomNone);
 	void moveToObject(const StelObjectP& target, float moveDuration = 1., ZoomingMode zooming = ZoomNone);
 
+	//! Move the view to a specified AltAzimuthal position.
+	//! @param aim The position to move to expressed as a vector in AltAz frame.
+	//! @param aimUp Up vector in AltAz coordinates. Can be usually (0/0/1) but may have to be exact for looking into the zenith/pole
+	//! @param moveDuration The time it takes for the move to complete.
+	//! @param zooming you want to zoom in, out or not (just center).
+	//! @code
+	//! // You can use the following code most of the times to find a valid aimUp vector:
+	//! StelMovementMgr* mvmgr = GETSTELMODULE(StelMovementMgr);
+	//! mvmgr->moveToAltAzi(pos, Vec3d(0., 0., 1.), mvmgr->getAutoMoveDuration());
+	//! @endcode
+	//! @note Panic function made March 2016. It turned out that using moveToJ2000 for alt-az-based moves behaves odd for long moves during fast timelapse: end vector is linked to the sky!
+	//! As of March 2016: This call does nothing when mount frame is not AltAzi!
+	void moveToAltAzi(const Vec3d& aim, const Vec3d &aimUp, float moveDuration = 1., ZoomingMode zooming = ZoomNone);
+
 	//! Change the zoom level.
 	//! @param aimFov The desired field of view in degrees.
 	//! @param moveDuration The time that the operation should take to complete. [seconds]
@@ -323,13 +337,19 @@ private:
 		Vec3d aim;
 		Vec3d startUp; // The Up vector at start time
 		Vec3d aimUp;   // The Up vector at end time of move
-		float speed;
-		float coef;
-		// If not null, move to the object.
+		float speed;   // set to 1/duration[ms] during automove setup.
+		float coef;    // Set to 0 at begin of an automove, runs up to 1.
+		// If not null, move to the object instead of the aim.
 		StelObjectP targetObject;
+		MountMode mountMode; // In which frame we shall move. This may be different from the frame the display is set to!
+		// The start and aim vectors are given in those coordinates, and are interpolated in the respective reference frames,
+		// then the view vector is derived from the current coef.
+		// AzAlt moves should be set to AltAz mode, else they will move towards the RA/Dec at begin of move which may have moved.
+		// It is an error to move in J2000 or Eq frame with fast timelapse!
+		// This is a March 2016 GZ hack. TODO: This class should be thought over a bit.
 	};
 
-	AutoMove move;          // Current auto movement
+	AutoMove move;          // Current auto movement. 2016-03: During setup, decide upon the frame for motion!
 	bool flagAutoMove;       // Define if automove is on or off
 	ZoomingMode zoomingMode;
 
@@ -356,7 +376,7 @@ private:
 	float beforeTimeDragTimeRate;
 
 	// Time mouse control
-	bool dragTimeMode;
+	bool dragTimeMode; // true during mouse time motion.
 
 	//! @internal
 	//! Store data for auto-zoom.
@@ -382,7 +402,7 @@ private:
 	MountMode mountMode;
 
 	Vec3d initViewPos;        // Default viewing direction
-	Vec3d initViewUp;         // original up vector. Usually 0/0/1, but maybe something else in rare setups.
+	Vec3d initViewUp;         // original up vector. Usually 0/0/1, but maybe something else in rare setups (e.g. Planetarium dome upwards fisheye projection).
 
 	// Viewing direction in equatorial J2000 coordinates
 	Vec3d viewDirectionJ2000;
