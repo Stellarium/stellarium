350c55a23f6ef06a9180878f584dfaac5351c688
Alexander Wolf <alex.v.wolf@gmail.com>
2016-04-24 01:32:29 +0700

    refactoring and cleaning of the code

--- src/core/modules/NebulaMgr.hpp
+++ src/core/modules/NebulaMgr.hpp
@@ -503,10 +503,6 @@ public slots:
 	float getHintsAmount(void) const {return hintsAmount;}
 
 private slots:
-	//! Sets the colors of the Nebula labels and markers according to the
-	//! values in a configuration object
-	void setStelStyle(const QString& section);
-
 	//! Update i18 names from English names according to passed translator.
 	//! The translation is done using gettext with translated strings defined
 	//! in translations.h
