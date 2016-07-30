8cd65a30f8065b685f680b61f2cbe7a5d36880a5
Alexander Wolf <alex.v.wolf@gmail.com>
2016-07-30 23:37:35 +0700

    Updated infrastructure for Windows package

--- src/gui/HelpDialog.cpp
+++ src/gui/HelpDialog.cpp
@@ -278,7 +278,7 @@ void HelpDialog::updateText(void)
 	newHtml = "<h1>" + StelUtils::getApplicationName() + "</h1>";
 	// Note: this legal notice is not suitable for traslation
 	newHtml += QString("<h3>Copyright &copy; %1 Stellarium Developers</h3>").arg(COPYRIGHT_YEARS);
-	newHtml += "<p><em>Version 0.15 is dedicated in memory of our team member Barry Gerdes.</em></p>";
+	// newHtml += "<p><em>Version 0.15 is dedicated in memory of our team member Barry Gerdes.</em></p>";
 	newHtml += "<p>This program is free software; you can redistribute it and/or ";
 	newHtml += "modify it under the terms of the GNU General Public License ";
 	newHtml += "as published by the Free Software Foundation; either version 2 ";
