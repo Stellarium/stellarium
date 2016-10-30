09d9abd807bb4fcc6f8a5b07b69908c93b663fd9
Alexander Wolf <alex.v.wolf@gmail.com>
2016-10-30 21:34:42 +0700

    Fixed distances for Caldwell catalogue and optimization for previous revision for Mio. ly distances

--- src/core/modules/Nebula.cpp
+++ src/core/modules/Nebula.cpp
@@ -271,42 +271,36 @@ QString Nebula::getInfoString(const StelCore *core, const InfoStringGroup& flags
 
 		if (oDistance>0.f)
 		{
-			QString dx, dy, du;
+			QString dx, dy;
+			float dc = 3262.f;
+			int ms = 1;
+			//TRANSLATORS: Unit of measure for distance - kiloparsecs
+			QString dupc = q_("kpc");
+			//TRANSLATORS: Unit of measure for distance - Light Years
+			QString duly = q_("ly");
+
+			if (nType==NebAGx || nType==NebGx || nType==NebRGx || nType==NebIGx || nType==NebQSO || nType==NebPossQSO || nType==NebISM)
+			{
+				dc = 3.262f;
+				ms = 3;
+				//TRANSLATORS: Unit of measure for distance - Megaparsecs
+				dupc = q_("Mpc");
+				//TRANSLATORS: Unit of measure for distance - Millions of Light Years
+				duly = q_("Mio. ly");
+			}
+
 			if (oDistanceErr>0.f)
 			{
 				dx = QString("%1%2%3").arg(QString::number(oDistance, 'f', 3)).arg(QChar(0x00B1)).arg(QString::number(oDistanceErr, 'f', 3));
-				dy = QString("%1%2%3").arg(QString::number(oDistance*3262, 'f', 1)).arg(QChar(0x00B1)).arg(QString::number(oDistanceErr*3262, 'f', 1));
+				dy = QString("%1%2%3").arg(QString::number(oDistance*dc, 'f', ms)).arg(QChar(0x00B1)).arg(QString::number(oDistanceErr*dc, 'f', ms));
 			}
 			else
 			{
 				dx = QString("%1").arg(QString::number(oDistance, 'f', 3));
-				dy = QString("%1").arg(QString::number(oDistance*3262, 'f', 1));
+				dy = QString("%1").arg(QString::number(oDistance*dc, 'f', ms));
 			}
 
-			if (nType==NebAGx || nType==NebGx || nType==NebRGx || nType==NebIGx || nType==NebQSO || nType==NebISM)
-			{
-				if (oDistanceErr>0.f)
-					dy = QString("%1%2%3").arg(QString::number(oDistance*3.262f, 'f', 1)).arg(QChar(0x00B1)).arg(QString::number(oDistanceErr*3.262f, 'f', 1));
-				else
-					dy = QString("%1").arg(QString::number(oDistance*3.262f, 'f', 1));
-				du = QString("%1 (%2 %3)")
-						//TRANSLATORS: Unit of measure for distance - Megaparsecs
-						.arg(q_("Mpc"))
-						.arg(dy)
-						//TRANSLATORS: Unit of measure for distance - Millions of Light Years
-						.arg(q_("Mio. ly"));
-
-			}
-			else
-			{
-				du = QString("%1 (%2 %3)")
-						//TRANSLATORS: Unit of measure for distance - kiloparsecs
-						.arg(q_("kpc"))
-						.arg(dy)
-						//TRANSLATORS: Unit of measure for distance - Light Years
-						.arg(q_("ly"));
-			}
-			oss << q_("Distance: %1 %2").arg(dx).arg(du) << "<br>";
+			oss << q_("Distance: %1 %2 (%3 %4)").arg(dx).arg(dupc).arg(dy).arg(duly) << "<br>";
 		}
 	}
 
