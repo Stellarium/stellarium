e8a408afbb60e52efc4182541d55e48272ab09cc
Alexander Wolf <alex.v.wolf@gmail.com>
2017-01-21 13:03:57 +0700

    Updated build_guide.bat

--- guide/build_guide.bat
+++ guide/build_guide.bat
@@ -3,3 +3,14 @@ makeindex guide.idx -s StyleInd.ist
 biber guide
 pdflatex guide
 pdflatex guide
+del *.aux
+del *.bbl
+del *.bcf
+del *.blg
+del *.idx
+del *.ilg
+del *.ind
+del *.log
+del *.ptc
+del *.run.xml
+del *.toc
