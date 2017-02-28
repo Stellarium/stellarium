ab412d4a569bc82b7a9ee9286235849bb9da9633
Alexander Wolf <alex.v.wolf@gmail.com>
2017-02-28 21:40:48 +0700

    Satellites: Let's start looking to the Iridium on 15 seconds before flash

--- plugins/Satellites/src/gui/SatellitesDialog.cpp
+++ plugins/Satellites/src/gui/SatellitesDialog.cpp
@@ -992,12 +992,14 @@ void SatellitesDialog::predictIridiumFlares()
 
 void SatellitesDialog::selectCurrentIridiumFlare(const QModelIndex &modelIndex)
 {
+	StelCore* core = StelApp::getInstance().getCore();
 	// Find the object
 	QString name = modelIndex.sibling(modelIndex.row(), IridiumFlaresSatellite).data().toString();
 	QString date = modelIndex.sibling(modelIndex.row(), IridiumFlaresDate).data().toString();
 	bool ok;
 	double JD  = StelUtils::getJulianDayFromISO8601String(date.left(10) + "T" + date.right(8), &ok);
-	JD -= StelApp::getInstance().getCore()->getUTCOffset(JD)/24.;
+	JD -= core->getUTCOffset(JD)/24.;
+	JD -= core->JD_SECOND*15; // Set start point on 15 seconds before flash (TODO: should be an option in the GUI?)
 
 	StelObjectMgr* objectMgr = GETSTELMODULE(StelObjectMgr);
 	if (objectMgr->findAndSelectI18n(name) || objectMgr->findAndSelect(name))
