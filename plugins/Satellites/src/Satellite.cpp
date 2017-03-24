039725a9bc0953948eb47d927d84e9698703fcf6
Alexander Wolf <alex.v.wolf@gmail.com>
2017-03-24 09:23:38 +0700

    Fixed COSPAR designator parser

--- plugins/Satellites/src/Satellite.cpp
+++ plugins/Satellites/src/Satellite.cpp
@@ -826,7 +826,7 @@ void Satellite::parseInternationalDesignator(const QString& tle1)
 	Q_ASSERT(!tle1.isEmpty());
 	
 	// The designator is encoded as columns 10-17 on the first line.
-	QString rawString = tle1.mid(9, 8);
+	QString rawString = tle1.mid(9, 6);
 	//TODO: Use a regular expression?
 	bool ok;
 	int year = rawString.left(2).toInt(&ok);
