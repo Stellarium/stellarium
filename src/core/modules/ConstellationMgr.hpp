99a2b87a8aea922c7f9dd089d0eb0c597b3dadcd
Alexander Wolf <alex.v.wolf@gmail.com>
2017-04-05 19:09:40 +0700

    Added support an asterisms for the sky cultures ('big asterisms' are supported at the moment)

--- src/core/modules/ConstellationMgr.hpp
+++ src/core/modules/ConstellationMgr.hpp
@@ -365,7 +365,7 @@ private:
 	Constellation* isStarIn(const StelObject *s) const;
 	Constellation* isObjectIn(const StelObject *s) const;
 	Constellation* findFromAbbreviation(const QString& abbreviation) const;
-	std::vector<Constellation*> asterisms;
+	std::vector<Constellation*> constellations;
 	QFont asterFont;
 	StarMgr* hipStarMgr;
 
