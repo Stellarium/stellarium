43e4248d7e950048d093541c8b6d43cc139789e6
Alexander Wolf <alex.v.wolf@gmail.com>
2017-05-20 22:49:56 +0700

    Updated API docs

--- src/core/modules/SolarSystem.hpp
+++ src/core/modules/SolarSystem.hpp
@@ -686,14 +686,17 @@ public slots:
 
 	//! Set the algorithm for computation of apparent magnitudes for planets in case observer on the Earth.
 	//! Possible values:
-	//! @li @c Planesas (algorithm provided by Pere Planesas (Observatorio Astronomico Nacional))
-	//! @li @c Mueller (G. Mueller, based on visual observations 1877-91. [Expl.Suppl.1961])
-	//! @li @c Harris (Astronomical Almanac 1984 and later. These give V (instrumental) magnitudes)
+	//! @li @c ExpSup1992 (algorithm provided by Pere Planesas (Observatorio Astronomico Nacional) [Explanatory Supplement to the Astronomical Almanac, 1992])
+	//! @li @c Mueller1893 (G. Mueller, based on visual observations 1877-91. [Explanatory Supplement to the Astronomical Almanac, 1961])
+	//! @li @c AstrAlm1984 (Astronomical Almanac 1984 and later. These give V (instrumental) magnitudes)
+	//! @li @c ExpSup2013 Explanatory Supplement to the Astronomical Almanac, 3rd edition, 2013
+	//! @li @c Generic Visual magnitude based on phase angle and albedo.
 	//! Details:
 	//! @li J. Meeus "Astronomical Algorithms" (2nd ed., with corrections as of August 10, 2009) p.283-286.
 	//! @li O. Montenbruck, T. Pfleger "Astronomy on the Personal Computer" (4th ed.) p.143-145.
 	//! @li Daniel L. Harris "Photometry and Colorimetry of Planets and Satellites" http://adsabs.harvard.edu/abs/1961plsa.book..272H
-	//! Hint: Default option in config.ini: astro/apparent_magnitude_algorithm = Harris
+	//! @li P. Kenneth Seidelmann "Explanatory Supplement to the Astronomical Almanac" (3rd edition, 2013)
+	//! Hint: Default option in config.ini: astro/apparent_magnitude_algorithm = ExpSup2013
 	//! @param algorithm the case in-sensitive algorithm name
 	//! @note: The structure of algorithms is almost identical, just the numbers are different! You should activate
 	//! Mueller's algorithm for simulate the eye's impression. (Esp. Venus!)
