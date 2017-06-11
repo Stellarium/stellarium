94cfef9eb9cf3d4b989788c005b6e2451ca5563a
Alexander Wolf <alex.v.wolf@gmail.com>
2017-06-11 17:13:29 +0700

    other couple tiny fixes...

--- plugins/Satellites/src/Satellite.cpp
+++ plugins/Satellites/src/Satellite.cpp
@@ -894,9 +894,7 @@ void Satellite::draw(StelCore* core, StelPainter& painter)
 
 	XYZ = getJ2000EquatorialPos(core);
 	StelSkyDrawer* sd = core->getSkyDrawer();
-	Vec3f drawColor = invisibleSatelliteColor;
-	if (visibility == gSatWrapper::VISIBLE) // Use hintColor for visible satellites only
-		drawColor = hintColor;
+	Vec3f drawColor = (visibility == gSatWrapper::VISIBLE) ? hintColor : invisibleSatelliteColor; // Use hintColor for visible satellites only
 	painter.setColor(drawColor[0], drawColor[1], drawColor[2], hintBrightness);
 
 	Vec3d win;
@@ -971,7 +969,7 @@ void Satellite::drawOrbit(StelCore *core, StelPainter& painter)
 		position = core->altAzToJ2000(orbitPoints[i].toVec3d());
 		position.normalize();
 
-		if (prj->project(position, onscreen)) // check position on the screen
+		if (prj->projectCheck(position, onscreen)) // check position on the screen
 		{
 			vertexArray.append(position);
 			drawColor = (visibilityPoints[i] == gSatWrapper::VISIBLE) ? orbitColor : invisibleSatelliteColor;
