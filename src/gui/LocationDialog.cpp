4a183d6580ceefe7d3f6d23a68a0881550509ecb
Alexander Wolf <alex.v.wolf@gmail.com>
2017-07-09 16:50:19 +0700

    Fixes for support new format of asteroid names

--- src/gui/LocationDialog.cpp
+++ src/gui/LocationDialog.cpp
@@ -344,8 +344,7 @@ void LocationDialog::populatePlanetList()
 
 	QComboBox* planets = ui->planetNameComboBox;
 	SolarSystem* ssystem = GETSTELMODULE(SolarSystem);
-	QStringList planetNames(ssystem->getAllPlanetEnglishNames());
-	const StelTranslator& trans = StelApp::getInstance().getLocaleMgr().getSkyTranslator();
+	QList<PlanetP> ss = ssystem->getAllPlanets();
 
 	//Save the current selection to be restored later
 	planets->blockSignals(true);
@@ -354,9 +353,9 @@ void LocationDialog::populatePlanetList()
 	planets->clear();
 	//For each planet, display the localized name and store the original as user
 	//data. Unfortunately, there's no other way to do this than with a cycle.
-	foreach(const QString& name, planetNames)
+	foreach(const PlanetP& p, ss)
 	{
-		planets->addItem(trans.qtranslate(name), name);
+		planets->addItem(p->getNameI18n(), p->getEnglishName());
 	}
 	//Restore the selection
 	index = planets->findData(selectedPlanetId, Qt::UserRole, Qt::MatchCaseSensitive);
