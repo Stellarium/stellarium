1131631a4835e38074cd017e09ecd31a60f46bff
Teresa Huertas <teresahuertas@gmail.com>
2017-07-27 20:19:11 +0200

    Add a new data

--- src/core/modules/SolarSystem.hpp
+++ src/core/modules/SolarSystem.hpp
@@ -5,7 +5,7 @@
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
+ * as published by the Free Software Foundation; either version
  * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
