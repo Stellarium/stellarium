adbf43ed34c1b407ff3e326697243f537a396542
Alexander Wolf <alex.v.wolf@gmail.com>
2017-08-10 23:39:28 +0700

    Added addendum checks for network connections

--- plugins/Supernovae/src/Supernovae.cpp
+++ plugins/Supernovae/src/Supernovae.cpp
@@ -594,11 +594,7 @@ void Supernovae::updateJSON(void)
 void Supernovae::updateDownloadComplete(QNetworkReply* reply)
 {
 	// check the download worked, and save the data to file if this is the case.
-	if (reply->error() != QNetworkReply::NoError)
-	{
-		qWarning() << "[Supernovae] FAILED to download" << reply->url() << " Error: " << reply->errorString();
-	}
-	else
+	if (reply->error() == QNetworkReply::NoError && reply->bytesAvailable()>0)
 	{
 		// download completed successfully.
 		QString jsonFilePath = StelFileMgr::findFile("modules/Supernovae", StelFileMgr::Flags(StelFileMgr::Writable|StelFileMgr::Directory)) + "/supernovae.json";
@@ -617,6 +613,8 @@ void Supernovae::updateDownloadComplete(QNetworkReply* reply)
 			jsonFile.close();
 		}
 	}
+	else
+		qWarning() << "[Supernovae] FAILED to download" << reply->url() << " Error: " << reply->errorString();
 
 	if (progressBar)
 	{
