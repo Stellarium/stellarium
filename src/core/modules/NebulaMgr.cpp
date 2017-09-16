1e669a2eaffc40271efc00a721b2ef0ef9624843
Alexander Wolf <alex.v.wolf@gmail.com>
2017-09-16 15:01:56 +0700

    Refactor drawing outlines of DSO

--- src/core/modules/NebulaMgr.cpp
+++ src/core/modules/NebulaMgr.cpp
@@ -436,6 +436,7 @@ struct DrawNebulaFuncObject
 			sPainter->getProjector()->project(n->XYZ,n->XY);
 			n->drawLabel(*sPainter, maxMagLabels);
 			n->drawHints(*sPainter, maxMagHints);
+			n->drawOutlines(*sPainter, maxMagHints);
 		}
 	}
 	float maxMagHints;
@@ -481,8 +482,8 @@ void NebulaMgr::draw(StelCore* core)
 
 	sPainter.setBlending(true, GL_ONE, GL_ONE);
 
-	// Use a 1 degree margin
-	const double margin = 1.*M_PI/180.*prj->getPixelPerRadAtCenter();
+	// Use a 4 degree margin (esp. for wide outlines)
+	const double margin = 4.*M_PI/180.*prj->getPixelPerRadAtCenter();
 	const SphericalRegionP& p = prj->getViewportConvexPolygon(margin, margin);
 
 	// Print all the nebulae of all the selected zones
