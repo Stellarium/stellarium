b2df051fe6d73a13081221ae02a901eff8ea2e96
Alexander Wolf <alex.v.wolf@gmail.com>
2017-09-26 15:42:50 +0700

    Fixed context support for constellations

--- src/core/modules/ConstellationMgr.cpp
+++ src/core/modules/ConstellationMgr.cpp
@@ -853,7 +853,7 @@ void ConstellationMgr::updateI18n()
 	vector < Constellation * >::const_iterator iter;
 	for (iter = constellations.begin(); iter != constellations.end(); ++iter)
 	{
-		(*iter)->nameI18 = trans.qtranslate((*iter)->englishName);
+		(*iter)->nameI18 = trans.qtranslate((*iter)->englishName, (*iter)->context);
 	}
 }
 
