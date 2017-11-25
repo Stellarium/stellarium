54e1798b92adc55adc5a7e1f7fa471516601fb5f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2017-11-25 10:43:48 +0100

    TelescopeClientINDI class created

---
+++ plugins/TelescopeControl/src/clients/TelescopeClientINDI.cpp
@@ -0,0 +1,6 @@
+#include "TelescopeClientINDI.h"
+
+TelescopeClientINDI::TelescopeClientINDI()
+{
+
+}
---
+++ plugins/TelescopeControl/src/clients/TelescopeClientINDI.hpp
@@ -0,0 +1,11 @@
+#ifndef TELESCOPECLIENTINDI_H
+#define TELESCOPECLIENTINDI_H
+
+
+class TelescopeClientINDI
+{
+public:
+    TelescopeClientINDI();
+};
+
+#endif // TELESCOPECLIENTINDI_H
