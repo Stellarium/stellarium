bd2a6180873bacbe173b1dc1d217eca7246e1011
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2017-11-26 19:31:27 +0100

    refactoring

--- plugins/TelescopeControl/src/clients/TelescopeClientINDI.hpp
+++ plugins/TelescopeControl/src/clients/TelescopeClientINDI.hpp
@@ -23,18 +23,13 @@
 
 #include "INDIConnection.hpp"
 
-class TelescopeClientINDI final : public TelescopeClient
+class TelescopeClientINDI : public TelescopeClient
 {
 public:
     TelescopeClientINDI(const QString &name, const QString& params);
     ~TelescopeClientINDI();
 
-    // StelObject interface
-public:
     Vec3d getJ2000EquatorialPos(const StelCore *core) const override;
-
-    // TelescopeClient interface
-public:
     void telescopeGoto(const Vec3d &j2000Pos, StelObjectP selectObject) override;
     bool isConnected() const override;
     bool hasKnownPosition() const override;
