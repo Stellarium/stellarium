183b399503b960686319b68085ca2f6be2c635cb
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2017-12-05 07:21:44 +0100

    [TCP] INDI test executable created

---
+++ plugins/TelescopeControl/src/INDI/test/testTelescopeControl_INDI.hpp
@@ -0,0 +1,19 @@
+#ifndef TESTTELESCOPECONTROL_INDI_HPP
+#define TESTTELESCOPECONTROL_INDI_HPP
+
+#include <QtTest>
+
+#include "TelescopeClientINDI.hpp"
+
+class testTelescopeControl_INDI : public QObject
+{
+    Q_OBJECT
+
+public:
+    testTelescopeControl_INDI();
+
+private:
+
+};
+
+#endif // TESTTELESCOPECONTROL_INDI_HPP
