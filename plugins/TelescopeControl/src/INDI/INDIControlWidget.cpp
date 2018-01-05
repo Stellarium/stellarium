74b5acb7af25a38cfbdd048b174ab43b52721627
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-05 18:25:45 +0100

    [TCP] TelescopeMoveWidget implementation copied in INDIControlWidget

--- plugins/TelescopeControl/src/INDI/INDIControlWidget.cpp
+++ plugins/TelescopeControl/src/INDI/INDIControlWidget.cpp
@@ -6,9 +6,66 @@ INDIControlWidget::INDIControlWidget(QWidget *parent) :
     ui(new Ui::INDIControlWidget)
 {
     ui->setupUi(this);
+
+	QObject::connect(ui->northButton, &QPushButton::pressed, this, &INDIControlWidget::onNorthButtonPressed);
+	QObject::connect(ui->northButton, &QPushButton::released, this, &INDIControlWidget::onNorthButtonReleased);
+	QObject::connect(ui->eastButton, &QPushButton::pressed, this, &INDIControlWidget::onEastButtonPressed);
+	QObject::connect(ui->eastButton, &QPushButton::released, this, &INDIControlWidget::onEastButtonReleased);
+	QObject::connect(ui->southButton, &QPushButton::pressed, this, &INDIControlWidget::onSouthButtonPressed);
+	QObject::connect(ui->southButton, &QPushButton::released, this, &INDIControlWidget::onSouthButtonReleased);
+	QObject::connect(ui->westButton, &QPushButton::pressed, this, &INDIControlWidget::onWestButtonPressed);
+	QObject::connect(ui->westButton, &QPushButton::released, this, &INDIControlWidget::onWestButtonReleased);
 }
 
 INDIControlWidget::~INDIControlWidget()
 {
     delete ui;
 }
+
+void INDIControlWidget::onNorthButtonPressed()
+{
+	emit move(0, speed());
+}
+
+void INDIControlWidget::onNorthButtonReleased()
+{
+	emit move(0, 0);
+}
+
+void INDIControlWidget::onEastButtonPressed()
+{
+	emit move(90, speed());
+}
+
+void INDIControlWidget::onEastButtonReleased()
+{
+	emit move(90, 0);
+}
+
+void INDIControlWidget::onSouthButtonPressed()
+{
+	emit move(180, speed());
+}
+
+void INDIControlWidget::onSouthButtonReleased()
+{
+	emit move(180, 0);
+}
+
+void INDIControlWidget::onWestButtonPressed()
+{
+	emit move(270, speed());
+}
+
+void INDIControlWidget::onWestButtonReleased()
+{
+	emit move(270, 0);
+}
+
+double INDIControlWidget::speed() const
+{
+	double speed = ui->speedSlider->value();
+	speed /= ui->speedSlider->maximum();
+	return speed;
+}
+
--- plugins/TelescopeControl/src/INDI/INDIControlWidget.hpp
+++ plugins/TelescopeControl/src/INDI/INDIControlWidget.hpp
@@ -15,7 +15,26 @@ public:
     explicit INDIControlWidget(QWidget *parent = 0);
     ~INDIControlWidget();
 
+signals:
+	void moveNorth(int speed);
+	void moveEast(int speed);
+	void moveWest(int speed);
+	void moveSouth(int speed);
+	void move(double angle, double speed);
+
+private slots:
+	void onNorthButtonPressed();
+	void onNorthButtonReleased();
+	void onEastButtonPressed();
+	void onEastButtonReleased();
+	void onSouthButtonPressed();
+	void onSouthButtonReleased();
+	void onWestButtonPressed();
+	void onWestButtonReleased();
+
 private:
+	double speed() const;
+
     Ui::INDIControlWidget *ui;
 };
 
