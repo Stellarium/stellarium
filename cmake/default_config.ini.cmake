640463c840e11d2a47d334ad6cc7e3ce4d71b1dd
Georg Zotti <Georg.Zotti@univie.ac.at>
2018-04-21 15:47:10 +0200

    GPS fixes (4): config defaults and Guide updates.

--- cmake/default_config.ini.cmake
+++ cmake/default_config.ini.cmake
@@ -117,10 +117,10 @@ auto_hide_horizontal_toolbar        = true
 auto_hide_vertical_toolbar          = true
 flag_enable_kinetic_scrolling       = true
 # These values are used on non-Windows systems supporting GPSD
-gpsd_hostname                       = "localhost"
+gpsd_hostname                       = localhost
 gpsd_port                           = 2947
 # These values are used on Windows only.
-gps_interface                       = "COM3"
+gps_interface                       = COM3
 gps_baudrate                        = 4800
 
 [color]
