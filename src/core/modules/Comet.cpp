369ee86708f0c2c6e6b955aa61118864aecfae4f
Georg Zotti <Georg.Zotti@univie.ac.at>
2018-06-16 10:06:55 +0200

    Print diameter, not "equatorial radius", for comets.
    For such irregular bodies, e.r. does not make too much sense.

--- src/core/modules/Comet.cpp
+++ src/core/modules/Comet.cpp
@@ -310,7 +310,8 @@ QString Comet::getInfoString(const StelCore *core, const InfoStringGroup &flags)
 
 	if (flags&Size)
 	{
-		oss << QString("%1: %2 %3").arg(q_("Equatorial radius of core"), QString::number(AU * getRadius(), 'f', 1) , qc_("km", "distance")) << "<br />";
+		// Given the very irregular shape, other terminology like "equatorial radius" do not make much sense.
+		oss << QString("%1: %2 %3").arg(q_("Core diameter"), QString::number(AU * 2.0 * getRadius(), 'f', 1) , qc_("km", "distance")) << "<br />";
 	}
 
 	postProcessInfoString(str, flags);
