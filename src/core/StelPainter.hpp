10c2c73033edaa1f788bbcc3f0aa411ab1e1779a
Guillaume Chereau <guillaume@noctua-software.com>
2018-08-04 17:57:56 +0800

    StelPainter: add saturation effect
    Add a new method of StelPainter: setSaturationEffect(float v), that
    allows to adjust the saturation of color textures.
    
    I haven't tested the speed much, I hope this is not too slow.

--- src/core/StelPainter.hpp
+++ src/core/StelPainter.hpp
@@ -260,6 +260,10 @@ public:
 	//! Sets the line width. Default is 1.0f.
 	void setLineWidth(float width);
 
+	//! Sets the color saturation effect value, from -1 (no saturation) to +1 (full saturation).
+	//! The default value of 0 means no adjustement.
+	void setSaturationEffect(float v) { saturation = v; }
+
 	//! Create the OpenGL shaders programs used by the StelPainter.
 	//! This method needs to be called once at init.
 	static void initGLShaders();
@@ -394,7 +398,9 @@ private:
 	QFont currentFont;
 
 	Vec4f currentColor;
-	
+	//! Saturation effect adjustment.
+	float saturation = 0.f;
+
 	static QOpenGLShaderProgram* basicShaderProgram;
 	struct BasicShaderVars {
 		int projectionMatrix;
@@ -426,6 +432,7 @@ private:
 		int texture;
 		int bayerPattern;
 		int rgbMaxValue;
+		int saturation;
 	};
 	static TexturesColorShaderVars texturesColorShaderVars;
 
