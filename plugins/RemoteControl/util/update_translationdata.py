4a0155f71cb9b83b32bf884aaa1ce9b62180290d
Iceflower <iceflower@gmx.de>
2019-03-15 02:16:59 +0100

    Update update_translationdata.py

--- plugins/RemoteControl/util/update_translationdata.py
+++ plugins/RemoteControl/util/update_translationdata.py
@@ -4,41 +4,46 @@
 
 import re
 import sys
+from pathlib import Path
 
 
 def main():
     '''
-    main expects 2 arguments:
+    main expects 2 arguments: <stellarium-remotecontrol.jst> <translationdata.js>
     '''
 
     if len(sys.argv) < 3:
         print("usage: update_translationdata.py <stellarium-remotecontrol.jst> <translationdata.js>")
         print(sys.argv)
         return
-    potfile = open(sys.argv[1], 'r')
-    jsfile = open(sys.argv[2], 'w')
 
-    p = re.compile(r'^msgid "([^"\\]*(?:\\.[^"\\]*)*)"$', re.MULTILINE)
+    jst_file = Path(sys.argv[1])
+    js_file = Path(sys.argv[2])
 
-    jsfile.write(
-        """
-    //This file is generated automatically by the stellarium-remotecontrol-update-translationdata target through update_translationdata.py from stellarium-remotecontrol.jst
-    //It contains all strings that can be translated through the StelTranslator in the JavaScript code by calling Main.tr()
-    //When this file is requested through the RemoteControl web server, the strings are translated using the current Stellarium app language
-    
-    define({
-        """)
+    if not jst_file.exists():
+        print(f"'{jst_file}' do not exist.")
+        return
+    if not js_file.exists():
+        print(f"'{js_file}' do not exist.")
+        return
 
-    first = True
+    msg_pattern = re.compile(r'^msgid "([^"\\]*(?:\\.[^"\\]*)*)"$', re.MULTILINE)
 
-    for i in p.finditer(potfile.read()):
-        if i.group(1):
-            if not first:
-                jsfile.write(',\n\t')
-            jsfile.write(
-                '"' + i.group(1).replace('"', '\\"') + '" : \'<?= tr("' + i.group(1).replace("'", "\\'") + '")?>\'')
-            first = False
-    jsfile.write("\n});")
+    with open(js_file, mode='w', encoding='utf-8') as js_writer:
+        js_writer.write(
+            """//This file is generated automatically by the stellarium-remotecontrol-update-translationdata target through update_translationdata.py from stellarium-remotecontrol.jst
+//It contains all strings that can be translated through the StelTranslator in the JavaScript code by calling Main.tr()
+//When this file is requested through the RemoteControl web server, the strings are translated using the current Stellarium app language
+
+define({
+""")
+        with open(jst_file, mode='r', encoding='utf-8') as jst_reader:
+            for find in msg_pattern.finditer(jst_reader.read()):
+                if find.group(1):
+                    key = find.group(1).replace('"', '\\"')
+                    value = find.group(1).replace("'", "\\'")
+                    js_writer.write(f'\t"{key}" : \'<?= tr("{value}")?>\',\n')
+        js_writer.write("});")
 
 
 if __name__ == '__main__':
