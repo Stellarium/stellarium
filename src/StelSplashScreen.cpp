94b450797a28835af2bbd045ef3248c0d97b3394
Ruslan Kabatsayev <b7.10110111@gmail.com>
2019-07-20 21:32:59 +0300

    Make splash screen more informative
    Now the splash screen provides some clue as to what Stellarium is actually
    doing during its not so fast loading process. Changing messages let the
    users feel that the process has not hung, and is still in progress.

---
+++ src/StelSplashScreen.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2019 Ruslan Kabatsayev
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+ */
+
+#include "StelSplashScreen.hpp"
+#include "StelFileMgr.hpp"
+#include "StelUtils.hpp"
+
+#include <QPainter>
+
+SplashScreen::SplashScreenWidget* SplashScreen::instance;
+
+static QPixmap makePixmap()
+{
+	QPixmap pixmap(StelFileMgr::findFile("data/splash.png"));
+	QPainter p(&pixmap);
+	p.setPen(Qt::white);
+	QFontMetrics metrics(p.font());
+	p.drawText(QPointF(metrics.averageCharWidth(), 1.3*metrics.height()), StelUtils::getApplicationVersion());
+	return pixmap;
+}
+
+void SplashScreen::present()
+{
+	static SplashScreenWidget splash(makePixmap());
+	instance=&splash;
+	splash.show();
+	splash.ensureFirstPaint();
+}
+
+void SplashScreen::showMessage(QString const& message)
+{
+	Q_ASSERT(instance);
+	instance->showMessage(message, Qt::AlignLeft|Qt::AlignBottom, Qt::white);
+}
+
+void SplashScreen::finish(QWidget* mainWindow)
+{
+	Q_ASSERT(instance);
+	instance->finish(mainWindow);
+}
+
+void SplashScreen::clearMessage()
+{
+	Q_ASSERT(instance);
+	instance->clearMessage();
+}
