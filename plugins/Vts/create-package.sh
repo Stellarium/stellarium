ac487187c8da8598598d70574389823efe6dde9e
Guillaume Chereau <guillaume@noctua-software.com>
2019-11-13 15:43:58 +0100

    Vts: copy more files

--- plugins/Vts/create-package.sh
+++ plugins/Vts/create-package.sh
@@ -19,6 +19,11 @@ cd -
 
 echo Copy all files to \"$OUT\"
 
+for file in landscapes nebulae skycultures stars textures data
+do
+    cp -rf $SCRIPT_DIR/../../$file $OUT/bin/
+done
+
 mkdir -p $OUT/bin $OUT/doc
 cp $SCRIPT_DIR/data/launcherStellarium.sh $OUT/bin/
 cp $SCRIPT_DIR/data/stellariumVtsConf.ini $OUT/doc/
@@ -36,5 +41,8 @@ done
 cp $QT_DIR/lib/libicui18n.so.56 $OUT/bin/
 cp $QT_DIR/lib/libicuuc.so.56 $OUT/bin/
 cp $QT_DIR/lib/libicudata.so.56 $OUT/bin/
+
+cp -rf $QT_DIR/plugins/xcbglintegrations $OUT/bin/
+cp -rf $QT_DIR/plugins/platforms $OUT/bin/
 
 cp $SCRIPT_DIR/../../data/icons/128x128/stellarium.png $OUT/doc/icon.png
