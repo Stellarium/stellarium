e9ceeeaae7e61c3dfa91d866c4161c2901569578
Georg Zotti <Georg.Zotti@univie.ac.at>
2019-12-14 12:43:23 +0100

    Allow suppression of a warning when a StelProperty cannot be found. - Should be used when requesting plugin properties!

--- src/core/StelCore.cpp
+++ src/core/StelCore.cpp
@@ -2456,7 +2456,7 @@ QString StelCore::getCurrentDeltaTAlgorithmValidRangeDescription(const double JD
 // TODO2: This could be moved to the SkyDrawer or even some GUI class, as it is used to decide a GUI thing.
 bool StelCore::isBrightDaylight() const
 {
-	if (propMgr->getStelPropertyValue("Oculars.enableOcular").toBool())
+	if (propMgr->getStelPropertyValue("Oculars.enableOcular", true).toBool())
 		return false;
 	SolarSystem* ssys = GETSTELMODULE(SolarSystem);
 	if (!ssys->getFlagPlanets())
