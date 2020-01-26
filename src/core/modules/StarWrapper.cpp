ed7208eb0c44e9d1755f60f87e5c5a193c67d31d
Georg Zotti <Georg.Zotti@univie.ac.at>
2020-01-26 18:31:58 +0100

    Fix a format string

--- src/core/modules/StarWrapper.cpp
+++ src/core/modules/StarWrapper.cpp
@@ -331,7 +331,7 @@ QString StarWrapper1::getInfoString(const StelCore *core, const InfoStringGroup&
 		if (pa<0)
 			pa += 360.f;
 
-		oss << QString("%1: %2%3 %4 %5%6").arg(q_("Proper motion"))
+		oss << QString("%1: %2 %3 %4 %5%6").arg(q_("Proper motion"))
 		       .arg(QString::number(std::sqrt(dx*dx + dy*dy), 'f', 1)).arg(qc_("mas/yr", "milliarc second per year"))
 		       .arg(qc_("towards", "into the direction of")).arg(QString::number(pa, 'f', 1)).arg(QChar(0x00B0)) << "<br />";
 		oss << QString("%1: %2 %3 (%4)").arg(q_("Proper motions by axes")).arg(QString::number(dx, 'f', 1)).arg(QString::number(dy, 'f', 1)).arg(qc_("mas/yr", "milliarc second per year")) << "<br />";
