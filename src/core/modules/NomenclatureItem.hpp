0588a06311f0ab827ce8ca1d85313bd0d88edd45
Alexander Wolf <alex.v.wolf@gmail.com>
2020-01-28 23:16:32 +0700

    Typofix.
    Thanks to Georg for notification!

--- src/core/modules/NomenclatureItem.hpp
+++ src/core/modules/NomenclatureItem.hpp
@@ -102,7 +102,7 @@ public:
 		niReticulum			= 54, // type="reticulum"
 		niSatelliteFeature		= 55, // type="satellite feature"
 		niTessera				= 56, // type="tessera"
-		niSaxa				= 56  // type="saxa"
+		niSaxa				= 57  // type="saxa"
 	};
 
 	NomenclatureItem(PlanetP nPlanet, int nId, const QString& nName, const QString& nContext, NomenclatureItemType nItemType, float nLatitude, float nLongitude, float nSize);
