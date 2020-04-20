69acd425b37fbb7308888150450787479b2bbfaa
Alexander Wolf <alex.v.wolf@gmail.com>
2020-04-20 20:26:47 +0700

    Release v0.20.1

--- cmake/stellarium.iss.cmake
+++ cmake/stellarium.iss.cmake
@@ -5,6 +5,7 @@
 [Setup]
 @ISS_ARCHITECTURE_SPECIFIC@
 DisableStartupPrompt=yes
+DisableDirPage=no
 WizardSmallImageFile=@CMAKE_SOURCE_DIR@\data\icon.bmp
 WizardImageFile=@CMAKE_SOURCE_DIR@\data\splash.bmp
 WizardImageStretch=no
