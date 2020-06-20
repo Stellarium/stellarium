66f577c53b4fc7d3c7266878513616aece3255f0
Georg Zotti <Georg.Zotti@univie.ac.at>
2020-06-21 01:23:47 +0200

    small cleanup

--- src/core/StelObject.cpp
+++ src/core/StelObject.cpp
@@ -118,13 +118,9 @@ float StelObject::getParallacticAngle(const StelCore* core) const
 // Checking position an object above mathematical horizon for current location
 bool StelObject::isAboveHorizon(const StelCore *core) const
 {
-	bool r = true;
 	float az, alt;
 	StelUtils::rectToSphe(&az, &alt, getAltAzPosAuto(core));
-	if (alt < 0.f)
-		r = false;
-
-	return r;
+	return (alt >= 0.f);
 }
 
 // Checking position an object above real horizon for current location
@@ -220,10 +216,7 @@ Vec3f StelObject::computeRTSTime(StelCore *core) const
 
 float StelObject::getSelectPriority(const StelCore* core) const
 {
-	float extMag = getVMagnitudeWithExtinction(core);
-	if (extMag>15.f)
-		extMag=15.f;
-	return extMag;
+	return qMin(getVMagnitudeWithExtinction(core), 15.0f);
 }
 
 float StelObject::getVMagnitudeWithExtinction(const StelCore* core) const
