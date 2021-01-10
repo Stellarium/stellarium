fa2fafb48b263f7c9faf7150d42c5f9ead3cc51a
Ruslan Kabatsayev <b7.10110111@gmail.com>
2021-01-10 21:49:41 +0300

    Split xyYToRGB tone reproducer function from the atmosphere vertex shader

--- data/shaders/xyYToRGB.glsl
+++ data/shaders/xyYToRGB.glsl
@@ -20,62 +20,13 @@
 const highp float pi = 3.1415926535897931;
 const highp float ln10 = 2.3025850929940459;
 
-// Variable for the xyYTo RGB conversion
 uniform highp float alphaWaOverAlphaDa;
 uniform highp float oneOverGamma;
 uniform highp float term2TimesOneOverMaxdLpOneOverGamma;
 uniform highp float brightnessScale;
 
-// Variables for the color computation
-uniform highp vec3 sunPos;
-uniform mediump float term_x, Ax, Bx, Cx, Dx, Ex;
-uniform mediump float term_y, Ay, By, Cy, Dy, Ey;
-
-// The current projection matrix
-uniform mediump mat4 projectionMatrix;
-
-// Contains the 2d position of the point on the screen (before multiplication by the projection matrix)
-attribute mediump vec2 skyVertex;
-
-// Contains the r,g,b,Y (luminosity) components.
-attribute highp vec4 skyColor;
-
-// The output variable passed to the fragment shader
-varying mediump vec3 resultSkyColor;
-
-void main()
+vec3 xyYToRGB(highp float x, highp float y, highp float Y)
 {
-	gl_Position = projectionMatrix*vec4(skyVertex, 0., 1.);
-
-	///////////////////////////////////////////////////////////////////////////
-	// First compute the xy color component
-	// skyColor contains the unprojected vertex position in r,g,b
-	// + the Y (luminance) component of the color in the alpha channel
-	highp vec3 unprojectedVertex = skyColor.xyz;
-	highp float Y = skyColor.w;
-	highp float x,y;
-	if (Y>0.01)
-	{
-		highp float cosDistSunq = dot(sunPos,unprojectedVertex);
-		highp float distSun=acos(cosDistSunq);
-		highp float oneOverCosZenithAngle = (unprojectedVertex.z==0.) ? 9999999999999. : 1. / unprojectedVertex.z;
-
-		cosDistSunq*=cosDistSunq;
-		x = term_x * (1. + Ax * exp(Bx*oneOverCosZenithAngle))* (1. + Cx * exp(Dx*distSun) + Ex * cosDistSunq);
-		y = term_y * (1. + Ay * exp(By*oneOverCosZenithAngle))* (1. + Cy * exp(Dy*distSun) + Ey * cosDistSunq);
-		if (x < 0. || y < 0.)
-		{
-			x = 0.25;
-			y = 0.25;
-		}
-	}
-	else
-	{
-		x = 0.25;
-		y = 0.25;
-	}
-
-
 	///////////////////////////////////////////////////////////////////////////
 	// Now we have the xyY components, need to convert to RGB
 
@@ -88,7 +39,7 @@ void main()
 		// special case for s = 0 (x=0.25, y=0.25)
 		Y *= 0.5121445;
 		Y = pow(abs(Y*pi*1e-4), alphaWaOverAlphaDa*oneOverGamma)* term2TimesOneOverMaxdLpOneOverGamma;
-		resultSkyColor = vec3(0.787077, 0.9898434, 1.9256125) * Y * brightnessScale;
+		return vec3(0.787077, 0.9898434, 1.9256125) * Y * brightnessScale;
 	}
 	else
 	{
@@ -117,7 +68,6 @@ void main()
 		const highp mat3 XYZ2sRGBl=mat3(vec3(3.2404542,-0.9692660,0.0556434),
 										vec3(-1.5371385,1.8760108,-0.2040259),
 										vec3(-0.4985314,0.0415560,1.0572252));
-		resultSkyColor = XYZ2sRGBl * XYZ;
-		resultSkyColor *= brightnessScale;
+		return XYZ2sRGBl * XYZ * brightnessScale;
 	}
 }
