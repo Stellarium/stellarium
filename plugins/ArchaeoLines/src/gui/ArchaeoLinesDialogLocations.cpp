715d79f9ed5b703fa4ca55dd057a85e782e8fc66
Georg Zotti <Georg.Zotti@univie.ac.at>
2021-03-30 23:44:43 +0200

    Hide unnecessary buttons

--- plugins/ArchaeoLines/src/gui/ArchaeoLinesDialogLocations.cpp
+++ plugins/ArchaeoLines/src/gui/ArchaeoLinesDialogLocations.cpp
@@ -80,9 +80,11 @@ void ArchaeoLinesDialogLocations::createDialogContent()
 	connect(ui->citySearchLineEdit, SIGNAL(textChanged(const QString&)), proxyModel, SLOT(setFilterWildcard(const QString&)));
 	connect(ui->citiesListView, SIGNAL(clicked(const QModelIndex&)), this, SLOT(setLocationFromList(const QModelIndex&)));
 
-	// TODO: This can probably be deleted.
-	connect(ui->setCustomLocationPushButton, SIGNAL(clicked()), this, SLOT(setLocationFromList()));
-	connect(ui->cancelPushButton, SIGNAL(clicked()), this, SLOT(close()));
+	// TODO: These can probably be deleted.
+	//connect(ui->setCustomLocationPushButton, SIGNAL(clicked()), this, SLOT(setLocationFromList()));
+	//connect(ui->cancelPushButton, SIGNAL(clicked()), this, SLOT(close()));
+	ui->setCustomLocationPushButton->hide();
+	ui->cancelPushButton->hide();
 
 }
 
