a1fe618ed7b456f8304fe04a6f224ea8bf2d7cea
Alexander Belopolsky <a@enlnt.com>
2021-05-09 11:01:16 -0400

    Regenerate dso_names.fab.
    $ python3.9 make_names.py
    No HIP number found for HR 8417 (17 xi Cep)
    No HIP number found for HR 5506 (36 eps Boo)
    No HIP number found for HR 5478 (30 zet Boo)
    No HIP number found for HR 7417 (6 bet1 Cyg)
    No HIP number found for HR 7751 (32 Cyg)
    No HIP number found for HR 854 (18 tau Per)
    No HIP number found for HR 6733 (69 tau Oph)
    No HIP number found for HR 8131 (8 alp Equ)
    No HIP number found for HR 888 (48 eps Ari)
    No HIP number found for HR 2891 (66 alp Gem)
    No HIP number found for HR 5978 (xi Sco)
    No HIP number found for HR 7776 (9 bet Cap)
    No HIP number found for HR 8817 (89 Aqr)
    No HIP number found for HR 5605 (pi Lup)

--- skycultures/almagest/make_names.py
+++ skycultures/almagest/make_names.py
@@ -1,8 +1,15 @@
 import sys
+STARS_FILE = 'star_names.fab'
+DSO_FILE = 'dso_names.fab'
 # HIP           SAO     HD      HR
 CROSS_ID = '../../stars/default/cross-id.dat'
 HIP = 0
 HR = 4
+DSO = {
+    'NGC 869/884': ['NGC 869', 'NGC 884'],
+    'NGC 2632 M44': ['M 44'],
+    'NGC 5139': ['NGC 5139'],
+}
 
 def parse_cross_id(path):
     hr_to_hip = {}
@@ -18,10 +25,9 @@ def parse_cross_id(path):
 
 CAT_FILE = 'almstars/cat1.dat'
 CAT_HR = slice(30, 34)
-CAT_ID = slice(36, 48)
+CAT_ID = slice(35, 48)
 CAT_DESC = slice(48, None)
 def parse_cat(path):
-    hr_desc = []
     with open(path) as f:
         next(f, None)   # skip header line
         for line in f:
@@ -31,11 +37,15 @@ def parse_cat(path):
             yield hr, modern_id, desc
 
 hr_to_hip = parse_cross_id(CROSS_ID)
-
-for hr, modern_id, desc in parse_cat(CAT_FILE):
-
-    try:
-        hip = hr_to_hip[hr]
-        print(f'{hip}|_("{desc}")')
-    except KeyError:
-        print(f'No HIP number found for HR {hr} ({modern_id})', file=sys.stderr)
+with open(STARS_FILE, 'w') as stars, open(DSO_FILE, 'w') as dso:
+    for hr, modern_id, desc in parse_cat(CAT_FILE):
+        tag = hr_to_hip.get(hr)
+        if tag is not None:
+            print(f'{tag}|_("{desc}")', file=stars)
+            continue
+        tags = DSO.get(modern_id)
+        if tags is not None:
+            for tag in tags:
+                print(f'{tag}|_("{desc}")', file=dso)
+        else:
+            print(f'No HIP number found for HR {hr} ({modern_id})', file=sys.stderr)
