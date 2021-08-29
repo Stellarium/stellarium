1ba8291fba4d0fe681e34d691ddd973d206a2f5b
Alexander V. Wolf <alex.v.wolf@gmail.com>
2021-08-29 18:10:45 +0700

    Update translations data

---
+++ src/translations_regions.h
@@ -0,0 +1,406 @@
+/*
+ * Stellarium
+ * Copyright (C) 2021 Alexander Wolf
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+ */
+
+class GeoRegionsTranslations
+{
+	void GeoRegionsTranslations(){;}
+
+	static translationGeoRegions()
+	{
+		// Generate Gettext strings for traduction
+		Q_ASSERT(0);
+
+		// =====================================================================
+		// List of regions
+		// TRANSLATORS: Name of region on Earth
+		N_("Northern Africa");
+		// TRANSLATORS: Name of region on Earth
+		N_("Eastern Africa");
+		// TRANSLATORS: Name of region on Earth
+		N_("Central Africa");
+		// TRANSLATORS: Name of region on Earth
+		N_("Southern Africa");
+		// TRANSLATORS: Name of region on Earth
+		N_("Western Africa");
+		// TRANSLATORS: Name of region on Earth
+		N_("Caribbean");
+		// TRANSLATORS: Name of region on Earth
+		N_("Central America");
+		// TRANSLATORS: Name of region on Earth
+		N_("Southern America");
+		// TRANSLATORS: Name of region on Earth
+		N_("Northern America");
+		// TRANSLATORS: Name of region on Earth
+		N_("Antarctica");
+		// TRANSLATORS: Name of region on Earth
+		N_("Northern Asia");
+		// TRANSLATORS: Name of region on Earth
+		N_("Central Asia");
+		// TRANSLATORS: Name of region on Earth
+		N_("Eastern Asia");
+		// TRANSLATORS: Name of region on Earth
+		N_("South-eastern Asia");
+		// TRANSLATORS: Name of region on Earth
+		N_("Southern Asia");
+		// TRANSLATORS: Name of region on Earth
+		N_("Western Asia");
+		// TRANSLATORS: Name of region on Earth
+		N_("Eastern Europe");
+		// TRANSLATORS: Name of region on Earth
+		N_("Northern Europe");
+		// TRANSLATORS: Name of region on Earth
+		N_("Southern Europe");
+		// TRANSLATORS: Name of region on Earth
+		N_("Western Europe");
+		// TRANSLATORS: Name of region on Earth
+		N_("Australasia");
+		// TRANSLATORS: Name of region on Earth
+		N_("Melanesia");
+		// TRANSLATORS: Name of region on Earth
+		N_("Micronesia");
+		// TRANSLATORS: Name of region on Earth
+		N_("Polynesia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Aparangi Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Borealis Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Budh Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Caloris Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Lugus Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Mearcair Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Odin Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Otaared Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Papsukkal Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Sihtu Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Sobkou Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Stilbon Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Suisei Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Tir Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Turms Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Utaridi Planitia");
+		// TRANSLATORS: Name of region on Mercury
+		N_("Catuilla Planum");
+		// TRANSLATORS: Name of region on Venus
+		N_("Aphrodite Terra");
+		// TRANSLATORS: Name of region on venus
+		N_("Ishtar Terra");
+		// TRANSLATORS: Name of region on Venus
+		N_("Lada Terra");
+		// TRANSLATORS: Name of region on Venus
+		N_("Alpha Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Asteria Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Atla Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Bell Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Beta Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Dione Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Dsonkwa Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Eistla Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Hyndla Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Imdr Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Ishkus Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Laufey Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Neringa Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Ovda Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Phoebe Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Themis Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Thetis Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Ulfrun Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Vasilisa Regio");
+		// TRANSLATORS: Name of region on Venus
+		N_("Aibarchin Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Aino Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Akhtamar Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Alma-Merghen Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Atalanta Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Audra Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Bereghinya Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Dzerassa Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Fonueha Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Ganiki Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Guinevere Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Gunda Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Helen Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Hinemoa Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Imapinua Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Kanykey Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Kawelu Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Laimdota Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Lavinia Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Leda Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Libuse Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Llorona Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Louhi Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Lowana Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Mugazo Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Navka Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Niobe Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Nsomeka Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Nuptadi Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Rusalka Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Sedna Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Snegurochka Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Sogolon Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Tahmina Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Tilli-Hanum Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Tinatin Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Undine Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Vellamo Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Vinmara Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Wawalag Planitia");
+		// TRANSLATORS: Name of region on Venus
+		N_("Zhibek Planitia");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Ocean of Storms");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Serpent Sea");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Southern Sea");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea that has become known");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Crises");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Fecundity");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Cold");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Humboldt");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Moisture");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Showers");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Cleverness");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Islands");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of the Edge");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Muscovy");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Nectar");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Clouds");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Eastern sea");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Serenity");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Smyth");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Foaming Sea");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Tranquility");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Waves");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Sea of Vapors");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Northern highlands");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Jura mountains");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Alps province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Caucasus province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Lake of Dreams");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Taurus mountains");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Bay of Dew");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Apennines province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Carpatian province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Seething Bay");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Aestuum upland");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Haemus province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Procellarum upland");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Central crater province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Macro-crater province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Southeastern crater province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Cratered plain province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Southwestern crater province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Nectaris crater province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Pyrenees province");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Western highlands");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Eastern highlands");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Bay of the center");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Marsh of Epidemics");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Far-side southern lowland");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Far-side northern lowland");
+		// TRANSLATORS: Name of region on the Moon
+		N_("Far-side central highland");
+		// TRANSLATORS: Name of region on Mars
+		N_("Acidalia Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Amazonis Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Arcadia Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Argyre Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Chryse Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Elysium Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Eridania Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Hellas Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Isidis Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Utopia Planitia");
+		// TRANSLATORS: Name of region on Mars
+		N_("Aonia Terra");
+		// TRANSLATORS: Name of region on Mars
+		N_("Arabia Terra");
+		// TRANSLATORS: Name of region on Mars
+		N_("Margaritifer Terra");
+		// TRANSLATORS: Name of region on Mars
+		N_("Noachis Terra");
+		// TRANSLATORS: Name of region on Mars
+		N_("Promethei Terra");
+		// TRANSLATORS: Name of region on Mars
+		N_("Tempe Terra");
+		// TRANSLATORS: Name of region on Mars
+		N_("Terra Cimmeria");
+		// TRANSLATORS: Name of region on Mars
+		N_("Terra Sabaea");
+		// TRANSLATORS: Name of region on Mars
+		N_("Terra Sirenum");
+		// TRANSLATORS: Name of region on Mars
+		N_("Tyrrhena Terra");
+		// TRANSLATORS: Name of region on Mars
+		N_("Xanthe Terra");
+		// TRANSLATORS: Name of region on Mars
+		N_("Vastitas Borealis");
+		// TRANSLATORS: Name of region on Mars
+		N_("Tharsis Rise");
+		// TRANSLATORS: Name of region on Mars
+		N_("Planum Boreum");
+		// TRANSLATORS: Name of region on Mars
+		N_("Planum Australe");
+		// TRANSLATORS: Name of region on Titan
+		N_("Garotman Terra");
+		// TRANSLATORS: Name of region on Titan
+		N_("Tollan Terra");
+		// TRANSLATORS: Name of region on Titan
+		N_("Tsiipiya Terra");
+		// TRANSLATORS: Name of region on Titan
+		N_("Yalaing Terra");
+		// TRANSLATORS: Name of region on Titan
+		N_("Xanadu");
+		// TRANSLATORS: Name of region on Titan
+		N_("Kraken Mare");
+		// TRANSLATORS: Name of region on Titan
+		N_("Ligeia Mare");
+		// TRANSLATORS: Name of region on Titan
+		N_("Punga Mare");
+	}
+};
