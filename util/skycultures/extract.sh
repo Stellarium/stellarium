1118e5bc290613b2c529e828a929fc87bbd5d034
Alexander V. Wolf <alex.v.wolf@gmail.com>
2021-09-19 22:58:04 +0700

    Let's remove unstranslatable strings

--- util/skycultures/extract.sh
+++ util/skycultures/extract.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-po4a --verbose --force --msgid-bugs-address stellarium@googlegroups.com --copyright-holder "Stellarium's team" ./po4a.config
+po4a -o untranslated="<notr>" --verbose --force --msgid-bugs-address stellarium@googlegroups.com --copyright-holder "Stellarium's team" ./po4a.config
 
 #for i in ../../po/stellarium-skycultures-descriptions/*.po
 #do
