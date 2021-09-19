02f713d9824d9096d6738adbc47efcb49efaf325
Alexander V. Wolf <alex.v.wolf@gmail.com>
2021-09-19 19:51:44 +0700

    Update tools and templates for translation of descriptions

--- util/skycultures/extract.sh
+++ util/skycultures/extract.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-po4a po4a.config
+po4a --verbose --force --msgid-bugs-address stellarium@googlegroups.com --copyright-holder "Stellarium's team" ./po4a.config
 
-for i in ../../po/stellarium-skycultures-descriptions/*.po
-do
-	msgmerge --previous -o $i $i ../../po/stellarium-skycultures-descriptions/stellarium-skycultures-descriptions.pot
-done
+#for i in ../../po/stellarium-skycultures-descriptions/*.po
+#do
+#	msgmerge --previous -o $i $i ../../po/stellarium-skycultures-descriptions/stellarium-skycultures-descriptions.pot
+#done
