58baf9fd6eb62735f282cae369e58072b977e1f4
Alexander V. Wolf <alex.v.wolf@gmail.com>
2021-10-07 10:25:07 +0700

    Update EphemWrapper.hpp

--- src/core/planetsephems/EphemWrapper.hpp
+++ src/core/planetsephems/EphemWrapper.hpp
@@ -95,15 +95,14 @@ void get_rhea_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
 void get_titan_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
 void get_hyperion_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
 void get_iapetus_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
+void get_helene_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
+void get_telesto_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
+void get_calypso_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
 
 void get_miranda_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
 void get_ariel_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
 void get_umbriel_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
 void get_titania_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
 void get_oberon_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
-
-void get_helene_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
-void get_telesto_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
-void get_calypso_parent_coordsv(double jd,double xyz[3], double xyzdot[3], void*);
 
 #endif // _EPHEMWRAPPER_HPP_
