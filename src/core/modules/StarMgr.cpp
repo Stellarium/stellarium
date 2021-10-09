17a489c2c35ab3cb9c47c9c2a6f7e09f8339fd1a
Georg Zotti <Georg.Zotti@univie.ac.at>
2021-10-09 15:34:37 +0200

    Attempt to filter away occulted stars. - does not work properly: tessellated Moon sphere is often smaller than its computed radius!

--- src/core/modules/StarMgr.cpp
+++ src/core/modules/StarMgr.cpp
@@ -46,6 +46,7 @@
 #include "StelUtils.hpp"
 #include "StelHealpix.hpp"
 #include "StelObject.hpp"
+#include "SolarSystem.hpp"
 
 #include <QGlobalStatic>
 #include <QTextStream>
@@ -1371,6 +1372,20 @@ void StarMgr::draw(StelCore* core)
 
 	// Prepare a table for storing precomputed RCMag for all ZoneArrays
 	RCMag rcmag_table[RCMAG_TABLE_SIZE];
+
+	// Try to filter away stars hidden by the Moon!
+	bool filterMoon=false;
+	PlanetP moon = GETSTELMODULE(SolarSystem)->getMoon();
+	Vec3d moonPos=moon->getJ2000EquatorialPos(core);
+	const double moonRadius = moon->getEquatorialRadius() * moon->getSphereScale();
+	double angularSize = atan2(moonRadius, moonPos.norm());
+	moonPos.normalize();
+	SphericalCap moonCap(moonPos, cos(angularSize));
+	for (auto  cap : viewportCaps)
+	{
+	    if (cap.intersects(moonCap))
+		filterMoon=true;
+	}
 	
 	// Draw all the stars of all the selected zones
 	for (const auto* z : std::as_const(gridLevels))
@@ -1418,11 +1433,11 @@ void StarMgr::draw(StelCore* core)
 			diffPos = core->getParallaxDiff(core->getJDE());
 		}
 		for (GeodesicSearchInsideIterator it1(*geodesic_search_result,z->level);(zone = it1.next()) >= 0;)
-			z->draw(&sPainter, zone, true, rcmag_table, limitMagIndex, core, maxMagStarName, names_brightness, viewportCaps, withAberration, vel, withParallax, diffPos, currentSkycultureUsesCommonStarnames);
+			z->draw(&sPainter, zone, true, rcmag_table, limitMagIndex, core, maxMagStarName, names_brightness, viewportCaps, withAberration, vel, withParallax, diffPos, currentSkycultureUsesCommonStarnames, filterMoon, moonCap);
 		for (GeodesicSearchBorderIterator it1(*geodesic_search_result,z->level);(zone = it1.next()) >= 0;)
-			z->draw(&sPainter, zone, false, rcmag_table, limitMagIndex, core, maxMagStarName,names_brightness, viewportCaps, withAberration, vel, withParallax, diffPos, currentSkycultureUsesCommonStarnames);
+			z->draw(&sPainter, zone, false, rcmag_table, limitMagIndex, core, maxMagStarName,names_brightness, viewportCaps, withAberration, vel, withParallax, diffPos, currentSkycultureUsesCommonStarnames, filterMoon, moonCap);
 		// always check the last zone because it is a global zone
-		z->draw(&sPainter, (20<<(z->level<<1)), false, rcmag_table, limitMagIndex, core, maxMagStarName, names_brightness, viewportCaps, withAberration, vel, withParallax, diffPos, currentSkycultureUsesCommonStarnames);
+		z->draw(&sPainter, (20<<(z->level<<1)), false, rcmag_table, limitMagIndex, core, maxMagStarName, names_brightness, viewportCaps, withAberration, vel, withParallax, diffPos, currentSkycultureUsesCommonStarnames, filterMoon, moonCap);
 	}
 	exit_loop:
 
