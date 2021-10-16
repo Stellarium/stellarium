ae9dda106d265b08f2485e6dd7d23b4209c06bf8
Georg Zotti <Georg.Zotti@univie.ac.at>
2021-10-16 19:26:55 +0200

    Add a StelDialog-based window as separate window

---
+++ src/gui/StelDialogSeparate.hpp
@@ -0,0 +1,46 @@
+/*
+ * Stellarium
+ * Copyright (C) 2008 Fabien Chereau
+ * Copyright (C) 2021 Georg Zotti
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+*/
+
+#ifndef STELDIALOGSEPARATE_HPP
+#define STELDIALOGSEPARATE_HPP
+
+#include "StelDialog.hpp"
+
+
+//! @class StelDialogSeparate Base class for windows NOT embedded in the graphics scene.
+//! Some GUI elements like QWebEngineView don't like to be embedded into our MainView.
+//! Another motivation may be to have windows on another screen.
+//!
+//! The dialog behaves almost like @class StelDialog. However, given how night mode is applied as screen effect,
+//! this dialog does not adhere to the global night mode.
+//!
+class StelDialogSeparate : public StelDialog
+{
+	Q_OBJECT
+public:
+	StelDialogSeparate(QString dialogName="Default", QObject* parent=Q_NULLPTR);
+	virtual ~StelDialogSeparate() Q_DECL_OVERRIDE {}
+
+public slots:
+	//! On the first call with "true" populates the window contents.
+	virtual void setVisible(bool) Q_DECL_OVERRIDE;
+};
+
+#endif // STELDIALOGSEPARATE_HPP
