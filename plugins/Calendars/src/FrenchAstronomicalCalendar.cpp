bbfc48da974aec8fb5c9e50c84d030ad2d390b85
Georg Zotti <Georg.Zotti@univie.ac.at>
2022-02-15 22:43:08 +0100

    docfix

--- plugins/Calendars/src/FrenchAstronomicalCalendar.cpp
+++ plugins/Calendars/src/FrenchAstronomicalCalendar.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Georg Zotti
+ * Copyright (C) 2022 Georg Zotti
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
