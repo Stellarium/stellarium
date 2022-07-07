b6ef1b51572908d758d36a70702c805261c299ce
Worachate Boonplod <41257965+worachate001@users.noreply.github.com>
2022-07-07 09:36:12 +0700

    Fix hourly motion of objects (#2505)

--- src/core/modules/Planet.cpp
+++ src/core/modules/Planet.cpp
@@ -598,16 +598,18 @@ QString Planet::getInfoString(const StelCore* core, const InfoStringGroup& flags
 		StelCore* core1 = StelApp::getInstance().getCore(); // we need non-const reference here.
 		const double currentJD=core1->getJDOfLastJDUpdate();
 		const qint64 millis=core1->getMilliSecondsOfLastJDUpdate();
-		core1->setJD(currentJD-StelCore::JD_HOUR);
-		core1->update(0);
-		Vec3d equPosPrev=getEquinoxEquatorialPos(core1);
+		StelCore* core2 = StelApp::getInstance().getCore(); // use to fix hourly motion
+		const double JD2=core2->getJD();
+		core2->setJD(JD2-StelCore::JD_HOUR*.1);
+		core2->update(0);
+		Vec3d equPosPrev=getEquinoxEquatorialPos(core2);
 		const double deltaEq=equPos.angle(equPosPrev);
 		double dec_equPrev, ra_equPrev;
 		StelUtils::rectToSphe(&ra_equPrev,&dec_equPrev,equPosPrev);
 		double pa=atan2(ra_equ-ra_equPrev, dec_equ-dec_equPrev); // position angle: From North counterclockwise!
 		if (pa<0) pa += 2.*M_PI;
-		oss << QString("%1: %2 %3 %4%5<br/>").arg(q_("Hourly motion"), withDecimalDegree ? StelUtils::radToDecDegStr(deltaEq) : StelUtils::radToDmsStr(deltaEq), qc_("towards", "into the direction of"), QString::number(pa*M_180_PI, 'f', 1), QChar(0x00B0));
-		oss << QString("%1: d&alpha;=%2 d&delta;=%3<br/>").arg(q_("Hourly motion"), withDecimalDegree ? StelUtils::radToDecDegStr(ra_equ-ra_equPrev) : StelUtils::radToDmsStr(ra_equ-ra_equPrev), withDecimalDegree ? StelUtils::radToDecDegStr(dec_equ-dec_equPrev) : StelUtils::radToDmsStr(dec_equ-dec_equPrev));
+		oss << QString("%1: %2 %3 %4%5<br/>").arg(q_("Hourly motion"), withDecimalDegree ? StelUtils::radToDecDegStr(deltaEq*10.) : StelUtils::radToDmsStr(deltaEq*10.), qc_("towards", "into the direction of"), QString::number(pa*M_180_PI, 'f', 1), QChar(0x00B0));
+		oss << QString("%1: d&alpha;=%2 d&delta;=%3<br/>").arg(q_("Hourly motion"), withDecimalDegree ? StelUtils::radToDecDegStr((ra_equ-ra_equPrev)*10.) : StelUtils::radToDmsStr((ra_equ-ra_equPrev)*10.), withDecimalDegree ? StelUtils::radToDecDegStr((dec_equ-dec_equPrev)*10.) : StelUtils::radToDmsStr((dec_equ-dec_equPrev)*10.));
 		core1->setJD(currentJD); // this calls sync() which sets millis
 		core1->setMilliSecondsOfLastJDUpdate(millis); // restore millis.
 		core1->update(0);
