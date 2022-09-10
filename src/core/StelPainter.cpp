1f5edae64b3058beeb855d5e4a4e14c83f6ec5d9
Ruslan Kabatsayev <b7.10110111@gmail.com>
2022-09-11 00:48:58 +0300

    Replace ordered Bayer dithering with the algorithm based on triangle-remapped blue noise
    Ordered dithering has advantage of relative simplicity to implement,
    which makes it ubiquitous. In particular, poor quality monitors, like
    those using LCD TN technology, use it to simulate 8 bits per channel
    while using 6-bit-per-channel screen.
    
    The disadvantage of ordered dithering is that when an application tries
    to display dithered contents on a dithered monitor, the image gets
    aliased due to a kind of interference of the patterns the monitor and
    the application use. Thus the problem of posterization, which dithering
    is supposed to fight, gets aggravated, turning the constant-color bands
    into another kind of banding pattern.
    
    Using an algorithm called "blue noise with triangular remapping" lets
    us have an independent pattern of noise, which we can generally generate
    new for each application (e.g. a 3D program like Stellarium and a
    compositing window manager), so that the patterns coming from these
    applications add statistically rather than interfere. And of course this
    pattern is independent from any ordered dithering pattern.

--- src/core/StelPainter.cpp
+++ src/core/StelPainter.cpp
@@ -162,8 +162,8 @@ void StelPainter::setProjector(const StelProjectorP& p)
 
 StelPainter::~StelPainter()
 {
-	if(bayerPatternTex)
-		glDeleteTextures(1, &bayerPatternTex);
+	if(ditherPatternTex)
+		glDeleteTextures(1, &ditherPatternTex);
 	//reset opengl state
 	glState.reset();
 
@@ -2135,7 +2135,7 @@ void StelPainter::initGLShaders()
 	texturesShaderVars.vertex = texturesShaderProgram->attributeLocation("vertex");
 	texturesShaderVars.texColor = texturesShaderProgram->uniformLocation("texColor");
 	texturesShaderVars.texture = texturesShaderProgram->uniformLocation("tex");
-	texturesShaderVars.bayerPattern = texturesShaderProgram->uniformLocation("bayerPattern");
+	texturesShaderVars.ditherPattern = texturesShaderProgram->uniformLocation("ditherPattern");
 	texturesShaderVars.rgbMaxValue = texturesShaderProgram->uniformLocation("rgbMaxValue");
 
 	// Texture shader program + interpolated color per vertex
@@ -2182,7 +2182,7 @@ void StelPainter::initGLShaders()
 	texturesColorShaderVars.vertex = texturesColorShaderProgram->attributeLocation("vertex");
 	texturesColorShaderVars.color = texturesColorShaderProgram->attributeLocation("color");
 	texturesColorShaderVars.texture = texturesColorShaderProgram->uniformLocation("tex");
-	texturesColorShaderVars.bayerPattern = texturesColorShaderProgram->uniformLocation("bayerPattern");
+	texturesColorShaderVars.ditherPattern = texturesColorShaderProgram->uniformLocation("ditherPattern");
 	texturesColorShaderVars.rgbMaxValue = texturesColorShaderProgram->uniformLocation("rgbMaxValue");
 	texturesColorShaderVars.saturation = texturesColorShaderProgram->uniformLocation("saturation");
 }
@@ -2267,10 +2267,10 @@ void StelPainter::drawFromArray(DrawingMode mode, int count, int offset, bool do
 		pr->enableAttributeArray(texturesShaderVars.texCoord);
 		//pr->setUniformValue(texturesShaderVars.texture, 0);    // use texture unit 0
 		glActiveTexture(GL_TEXTURE1);
-		if(!bayerPatternTex)
-			bayerPatternTex=makeBayerPatternTexture(*this);
-		glBindTexture(GL_TEXTURE_2D, bayerPatternTex);
-		pr->setUniformValue(texturesShaderVars.bayerPattern, 1);
+		if(!ditherPatternTex)
+			ditherPatternTex=makeDitherPatternTexture(*this);
+		glBindTexture(GL_TEXTURE_2D, ditherPatternTex);
+		pr->setUniformValue(texturesShaderVars.ditherPattern, 1);
 		pr->setUniformValue(texturesShaderVars.rgbMaxValue, rgbMaxValue[0], rgbMaxValue[1], rgbMaxValue[2]);
 	}
 	else if (texCoordArray.enabled && colorArray.enabled && !normalArray.enabled)
@@ -2286,10 +2286,10 @@ void StelPainter::drawFromArray(DrawingMode mode, int count, int offset, bool do
 		pr->enableAttributeArray(texturesColorShaderVars.color);
 		//pr->setUniformValue(texturesShaderVars.texture, 0);    // use texture unit 0
 		glActiveTexture(GL_TEXTURE1);
-		if(!bayerPatternTex)
-			bayerPatternTex=makeBayerPatternTexture(*this);
-		glBindTexture(GL_TEXTURE_2D, bayerPatternTex);
-		pr->setUniformValue(texturesColorShaderVars.bayerPattern, 1);
+		if(!ditherPatternTex)
+			ditherPatternTex=makeDitherPatternTexture(*this);
+		glBindTexture(GL_TEXTURE_2D, ditherPatternTex);
+		pr->setUniformValue(texturesColorShaderVars.ditherPattern, 1);
 		pr->setUniformValue(texturesColorShaderVars.rgbMaxValue, rgbMaxValue[0], rgbMaxValue[1], rgbMaxValue[2]);
 		pr->setUniformValue(texturesColorShaderVars.saturation, saturation);
 	}
