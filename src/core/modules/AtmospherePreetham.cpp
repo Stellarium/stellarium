3abe3b0cb10da1203fea3602639c8477787467a7
Georg Zotti <Georg.Zotti@univie.ac.at>
2022-09-14 10:58:18 +0200

    Milky Way and Zodiacal Light rebalance (#2648)
    * Better balance for MilkyWay and ZodiacalLight with ShowMySky atmosphere
    Only visual match. A better physics-based re-estimate should still be developed later.

--- src/core/modules/AtmospherePreetham.cpp
+++ src/core/modules/AtmospherePreetham.cpp
@@ -291,7 +291,6 @@ void AtmospherePreetham::computeColor(StelCore* core, const double JD, const Pla
 	Vec3d point(1., 0., 0.);
 	float lumi=0.f;
 
-	skylightStruct2 skylight;
 	// Compute the sky color for every point above the ground
 	for (unsigned int i=0; i<(1+skyResolutionX)*(1+skyResolutionY); ++i)
 	{
@@ -319,6 +318,7 @@ void AtmospherePreetham::computeColor(StelCore* core, const double JD, const Pla
 			}
 			else // Experimental: Re-allow CIE/Preetham brightness instead.
 			{
+				skylightStruct2 skylight;
 				skylight.pos[0]=pointF.v[0];
 				skylight.pos[1]=pointF.v[1];
 				skylight.pos[2]=pointF.v[2];
--- src/core/modules/AtmosphereShowMySky.cpp
+++ src/core/modules/AtmosphereShowMySky.cpp
@@ -27,7 +27,6 @@
 #include "StelToneReproducer.hpp"
 #include "StelCore.hpp"
 #include "StelPainter.hpp"
-#include "StelFileMgr.hpp"
 #include "Dithering.hpp"
 #include "StelTranslator.hpp"
 
@@ -805,7 +804,7 @@ void AtmosphereShowMySky::computeColor(StelCore* core, const double JD, const Pl
 		const auto meanY=meanPixelValue[1];
 		Q_ASSERT(std::isfinite(meanY));
 
-		averageLuminance = meanY;
+		averageLuminance = meanY+0.0001f; // Add (assumed) star background luminance
 	}
 }
 
