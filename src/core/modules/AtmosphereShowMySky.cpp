2ad0ea350475a8733b2d208d945f94c98c115502
uwes-ufo <uwes-ufo@t-online.de>
2022-09-23 00:52:13 +0200

    qDebug dynamic resolution

--- src/core/modules/AtmosphereShowMySky.cpp
+++ src/core/modules/AtmosphereShowMySky.cpp
@@ -741,7 +741,7 @@ bool AtmosphereShowMySky::dynamicResolution(StelProjectorP prj, Vec3d &currPos, 
 
 	bool verbose=qApp->property("verbose").toBool();
 	if (verbose)
-		qDebug() << "dynResTimer =" << dynResTimer << "\tatmoRes =" << atmoRes << "\tchangeOfView =" << changeOfView.length() << changeOfView;
+		qDebug() << "dynResTimer" << dynResTimer << "atmoRes" << atmoRes << "changeOfView" << changeOfView.length() << changeOfView;
 
 	// At reduced resolution, we hurry to redraw - at full resolution, we have time.
 	dynResTimer=dynResTimer>0?6:18;
