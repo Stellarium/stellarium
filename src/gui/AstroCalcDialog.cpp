c69a41c801da47c4f820c9851b99c9a9850c5e2d
Worachate Boonplod <worachateb@gmail.com>
2022-09-28 17:29:12 +0700

    remove a solution

--- src/gui/AstroCalcDialog.cpp
+++ src/gui/AstroCalcDialog.cpp
@@ -4912,7 +4912,6 @@ void AstroCalcDialog::saveSolarEclipseKML()
 			// Curve of maximum eclipse at sunrise/sunset
 			// There are two parts of the curve
 			bool first = true;
-			double startLat1 = 0., startLon1 = 0., endLat1 = 0., endLon1 = 0.;
 			for (int j = 0; j < 2; j++)
 			{
 				if ( j!= 0) first = false;
@@ -4937,29 +4936,11 @@ void AstroCalcDialog::saveSolarEclipseKML()
 							stream << "<Placemark>\n<name>MaxEclipseSunriseSunset</name>\n<styleUrl>#PLimits</styleUrl>\n<LineString>\n<extrude>1</extrude>\n";
 							stream << "<tessellate>1</tessellate>\n<altitudeMode>absoluto</altitudeMode>\n<coordinates>\n";
 						}
-						if (!first && count == 1 && bothPenumbralLimits)
-						{
-							stream << startLon1 << "," << startLat1 << ",0.0\n"; // connect start of part 1 to start of part 2
-						}
 						stream << coordinates.second << "," << coordinates.first << ",0.0\n";
-						if (first && bothPenumbralLimits)
-						{
-							endLat1 = coordinates.first;
-							endLon1 = coordinates.second;
-						}
-						if (first && count == 1 && bothPenumbralLimits)
-						{
-							startLat1 = coordinates.first;
-							startLon1 = coordinates.second;
-						}
 						lat0 = coordinates.first;
 						lon0 = coordinates.second;
 					}
 					i++;
-				}
-				if (!first && bothPenumbralLimits)
-				{
-					stream << endLon1 << "," << endLat1 << ",0.0\n"; // connect end of part 2 to end of part 1
 				}
 				stream << "</coordinates>\n</LineString>\n</Placemark>\n";
 			}
