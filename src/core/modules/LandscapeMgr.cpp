439bd0b73c9c02149b0033d8760e990181eecffe
Ruslan Kabatsayev <b7.10110111@gmail.com>
2022-10-11 13:16:50 +0300

    Current atmosphere should be deleted immediately if we need to reset to fallback

--- src/core/modules/LandscapeMgr.cpp
+++ src/core/modules/LandscapeMgr.cpp
@@ -678,6 +678,7 @@ void LandscapeMgr::createAtmosphere()
 void LandscapeMgr::resetToFallbackAtmosphere()
 {
 	StelApp::getInstance().getSettings()->setValue(ATMOSPHERE_MODEL_CONFIG_KEY, ATMOSPHERE_MODEL_CONF_VAL_PREETHAM);
+	atmosphere.reset();
 	createAtmosphere();
 }
 
