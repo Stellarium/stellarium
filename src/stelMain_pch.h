325a7da30dd857dc830cd37cd8fc8abd0c02d838
Georg Zotti <Georg.Zotti@univie.ac.at>
2022-11-11 17:41:33 +0100

    Better-informed way to utilize PCH - make it a cmake option - select the actual headers for precompiling

---
+++ src/stelMain_pch.h
@@ -0,0 +1,31 @@
+/*
+ * Stellarium
+ * Copyright (C) 2022 Georg Zotti
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+ */
+
+#ifndef STELMAIN_PCH_H
+#define STELMAIN_PCH_H
+
+// Put most-used header files of the project here
+// ignore clangd warnings about unused headers!
+
+#include "StelApp.hpp"
+#include "StelPainter.hpp"
+#include "StelUtils.hpp"
+#include "VecMath.hpp"
+
+#endif // STELMAIN_PCH_H
