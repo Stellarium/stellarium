3f97894bcfc8a2bb0fa7696b7c7ab400b5d68ff8
Ruslan Kabatsayev <b7.10110111@gmail.com>
2023-01-21 23:01:14 +0600

    Use a ratio of FoV and sphere scale when calculating brightness coefficient for the Moon
    Otherwise we get blown-out highlights when the Moon is still too large,
    which is undesirable for aesthetic reasons.

--- src/core/modules/Planet.cpp
+++ src/core/modules/Planet.cpp
@@ -3568,9 +3568,9 @@ void Planet::draw3dModel(StelCore* core, StelProjector::ModelViewTranformP trans
 			const float fov=core->getProjection(transfo)->getFov();
 			float fovFactor=1.3f;
 			// scale brightness to reduce if fov smaller than 5 degrees. Min brightness (to avoid glare) if fov=2deg.
-			if (fov<5.0f)
+			if (fov/sphereScale<5.0f)
 			{
-				fovFactor -= 0.1f*(5.0f-qMax(2.0f, fov));
+				fovFactor -= 0.1f*(5.0f-qMax(2.0f, fov/sphereScale));
 			}
 			// Special case for the Moon. Was 1.6, but this often is too bright.
 			light.diffuse.set(fovFactor,magFactorGreen*fovFactor,magFactorBlue*fovFactor);
