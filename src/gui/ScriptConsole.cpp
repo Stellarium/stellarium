Stellarium/stellarium

commit 67d8bc6e97a2ef14258e5a8baa3d11a6c7de0d73

Add a check for screenshot file format

src/scripting/StelMainScriptAPI.cpp
@@ -342,11 +342,18 @@ QStringList StelMainScriptAPI::getAllTimezoneNames()
 
 void StelMainScriptAPI::screenshot(const QString& prefix, bool invert, const QString& dir, const bool overwrite, const QString &format)
 {
-	bool oldInvertSetting = StelMainView::getInstance().getFlagInvertScreenShotColors();
-	QString oldFormat=StelMainView::getInstance().getScreenshotFormat();
-	StelMainView::getInstance().setFlagInvertScreenShotColors(invert);
+	const bool oldInvertSetting = StelMainView::getInstance().getFlagInvertScreenShotColors();
+	const QString oldFormat=StelMainView::getInstance().getScreenshotFormat();
 	if ((format.length()>0) && (format.length()<=4))
 		StelMainView::getInstance().setScreenshotFormat(format);
+	// Check requested against set image format.
+	if (StelMainView::getInstance().getScreenshotFormat() != format)
+	{
+		qWarning() << "Screenshot format" << format << "not supported. Not saving screenshot.";
+		return;
+	}
+
+	StelMainView::getInstance().setFlagInvertScreenShotColors(invert);
 	StelMainView::getInstance().setFlagOverwriteScreenShots(overwrite);
 	StelMainView::getInstance().saveScreenShot(prefix, dir, overwrite);
 	StelMainView::getInstance().setFlagInvertScreenShotColors(oldInvertSetting);

commit 5d15479fdb73ad975ce23b71539d9b90531d3736

Require user consent to let scripts store screenshots to paths given in the script.
It may be seen by some as security issue in doing so (#3111)

Also fix a bug to store screenshot with given type from script (introduced in 67d8bc6e97a2ef14258e5a8baa3d11a6c7de0d73)

guide/app_config_ini.tex
@@ -738,6 +738,7 @@ password   & string & Password for proxy. E.g. \emph{xxxxx}\\\bottomrule
 startup\_script                            & string & startup.ssc & name of script executed on program start\\
 flag\_script\_allow\_absolute\_path        & bool   & false       & set true to allow scripts from absolute pathnames.
                                                                     This may pose a security risk if you run arbitrary scripts.\\
+flag\_allow\_screenshot\_dir               & bool   & false       & Allow scripts to set a target directory.\\
 flag\_script\_allow\_write\_absolute\_path & bool   & false       & set true to let scripts store files to absolute pathnames.
                                                                     This may pose a security risk if you run scripts from other authors
                                                                     without checking what they are doing.\\\bottomrule 

src/gui/ScriptConsole.cpp
@@ -143,6 +143,9 @@ void ScriptConsole::createDialogContent()
 	connect(ui->closeWindowAtScriptRunCheckbox, SIGNAL(toggled(bool)), this, SLOT(setFlagHideWindow(bool)));
 	connect(ui->clearOutputCheckbox, SIGNAL(toggled(bool)), this, SLOT(setFlagClearOutput(bool)));
 
+	ui->allowScreenshotDirCheckBox->setChecked(StelApp::getInstance().getScriptMgr().getFlagAllowExternalScreenshotDir());
+	connect(ui->allowScreenshotDirCheckBox, SIGNAL(clicked(bool)), &StelApp::getInstance().getScriptMgr(), SLOT(setFlagAllowExternalScreenshotDir(bool)), Qt::AutoConnection);
+
 	dirty = false;
 }
 

src/gui/scriptConsole.ui
@@ -482,6 +482,16 @@
            </widget>
           </item>
           <item>
+           <widget class="QCheckBox" name="allowScreenshotDirCheckBox">
+            <property name="toolTip">
+             <string>If disabled, all screenshots go to the directory set in the Configuration dialog.</string>
+            </property>
+            <property name="text">
+             <string>Allow scripts to store screenshots to other directories</string>
+            </property>
+           </widget>
+          </item>
+          <item>
            <spacer name="verticalSpacer">
             <property name="orientation">
              <enum>Qt::Vertical</enum>
@@ -620,7 +630,6 @@
   </customwidget>
  </customwidgets>
  <resources>
-  <include location="../../data/gui/guiRes.qrc"/>
   <include location="../../data/gui/guiRes.qrc"/>
  </resources>
  <connections/>

src/scripting/StelMainScriptAPI.cpp
@@ -342,12 +342,23 @@ QStringList StelMainScriptAPI::getAllTimezoneNames()
 
 void StelMainScriptAPI::screenshot(const QString& prefix, bool invert, const QString& dir, const bool overwrite, const QString &format)
 {
+	QString realDir("");
+	if ((!dir.isEmpty()) && (!StelApp::getInstance().getScriptMgr().getFlagAllowExternalScreenshotDir()))
+	{
+		qWarning() << "SCRIPT CONFIGURATION ISSUE: the script wants to store a screenshot" << prefix << "." << format << "to an external directory " << dir;
+		qWarning() << "  To enable this, check the settings in the script console";
+		qWarning() << "  or set entry scripts/flag_allow_screenshots_dir=true in config.ini.";
+	}
+	else
+		realDir=dir;
+
+
 	const bool oldInvertSetting = StelMainView::getInstance().getFlagInvertScreenShotColors();
 	const QString oldFormat=StelMainView::getInstance().getScreenshotFormat();
 	if ((format.length()>0) && (format.length()<=4))
 		StelMainView::getInstance().setScreenshotFormat(format);
 	// Check requested against set image format.
-	if (StelMainView::getInstance().getScreenshotFormat() != format)
+	if ((format.length()>0) && (StelMainView::getInstance().getScreenshotFormat() != format))
 	{
 		qWarning() << "Screenshot format" << format << "not supported. Not saving screenshot.";
 		return;
@@ -355,7 +366,7 @@ void StelMainScriptAPI::screenshot(const QString& prefix, bool invert, const QSt
 
 	StelMainView::getInstance().setFlagInvertScreenShotColors(invert);
 	StelMainView::getInstance().setFlagOverwriteScreenShots(overwrite);
-	StelMainView::getInstance().saveScreenShot(prefix, dir, overwrite);
+	StelMainView::getInstance().saveScreenShot(prefix, realDir, overwrite);
 	StelMainView::getInstance().setFlagInvertScreenShotColors(oldInvertSetting);
 	StelMainView::getInstance().setScreenshotFormat(oldFormat);
 }

src/scripting/StelScriptMgr.cpp
@@ -505,6 +505,8 @@ StelScriptMgr::StelScriptMgr(QObject *parent): QObject(parent)
 	engine->setAgent(agent);
 #endif
 	initActions();
+	QSettings *conf=StelApp::getInstance().getSettings();
+	flagAllowExternalScreenshotDir=conf->value("scripts/flag_allow_screenshots_dir", false).toBool();
 }
 
 void StelScriptMgr::initActions()
@@ -1204,3 +1206,14 @@ void StelScriptEngineAgent::positionChange(qint64 scriptId, int lineNumber, int 
 	}
 }
 #endif
+
+void StelScriptMgr::setFlagAllowExternalScreenshotDir(bool flag)
+{
+	if (flag!=flagAllowExternalScreenshotDir)
+	{
+		flagAllowExternalScreenshotDir=flag;
+		QSettings* conf = StelApp::getInstance().getSettings();
+		conf->setValue("scripts/flag_allow_screenshots_dir", flag);
+		emit flagAllowExternalScreenshotDirChanged(flag);
+	}
+}

src/scripting/StelScriptMgr.hpp
@@ -54,6 +54,8 @@ class StelScriptMgr : public QObject
 	Q_OBJECT
 
 	Q_PROPERTY(QString runningScriptId READ runningScriptId NOTIFY runningScriptIdChanged)
+	// CAVEAT: Do not convert this class to a StelModule: The Qt properties (esp. the following) should not be available in the StelProperty system!
+	Q_PROPERTY(bool flagAllowExternalScreenshotDir READ getFlagAllowExternalScreenshotDir WRITE setFlagAllowExternalScreenshotDir NOTIFY flagAllowExternalScreenshotDirChanged)
 		
 #ifdef ENABLE_SCRIPT_CONSOLE
 friend class ScriptConsole;
@@ -249,6 +251,11 @@ public slots:
 	//! @note This method only works with the Qt5-based scripting engine.
 	void resumeScript();
 
+	//! Ensure that users must actively enable storing screenshots to directories configured in scripts.
+	//! If this is false, a directory dir given in StelMainScriptAPI::screenshot(prefix, invert, dir, ...) is ignored.
+	bool getFlagAllowExternalScreenshotDir() {return flagAllowExternalScreenshotDir;}
+	void setFlagAllowExternalScreenshotDir(bool flag);
+
 private slots:
 	//! Called at the end of the running threa
 	void scriptEnded();
@@ -265,6 +272,8 @@ signals:
 	void scriptDebug(const QString&);
 	//! Notification of a script event - output line.
 	void scriptOutput(const QString&);
+	//! Notification that screenshots to script-defined directories are allowed
+	void flagAllowExternalScreenshotDirChanged(bool flag);
 
 private:
 	// Utility functions for preprocessor. DEAD CODE!
@@ -310,6 +319,10 @@ private:
 
 	// Registry for include files
 	QSet<QString> includeSet;
+
+	//! Ensure that users must actively allow storing screenshots to directories configured in scripts.
+	//! If this is false, a directory dir given in StelMainScriptAPI::screenshot(prefix, invert, dir, ...) is ignored.
+	bool flagAllowExternalScreenshotDir;
 };
 
 #endif // STELSCRIPTMGR_HPP