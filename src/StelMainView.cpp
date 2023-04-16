e4a6da3cf7b48a3f16089646ddb22880d56c066d
Ruslan Kabatsayev <b7.10110111@gmail.com>
2023-04-16 17:32:58 +0400

    Simplify setting OpenGL version

--- src/StelMainView.cpp
+++ src/StelMainView.cpp
@@ -785,8 +785,7 @@ QSurfaceFormat StelMainView::getDesiredGLFormat(QSettings* configuration)
 	if (openGLModuleType==QOpenGLContext::LibGL)
 	{
 		fmt.setRenderableType(QSurfaceFormat::OpenGL);
-		fmt.setMajorVersion(3);
-		fmt.setMinorVersion(3);
+		fmt.setVersion(3, 3);
 		fmt.setProfile(QSurfaceFormat::CoreProfile);
 
 		if (qApp && qApp->property("onetime_opengl_compat").toBool())
