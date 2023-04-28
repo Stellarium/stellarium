Stellarium/stellarium

commit afe4a846a00044234727bd178c9a33df8ed9257b

Few fixes for potential problems (include CWE-476)
Thanks for PVS-Studio and Coverity Scan

src/core/StelPainter.cpp
@@ -719,7 +719,7 @@ void StelPainter::drawText(float x, float y, const QString& str, float angleDeg,
 		for (int i=0;i<4;i++)
 		{
 			texCoords[i*2+0] = static_cast<float>(tex->getTexSize().width()) * (i % 2);
-			texCoords[i*2+1] = static_cast<float>(tex->getTexSize().height()) * (1 - i / 2);
+			texCoords[i*2+1] = static_cast<float>(tex->getTexSize().height()) * (1 - static_cast<float>(i) / 2);
 		}
 		setTexCoordPointer(2, GL_FLOAT, texCoords);
 

plugins/MeteorShowers/src/MeteorShowersMgr.cpp
@@ -295,7 +295,7 @@ void MeteorShowersMgr::repaint()
 
 void MeteorShowersMgr::checkForUpdates()
 {
-	if (m_enableAutoUpdates && m_lastUpdate.addSecs(static_cast<qint64>(m_updateFrequencyHours * 3600)) <= QDateTime::currentDateTime() && m_networkManager->networkAccessible()==QNetworkAccessManager::Accessible)
+	if (m_enableAutoUpdates && m_lastUpdate.addSecs(static_cast<qint64>(m_updateFrequencyHours) * 3600) <= QDateTime::currentDateTime() && m_networkManager->networkAccessible()==QNetworkAccessManager::Accessible)
 	{
 		updateCatalog();
 	}

plugins/Satellites/src/gui/SatellitesDialog.cpp
@@ -64,8 +64,7 @@ SatellitesDialog::SatellitesDialog()
 	, importWindow(Q_NULLPTR)
 	, filterModel(Q_NULLPTR)
 	, checkStateRole(Qt::UserRole)
-	, delimiter(", ")
-	, acEndl("\n")
+	, delimiter(", ")	
 {
 	ui = new Ui_satellitesDialog;
 	iridiumFlaresHeader.clear();
@@ -120,12 +119,6 @@ void SatellitesDialog::createDialogContent()
 		enableKineticScrolling(gui->getFlagUseKineticScrolling());
 		connect(gui, SIGNAL(flagUseKineticScrollingChanged(bool)), this, SLOT(enableKineticScrolling(bool)));
 	}
-
-#ifdef Q_OS_WIN
-	acEndl="\r\n";
-#else
-	acEndl="\n";
-#endif
 
 	// Set symbols on buttons
 	ui->addSatellitesButton->setText(QChar(0x2795)); // Heavy plus symbol
@@ -397,7 +390,7 @@ void SatellitesDialog::savePredictedIridiumFlares()
 		QTextStream predictedIridiumFlaresList(&predictedIridiumFlares);
 		predictedIridiumFlaresList.setCodec("UTF-8");
 
-		predictedIridiumFlaresList << iridiumFlaresHeader.join(delimiter) << acEndl;
+		predictedIridiumFlaresList << iridiumFlaresHeader.join(delimiter) << StelUtils::getEndLineChar();
 
 		for (int i = 0; i < count; i++)
 		{
@@ -408,7 +401,7 @@ void SatellitesDialog::savePredictedIridiumFlares()
 				if (j<columns-1)
 					predictedIridiumFlaresList << delimiter;
 				else
-					predictedIridiumFlaresList << acEndl;
+					predictedIridiumFlaresList << StelUtils::getEndLineChar();
 			}
 		}
 		predictedIridiumFlares.close();

plugins/Satellites/src/gui/SatellitesDialog.hpp
@@ -165,7 +165,7 @@ private:
 	//! Makes sure that newly added source lines are as checkable as the rest.
 	Qt::ItemDataRole checkStateRole;
 
-	QString delimiter, acEndl;
+	QString delimiter;
 	QStringList iridiumFlaresHeader;
 
 	// colorpickerbutton's color

plugins/Scenery3d/src/gui/Scenery3dDialog.cpp
@@ -172,8 +172,8 @@ void Scenery3dDialog::createDialogContent()
 	}
 
 	StelGui* gui = dynamic_cast<StelGui*>(StelApp::getInstance().getGui());
-	Q_ASSERT(gui);
-	ui->scenery3dTextBrowser->document()->setDefaultStyleSheet(QString(gui->getStelStyle().htmlStyleSheet));
+	if (gui)
+		ui->scenery3dTextBrowser->document()->setDefaultStyleSheet(QString(gui->getStelStyle().htmlStyleSheet));
 	ui->checkBoxDefaultScene->setVisible(false);
 	ui->pushButtonOpenStoredViewDialog->setVisible(false);
 

plugins/Scenery3d/src/gui/StoredViewDialog.cpp
@@ -62,8 +62,8 @@ void StoredViewDialog::createDialogContent()
 	connect(ui->textEditDescription, &CustomTextEdit::editingFinished, this, &StoredViewDialog::updateCurrentView);
 
 	StelGui* gui = dynamic_cast<StelGui*>(StelApp::getInstance().getGui());
-	Q_ASSERT(gui);
-	ui->textEditDescription->document()->setDefaultStyleSheet(QString(gui->getStelStyle().htmlStyleSheet));
+	if (gui)
+		ui->textEditDescription->document()->setDefaultStyleSheet(QString(gui->getStelStyle().htmlStyleSheet));
 
 	//we use a sorta MVC system here
 	viewModel = new StoredViewModel(ui->listView);

plugins/TelescopeControl/src/gui/TelescopeDialog.cpp
@@ -511,9 +511,11 @@ void TelescopeDialog::setAboutText()
 	helpPage += "</body></html>";
 	
 	StelGui* gui = dynamic_cast<StelGui*>(StelApp::getInstance().getGui());
-	Q_ASSERT(gui);
-	ui->textBrowserAbout->document()->setDefaultStyleSheet(QString(gui->getStelStyle().htmlStyleSheet));
-	ui->textBrowserHelp->document()->setDefaultStyleSheet(QString(gui->getStelStyle().htmlStyleSheet));
+	if (gui)
+	{
+		ui->textBrowserAbout->document()->setDefaultStyleSheet(QString(gui->getStelStyle().htmlStyleSheet));
+		ui->textBrowserHelp->document()->setDefaultStyleSheet(QString(gui->getStelStyle().htmlStyleSheet));
+	}
 	ui->textBrowserAbout->setHtml(aboutPage);
 	ui->textBrowserHelp->setHtml(helpPage);
 }
@@ -1040,9 +1042,8 @@ void TelescopeDialog::updateStyle()
 	if (dialog)
 	{
 		StelGui* gui = dynamic_cast<StelGui*>(StelApp::getInstance().getGui());
-		Q_ASSERT(gui);
-		QString style(gui->getStelStyle().htmlStyleSheet);
-		ui->textBrowserAbout->document()->setDefaultStyleSheet(style);
+		if (gui)
+			ui->textBrowserAbout->document()->setDefaultStyleSheet(gui->getStelStyle().htmlStyleSheet);
 	}
 }
 

src/core/StelIniParser.cpp
@@ -18,6 +18,7 @@
  */
 
 #include "StelIniParser.hpp"
+#include "StelUtils.hpp"
 
 #include <QSettings>
 #include <QString>
@@ -78,6 +79,11 @@ bool readStelIniFile(QIODevice &device, QSettings::SettingsMap &map)
 
 bool writeStelIniFile(QIODevice &device, const QSettings::SettingsMap &map)
 {
+	// QSettings only gives us a binary file handle when writing the
+	// ini file, so we must handle alternative end of line marks
+	// ourselves.
+	const QString stelEndl = StelUtils::getEndLineChar();
+
 	int maxKeyWidth = 30;
 	QRegExp reKeyXt("^([^/]+)/(.+)$");  // for extracting keys/values
 

commit e25e8b508284463a038a153203cad012062639b9

Fix skewed text in -t mode
It was broken by afe4a846a00044234727bd178c9a33df8ed9257b.

src/core/StelPainter.cpp
@@ -736,7 +736,7 @@ void StelPainter::drawText(float x, float y, const QString& str, float angleDeg,
 		for (int i=0;i<4;i++)
 		{
 			texCoords[i*2+0] = static_cast<float>(tex->getTexSize().width()) * (i % 2);
-			texCoords[i*2+1] = static_cast<float>(tex->getTexSize().height()) * (1 - static_cast<float>(i) / 2);
+			texCoords[i*2+1] = static_cast<float>(tex->getTexSize().height()) * (1 - i / 2);
 		}
 		setTexCoordPointer(2, GL_FLOAT, texCoords);
 