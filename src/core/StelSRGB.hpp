7255a1e2cb31adfa279fffe2c88f5c818c23daec
Ruslan Kabatsayev <b7.10110111@gmail.com>
2023-07-13 02:14:21 +0400

    Add C++ versions of sRGB-to-linear conversions

--- src/core/StelSRGB.hpp
+++ src/core/StelSRGB.hpp
@@ -20,7 +20,31 @@
 #ifndef INCLUDE_ONCE_E0C038F5_4B24_4B2C_9AFC_B1B8F0BC4BAF
 #define INCLUDE_ONCE_E0C038F5_4B24_4B2C_9AFC_B1B8F0BC4BAF
 
+#include <cmath>
 #include <QByteArray>
+#include "VecMath.hpp"
+
+template<typename F>
+F srgbToLinear(const F c)
+{
+	return c > F(0.04045) ? std::pow((c+F(0.055))/F(1.055), F(2.4))
+	                      : c / F(12.92);
+}
+
+template<typename F>
+Vector4<F> srgbToLinear(const Vector4<F>& srgba)
+{
+	return {srgbToLinear(srgba[0]),
+	        srgbToLinear(srgba[1]),
+	        srgbToLinear(srgba[2]),
+	        srgba[3]};
+}
+
+template<typename F>
+Vector3<F> srgbToLinear(const Vector3<F>& srgb)
+{
+	return {srgbToLinear(srgb[0]), srgbToLinear(srgb[1]), srgbToLinear(srgb[2])};
+}
 
 inline QByteArray makeSRGBUtilsShader()
 {
