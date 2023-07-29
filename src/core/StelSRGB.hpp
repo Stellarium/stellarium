61ca442fa52a9e342db687451c63cde606cc8c4f
Ruslan Kabatsayev <b7.10110111@gmail.com>
2023-07-29 13:54:57 +0400

    Fix alignment of continuation lines in StelSRGB.hpp

--- src/core/StelSRGB.hpp
+++ src/core/StelSRGB.hpp
@@ -55,7 +55,7 @@ vec3 srgbToLinear(vec3 srgb)
 	vec3 s = step(vec3(0.04045), srgb);
 	vec3 d = vec3(1) - s;
 	return s * pow((srgb+0.055)/1.055, vec3(2.4)) +
-		   d * srgb/12.92;
+	       d * srgb/12.92;
 }
 
 vec3 linearToSRGB(vec3 lin)
@@ -63,7 +63,7 @@ vec3 linearToSRGB(vec3 lin)
 	vec3 s = step(vec3(0.0031308), lin);
 	vec3 d = vec3(1) - s;
 	return s * (1.055*pow(lin, vec3(1./2.4))-0.055) +
-		   d *  12.92*lin;
+	       d *  12.92*lin;
 }
 #line 1 0
 )";
