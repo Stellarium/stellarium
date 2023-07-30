fc12b6f724cf850f1ac9b57504c7fc13b8a52df1
Ruslan Kabatsayev <b7.10110111@gmail.com>
2023-07-30 20:10:57 +0400

    Move makeSRGBUtilsShader() to a .cpp file

--- src/core/StelSRGB.cpp
+++ src/core/StelSRGB.cpp
@@ -17,35 +17,30 @@
  * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
  */
 
-#ifndef INCLUDE_ONCE_E0C038F5_4B24_4B2C_9AFC_B1B8F0BC4BAF
-#define INCLUDE_ONCE_E0C038F5_4B24_4B2C_9AFC_B1B8F0BC4BAF
+#include "StelSRGB.hpp"
+#include "StelMainView.hpp"
 
-#include <cmath>
-#include <QByteArray>
-#include "VecMath.hpp"
-
-template<typename F>
-F srgbToLinear(const F c)
+QByteArray makeSRGBUtilsShader()
 {
-	return c > F(0.04045) ? std::pow((c+F(0.055))/F(1.055), F(2.4))
-	                      : c / F(12.92);
+	static const QByteArray src = 1+R"(
+#line 1 108
+vec3 srgbToLinear(vec3 srgb)
+{
+	vec3 s = step(vec3(0.04045), srgb);
+	vec3 d = vec3(1) - s;
+	return s * pow((srgb+0.055)/1.055, vec3(2.4)) +
+	       d * srgb/12.92;
 }
 
-template<typename F>
-Vector4<F> srgbToLinear(const Vector4<F>& srgba)
+vec3 linearToSRGB(vec3 lin)
 {
-	return {srgbToLinear(srgba[0]),
-	        srgbToLinear(srgba[1]),
-	        srgbToLinear(srgba[2]),
-	        srgba[3]};
+	vec3 s = step(vec3(0.0031308), lin);
+	vec3 d = vec3(1) - s;
+	return s * (1.055*pow(lin, vec3(1./2.4))-0.055) +
+	       d *  12.92*lin;
 }
+#line 1 0
+)";
 
-template<typename F>
-Vector3<F> srgbToLinear(const Vector3<F>& srgb)
-{
-	return {srgbToLinear(srgb[0]), srgbToLinear(srgb[1]), srgbToLinear(srgb[2])};
+	return src;
 }
-
-QByteArray makeSRGBUtilsShader();
-
-#endif
