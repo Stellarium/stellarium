3d1b33df7229194af07e091888d0611c268c7b93
Georg Zotti <Georg.Zotti@univie.ac.at>
2023-10-12 15:30:46 +0200

    Request/override required OpenGL version.

--- src/CLIProcessor.cpp
+++ src/CLIProcessor.cpp
@@ -59,6 +59,7 @@ void CLIProcessor::parseCLIArgsPreQApp(const QStringList argList)
 	if (argsGetOption(argList, "-m", "--mesa-mode"))
 	{
 		qputenv("QT_OPENGL", "software");
+		qputenv("MESA_GL_VERSION_OVERRIDE", "3.3"); // The Mesa 20.1.8 library reports providing 3.1 only. This does the trick for us.
 		// These prepare using current Mesa3D libraries, should the user install them. Else the vars are harmless.
 		qputenv("QT_OPENGL_DLL", "opengl32sw.dll");
 		qputenv("GALLIUM_DRIVER", "llvmpipe");
