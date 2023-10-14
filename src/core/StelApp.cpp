b0cd970e486bd3d122c91e57fd3e07c8b704ef90
Georg Zotti <Georg.Zotti@univie.ac.at>
2023-10-15 00:21:26 +0200

    Suppress drawing of overly bright scene elements in "starstreak mode"

--- src/core/StelApp.cpp
+++ src/core/StelApp.cpp
@@ -1038,10 +1038,17 @@ void StelApp::highGraphicsModeDraw()
 
 	const QList<StelModule*> modules = moduleMgr->getCallOrders(StelModule::ActionDraw);
 
-	for(auto* module : modules)
-	{
-		module->draw(core);
-	}
+	if (core->getFlagClearSky())
+		for (auto* module : modules)
+		{
+			module->draw(core);
+		}
+	else
+		for (auto* module : modules)
+		{
+			if (! QStringList({"MilkyWay", "ZodiacalLight", "LandscapeMgr", "GridlinesMgr"}).contains(module->objectName()))
+				module->draw(core);
+		}
 
 	if(sceneMultisampledFBO)
 	{
@@ -1111,10 +1118,17 @@ void StelApp::draw()
 	else
 	{
 		const QList<StelModule*> modules = moduleMgr->getCallOrders(StelModule::ActionDraw);
-		for (auto* module : modules)
-		{
-			module->draw(core);
-		}
+		if (core->getFlagClearSky())
+			for (auto* module : modules)
+			{
+				module->draw(core);
+			}
+		else
+			for (auto* module : modules)
+			{
+				if (! QStringList({"MilkyWay", "ZodiacalLight", "LandscapeMgr", "GridlinesMgr"}).contains(module->objectName()))
+					module->draw(core);
+			}
 	}
 
 	core->postDraw();
