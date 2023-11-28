b4837ef6fdb0ab02208b3882358987e8098fc9f8
Alexander V. Wolf <alex.v.wolf@gmail.com>
2023-11-28 13:00:11 +0700

    Extract new lines for translation

--- util/landscapes/extract.sh
+++ util/landscapes/extract.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-po4a --verbose --force --msgid-bugs-address stellarium@googlegroups.com --copyright-holder "Stellarium's team" ./po4a.config
+po4a -M utf-8 -L utf-8 --verbose --force --msgid-bugs-address stellarium@googlegroups.com --copyright-holder "Stellarium's team" ./po4a.config
 
 #for i in ../../po/stellarium-landscapes-descriptions/*.po
 #do
