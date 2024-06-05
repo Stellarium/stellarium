c1d310d3a066f94e62678a20ea045e2c51ca7d43
uwes-ufo <uwes-ufo@t-online.de>
2024-06-05 12:20:51 +0200

    APIENTRYP problem on windows qt 6.7.1

--- plugins/Scenery3d/src/GLFuncs.hpp
+++ plugins/Scenery3d/src/GLFuncs.hpp
@@ -24,6 +24,10 @@
 #include <QOpenGLContext>
 #include <QOpenGLFunctions_1_0>
 
+#if defined(_WIN32)
+#include <qt_windows.h>
+#endif
+
 #if !QT_CONFIG(opengles2)
 //! Defines some OpenGL functions not resolved through StelOpenGL (which only contains base OpenGL ES2 functions)
 //! Using the QOpenGLFunctions_*_* directly would solve this better, but it conflicts with the
@@ -34,8 +38,8 @@ public:
 	//! Since 3.2
 	PFNGLFRAMEBUFFERTEXTUREPROC glFramebufferTexture;
 	//! Since 1.0 but not found by QOpenGLFunctions_1_0 when in Core profile
-   void (__stdcall* glDrawBuffer)(GLenum);
-   void (__stdcall* glReadBuffer)(GLenum);
+   void (APIENTRYP glDrawBuffer)(GLenum);
+   void (APIENTRYP glReadBuffer)(GLenum);
 
 	void init(QOpenGLContext* ctx)
 	{
