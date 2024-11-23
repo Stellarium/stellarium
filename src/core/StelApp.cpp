01767163288a1b52f3977f2b3c8e2e8e7b102849
Josh Meyers <jmeyers314@gmail.com>
2024-11-23 20:07:19 -0300

    Make MosaicCamera a separate plugin

--- src/core/StelApp.cpp
+++ src/core/StelApp.cpp
@@ -216,6 +216,10 @@ Q_IMPORT_PLUGIN(VtsStelPluginInterface)
 Q_IMPORT_PLUGIN(OnlineQueriesPluginInterface)
 #endif
 
+#ifdef USE_STATIC_PLUGIN_MOSAICCAMERA
+Q_IMPORT_PLUGIN(MosaicCameraStelPluginInterface)
+#endif
+
 // Initialize static variables
 StelApp* StelApp::singleton = Q_NULLPTR;
 qint64 StelApp::startMSecs = 0;
