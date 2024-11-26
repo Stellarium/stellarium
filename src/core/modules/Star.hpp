e33975be0ad6be0f10e10f774d0189a16d85178d
Henry Leung <henryskyleung@gmail.com>
2024-11-25 22:53:30 -0500

    consider memory alignment for Star123

--- src/core/modules/Star.hpp
+++ src/core/modules/Star.hpp
@@ -46,7 +46,7 @@ struct SpecialZoneData;
 
 // structs for storing the stars in binary form. The idea is
 // to store much data for bright stars (Star1), but only little or even
-// very little data for faints stars (Star3). Using only 6 bytes for Star3
+// very little data for faints stars (Star3). Using only less bytes for Star3
 // makes it feasible to store hundreds of millions of them in main memory.
 
 static inline float IndexToBV(int bV)
@@ -76,8 +76,8 @@ struct Star
       return static_cast<const Derived *>(this)->getX2();
    } // either in rad or in internal astrometric unit
    inline int     getBVIndex() const { return static_cast<const Derived *>(this)->getBVIndex(); }
-   inline double  getMag() const { return static_cast<const Derived *>(this)->getMag(); }
-   inline double  getBV() const { return static_cast<const Derived *>(this)->getBV(); }
+   inline double  getMag() const { return static_cast<const Derived *>(this)->getMag(); } // should return in millimag
+   inline double  getBV() const { return static_cast<const Derived *>(this)->getBV(); }   // should return in mag
    inline bool    isVIP() const { return static_cast<const Derived *>(this)->isVIP(); }
    inline bool    hasName() const { return static_cast<const Derived *>(this)->hasName(); }
    inline QString getNameI18n() const { return static_cast<const Derived *>(this)->getNameI18n(); }
@@ -88,7 +88,7 @@ struct Star
    {
       return static_cast<const Derived *>(this)->getPMTotal();
    } // should return in mas/yr
-   inline double  getDx0() const
+   inline double getDx0() const
    {
       return static_cast<const Derived *>(this)->getDx0();
    } // should return in mas/yr (if pmra, then without cos(dec) components), 0 means no 1rd proper motion
@@ -215,7 +215,7 @@ struct Star
          Plx = Plx2;
          RV  = (pmr1 / MAS2RAD / plx) * (AU / JYEAR_SECONDS);
       } else {
-		 DE    = getX1();
+         DE    = getX1();
          pmra  = getDx0() * cos(DE);
          pmdec = getDx1();
          RA    = getX0() + dyrs * getDx0() * MAS2RAD;
@@ -282,24 +282,23 @@ struct Star
    // }
 };
 
-#pragma pack(push, 1)
 struct Star1 : public Star<Star1>
 {
 private:
    struct Data
    {
-      quint8  hip[3];       // 3 bytes
       qint64  gaia_id;      // 8 bytes
       quint8  componentIds; // 1 byte
+      quint8  hip[3];       // 3 bytes
       qint32  x0;           // 4 bytes, internal astrometric unit
       qint32  x1;           // 4 bytes, internal astrometric unit
       qint32  x2;           // 4 bytes, internal astrometric unit
-      qint16  b_v;          // 2 bytea, B-V in milli-mag
-      qint16  vmag;         // 2 bytes, V magnitude in milli-mag
-      quint16 spInt;        // 2 bytes
       qint32  dx0;          // 4 bytes, uas/yr
       qint32  dx1;          // 4 bytes, uas/yr
       qint32  dx2;          // 4 bytes, uas/yr
+      qint16  b_v;          // 2 bytea, B-V in milli-mag
+      qint16  vmag;         // 2 bytes, V magnitude in milli-mag
+      quint16 spInt;        // 2 bytes
       quint16 plx;          // 2 bytes, parallax in 20 uas
       quint16 plx_err;      // 2 bytes, parallax error in 10 uas
       qint16  rv;           // 2 bytes, radial velocity in 100 m/s
@@ -308,7 +307,7 @@ private:
 public:
    StelObjectP   createStelObject(const SpecialZoneArray<Star1> * a, const SpecialZoneData<Star1> * z) const;
    inline int    getBVIndex() const { return BVToIndex(getBV()); }
-   inline int    getMag() const { return d.vmag; }  // in milli-mag
+   inline int    getMag() const { return d.vmag; } // in milli-mag
    inline int    getSpInt() const { return d.spInt; }
    inline double getX0() const { return d.x0 / 2.e9; }
    inline double getX1() const { return d.x1 / 2.e9; }
@@ -326,7 +325,7 @@ public:
       // the star need to has parallax, proper motion, or radial velocity
       // use OR in each in case one of them is actually exactly 0
       // no point of doing proper propagation if any of them is missing
-      return (getPlx() || getPlxErr()) &&  (getPlx() / getPlxErr() > 5) && (getDx0() || getDx1() || getDx2()) && getRV();
+      return (getPlx() || getPlxErr()) && (getPlx() / getPlxErr() > 5) && (getDx0() || getDx1() || getDx2()) && getRV();
    }
    inline int getHip() const
    {
@@ -345,21 +344,21 @@ public:
    int         hasComponentID(void) const;
 };
 static_assert(sizeof(Star1) == 48, "Size of Star1 must be 48 bytes");
-#pragma pack(pop) // Restore the previous packing alignment
 
-#pragma pack(push, 1)
 struct Star2 : public Star<Star2>
 {
 private:
    struct Data
    {
-      qint64 gaia_id; // 8 bytes
-      qint32 x0;      // 4 bytes, RA in mas
-      qint32 x1;      // 4 bytes, DEC in mas
-      qint32 dx0;     // 4 bytes, pmra in uas/yr
-      qint32 dx1;     // 4 bytes, pmdec in uas/yr
-      qint16 b_v;     // 2 byte
-      qint16 vmag;    // 2 bytes
+      qint64  gaia_id; // 8 bytes
+      qint32  x0;      // 4 bytes, RA in mas
+      qint32  x1;      // 4 bytes, DEC in mas
+      qint32  dx0;     // 4 bytes, pmra in uas/yr
+      qint32  dx1;     // 4 bytes, pmdec in uas/yr
+      qint16  b_v;     // 2 byte
+      qint16  vmag;    // 2 bytes
+      quint16 plx;     // 2 bytes, parallax in 20 uas
+      quint16 plx_err; // 2 bytes, parallax error in 10 uas
    } d;
 
 public:
@@ -370,54 +369,55 @@ public:
    inline double getDx1() const { return d.dx1 / 1000.; }
    inline double getDx2() const { return 0.; }
    inline int    getBVIndex() const { return BVToIndex(getBV()); }
-   inline int    getMag() const { return d.vmag; }    // in milli-mag
-   inline double getPMTotal() const { return sqrt((getDx0()*cos(getX1())) * getDx0()*cos(getX1()) + getDx1() * getDx1()); }
-   StelObjectP   createStelObject(const SpecialZoneArray<Star2> * a, const SpecialZoneData<Star2> * z) const;
-   inline long   getGaia() const { return d.gaia_id; }
-   float         getBV(void) const { return static_cast<float>(d.b_v) / 1000.f; }
-   QString       getNameI18n(void) const { return QString(); }
-   QString       getScreenNameI18n(void) const { return QString(); }
-   QString       getDesignation(void) const { return QString(); }
-   int           hasComponentID(void) const { return 0; }
-   bool          isVIP() const { return false; }
-   bool          hasName() const { return getGaia(); }
-   double        getPlx() const { return 0.; }
-   double        getPlxErr() const { return 0.; }
-   double        getRV() const { return 0.; }
-   bool          getPreciseAstrometricFlag() const
+   inline int    getMag() const { return d.vmag; } // in milli-mag
+   inline double getPMTotal() const
+   {
+      return sqrt((getDx0() * cos(getX1())) * getDx0() * cos(getX1()) + getDx1() * getDx1());
+   }
+   StelObjectP createStelObject(const SpecialZoneArray<Star2> * a, const SpecialZoneData<Star2> * z) const;
+   inline long getGaia() const { return d.gaia_id; }
+   float       getBV(void) const { return static_cast<float>(d.b_v) / 1000.f; }
+   QString     getNameI18n(void) const { return QString(); }
+   QString     getScreenNameI18n(void) const { return QString(); }
+   QString     getDesignation(void) const { return QString(); }
+   int         hasComponentID(void) const { return 0; }
+   bool        isVIP() const { return false; }
+   bool        hasName() const { return getGaia(); }
+   double      getPlx() const { return d.plx / 100.; }
+   double      getPlxErr() const { return d.plx_err / 100.; }
+   double      getRV() const { return 0.; }
+   bool        getPreciseAstrometricFlag() const
    { // Flag if the star should have time dependent astrometry computed
       return false;
    }
    void print(void) const;
 };
-static_assert(sizeof(Star2) == 28, "Size of Star2 must be 28 bytes");
-#pragma pack(pop) // Restore the previous packing alignment
+static_assert(sizeof(Star2) == 32, "Size of Star2 must be 32 bytes");
 
-#pragma pack(push, 1)
 struct Star3 : public Star<Star3>
 {
 private:
    struct Data
    {
-      qint64 gaia_id; // 8 bytes
-      quint8 x0[3];      // 3 bytes, RA in arcsecond
-      quint8 x1[3];      // 3 bytes, DEC in arcsecond
-      quint8 b_v;     // 1 byte, B-V in 0.1 mag
-      quint16 vmag;    // 2 bytes, V magnitude in 0.1 mag
+      qint64  gaia_id; // 8 bytes
+      quint8  x0[3];   // 3 bytes, RA in arcsecond
+      quint8  x1[3];   // 3 bytes, DEC in arcsecond
+      quint8  b_v;     // 1 byte, B-V in 0.1 mag
+      quint8  vmag;    // 1 bytes, V magnitude in 0.1 mag
    } d;
 
 public:
    StelObjectP   createStelObject(const SpecialZoneArray<Star3> * a, const SpecialZoneData<Star3> * z) const;
-	inline double getX0() const 
-	{ 
-		qint32 x0 = d.x0[0] | (d.x0[1] << 8) | (d.x0[2] << 16);
-		return static_cast<double>(x0) * 1000. * MAS2RAD; 
-	}
-	inline double getX1() const 
-	{ 
-		qint32 x0 = d.x1[0] | (d.x1[1] << 8) | (d.x1[2] << 16);
-		return static_cast<double>(x0) * 1000. * MAS2RAD; 
-	}   
+   inline double getX0() const
+   {
+      qint32 x0 = d.x0[0] | (d.x0[1] << 8) | (d.x0[2] << 16);
+      return static_cast<double>(x0) * 1000. * MAS2RAD;
+   }
+   inline double getX1() const
+   {
+      qint32 x0 = d.x1[0] | (d.x1[1] << 8) | (d.x1[2] << 16);
+      return static_cast<double>(x0) * 1000. * MAS2RAD;
+   }
    inline double getX2() const { return 0.; }
    inline double getDx0() const { return 0.; }
    inline double getDx1() const { return 0.; }
@@ -427,7 +427,7 @@ public:
    double        getPlxErr() const { return 0.; }
    double        getRV() const { return 0.; }
    double        getBVIndex() const { return d.b_v / 10.; }
-   double        getMag() const { return d.vmag * 100.; }  // in milli-mag
+   double        getMag() const { return d.vmag * 100.; } // in milli-mag
    float         getBV() const { return IndexToBV(getBVIndex()); }
    inline long   getGaia() const { return d.gaia_id; }
    QString       getNameI18n() const { return QString(); }
@@ -441,7 +441,6 @@ public:
       return false;
    }
 };
-static_assert(sizeof(Star3) == 17, "Size of Star3 must be 17 bytes");
-#pragma pack(pop) // Restore the previous packing alignment
+static_assert(sizeof(Star3) == 16, "Size of Star3 must be 16 bytes");
 
 #endif // STAR_HPP
