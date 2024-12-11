0becf7038fa952216c03ab3563c6de364aa5f659
null <null>
2024-12-11 13:51:40 +0800

    Add solving plugin.

---
+++ plugins/NebulaTextures/src/NebulaTextures.cpp
@@ -0,0 +1,89 @@
+/*
+ * Nebula Textures plug-in for Stellarium
+ *
+ * Copyright (C) 2024 ultrapre@github.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "StelProjector.hpp"
+#include "StelPainter.hpp"
+#include "StelApp.hpp"
+#include "StelCore.hpp"
+#include "StelLocaleMgr.hpp"
+#include "StelModuleMgr.hpp"
+#include "NebulaTextures.hpp"
+
+#include "StelModule.hpp"
+#include "gui/NebulaTexturesDialog.hpp"
+
+#include <QDebug>
+
+StelModule* NebulaTexturesStelPluginInterface::getStelModule() const
+{
+   return new NebulaTextures();
+}
+
+StelPluginInfo NebulaTexturesStelPluginInterface::getPluginInfo() const
+{
+   // Allow to load the resources when used as a static plugin
+   // Q_INIT_RESOURCE(NebulaTextures);
+
+	StelPluginInfo info;
+   info.id = "NebulaTextures";
+   info.displayedName = "Nebula Textures plugin";
+	info.authors = "Stellarium team";
+	info.contact = "www.stellarium.org";
+   info.description = "User Define Nebula Textures.";
+	return info;
+}
+
+NebulaTextures::NebulaTextures()
+{
+   setObjectName("NebulaTextures");
+	font.setPixelSize(25);
+   configDialog = new NebulaTexturesDialog();
+}
+
+NebulaTextures::~NebulaTextures()
+{
+   // delete configDialog;
+}
+
+bool NebulaTextures::configureGui(bool show)
+{
+   if (show)
+      configDialog->setVisible(true);
+   return true;
+}
+
+double NebulaTextures::getCallOrder(StelModuleActionName actionName) const
+{
+	if (actionName==StelModule::ActionDraw)
+		return StelApp::getInstance().getModuleMgr().getModule("NebulaMgr")->getCallOrder(actionName)+10.;
+	return 0;
+}
+
+void NebulaTextures::init()
+{
+   qDebug() << "init called for NebulaTextures";
+   // addAction("actionShow_NebulaTextures_ConfigDialog",
+   //           N_("Nebula Textures"), N_("Open Nebula Textures configuration dialog"),
+   //           configDialog, "visible");
+}
+
+void NebulaTextures::draw(StelCore* core)
+{
+}
+
---
+++ plugins/NebulaTextures/src/gui/NebulaTexturesDialog.hpp
@@ -0,0 +1,82 @@
+/*
+ * Nebula Textures plug-in for Stellarium
+ *
+ * Copyright (C) 2024 ultrapre@github.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NEBULATEXTURESDIALOG_HPP
+#define NEBULATEXTURESDIALOG_HPP
+
+#include "StelDialog.hpp"
+#include <QString>
+
+class Ui_nebulaTexturesDialog;
+
+//! Main window of the Nebula Textures plug-in.
+//! @ingroup nebulaTextures
+class NebulaTexturesDialog : public StelDialog
+{
+	Q_OBJECT
+
+public:
+	NebulaTexturesDialog();
+	~NebulaTexturesDialog() override;
+
+public slots:
+	void retranslate() override;
+
+protected:
+	void createDialogContent() override;
+
+private slots:
+   void restoreDefaults();
+
+   void on_openFileButton_clicked();
+   void on_uploadImageButton_clicked();
+   void on_renderButton_clicked();
+
+   void onLoginReply(QNetworkReply *reply);
+   void onUploadReply(QNetworkReply *reply);
+   void onsubStatusReply(QNetworkReply *reply);
+   void onJobStatusReply(QNetworkReply *reply);
+   void onWcsDownloadReply(QNetworkReply *reply);
+
+   void checkSubStatus();
+   void checkJobStatus();
+
+private:
+	Ui_nebulaTexturesDialog* ui;
+
+
+   QNetworkAccessManager *networkManager;
+   QString session;
+   QString subId;
+   QString jobId;
+   QTimer *subStatusTimer;
+   QTimer *jobStatusTimer;
+   QString API_URL = "http://nova.astrometry.net/";
+
+	void setAboutHtml();
+   void updateStatus(const QString &status);
+
+   double calculateRA(int X, int Y, double CRPIX1, double CRPIX2, double CRVAL1, double CRVAL2,
+                                        double CD1_1, double CD1_2, double CD2_1, double CD2_2);
+
+   double calculateDec(int X, int Y, double CRPIX1, double CRPIX2, double CRVAL1, double CRVAL2,
+                                         double CD1_1, double CD1_2, double CD2_1, double CD2_2);
+};
+
+#endif /* NEBULATEXTURESDIALOG_HPP */
