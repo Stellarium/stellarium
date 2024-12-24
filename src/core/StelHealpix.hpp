7b4bc03e45840a9614e08c7fb018ced6ee9a6b09
Alexander V. Wolf <aw@altspu.ru>
2024-12-24 15:23:04 +0700

    A few cosmetic fixes
    without testing though...

--- src/core/StelHealpix.hpp
+++ src/core/StelHealpix.hpp
@@ -1,3 +1,23 @@
+/*
+ * Stellarium
+ * Copyright (C) 2017 Guillaume Chereau
+ * Copyright (C) 2024 Henry Leung
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 /* utab[m] = (short)(
       (m&0x1 )       | ((m&0x2 ) << 1) | ((m&0x4 ) << 2) | ((m&0x8 ) << 3)
     | ((m&0x10) << 4) | ((m&0x20) << 5) | ((m&0x40) << 6) | ((m&0x80) << 7)); */
