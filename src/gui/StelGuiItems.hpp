77b064c69f7cee75d2335617b3e51914b90266f8
Ruslan Kabatsayev <b7.10110111@gmail.com>
2025-01-07 21:05:48 +0800

    Remove stale declaration

--- src/gui/StelGuiItems.hpp
+++ src/gui/StelGuiItems.hpp
@@ -137,7 +137,6 @@ public:
 	QRectF boundingRect() const override;
 
 	static double getInputPixmapsDevicePixelRatio() { return GUI_INPUT_PIXMAPS_SCALE; }
-	static double fontSizeRatio();
 
 
 signals:
