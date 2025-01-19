e8901ca223eaa89739e4fd92854990bc9574272c
Alexander V. Wolf <alex.v.wolf@gmail.com>
2025-01-20 00:30:37 +0700

    Bump version of star catalogs to avoid possible mixture the data (should fix #4090)

--- src/core/modules/StarMgr.cpp
+++ src/core/modules/StarMgr.cpp
@@ -65,7 +65,7 @@ static QStringList objtype_array;
 // This number must be incremented each time the content or file format of the stars catalogs change
 // It can also be incremented when the defaultStarsConfig.json file change.
 // It should always match the version field of the defaultStarsConfig.json file
-static const int StarCatalogFormatVersion = 18;
+static const int StarCatalogFormatVersion = 19;
 
 // Initialise statics
 bool StarMgr::flagSciNames = true;
