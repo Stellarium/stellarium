abb2b07952f2dfd9923a6993c228e275348c9171
Georg Zotti <Georg.Zotti@univie.ac.at>
2025-04-23 12:59:24 +0200

    Allow printing hull areas to logfile

--- src/core/modules/ConstellationMgr.cpp
+++ src/core/modules/ConstellationMgr.cpp
@@ -1432,3 +1432,15 @@ Constellation* ConstellationMgr::isObjectIn(const StelObject *s) const
 	}
 	return nullptr;
 }
+
+void ConstellationMgr::dumpHullAreas()
+{
+
+	foreach(const Constellation *constellation, constellations)
+	{
+		double area_sr=constellation->convexHull->getArea();
+		qInfo().nospace() << constellation->getEnglishName() << ": "
+				  <<  area_sr << "sr or " << area_sr*(M_180_PI*M_180_PI) << "°²";
+	}
+
+}
