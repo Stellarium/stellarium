1c71ba8a1ba75e9c018c2facb4dc81a256c17fcd
Alexander V. Wolf <alex.v.wolf@gmail.com>
2025-08-21 02:43:46 +0700

    Fix type conversion

--- src/core/modules/ZoneArray.cpp
+++ src/core/modules/ZoneArray.cpp
@@ -666,7 +666,7 @@ void SpecialZoneArray<Star>::searchGaiaIDepochPos(const StarId source_id,
                                                   double &      RV) const
 {
    // loop through each zone in the level which is 20 * 4 ** level + 1 as index
-   for (int i = 0; i < 20 * pow(4, (level)) + 1; i++) {
+   for (int i = 0; i < 20 * pow(4., (level)) + 1; i++) {
       // get the zone data
       const SpecialZoneData<Star> * const z = getZones() + i;
       // loop through the stars in the zone
