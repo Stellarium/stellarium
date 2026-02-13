1aaaaf157c7be5f67c47c56ec7e6dc015163e2bb
Atque <37028368+Atque@users.noreply.github.com>
2026-02-13 15:45:54 +0100

    Properly doing identation

--- src/gui/ViewDialog.cpp
+++ src/gui/ViewDialog.cpp
@@ -422,7 +422,7 @@ void ViewDialog::createDialogContent()
 	connect(lmgr, SIGNAL(landscapesChanged()), this, SLOT(populateLists()));
 	connect(ui->pushButtonAddRemoveLandscapes, SIGNAL(clicked()), this, SLOT(showAddRemoveLandscapesDialog()));
     // Connect grid spacing combo box
-    connect(ui->gridSpacingComboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(gridSpacingChanged(int)));
+	connect(ui->gridSpacingComboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(gridSpacingChanged(int)));
 
 	// Grid and lines
 	connectGroupBox(ui->celestialSphereGroupBox,              "actionShow_Gridlines");
@@ -1819,51 +1819,51 @@ void ViewDialog::populatePlanetMagnitudeAlgorithmDescription()
 }
 void ViewDialog::gridSpacingChanged(int index)
 {
-        GridLinesMgr* gridMgr = GETSTELMODULE(GridLinesMgr);
+	GridLinesMgr* gridMgr = GETSTELMODULE(GridLinesMgr);
 
-        // Map combo box index to multiplier values
-        // Index 0 = Fine (2.0x), Index 1 = Normal (1.0x), Index 2 = Coarse (0.5x)
-        double multiplier = 1.0;
-        switch(index)
-        {
-        case 0: // Fine
-                multiplier = 2.0;
-                break;
+		// Map combo box index to multiplier values
+		// Index 0 = Fine (2.0x), Index 1 = Normal (1.0x), Index 2 = Coarse (0.5x)
+	double multiplier = 1.0;
+	switch(index)
+	{
+		case 0: // Fine
+			multiplier = 2.0;
+			break;
         case 1: // Normal
-                multiplier = 1.0;
-                break;
-        case 2: // Coarse
-                multiplier = 0.5;
-                break;
-        default:
-                multiplier = 1.0;
-        }
+			multiplier = 1.0;
+			break;
+		case 2: // Coarse
+			multiplier = 0.5;
+			break;
+		default:
+			multiplier = 1.0;
+	}
 
-        gridMgr->setGridSpacingMultiplier(multiplier);
+	gridMgr->setGridSpacingMultiplier(multiplier);
 
-        // Save to settings
-        QSettings* conf = StelApp::getInstance().getSettings();
-        conf->setValue("viewing/grid_spacing_multiplier", multiplier);
+	// Save to settings
+	QSettings* conf = StelApp::getInstance().getSettings();
+	conf->setValue("viewing/grid_spacing_multiplier", multiplier);
 }
 
 
 void ViewDialog::updateGridSpacingComboBox()
 {
-        GridLinesMgr* gridMgr = GETSTELMODULE(GridLinesMgr);
-        double multiplier = gridMgr->getGridSpacingMultiplier();
+	GridLinesMgr* gridMgr = GETSTELMODULE(GridLinesMgr);
+	double multiplier = gridMgr->getGridSpacingMultiplier();
 
-        // Set the appropriate index based on multiplier
-        int index = 1; // Default to Normal
-        if (multiplier >= 1.8) // Close to 4.0
-                index = 0; // Fine
-        else if (multiplier <= 0.7) // Close to 0.5
-                index = 2; // Coarse
-        else
-                index = 1; // Normal
+	// Set the appropriate index based on multiplier
+	int index = 1; // Default to Normal
+	if (multiplier >= 1.8) // Close to 4.0
+		index = 0; // Fine
+	else if (multiplier <= 0.7) // Close to 0.5
+		index = 2; // Coarse
+	else
+		index = 1; // Normal
 
-        // Block signals to avoid triggering the slot
-        ui->gridSpacingComboBox->blockSignals(true);
-        ui->gridSpacingComboBox->setCurrentIndex(index);
-        ui->gridSpacingComboBox->blockSignals(false);
-        updateGridSpacingComboBox();
+	// Block signals to avoid triggering the slot
+	ui->gridSpacingComboBox->blockSignals(true);
+	ui->gridSpacingComboBox->setCurrentIndex(index);
+	ui->gridSpacingComboBox->blockSignals(false);
+	updateGridSpacingComboBox();
 }
