4226953f4f0f0eaff46099e8acbdac338f69bf58
Georg Zotti <Georg.Zotti@univie.ac.at>
2026-08-20 13:23:51 +0200

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -476,21 +476,11 @@ int main(int argc, char **argv)
 	const auto qscreen = qApp->screens().at(screen);
 	const QRect screenGeom = qscreen->geometry();
 
-	qDebug() << "Reading \"video/screen_w\" =" << confSettings->value("video/screen_w").toString() << "as" << confSettings->value("video/screen_w", 0).toInt();
-	qDebug() << "ScreenGeom.width() =" << screenGeom.width();
-	qDebug() << "Reading \"video/screen_h\" =" << confSettings->value("video/screen_h").toString() << "as" << confSettings->value("video/screen_h", 0).toInt();
-	qDebug() << "ScreenGeom.height() =" << screenGeom.height();
-
 	const auto virtSize = QSize(confSettings->value("video/screen_w", screenGeom.width()).toInt(),
 								confSettings->value("video/screen_h", screenGeom.height()).toInt());
-//#ifdef Q_OS_WIN
-//	const auto size = QSize(std::lround(virtSize.width()),
-//				    std::lround(virtSize.height()));
-//#else
 	const auto pixelRatio = qscreen->devicePixelRatio();
 	const auto size = QSize(std::lround(virtSize.width()/pixelRatio),
 				    std::lround(virtSize.height()/pixelRatio));
-//#endif
 	mainWin.resize(size);
 
 	const bool fullscreen = confSettings->value("video/fullscreen", true).toBool();
@@ -508,13 +498,8 @@ int main(int argc, char **argv)
 	{
 		const int x = confSettings->value("video/screen_x", 0).toInt();
 		const int y = confSettings->value("video/screen_y", 0).toInt();
-//#ifdef Q_OS_WIN
-//		mainWin.move(screenGeom.x() + x,
-//			     screenGeom.y() + y);
-//#else
 		mainWin.move(screenGeom.x() + x/pixelRatio,
 			     screenGeom.y() + y/pixelRatio);
-//#endif
 	}
 
 	mainWin.show();
