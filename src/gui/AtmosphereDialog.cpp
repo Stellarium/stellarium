5df29eeec8011d0877d80f9c6756a7ccfb6a2818
Georg Zotti <Georg.Zotti@univie.ac.at>
2026-09-21 11:42:13 +0200

    Cosmetic change

--- src/gui/AtmosphereDialog.cpp
+++ src/gui/AtmosphereDialog.cpp
@@ -110,11 +110,13 @@ void AtmosphereDialog::createDialogContent()
 	connectBoolProperty(ui->showMySky_pseudoMirrorEnabled, "LandscapeMgr.flagAtmospherePseudoMirror");
 	connectIntProperty(ui->showMySky_eclipseSimulationQualitySpinBox, ECLIPSE_SIM_QUALITY_PROPERTY);
 
-	if ((StelMainView::getInstance().getGLInformation().isGLES)
-		#ifndef	ENABLE_SHOWMYSKY
-			|| true
-		#endif
-			)
+	#ifdef ENABLE_SHOWMYSKY
+	const bool showMySkyDisabled = false;
+	#else
+	const bool showMySkyDisabled = true;
+	#endif
+
+	if ((StelMainView::getInstance().getGLInformation().isGLES) || showMySkyDisabled)
 	{
 		for (int row = 0; row < ui->atmosphereModel->count(); ++row)
 		{
